eb2ac32e93379b1910abdca21459fefc9d84a6f1
Aaron Ponti <aponti>
2004-02-18 19:39:13 +0000

    Made the introduction of calibration parameters more user-friendly.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@201 0bcb941b-74d1-0310-bb26-e6636707f451

--- fsmCenter/tools/fsmCalcNoiseParam.m
+++ fsmCenter/tools/fsmCalcNoiseParam.m
@@ -158,9 +158,19 @@ close(h);
 % Calcaulate GaussRatio as the mean of the vector GaussRatios
 GaussRatio=mean(GaussRatios);
 
-fprintf(1,' I0\t\t\t= %.8f\n',I0);
-fprintf(1,' sDN\t\t= %.8f\n',sDN);
-fprintf(1,' GaussRatio\t= %.8f\n',GaussRatio);
+fprintf(1,'\nTo add this experiment to your database:\n');
+fprintf(1,'(1) Click on ''Edit experiment parameters'' in fsmCenter.\n');
+fprintf(1,'(2) Copy/paste this record at the end of your experiment settings file.\n');
+fprintf(1,'(3) Edit the LABEL and DESCRIPTION fields.\n');
+fprintf(1,'-------------------------------------------------------------------\n');
+fprintf(1,'LABEL\t\t\t"Please change this"\n');
+fprintf(1,'DESCRIPTION\t\t"Please change this"\n');
+fprintf(1,'BIT DEPTH\t\t"%s"\n',num2str(bitDepth));
+fprintf(1,'NOISE PARAMS\t"%1.8f 2e-4 %1.8f"\n',sDN,I0);
+fprintf(1,'GAUSS RATIO\t\t"%1.2f"\n',GaussRatio);
+fprintf(1,'#\n');
+fprintf(1,'-------------------------------------------------------------------\n');
+fprintf(1,'(Don''t forget the ''#'')\n');
 
 % Back to old directory
 cd(currDir);
