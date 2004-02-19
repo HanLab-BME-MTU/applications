f853d75f36d5393666f0f47e9d2194fdb74b6e52
Jonas Dorn <jonas.dorn@gmail.com>
2004-02-19 01:37:49 +0000

    first step to making r3c-movies obsolete
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/chromdyn/main@207 0bcb941b-74d1-0310-bb26-e6636707f451

--- utilities/iofun/r3dread.m
+++ utilities/iofun/r3dread.m
@@ -1,18 +1,31 @@
-function [image, filename]= r3dread(filename,start,nTimes,MOVIERANGE)
-%3RDREAD reads the SOFTWORX *.r3d files into matlab (UNIX byte ordering)
+function [image, filename, r3dMovieHeader]= r3dread(filename,start,nTimes,MOVIERANGE,correctionDataFromVar)
+%R3DREAD reads the SOFTWORX *.r3d files into matlab (UNIX byte ordering)
 %
-% SYNOPSIS image = r3dread(filename)
+% SYNOPSIS [image, filename, header]= r3dread(filename,start,nTimes,MOVIERANGE,correctionDataFromVar)
 %
 % INPUT (all optional)
-%       filename : string, if the filename is omitted,
-%                  a file selection dialog is opened
-%       start    : frame with which to start reading {default: 1}
-%       nTimes   : number of frames to read {default: length(movie)}
-%       MOVIERANGE: possible range of values (256 for 8-bit-movies) {2^12}
+%       filename :              string, if the filename is omitted,
+%                               a file selection dialog is opened
+%       start    :              frame with which to start reading {default: 1}
+%       nTimes   :              number of frames to read {default: length(movie)}
+%       MOVIERANGE :            possible range of values (256 for 8-bit-movies) {2^12}
+%       correctionDataFromVar : struct for background correction OR 0. 
+%                                   fields: .info.correctFile: filenames of
+%                                                   dark movies
+%                                           .info.correctFrames: [firstN,
+%                                                   lastN] frames used for
+%                                                   correction
+%                                           .image: image used for
+%                                                   correction
+%                               if zero, raw movie is loaded. If not, the
+%                               program will return a corrected movie, if
+%                               there is either correctionDataFromVar or
+%                               correctionDataFromFile. 
 %
 %
 % OUTPUT image : image series =[X,Y,Z,WVL,TIME]
 %        fname : (optional) string 
+%        r3dMovieHeader: structure containing the information from the movie header
 
 % c: 6/3/00	dT
 % @ 7/3/01 : the header contains a lot information that is ignored for the moment
@@ -37,26 +50,48 @@ end
 
 %if no filename, open file selection dialog
 if(nargin==0 | isempty(filename))
-   [fname,path]=uigetfile('*.r3d','select image file');
-   if(fname(1)==0)
-       image=[];
-       fname=[];
-      return;
-   end;
-   cd(path);
-   filename=[path fname];
+    [fname,path]=uigetfile('*.r3d;*_R3D.dv','select movie file');
+    if(fname(1)==0)
+        image=[];
+        fname=[];
+        return;
+    end;
+    cd(path);
+    filename=[path fname];
+else
+    path = [];
 end;
+
+%fill in path
+if isempty(path)
+    % is there a path in the filename?
+    fileSepIdx = strfind(filename,filesep);
+    if isempty(fileSepIdx)
+        % no, we are in the right directory
+        path = [pwd,filesep];
+    else
+        % take path from filename
+        path = filename(1:fileSepIdx(end));
+    end
+end
 
 % add filenameextension if neccessary
 if isempty(findstr(filename,'.'))
-	filename=[filename,'.r3d'];
+    filename=[filename,'.r3d'];
 end;
-  
+
 [file, message] = fopen(filename,'r','b');
 if file == -1
     image=[];
     filename =['file ',filename,' not found.'];
     return;
+end
+
+% read full header for output if needed
+if nargout > 2
+    r3dMovieHeader = readr3dheader(filename);
+else
+    r3dMovieHeader = [];
 end
 
 
@@ -78,7 +113,9 @@ end;
 % rewind
 fseek(file,0,-1);
 
+%-------------
 % read header
+%--------------
 
 %first BLOCK
 block = fread(file,24,'int32');
@@ -96,6 +133,9 @@ numTimes=fread(file,1,'short');
 % set to all images if not exist
 if nargin < 3 | isempty(nTimes)
     nTimes=numTimes;
+elseif nTimes == 0
+    image = [];
+    return
 end
 
 % max nTimes to read
@@ -106,18 +146,71 @@ fseek(file,196,-1);
 numWvl=fread(file,1,'short');
 numZ=numImages/(numTimes*numWvl);
 
+%-------------------------------
+% check for backgroundCorrection
+%-------------------------------
+
+% test input
+if nargin < 5 | isempty(correctionDataFromVar)
+    checkForBGFile = 1;
+    correctionDataFromVar = [];
+    correctBG = 0; % we don't know yet whether to correct
+else
+    % if struct: still check for BG, if not, don't correct BG
+    if isstruct(correctionDataFromVar)
+        checkForBGFile = 1;
+        correctBG = 1; % we will check whether there is a file or not
+    else % if not zero: bad user, plz read help
+        checkForBGFile = 0;
+        correctBG = 0;
+    end
+end
+
+% look for file
+if checkForBGFile
+    if exist([path,'correctionData.mat'],'file')
+        loadStruct = load([path,'correctionData.mat']);
+        correctionDataFromFile = loadStruct.correctionData;
+        correctBG = 1;
+    else
+        % there is no file
+        correctionDataFromFile = [];
+        % leave correctBG as is
+    end
+end
+
+if correctBG
+    %calculate background correction image
+    [backgroundCorrectionImage,minStart,maxEnd] = r3dreadCorrectBackground(filename,numRow,numCol,numZ,numWvl,numTimes,correctionDataFromFile,correctionDataFromVar,MOVIERANGE);
+    
+    %adjust start and nTimes if necessary
+    start = start + minStart - 1;
+    nTimes=min(nTimes,maxEnd-start+1);
+else
+    backgroundCorrectionImage = zeros(numRow,numCol,numZ,numWvl);
+end
+
+%------------
+% load movie
+%------------
+
+
+
 %allocate mem
 image=zeros(numRow,numCol,numZ,numWvl,nTimes);
 readIm=zeros(numCol,numRow,numZ);
 fseek(file,HEADER_SIZE+firstImage+(start-1)*numCol*numRow*numZ*2,-1);
 for t=1:nTimes
-   for w=1:numWvl
-      readIm(:)=fread(file,numCol*numRow*numZ,'int16');
-      %and rotate and normalize
+    for w=1:numWvl
+        readIm(:)=fread(file,numCol*numRow*numZ,'int16');
+        %and rotate and normalize
         for z=1:size(readIm,3)
-            image(:,:,z,w,t)=readIm(:,:,z)'/MOVIERANGE;
+            % do backgroundCorrection in loop because we 
+            image(:,:,z,w,t) = readIm(:,:,z)';
         end;
-   end;
+        % adjust image range and correct background
+        image(:,:,:,w,t) = (image(:,:,:,w,t)/MOVIERANGE)-backgroundCorrectionImage(:,:,:,w);
+    end;
 end;
 
 fclose(file);
