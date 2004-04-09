592cc920d341e0be04042448210ad1389375e33c
Aaron Ponti <aponti>
2004-04-09 15:41:10 +0000

    Added function fsmCenterCB_addTooldMenu.m.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@463 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ fsmCenter/gui/fsmCenterCB_addToolsMenu.m
@@ -0,0 +1,23 @@
+function fsmCenterCB_addToolsMenu(handle)
+% fsmCenterCB_addToolsMenu
+%
+% This function adds the menu "More Tools" to the figure with handle 'handle'
+% allowing the functions of the fsmCenter "View Panel" to be called.
+%
+% INPUT   handle: handle of the figure to which the menu has to be attached
+%
+% OUTPUT  None
+%
+% Aaron Ponti, 04/07/2004
+
+% Create menu with additional tools
+figure(handle);
+hMenu=uimenu('Label','Common Tools');
+% uimenu(hMenu,'Label','Load image','Callback','fsmCenterCB_loadNewImage;','Accelerator','L');
+uimenu(hMenu,'Label','Draw polygon','Callback','fsmCenterCB_drawPolygon;','Accelerator','D'); %'Separator','On'
+% uimenu(hMenu,'Label','Find edges','Callback','fsmCenterCB_findEdges;','Accelerator','E');
+uimenu(hMenu,'Label','Filter image','Callback','fsmCenterCB_filterImage;','Accelerator','F');
+% uimenu(hMenu,'Label','Auto contrast','Callback','fsmCenterCB_autoContrast;','Accelerator','T');
+% uimenu(hMenu,'Label','Show speckle selection','Callback','fsmCenterCB_showCands;','Accelerator','K','Separator','On');
+uimenu(hMenu,'Label','Invert image','Callback','fsmCenterCB_invertImage;','Accelerator','I');
+
