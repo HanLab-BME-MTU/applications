6ad78c5fb260b8aae275a53370176b1de9f7fd1b
Aaron Ponti <aponti>
2004-04-23 18:32:34 +0000

    Fixed bug in fsmCenterCB_findEdges.m.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@528 0bcb941b-74d1-0310-bb26-e6636707f451

--- fsmCenter/gui/fsmCenterCB_findEdges.m
+++ fsmCenter/gui/fsmCenterCB_findEdges.m
@@ -32,14 +32,17 @@ if isempty(bitDepth)
     return
 end
 
+% Change bitDepth to double
+bitDepth=str2num(bitDepth);
+
 % Try to extract edges
 try
     img=double(img);
     
     % Normalize the image
-    img=img/(2^bitdepth-1);
+    img=img/(2^bitDepth-1);
     
-    [ans,img_edge,img_bg,edge_pixel,length_edge,frame_pos]=imFindCellEdge(double(img),'',0,'filter_image',1,'img_sigma',1,'bit_depth',2^bitdepth-1);
+    [ans,img_edge,img_bg,edge_pixel,length_edge,frame_pos]=imFindCellEdge(double(img),'',0,'filter_image',1,'img_sigma',1,'bit_depth',2^bitDepth-1);
     figure(gcf);
     hold on
     plot(edge_pixel(:,1),edge_pixel(:,2),'y-');
