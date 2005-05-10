9de91c58aadc0213da7c2bcc1bf5c18efca0920f
Jonas Dorn <jonas.dorn@gmail.com>
2005-05-10 13:36:22 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/chromdyn/main@1506 0bcb941b-74d1-0310-bb26-e6636707f451

--- experiments/spotDetector/findoverlap.m
+++ experiments/spotDetector/findoverlap.m
@@ -1,4 +1,4 @@
-function cord=findoverlap(data,cord,dataProperties)
+function cordOut=findoverlap(data,cord,dataProperties)
 %FINDOVERLAP locates fluorescent tags in 3D data
 %
 % SYNOPSIS cords=findoverlap(data,cord)
@@ -12,12 +12,18 @@ function cord=findoverlap(data,cord,dataProperties)
 %CONST DEFINITIONS
 MAXSPOTS=dataProperties.MAXSPOTS;
 
+maxNumSpots = 99999; % maximum number of spots that can be fitted simultaneously
+maxNumCoords = 99999; % maximum number of coordinates that can be checked by discernspots
 tsteps=size(data,5);
 %wb_hdl=waitbar(0,'Checking for multiple spots...');
 h= mywaitbar(0,[],tsteps,'Checking for multiple spots...');
 
+% init cordOut. Code changed to no longer overwrite input cord, but to
+% build a new structure from scratch. This could potentially be a problem
+% with huge structures, but it is much less error-prone
+cordOut(1:tsteps) = struct('sp',[],'mnint',[],'statistics',[],'parms',[]);
+
 for t=1:tsteps
-    ncord=struct('sp',[],'mnint',[],'statistics',[],'parms',[]); %init now so that order of fields stays constant
     Q=[];
     chi=[];
     snr=[];
@@ -25,95 +31,116 @@ for t=1:tsteps
     cord(t).statistics=[];
     cord(t).parms=[];
     
-        %======DEBUG
-        t;
-        if any(t == [999])
-            disp('press dbstep to continue!')
-            keyboard
-        end
-        %======DEBUG
+    %======DEBUG
+    %         t;
+    %         if any(t == [999])
+    %             disp('press dbstep to continue!')
+    %             keyboard
+    %         end
+    %======DEBUG
     
+    % if there are too many local maxima, the code could crash
+    % due to memory issues. therefore, try/catch to have only one bad
+    % frame, not lose an entire movie
     
-    
-    % if no spots in this time frame -> go back
-    if isfield(cord(t),'sp') &  ~isempty(cord(t).sp)
-        cordList=cat(1,cord(t).sp.cord);
-        %change to matlab coords
-        tc2=cordList(:,2);
-        cordList(:,2)=cordList(:,1);
-        cordList(:,1)=tc2;
-        
-        nspots=size(cordList,1);
-        
-        imgStk=data(:,:,:,1,t);
-        %resize data if necessary (odd size)
-        idx=~rem(size(imgStk),2);
-        imgStk=imgStk(1:end-idx(1),1:end-idx(2),1:end-idx(3));
-        
-        while (~isempty(cordList))
-            [spotsidx mask] = discernspots(cordList,size(imgStk),dataProperties);
-            idxList=find(mask);
-%             % less than 1.5*MAXSPOTS spots found -- do not do that
-%             anymore - fitTest should be able to remove the respective tags!
-%             if nspots<1.5*MAXSPOTS
+    try
+        % if no spots in this time frame -> go back
+        if isfield(cord(t),'sp') &  ~isempty(cord(t).sp)
+            cordList=cat(1,cord(t).sp.cord);
+            %change to matlab coords
+            tc2=cordList(:,2);
+            cordList(:,2)=cordList(:,1);
+            cordList(:,1)=tc2;
+            
+            nspots=size(cordList,1);
+            
+            imgStk=data(:,:,:,1,t);
+            %resize data if necessary (odd size)
+            idx=~rem(size(imgStk),2);
+            imgStk=imgStk(1:end-idx(1),1:end-idx(2),1:end-idx(3));
+            
+            while (~isempty(cordList))
+                discerned = 0;
+                if length(cordList) >= maxNumCoords
+                    error(sprintf('too many coords (%i) - Matlab will run out of memory!',length(cordList)));
+                end
+                [spotsidx mask] = discernspots(cordList,size(imgStk),dataProperties);
+                discerned = 1;
+                if length(spotsidx) >= maxNumSpots
+                    error(sprintf('too many spots (%i) - Matlab will run out of memory!',length(spotsidx)));
+                end
+                idxList=find(mask);
+                %             % less than 1.5*MAXSPOTS spots found -- do not do that
+                %             anymore - fitTest should be able to remove the respective tags!
+                %             if nspots<1.5*MAXSPOTS
                 mskData=imgStk(idxList);
                 [numDist,ncordList,ampList,bg,statistics]=fitTest(mskData,cordList(spotsidx,:),idxList,size(imgStk),dataProperties);
-%             else
-%                 numDist=length(spotsidx);
-%                 ncordList=cordList(spotsidx,:);
-%                 ampList=zeros(numDist,1);
-%                 bg=0;
-            if isempty(ncordList)
-                statistics.parms=[];
-                statistics.multi=0;
-                statistics.chi=[];
-                statistics.Qxx=[];
-                statistics.snr=[];
+                %             else
+                %                 numDist=length(spotsidx);
+                %                 ncordList=cordList(spotsidx,:);
+                %                 ampList=zeros(numDist,1);
+                %                 bg=0;
+                if isempty(ncordList)
+                    statistics.parms=[];
+                    statistics.multi=0;
+                    statistics.chi=[];
+                    statistics.Qxx=[];
+                    statistics.snr=[];
+                end;
+                %define new cordLst
+                tempidx=1:size(cordList,1);
+                tempidx(spotsidx)=0;
+                tempidx=nonzeros(tempidx);
+                cordList=cordList(tempidx,:);
+                
+                % add Q of spot(s)
+                Q=blkdiag(Q,statistics.Qxx);
+                snr=[snr statistics.snr];
+                
+                %insert coordinates
+                
+                for i= 1:numDist
+                    % insert new center coords
+                    patch=stamp3d(imgStk,[2 2 2],round(ncordList(i,:)));
+                    %change back to standard coords
+                    cordOut(t).sp(ct).cord=[ncordList(i,2) ncordList(i,1) ncordList(i,3)];
+                    cordOut(t).sp(ct).amp=ampList(i);
+                    cordOut(t).sp(ct).bg=bg;
+                    cordOut(t).sp(ct).mnint=mean(patch(:));
+                    % overlapping or single spot
+                    multi_1=numDist-i+1>size(cordList,1);
+                    cordOut(t).sp(ct).mult=statistics.multi;
+                    %                    cordOut.sp(ct).parms=statistics.parms;
+                    ct=ct+1;
+                    chi=[chi statistics.chi];
+                end; % for i= 1:numDist
+            end; %  while (~isempty(cordList))
+            
+            
+            % test for zero distance
+            cordOut(t).parms=statistics.parms;
+            cordOut(t).mnint=cord(t).mnint;
+            if ~isempty(Q)
+                cordOut(t) = testdistance(cordOut(t),Q,chi,snr,dataProperties);
+            else
+                % the if and else here  is only because the nspots>=MAXSPOTS
+                cordOut(t).parms=[];
             end;
-            %define new cordLst
-            tempidx=1:size(cordList,1);
-            tempidx(spotsidx)=0;
-            tempidx=nonzeros(tempidx);
-            cordList=cordList(tempidx,:);
             
-            % add Q of spot(s)
-            Q=blkdiag(Q,statistics.Qxx);
-            snr=[snr statistics.snr];
-            
-            %insert coordinates
-            
-            for i= 1:numDist
-                % insert new center coords
-                patch=stamp3d(imgStk,[2 2 2],round(ncordList(i,:)));
-                %change back to standard coords
-                ncord.sp(ct).cord=[ncordList(i,2) ncordList(i,1) ncordList(i,3)];
-                ncord.sp(ct).amp=ampList(i);
-                ncord.sp(ct).bg=bg;
-                ncord.sp(ct).mnint=mean(patch(:));
-                % overlapping or single spot
-                multi_1=numDist-i+1>size(cordList,1);
-                ncord.sp(ct).mult=statistics.multi;
-                %                    ncord.sp(ct).parms=statistics.parms;
-                ct=ct+1;
-                chi=[chi statistics.chi];
-            end;
-        end;
-        
-        
-        % test for zero distance
-        ncord.parms=statistics.parms;
-        ncord.mnint=cord(t).mnint;
-        if ~isempty(Q)
-            ncord = testdistance(ncord,Q,chi,snr,dataProperties);
+        end; % if isfield(cord(t),'sp') &  ~isempty(cord(t).sp)
+    catch
+        % remember length of spotsIdx
+        if discerned
+            maxNumSpots = min(maxNumSpots,length(spotsidx));
         else
-            % the if and else here  is only because the nspots>=MAXSPOTS
-            ncord.parms=[];
-        end;
-        cord(t)=ncord;
-    end;
+            maxNumCoords = min(maxNumCoords,length(cordList));
+        end
+        
+        disp(sprintf('MMF aborted in frame %i:\n%s',t,lasterr))      
+    end
     % progressbar
     mywaitbar(t/tsteps,h,tsteps);
     %waitbar(t/tsteps,wb_hdl);
-end;
+end; % loop time
 
 close(h);
