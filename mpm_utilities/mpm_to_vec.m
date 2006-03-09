31876bc6a3998cbc71d289ccc4f57e20ae8b2678
Matthias Machacek <matthias.machacek@hispeed.ch>
2006-03-09 21:18:22 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@1942 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ mpm_utilities/mpm_to_vec.m
@@ -0,0 +1,37 @@
+clear
+%mpm_variable = load('mpm_org.mat');
+%mpm_variable = load('mpm_frommlg');
+%mpm_variable = load('mpm_yx_Feb24');
+mpm_variable = load('mpm');
+
+a=char(fieldnames(mpm_variable));
+if size(a,1) > 1
+    MPM = mpm_variable.(a(2,:));
+else
+    MPM = mpm_variable.(a);
+end
+clear mpm_variable 
+
+%img_h = 779;
+
+% trace MPM
+t_n =1;
+track_length(1)=0;
+
+for t=1:2:size(MPM,2)-2
+    nr=1;
+    for i=1:size(MPM,1)
+        if MPM(i,t) ~= 0 & MPM(i,t+2) ~= 0
+            vector(nr,:) = MPM(i,t:t+3);
+            
+%             vector(nr,1) = img_h - MPM(i,t+1);
+%             vector(nr,2) = MPM(i,t);
+%             vector(nr,3) = img_h - MPM(i,t+3);
+%             vector(nr,4) = MPM(i,t+2);
+            nr=nr+1;
+        end
+    end
+    % save 
+    num_ext = num2str(int8(t/2), '%03i')
+    save(['vector' num_ext],'vector');
+end
