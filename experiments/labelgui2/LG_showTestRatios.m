e3a029c9e18e2bdbb9086935747cdf6bc0839a53
Jonas Dorn <jonas.dorn@gmail.com>
2006-03-22 17:18:35 +0000

    Added callback from labelgui2 that can be used for labelgui
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/chromdyn/main@1950 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ experiments/labelgui2/LG_showTestRatios.m
@@ -0,0 +1,32 @@
+function figureHandle = LG_showTestRatios(testRatios, dataProperties,figurePosition)
+%LG_showTestRatios is the routine to display testRatios and the current cutoff
+% figurePosition is optional
+
+% read amplitude cutoff
+cutValue = dataProperties.amplitudeCutoff;
+
+% create figure
+figureHandle = figure('Name',sprintf('cutoff for %s',dataProperties.name));
+% place correctly if there is a remembered position
+if nargin > 2 && ~isempty(figurePosition)
+    set(figureHandle,'Position',figurePosition);
+end
+
+% read testRatios
+tmp = cat(1,testRatios{:});
+times = (tmp(:,1));
+ratios = tmp(:,2);
+nTimepoints = max(times);
+
+% plot testRatios
+t1idx = ismember(times,1:3:nTimepoints);
+t2idx = ismember(times,2:3:nTimepoints);
+t3idx = ismember(times,3:3:nTimepoints);
+plot(times(t1idx),ratios(t1idx),'+r',...
+    times(t2idx),ratios(t2idx),'+g',...
+    times(t3idx),ratios(t3idx),'+b'),
+
+% plot cutoff
+hold on
+plot([1,nTimepoints],[cutValue,cutValue],'r')
+  
