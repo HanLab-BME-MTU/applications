69dada8043ab18e81ca44ad9da3fc8cf8a3b02d6
Jonas Dorn <jonas.dorn@gmail.com>
2006-11-10 01:29:40 +0000

    bilobeDistribution: added option for spindle-binning
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/chromdyn/main@2221 0bcb941b-74d1-0310-bb26-e6636707f451

--- analysis/bilobedDistribution.m
+++ analysis/bilobedDistribution.m
@@ -1,19 +1,27 @@
-function bilobeData = bilobedDistribution(correct4Tag)
+function [bilobeDataIdlist, bilobeDataSpindleLength] = bilobedDistribution(correct4Tag,spindleBins)
 %BIOLOBEDDISTRIBUTION collects and plots tag positions along the spindle axis
 %
-% SYNOPSIS bilobeData = bilobedDistribution(kinetochcorrect4TagoreCorrection)
+% SYNOPSIS bilobeDataIdlist = bilobedDistribution(kinetochcorrect4TagoreCorrection)
 %
 % INPUT  correct4Tag : (opt) correction for the distance between the
-%                              kinetochore and the tag 
+%                              kinetochore and the tag
 %                              {0} No correction
 %                               1  Correction by 0.1 um
 %                               any other number: correction (in um)
+%        spindleBins (opt) : vector of bin centers for spindle length.
+%                            Default : [1,1.25,1.5,1.75,2]
+%                            Edge bins include extreme data
 %
-% OUTPUT bilobeData:  structure array. Contains for every idlist used:
+% OUTPUT bilobeDataXX:  structure array.
+%                       XX indicates the grouping of the data - either
+%                       according to the individual movies, or according to
+%                       spindle length. Both arrays have the fields
 %                       .spindleLength  ntp-by-1. Spindle length in microns
 %                       .cenPositions   ntp-by-2. Relative cen position
-%                       .time           ntp-by-1. Timepoints
+%                       .time           ntp-by-1. Timepoints (movie# for
+%                                       XX==spindleLength)
 %                       .name           char. directory name
+
 %
 % MATLAB VERSION (originally written on): 7.1.0.246 (R14) Service Pack 3 Windows_NT
 %
@@ -35,7 +43,9 @@ switch correct4Tag
     otherwise
         tagCorrection = correct4Tag;
 end
-
+if nargin < 2 || isempty(spindleBins)
+    spindleBins = 1:0.25:2;
+end
 
 %========================
 % LOAD DATA
@@ -58,7 +68,7 @@ idlistList = loadIdlistList(cdBiodata(4),...
 % this part of the code is not very clean because it has been copied from
 % bilobeProjection.m
 nIdlists = length(idlistList);
-bilobeData(1:nIdlists) = struct('spindleLength',[],...
+bilobeDataIdlist(1:nIdlists) = struct('spindleLength',[],...
     'cenPositions',[],'time',[],'name',[]);
 
 for iIdlist = 1:nIdlists
@@ -117,16 +127,16 @@ for iIdlist = 1:nIdlists
 
     % normalize spindleVector
     [n_spindleVector, e_spindleVector] = normList(spindleVector);
-    
+
     % In case we want to correct for the centromere, we need to have the
     % sxcx vectors normed, too
     [n_s1c1Vector, e_s1c1Vector] = normList(s1c1Vector);
     [n_s2c2Vector, e_s2c2Vector] = normList(s2c2Vector);
-    
+
     % correct - but don't make the length negative!
     s1c1Vector = s1c1Vector - repmat(min(tagCorrection,n_s1c1Vector-0.08),1,3) .* e_s1c1Vector;
     s2c2Vector = s2c2Vector - repmat(min(tagCorrection,n_s2c2Vector-0.08),1,3) .* e_s2c2Vector;
-    
+
 
     % project spb-cen vectors. Distance from spb1
     cen1Dist = dot(s1c1Vector, e_spindleVector, 2);
@@ -136,10 +146,10 @@ for iIdlist = 1:nIdlists
     cenPosNorm = [cen1Dist./n_spindleVector, cen2Dist./n_spindleVector];
 
     % store data
-    bilobeData(iIdlist).spindleLength = n_spindleVector;
-    bilobeData(iIdlist).cenPosition = cenPosNorm;
-    bilobeData(iIdlist).time = find(goodTime);
-    bilobeData(iIdlist).name = idlistList(iIdlist).name;
+    bilobeDataIdlist(iIdlist).spindleLength = n_spindleVector;
+    bilobeDataIdlist(iIdlist).cenPosition = cenPosNorm;
+    bilobeDataIdlist(iIdlist).time = find(goodTime);
+    bilobeDataIdlist(iIdlist).name = idlistList(iIdlist).name;
 
     %-------------------
     % individual plots
@@ -164,7 +174,7 @@ for iIdlist = 1:nIdlists
 
     % histogram: cumulated positions
     ah = subplot(2,2,3);
-    histogram(cenPosNorm,[],ah);
+    histogram(ah,cenPosNorm);
     xlim(ah,[0,1])
     title('cumulated positions')
 
@@ -172,12 +182,41 @@ for iIdlist = 1:nIdlists
     ah = subplot(2,2,4);
     % flip 50% cenPosNorm - it's already stored
     cenPosNorm(2:2:end,:) = 1-cenPosNorm(2:2:end,:);
-    histogram(cenPosNorm,[],ah);
+    histogram(ah,cenPosNorm);
     xlim(ah,[0,1])
     title('cumulated positions - 50% flipped')
 
 end % loop
 
+%========================
+% BIN SPINDLE LENGTHS
+%========================
+
+% collect data
+spindleLength = cat(1,bilobeDataIdlist.spindleLength);
+allCenPos = cat(1,bilobeDataIdlist.cenPosition);
+time = cat(1,bilobeDataIdlist.time);
+tIdx = 0;
+for i=1:nIdlists
+    nTime = length(bilobeDataIdlist(i).time);
+    time(tIdx+1:tIdx+nTime) = i;
+    tIdx = tIdx + nTime;
+end
+
+% make spindle edges
+spindleEdges = [-inf,(spindleBins(2:end)+spindleBins(1:end-1))/2,inf];
+nBins = length(spindleBins);
+bilobeDataSpindleLength(1:nBins) = deal(bilobeDataIdlist(1));
+for i=1:nBins
+    spindleIdx = spindleLength > spindleEdges(i) & ...
+        spindleLength < spindleEdges(i+1);
+    bilobeDataSpindleLength(i).spindleLength = spindleLength(spindleIdx);
+    bilobeDataSpindleLength(i).cenPosition = allCenPos(spindleIdx,:);
+    bilobeDataSpindleLength(i).time = time(spindleIdx,:);
+    bilobeDataSpindleLength(i).name = ...
+        sprintf('Spindle length: %1.2f-%1.2f',...
+        spindleEdges(i),spindleEdges(i+1));
+end
 
 
 %========================
@@ -188,16 +227,32 @@ end % loop
 % smooth histograms of true and 50% flipped positions
 % --> for simplicity moved into loop
 
+% plot bins
+for i=1:nBins
+    figure('Name',bilobeDataSpindleLength(i).name);
+    cenPos = bilobeDataSpindleLength(i).cenPosition;
+
+    ah = subplot(1,2,1);
+    histogram(ah,cenPos);
+    xlim(ah,[0,1])
+    title(sprintf('%i normalized cen positions',numel(cenPos)));
+    cenPos(2:2:end,:) = 1-cenPos(2:2:end,:);
+    ah = subplot(1,2,2);
+    histogram(ah,cenPos);
+    xlim(ah,[0,1])
+    title('normalized cen positions - 50% flipped')
+end
+
 % then plot all (norm, 50% flipped cumulated)
 % potentially: group by spindle length with histogram
-allCenPos = cat(1,bilobeData.cenPosition);
+% allCenPos = cat(1,bilobeDataIdlist.cenPosition);
 figure('Name',sprintf('%i movies cumulated',nIdlists))
 ah = subplot(1,2,1);
-histogram(allCenPos,[],ah);
+histogram(ah,allCenPos);
 xlim(ah,[0,1])
 title('cumulated positions')
 allCenPos(2:2:end,:) = 1-allCenPos(2:2:end,:);
 ah = subplot(1,2,2);
-histogram(allCenPos,[],ah);
+histogram(ah,allCenPos);
 xlim(ah,[0,1])
 title('cumulated positions - 50% flipped')
