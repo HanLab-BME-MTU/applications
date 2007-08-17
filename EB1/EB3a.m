f66a1dfa15f43eca9b1c8838ec513377a73f427b
Alexandre Matov <amatov@scripps.edu>
2007-08-17 17:22:01 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@2481 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ EB1/EB3a.m
@@ -0,0 +1,177 @@
+function EB3a
+
+
+for i = 1:1
+
+    s = 3;
+    strg=sprintf('%%.%dd',s);
+    indxStr=sprintf(strg,i);
+
+    %     I = imread(['Z:\AlexData\EB1_Katsu\20041113EB1-GFP',indxStr,'.tif']);
+    %     I = imread(['Z:\AlexData\EB1_Claudio\controlmovie5\EB1-GFP_control05_T',indxStr,'.tif']);
+    %     I = imread(['Z:\AlexData\EB1_Lisa\extract\extract_tiffs\xjul03_r11\xjul03_r11tiff',indxStr,'.tif']);
+
+    %        I = imread(['Z:\AlexData\EB1_Katsu_2s_1\EB1-GFP-5',indxStr,'.tif']);
+    %           I = imread(['Z:\AlexData\EB1_Katsu_2s_2\EB1-GFP-6',indxStr,'.tif']);
+    %-----CLAUDIO--------------
+    % I = imread(['Z:\AlexData\Ben\EB1tracking\Op18Spindles\102406_Op18\Op18_movie_2\images\Op18_movie2_',indxStr,'.tif']);
+        I = imread(['X:\AlexData\Yukako\060707_2\2_',indxStr,'.tif']);
+%     I = imread(['X:\AlexData\Jay\EB1_60x2x2binning_25uMSTC_4a\images\EB1_60x_2x2binning_25uMSTC_4a',indxStr,'.tif']);
+    
+%     I = imread(['P:\forAlex\images300\RNAi_RACCA_3_',indxStr,'.tif']);
+  
+% I = imread(['X:\AlexData11\786Opar\786Opar_NaCl01_R3D\images\786Opar_NaCl01_T',indxStr,'.tif']);
+    %------KATSU----------------
+    % I = imread(['Z:\AlexData\Katsu\cell7\images\EB1-GFP',indxStr,'.tif']);
+
+    I=double(I);
+    aux = Gauss2D(I,1);%1 
+    I2 = Gauss2D(I,7); %4 (Yukako 10)
+    I3 = aux - I2;
+    [cutoffInd, cutoffV] = cutFirstHistMode(I3,0);
+
+    coef = 2;% coef = 4 Katsu; coef = 1 Claudio; coef = 1 Lisa_xju103_r11; 
+    I4 = I3>cutoffV*coef; % REMOVE THE NOISE FEATURES %no 3
+
+    X = bwlabel(I4);
+    warningState = warning;
+    warning off all
+    stats = regionprops(X,'all'); % Warning: Out of range value converted to intmin('uint8') or intmax('uint8').
+    warning(warningState)
+
+    % Initialize 'feats' structure
+    feats=struct(...
+        'pos',[0 0],...                  % Centroid - [y x]
+        'ecc',0,...                      % Eccentricity
+        'ori',0);   % Orientation
+
+    for j = 1:length(stats)
+        feats.pos(j,1) = stats(j).Centroid(1);
+        feats.pos(j,2) = stats(j).Centroid(2);
+        feats.ecc(j,1) = stats(j).Eccentricity;
+        feats.ori(j,1) = stats(j).Orientation;
+        feats.len(j,1) = stats(j).MajorAxisLength;
+
+        e1 = [-cos(stats(j).Orientation*pi/180) sin(stats(j).Orientation*pi/180) 0];
+        e2 = [sin(stats(j).Orientation*pi/180) cos(stats(j).Orientation*pi/180) 0];
+        e3 = [0 0 1];
+        Ori = [stats(j).Centroid  0];
+        v1 = [-10 10];
+        v2 = [-5 5];
+        v3 = [0 0];
+        [xGrid,yGrid]=arbitraryGrid(e1,e2,e3,Ori,v1,v2,v3);
+
+        Crop(:,:,j) = interp2(I,xGrid,yGrid);
+        %         Crop(:,:,j) = interp2(I,xGrid,yGrid,'*linear');
+
+        e1 = [];e2 = [];e3 = []; Ori = []; v1 = []; v2 = []; xGrid = []; yGrid = [];
+    end
+
+    %-------------------------------------
+    %     aaux = 5;
+    %     If=Gauss2D(I,1);
+    %     figure, imshow(If(1+aaux:end-aaux,1+aaux:end-aaux),[]);%I4
+    %     hold on
+    %     for i = 1:length(feats.ori)
+    %         h = quiver(feats.pos(i,1)-aaux,feats.pos(i,2)-aaux,-cos(feats.ori(i)*pi/180),sin(feats.ori(i)*pi/180),3,'r');
+    %         set(h,'LineWidth',2)
+    %     end
+    %------------------------------------
+
+    Cm = nanmean(Crop,3); % MEAN/REPRESENTATIVE EB1 CROP
+    Crop(isnan(Crop))=0;% border effect - some NaN
+    Cm1 = bwlabel(Cm);
+    statsC = regionprops(Cm1,'all');
+
+    %     figure,imshow(Cm,[])
+    %
+    %     uiviewpanel,imshow(Cm,[])
+
+
+    sC = size(Crop);
+    Cm3d = repmat(Cm,[1,1,size(Crop,3)]);
+    dC = Crop - Cm3d;
+    sqC = dC.^2;
+    ssqC = squeeze(sum(sum(sqC,1),2)); %LIST OF DIFFERENCES AFTER SUBTRACTION
+
+    B = Cm(:);
+    A = ones(length(B),2);
+
+    for m = 1:size(Crop,3)
+        CR = Crop(:,:,m);
+        A(:,2) = CR(:);
+        goodRows = find(A(:,2) ~= 0 & isfinite(B));
+        XX = lscov(A(goodRows,:),B(goodRows));
+        RES = B(goodRows) - A(goodRows,:)*XX;
+        OUT(m,:) = [mean(RES(:).^2),XX'];
+    end
+
+    [Ind,V]=cutFirstHistMode(OUT(:,1),0);% switch to 1 to see HIST
+
+    goodFeats = find(OUT(:,1)<V); % SPOTS WHICH FIT WELL WITH THE MEAN EB1 SPOT
+
+    %     featNames = fieldnames(feats);
+    %     for field = 1:length(featNames)
+    %         feats.(featNames{field}) = feats.(featNames{field})(goodFeats,:);
+    %     end
+
+    %     CC = Crop(:,:,goodFeats);
+    %     CCm = nanmean(CC,3); %MEAN EB1 BASED on GOOD/FITTED EB1 SPOTS
+    %     CC(isnan(CC))=0;% border effect - some NaN
+    %     CCm1 = bwlabel(CCm);
+    %     statsCC = regionprops(CCm1,'all');
+
+    %     figure,imshow(CCm,[])
+    %
+    %     figure,histogram(ssqC)
+    %     figure,histogram(ssqC(goodFeats))
+    %     [cutoffIndex, cutoffValue] = cutFirstHistMode(ssqC(goodFeats),0);
+    %     [cutoffIndex, cutoffValue] = cutFirstHistMode(ssqC,0);
+    %
+    % %     veryGoodFeats = find(ssqC(goodFeats)<cutoffValue);
+    %     veryGoodFeats = find(ssqC<cutoffValue);
+
+
+
+    featNames = fieldnames(feats);
+    for field = 1:length(featNames)
+        feats.(featNames{field}) = feats.(featNames{field})(goodFeats,:);
+    end
+
+
+
+    %-------------------------------------
+    aaux = 5;
+    If=Gauss2D(I,1);
+    figure, imshow(If(1+aaux:end-aaux,1+aaux:end-aaux),[]);%I4
+    hold on
+    for i = 1:length(feats.ori)
+        h = quiver(feats.pos(i,1)-aaux,feats.pos(i,2)-aaux,-cos(feats.ori(i)*pi/180),sin(feats.ori(i)*pi/180),3,'r');
+        set(h,'LineWidth',2)
+    end
+    %------------------------------------
+    %        save(['Z:\AlexData\Torsten\control_1\cands\feats',indxStr],'feats')
+    % clear all
+    % close all
+end
+
+% aaux = 5;
+% If=Gauss2D(I,1);
+% figure, imshow(If(1+aaux:end-aaux,1+aaux:end-aaux),[]);%I4
+% hold on
+% for i = 1:length(feats.ori)
+%     h = quiver(feats.pos(i,1)-aaux,feats.pos(i,2)-aaux,-cos(feats.ori(i)*pi/180),sin(feats.ori(i)*pi/180),3,'r');
+%     set(h,'LineWidth',2)
+% end
+
+% stats
+%
+%
+%
+% figure,imshow(I,[])
+% hold on
+% plot(xGrid(:),yGrid(:),'g.')
+%
+% figure,imshow(Crop1,[])
+
+
---
+++ EB1/groupingLAP.m
@@ -0,0 +1,164 @@
+function group = groupingLAP(load_mat)
+
+% pass the TRACKS struct as input
+nbFrames = 125;
+LifeTime = 4; %4
+
+MaxTimeSpan = 15;
+coneAngPos = cos(pi/4);%45
+coneAngNeg = cos(pi/18);%10
+trackAng = cos(pi/3);%60
+constVel = 1; %debug100
+duConst = 1000000;
+if nargin == 1
+    [fileName,dirName] = uigetfile('*.mat','Choose a .mat file');
+    load([dirName,filesep,fileName]);
+else
+    %     load(['/mnt/alex10/AlexData11/786O/786O_parental/786Opar_NaCl02_R3D/point_files/config001_5p00_track_bidir.mat']);
+    load(['D:\amatov\data\786O\config001_5p00_track_bidir.mat']);
+end
+
+indx = find( [tracks.len] >= LifeTime);
+TimeSpan = min(max([tracks.len]),MaxTimeSpan); 
+traj = tracks(indx);
+leIndx = length(indx);
+
+% calculate trajectory information
+for i = 1:leIndx
+    traj(i).endID = traj(i).startID + traj(i).len - 1;
+    dY = traj(i).points(end,1) - traj(i).points(1,1);
+    dX = traj(i).points(end,2) - traj(i).points(1,2);
+    traj(i).vec = [dX; dY];
+    traj(i).vel = sqrt(dY^2+dX^2)/traj(i).len;
+end
+% exclude tracks with NO motion
+traj = traj(find([traj.vel]>0)); % SOME DONT MOVE!?
+leIndx = length(traj);
+
+% get start & end points of all tracks for distance matrix calculation
+for i = 1:leIndx
+    E(i,:)=traj(i).points(end,1:2);
+    B(i,:)=traj(i).points(1,1:2);
+end
+
+[listStartID,indxStartID] = sort([traj.startID]);
+M = sparse(leIndx,leIndx); % angle multiplication matrix
+C = sparse(leIndx,leIndx); % distance cut-off matrix
+
+progressText(0,'For-loop') % Create text
+for i = 1:leIndx
+    if TimeSpan > nbFrames-traj(i).endID
+        T = nbFrames-traj(i).endID;
+    else
+        T = TimeSpan;
+    end
+    if (traj(i).endID < (nbFrames - LifeTime))
+
+        aux1 = find(listStartID>(traj(i).endID+1));
+        firstIndx =  aux1(1);
+
+        aux2 = find(listStartID>(traj(i).endID+T+1));
+        if ~isempty(aux2)
+            finalIndx =  aux2(1)-1;
+        else
+            finalIndx = length(listStartID);
+        end
+
+        a = indxStartID(firstIndx:finalIndx);
+
+        magTrajVec = sqrt(sum(traj(i).vec.^2,1));
+        magTrajA = sqrt(sum([traj(a).vec]'.^2,2));
+        cos_trackAng = ([traj(a).vec]'*traj(i).vec)./(magTrajA*magTrajVec);
+        
+        listA = find(cos_trackAng>trackAng);
+        aa = a(listA);
+        cos_trA = cos_trackAng(listA);
+   
+        if ~isempty(aa)
+            dxCo = []; dyCo = [];
+            for j = 1:length(aa)
+                dyCo(j) = traj(aa(j)).points(1,1) - traj(i).points(end,1); % get it out of the loop - to the other loop up
+                dxCo(j) = traj(aa(j)).points(1,2) - traj(i).points(end,2);
+            end
+            aVecs = ([dxCo; dyCo])';
+
+            magAvecs = sqrt(sum(aVecs.^2,2));
+            magAvecs(find(magAvecs==0)) = 0.001; % some new/t+4 traj begin where an old/t one ended
+            cos_coneAng = (aVecs*traj(i).vec)./(magAvecs*magTrajVec);
+
+            posC = find(cos_coneAng>coneAngPos);
+            negC = find(cos_coneAng<-coneAngNeg);
+
+            if ~isempty(posC)
+                C(i,aa(posC)) = traj(i).vel * sqrt([traj(aa(posC)).startID]-traj(i).endID) ;%* constVel;
+                M(i,aa(posC)) = abs(cos_trA(posC) - cos_coneAng(posC));
+            end
+            if ~isempty(negC)
+                C(i,aa(negC)) = traj(i).vel * sqrt([traj(aa(negC)).startID]-traj(i).endID) ;%* constVel;
+                M(i,aa(negC)) = abs(cos_trA(negC) - abs(cos_coneAng(negC)));
+            end
+        end
+    end
+    progressText(i/leIndx);
+end
+
+R = max(C(:));
+D=createSparseDistanceMatrix(E,B,R); % dimentions - leIndx
+radIndx = find(D<=C);
+C(radIndx) = D(radIndx).*M(radIndx); % overwrite old cut-off matrix to generate cost matrix
+
+[links12, links21] = lap(C,-1,0,1);
+lnk = links12(1:leIndx);
+
+grIndx = find(lnk<=leIndx);
+group=struct('list',[]);
+assocTracks = zeros(leIndx,1);
+grNb = 0; 
+for k = 1:grIndx
+    if (assocTracks(k) == 0) && (lnk(k) <= leIndx)
+        if assocTracks(lnk(k)) == 0
+            grNb = grNb + 1;
+            assocTracks(k) = grNb;
+            assocTracks(lnk(k)) = grNb;
+            group(grNb).list = [k lnk(k)];
+            traj(k).next = lnk(k);
+            traj(lnk(k)).prev = k;
+            traj(k).g_nb = grNb;
+            traj(lnk(k)).g_nb = grNb;
+            k = lnk(k);
+            while k <= leIndx
+                if lnk(k) <= leIndx
+                    assocTracks(lnk(k)) = grNb;
+                    group(grNb).list = [group(grNb).list,lnk(k)];
+                    traj(k).next = lnk(k);
+                    traj(lnk(k)).prev = k;
+                    traj(lnk(k)).g_nb = grNb;
+                end
+                k = lnk(k);
+            end
+        else
+            group(assocTracks(lnk(k))).list = [k,group(assocTracks(lnk(k))).list];
+        end
+
+    end
+end
+
+grNb = length(group);
+figure
+for i = 1:grNb
+    k = group(i).list;
+    le_gr = length(k);
+    plot(traj(k(1)).points(1,2),traj(k(1)).points(1,1),'ks') 
+    for j = 1:le_gr
+        plot(traj(k(j)).points(:,2),traj(k(j)).points(:,1),'r-')
+        hold on
+        plot(traj(k(j)).points(end,2),traj(k(j)).points(end,1),'r*')
+        plot([traj(k(j)).points(1,2),traj(k(j)).points(end,2)],[traj(k(j)).points(1,1),traj(k(j)).points(end,1)],'k:')
+        if j < le_gr
+            plot([traj(k(j)).points(end,2),traj(k(j+1)).points(1,2)],[traj(k(j)).points(end,1),traj(k(j+1)).points(1,1)],'g-')
+            plot(traj(k(j+1)).points(1,2),traj(k(j+1)).points(1,1),'g*')
+        end
+    end
+end
+hold off
+disp(sprintf('Number of groups %d',grNb));
