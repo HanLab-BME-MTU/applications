bcdfd32a260e64c33a3c1e1e2a0e0d48781e4fed
Alexandre Matov <amatov@scripps.edu>
2007-09-05 01:21:25 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@2516 0bcb941b-74d1-0310-bb26-e6636707f451

--- EB1/plotGroup.m
+++ EB1/plotGroup.m
@@ -5,15 +5,17 @@ figure
 for i = 1:grNb
     k = group(i).list;
     le_gr = length(k);
-    plot(traj(k(1)).points(1,2),traj(k(1)).points(1,1),'ks') 
-    for j = 1:le_gr
-        plot(traj(k(j)).points(:,2),traj(k(j)).points(:,1),'r-')
+    if le_gr > 0
+        plot(traj(k(1)).points(1,2),traj(k(1)).points(1,1),'ks')
         hold on
-        plot(traj(k(j)).points(end,2),traj(k(j)).points(end,1),'r*')
-        plot([traj(k(j)).points(1,2),traj(k(j)).points(end,2)],[traj(k(j)).points(1,1),traj(k(j)).points(end,1)],'k:')
-        if j < le_gr
-            plot([traj(k(j)).points(end,2),traj(k(j+1)).points(1,2)],[traj(k(j)).points(end,1),traj(k(j+1)).points(1,1)],'g-')
-            plot(traj(k(j+1)).points(1,2),traj(k(j+1)).points(1,1),'g*')
+        for j = 1:le_gr
+            plot(traj(k(j)).points(:,2),traj(k(j)).points(:,1),'r-')
+            plot(traj(k(j)).points(end,2),traj(k(j)).points(end,1),'r*')
+            plot([traj(k(j)).points(1,2),traj(k(j)).points(end,2)],[traj(k(j)).points(1,1),traj(k(j)).points(end,1)],'k:')
+            if j < le_gr
+                plot([traj(k(j)).points(end,2),traj(k(j+1)).points(1,2)],[traj(k(j)).points(end,1),traj(k(j+1)).points(1,1)],'g-')
+                plot(traj(k(j+1)).points(1,2),traj(k(j+1)).points(1,1),'g*')
+            end
         end
     end
 end
