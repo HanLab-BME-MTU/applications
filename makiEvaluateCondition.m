b304bbc6fd62a64c761a1abc02b0be326796a8ae
Jonas Dorn <jonas.dorn@gmail.com>
2008-03-18 14:25:11 +0000

    makiEvaluateCondition.m: Now takes into account unequal cropping
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/maki/main@2752 0bcb941b-74d1-0310-bb26-e6636707f451

--- makiEvaluateCondition.m
+++ makiEvaluateCondition.m
@@ -7,9 +7,9 @@ function makiEvaluateCondition(cutoff,serverType)
 %           serverType  : ID to indicate gui-loading path
 %
 %           The function will load three movies with frames taken in
-%           the order 123123123... 
+%           the order 123123123...
 %
-% OUTPUT 
+% OUTPUT
 %
 % REMARKS makiEvaluateCondition returns the percentage of false positives
 %           and false negatives in the initial spot detection using the
@@ -45,6 +45,10 @@ end
 % truth) and false negatives (points only in ground truth). Maximum
 % displacement allowed is 500 nm
 
+pix2mu = [data(1).dataStruct.dataProperties.PIXELSIZE_XY,data(1).dataStruct.dataProperties.PIXELSIZE_XY,...
+    data(1).dataStruct.dataProperties.PIXELSIZE_Z];
+imSize = [data(1).dataStruct.movieHeader.numRows,data(1).dataStruct.movieHeader.numCols,data(1).dataStruct.movieHeader.numZSlices];
+
 % all dataProperties are the same
 nData = 3;%length(data);
 for iData = nData:-1:1; % count backwards so that we have nSpots defined on time
@@ -54,13 +58,34 @@ for iData = nData:-1:1; % count backwards so that we have nSpots defined on time
     data(iData).nSpots = [...
         catStruct(1,'dataStruct.initCoord.nSpots'),...
         zeros(nTimepoints,2)]; %number, false pos, false neg, 100%
+
+    % check for cropping
+    if isempty(dataStruct.dataProperties.crop)
+        dataStruct.dataProperties.crop = zeros(2,3);
+    end
+    crop = dataStruct.dataProperties.crop(:,1:3);
+    isCrop = any(crop,1);
+    crop(1,~isCrop) = 1;
+    crop(2,~isCrop) = imSize(~isCrop);
+
     for t = 1:nTimepoints
-        
+
+
+
         % compare coordinates to #3
-        if iData < 3
-            coordTest = dataStruct.initCoord(t).allCoord(:,1:3);
-            coordTruth = data(3).dataStruct.initCoord(t).allCoord(:,1:3);
-            
+        if iData == 3
+            coords = dataStruct.initCoord(t).allCoordPix(:,1:3);
+            coordTruth = coords + repmat(crop(1,:)-1,size(coords,1),1);
+            coordTruth = coordTruth.*repmat(pix2mu,size(coords,1),1);
+        else
+
+
+            coords = dataStruct.initCoord(t).allCoordPix(:,1:3);
+            coordTest = coords + repmat(crop(1,:)-1,size(coords,1),1);
+            coordTest = coordTest.*repmat(pix2mu,size(coords,1),1);
+
+
+
             % get distance
             dm = createDistanceMatrix(coordTest,coordTruth);
             % cutoff at 1 um
@@ -73,7 +98,7 @@ for iData = nData:-1:1; % count backwards so that we have nSpots defined on time
             data(iData).nSpots(t,3) = sum(fromTruth(1:data(3).nSpots(t,1))>data(iData).nSpots(t,1));
             % 100%: nSpots of ground truth
             data(iData).nSpots(t,4) = data(3).nSpots(t,1);
-            
+
         end
     end
 end
@@ -87,17 +112,17 @@ end
 % evaluate: plot percent fp/fn
 figure('Name',sprintf('%s cutoff %1.1f',data(3).dataStruct.projectName,cutoff));
 for i=1:2
-subplot(2,2,i)
-plot(1:nTimepoints,100*(data(i).nSpots(:,2))./data(i).nSpots(:,4),'+r',...
-    1:nTimepoints,100*(data(i).nSpots(:,3))./data(i).nSpots(:,4),'ob');
-ylim([0,20]);
-hold on
-avgNeg = 100*mean(data(i).nSpots(:,3)./data(i).nSpots(:,4));
-avgPos = 100*mean(data(i).nSpots(:,2)./data(i).nSpots(:,4));
-plot([1,nTimepoints],[avgPos,avgPos],'r',...
-    [1,nTimepoints],[avgNeg,avgNeg],'b')
-title(data(i).dataStruct.projectName)
-legend('false positives%','false negatives%')
+    subplot(2,2,i)
+    plot(1:nTimepoints,100*(data(i).nSpots(:,2))./data(i).nSpots(:,4),'+r',...
+        1:nTimepoints,100*(data(i).nSpots(:,3))./data(i).nSpots(:,4),'ob');
+    ylim([0,20]);
+    hold on
+    avgNeg = 100*mean(data(i).nSpots(:,3)./data(i).nSpots(:,4));
+    avgPos = 100*mean(data(i).nSpots(:,2)./data(i).nSpots(:,4));
+    plot([1,nTimepoints],[avgPos,avgPos],'r',...
+        [1,nTimepoints],[avgNeg,avgNeg],'b')
+    title(data(i).dataStruct.projectName)
+    legend('false positives%','false negatives%')
 end
 subplot(2,2,3),plot(data(1).nSpots(:,1),'xr'),
 hold on,
