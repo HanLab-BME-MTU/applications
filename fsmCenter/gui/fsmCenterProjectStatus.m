2a82ccfed0100398ab3849588b25d351686570a2
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2009-03-20 02:07:02 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@3206 0bcb941b-74d1-0310-bb26-e6636707f451

--- fsmCenter/gui/fsmCenterProjectStatus.m
+++ fsmCenter/gui/fsmCenterProjectStatus.m
@@ -22,7 +22,7 @@ function varargout = fsmCenterProjectStatus(varargin)
 
 % Edit the above text to modify the response to help fsmCenterProjectStatus
 
-% Last Modified by GUIDE v2.5 18-Mar-2009 19:11:26
+% Last Modified by GUIDE v2.5 19-Mar-2009 18:27:47
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -55,6 +55,24 @@ function fsmCenterProjectStatus_OpeningFcn(hObject, eventdata, handles, varargin
 % Choose default command line output for fsmCenterProjectStatus
 handles.output = hObject;
 
+%
+% Set to 'Not done' every field
+%
+
+setElementStatus(handles.textEdgeTracker, getStatus('edgeTracker'));
+setElementStatus(handles.textKymographAnalysis, getStatus('kymographAnalysis'));
+setElementStatus(handles.textPreprocessingModule, getStatus('preprocessingModule'));
+setElementStatus(handles.textTrackerModule, getStatus('trackerModule'));
+setElementStatus(handles.textBuilderModule, getStatus('builderModule'));
+setElementStatus(handles.textKineticsAnalysisModule, getStatus('kineticsAnalysisModule'));
+setElementStatus(handles.textResultDisplayModule, getStatus('resultDisplayModule'));
+setElementStatus(handles.textPostprocessingModule, getStatus('postprocessingModule'));
+
+%
+% Check Status of Preprocessing Module
+%
+% handles.projDir filesep handles.tackDir filesep 'cands'
+
 % Update handles structure
 guidata(hObject, handles);
 
@@ -80,3 +98,33 @@ function pushOK_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 close(get(hObject, 'Parent'));
+
+function status = getStatus(moduleName)
+    status = 0;
+    
+    switch moduleName
+        case 'edgeTracker', status = 0;
+        case 'kymographAnalysis', status  = 0;
+        case 'preprocessingModule', status  = 0;
+        case 'trackerModule', status  = 0;
+        case 'builderModule', status  = 0;
+        case 'kineticsAnalysisModule', status  = 0;
+        case 'resultDisplayModule', status  = 0;
+        case 'postprocessingModule', status  = 0;
+        otherwise, error('Unable to get status: invalid module name.');
+    end
+
+
+function setElementStatus(hElement, status)
+colorStatus = zeros(1, 3);
+stringStatus = '';
+
+switch status
+    case 0, colorStatus = [0.847, 0.161, 0.0]; stringStatus = 'Not done';
+    case 1, colorStatus = [1.0, 1.0, 0]; stringStatus = 'Problem';
+    case 2, colorStatus = [0, 0.49, 0]; stringStatus = 'Done';
+end
+
+set(hElement, 'String', stringStatus);
+set(hElement, 'ForegroundColor', colorStatus);
+
