0de665f70f089e054a084cf6601165c423e6cad3
Dinah Loerke <dinah.loerke@du.edu>
2009-04-30 00:06:23 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@3345 0bcb941b-74d1-0310-bb26-e6636707f451

--- loadIndividualMovies.m
+++ loadIndividualMovies.m
@@ -28,9 +28,46 @@ while loadData == 1
     end
 end
 
-%list all movies and ask user to select movies to use in analysis
-[selection, selectionList] = listSelectGUI({experiment(:).source},[],'move',[]);
+% print out all movie paths with an identifying integer
+for iMovie = 1:length(experiment);
+    fprintf([int2str(iMovie) '\t' experiment(iMovie).source '\n'])
+end
 
-exp = experiment(selection);
+% ask user to choose in between tracking all movies, listing the movies
+% that should be tracked, or listing the movies that should not be tracked
+movies2Track = input('Enter:\n 1 to include all movies\n 2 to list the movies to include\n 3 to list the movies to not include\n');
+
+%if user inputs 1 analyze all movies that need analysis
+if movies2Track == 1
+    exp = experiment;
+
+%if user inputs 2 ask him to specify movies to analyzed (if they need to be
+%analyzed)
+elseif movies2Track == 2
+    movies2Track = input('List identifying integers of movies as vector (inside square brackets separated by commas or spaces).\n');
+    %delete all other movies from experiment structure
+    for iMovie = 1:length(movies2Track)
+        exp(iMovie) = experiment(movies2Track(iMovie));
+    end %of for loop
+
+%if user inputs 3 ask him to specify movies to leave out
+elseif movies2Track == 3
+    %ask user to input integers of movies to exclude
+    movies2Track = input('List identifying integers of movies as vector (inside square brackets separated by commas or spaces).\n');
+    %delete these movies from experiment structure if can find an entry in
+    %movies2Track that matches the movie index number in the experiment
+    %structure
+    %for all movies
+    count = 1; %initiate counter
+    for iExperiment = 1:length(experiment)
+        %if all entries in movies2Track fail to match experiment index of movie
+        if isempty(find(movies2Track == iExperiment,1));
+            %save movie 
+            exp(count) = experiment(iExperiment);
+            count = count + 1;
+        end %of if loop
+    end %of while loop
+end
+
 
 end %of function
