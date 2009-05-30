34f6781e03017f031ce157a75e516ef6b2802224
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2009-05-30 01:42:19 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@3438 0bcb941b-74d1-0310-bb26-e6636707f451

--- fsmDataViewer/displayLayers.m
+++ fsmDataViewer/displayLayers.m
@@ -10,22 +10,34 @@ hAxes = get(hFig, 'CurrentAxes');
 % Get the channel plugins list
 [channelPlugins layerPlugins] = getPlugins();
 
+% Clear all layers
+for iLayer = 1:numLayers
+    tag = settings.layers{iLayer}.tag;
+    
+    hLayers = findall(get(hAxes, 'Children'), 'Tag', tag);
+    for i=1:length(hLayers)
+        delete(hLayers(i));        
+    end
+end
+
+h = waitbar(0, 'please wait...');
+
+% Draw new layers
 for iLayer = 1:numLayers
     layerTypeID = settings.layers{iLayer}.type;
     layerColor = settings.layers{iLayer}.color;
     tag = settings.layers{iLayer}.tag;
 
-    % Clear previous layer
-    hLayers = findall(get(hAxes, 'Children'), 'Tag', tag);
-    for h=1:length(hLayers)
-        delete(hLayers(h));        
-    end
                 
     % Display layer
     fileName = [settings.layers{iLayer}.path ...
         filesep settings.layers{iLayer}.fileNames{iFrame}];
     
     layerPlugins(layerTypeID).displayFunc(hAxes, tag, fileName, layerColor);
+    
+    waitbar(iLayer / numLayers, h);
 end
+
+close(h);
 
 end
