3071cd031dae8656e2432a1bf7eca1d96586e9a6
Jonas Dorn <jonas.dorn@gmail.com>
2009-06-03 20:42:14 +0000

    added spb1* to mark certain cells
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/chromdyn/main@3443 0bcb941b-74d1-0310-bb26-e6636707f451

--- experiments/labelgui2/LG_tagPopupMenu_Callback.m
+++ experiments/labelgui2/LG_tagPopupMenu_Callback.m
@@ -34,6 +34,9 @@ linklist = movieWindowHandles.idlist(currentTime).linklist;
 if size(linklist,1) > 2
     labellist{end+1} = 'cen1*';
 end
+if size(linklist,1) > 1
+    labellist{end+1} = 'sbp1*';
+end
 
 % delete previous children first!
 delete(get(renameTagH, 'Children'))
