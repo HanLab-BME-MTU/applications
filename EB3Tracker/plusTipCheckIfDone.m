70248f615459ca3809255f1a9e2696a86120e638
Kathryn Applegate <kathryn.applegate@gmail.com>
2009-06-23 20:53:28 +0000

    plusTipCheckIfDone.m now has option to check if detection has been completed on a list of movies (in addition to final tracking)
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@3479 0bcb941b-74d1-0310-bb26-e6636707f451

--- EB3Tracker/plusTipCheckIfDone.m
+++ EB3Tracker/plusTipCheckIfDone.m
@@ -1,10 +1,16 @@
-function [result,notDone]=plusTipCheckIfDone
-% plusTipCheckIfDone gives projData timestamp for each project and list of unanalyzed projects
-%
-% synopsis: [result,notDone]=plusTipCheckIfDone
-%
+function [result,notDone]=plusTipCheckIfDone(fileType)
+% plusTipCheckIfDone returns directory name and timestamp for creation of either
+% movieInfo.mat (if fileType=1) or projData.mat (if fileType=2) for each
+% project, as well as which projects have not been analyzed.
+
 result=[];
 notDone=[];
+
+if nargin < 1
+    disp('--checkIfDone: Incorrect number of input arguments!');
+    errFlag  = 1;
+    return
+end
 
 [FileName,PathName] = uigetfile('*.mat','Select projList.mat file');
 
@@ -22,17 +28,30 @@ end
 % get sorted list of all projects
 allProjList=struct2cell(projList);
 allProjList=allProjList(2,:)';
-allProjList=sort(allProjList);
+%allProjList=sort(allProjList);
 allProjList=cellfun(@(i) formatPath(i),allProjList,'uniformoutput',0);
 
-% list of all the files in each project directory
-dirContents=cellfun(@(i) dir([i filesep 'meta']),allProjList,'uniformoutput',0);
-% convert each structure to a cell array
-dirContentsCell=cellfun(@(i) struct2cell(i),dirContents,'uniformoutput',0);
-% get index for projData file, if it exists
-projListLoc=cellfun(@(i) find(strcmpi(i(1,:),'projData.mat')),dirContentsCell,'uniformoutput',0);
-% get timestamp for each projList file
-dates=cellfun(@(i,j) i(2,j),dirContentsCell,projListLoc,'uniformoutput',0); 
+switch fileType
+    case 1
+
+        % list of all the files in each project directory
+        dirContents=cellfun(@(i) dir([i filesep 'feat']),allProjList,'uniformoutput',0);
+        % convert each structure to a cell array
+        dirContentsCell=cellfun(@(i) struct2cell(i),dirContents,'uniformoutput',0);
+        % get index for movieInfo file, if it exists
+        movieInfoLoc=cellfun(@(i) find(strcmpi(i(1,:),'movieInfo.mat')),dirContentsCell,'uniformoutput',0);
+        % get timestamp for each movieInfo file
+        dates=cellfun(@(i,j) i(2,j),dirContentsCell,movieInfoLoc,'uniformoutput',0);
+    case 2
+        % list of all the files in each project directory
+        dirContents=cellfun(@(i) dir([i filesep 'feat']),allProjList,'uniformoutput',0);
+        % convert each structure to a cell array
+        dirContentsCell=cellfun(@(i) struct2cell(i),dirContents,'uniformoutput',0);
+        % get index for projData file, if it exists
+        projListLoc=cellfun(@(i) find(strcmpi(i(1,:),'projData.mat')),dirContentsCell,'uniformoutput',0);
+        % get timestamp for each projList file
+        dates=cellfun(@(i,j) i(2,j),dirContentsCell,projListLoc,'uniformoutput',0);
+end
 
 % find out if any of them don't have dates
 notDone=find(cell2mat(cellfun(@(i) isempty(i),dates,'uniformoutput',0)));
