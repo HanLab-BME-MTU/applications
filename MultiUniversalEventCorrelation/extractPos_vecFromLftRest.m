445bc9984333f1ef47e9176d2bc3998496f4f9af
Dinah Loerke <dinah.loerke@du.edu>
2009-06-24 18:29:28 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@3483 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ MultiUniversalEventCorrelation/extractPos_vecFromLftRest.m
@@ -0,0 +1,198 @@
+function [posvec] = extractPos_vecFromLftRest(lftinfopath, restvector, fr);
+% this function extracts an MPM of points of interest from trackInfo using
+% specified restrictions, and time delays in the third dimension
+% 
+% SYNOPSIS [MPMglobal] = CorrelateData2Pos_extractMPM(trackinfo,restvector, tvec, ttype);
+%
+% INPUT     lftinfopath       
+%
+%           restvector      = restriction vector, defined by
+%               dstat       = restvector(1) = status [1,2,3];
+%               dapp        = restvector(2) = disappearance [-1,0,1];
+%               dminfr      = restvector(3) = min lifetime in frames;
+%               minlft      = restvector(4) = min lifetime in seconds;;
+%               maxlft      = restvector(5) = min lifetime in seconds;;
+%               minlft_fr   = round(minlft/fr);
+%               maxlft_fr   = round(maxlft/fr);
+%               
+%               OPTIONAL:
+%               intrank_min = restvector(6)  = minimum intensity rank
+%                             percentile, e.g. 0.80
+%               intrank_max = restvector(7)  = maxmimum intensity rank
+%                             percentile, e.g. 0.95
+%
+%           fr              = framerate; default 1
+%
+%
+% OUTPUT:   posvec       = result MPM
+%
+% last modified: Dinah Loerke   01/30/2009
+
+
+
+
+%% determine framerate
+framerate = 1;
+if nargin>2
+    if ~isempty(fr)
+        framerate = fr;
+    end
+end
+
+
+%% calculate lft matrices
+cd(lftinfopath);
+loadfile = open('lftInfo.mat');
+lmat = loadfile.lftInfo;
+
+mat_lft   = full(lmat.Mat_lifetime);
+mat_stat  = full(lmat.Mat_status);
+mat_x     = full(lmat.Mat_xcoord);
+mat_y     = full(lmat.Mat_ycoord);
+mat_da    = full(lmat.Mat_disapp);
+
+vec_lft   = max(mat_lft,[],2);
+mat_stat(mat_stat==0) = nan;
+vec_stat = nanmin(mat_stat,[],2);
+
+% frame number
+[nx,nf] = size(mat_lft);
+    
+
+% if intensity information is used, upload trackInfo and extract intensity
+% data here
+if length(restvector)>5
+    % remove 'LifetimeInfo' from lftinfopath
+    trackpath = strrep(lftinfopath, 'LifetimeInfo', 'TrackInfoMatrices');
+    cd(trackpath);
+    loadfile2 = open('trackInfo.mat');
+    tmat = full(loadfile2.trackInfo);
+    imat = tmat(:,4:8:size(tmat,2));
+    % maximum intensity
+    ivec = nanmax(imat,[],2);
+    [inx,iny] = size(imat);
+    % intensity vector of all usable positions (>4 frames lft)
+    ivec_all = ivec;
+    ivec_all(find(vec_lft<4)) = nan;
+    % calculate intensity percentile rank vector
+    ivec_rank_all = intensityPercentageRank(ivec_all);
+        
+end
+
+
+%% =====================================================================
+% DESIRED CONDITIONS
+% for all frames in the movie, collect those locations of points that
+% fulfill a number of requirements
+% specifically status, minimum/maximum lifetime
+[rx,ry] = size(restvector);
+if min(rx,ry)>1
+    nres = size(restvector,2);
+else
+    nres = 1;
+end
+
+for r=1:nres
+    
+    if nres>1
+        crestvector = restvector(r,:);
+    else
+        crestvector = restvector;
+    end
+       
+    % desired conditions
+    dstat       = crestvector(r,1);
+    dminfr      = crestvector(r,3);
+    minlft      = crestvector(r,4);
+    maxlft      = crestvector(r,5);
+      
+    minlft_fr   = round(minlft/framerate);
+    maxlft_fr   = round(maxlft/framerate);
+    
+    if length(restvector)>5
+        min_pint = crestvector(r,6);
+        max_pint = crestvector(r,7);
+    end
+    
+
+    %% =====================================================================
+
+    % find positions that fulfill required conditions, as logical matrices
+    % correct status
+    if isfinite(dstat)
+        findpos_stat = ( vec_stat==dstat );
+    else
+        findpos_stat = isfinite(vec_stat);
+    end
+
+    % correct lifetime    
+    findpos_lft = ( (vec_lft>dminfr) & (vec_lft>=minlft_fr) & (vec_lft<=maxlft_fr) );
+         
+    % combine positions
+    findpos_all = find(findpos_stat & findpos_lft );
+    
+    if length(restvector)>5
+        
+        % OPTION 1 (default) - continued below: 
+        % USE PERCENTAGE RANK RELATIVE TO ALL USABLE POSITIONS
+              
+%             % OPTION 2: (uncomment this mini-paragraph)
+%             % USE PERCENTAGE RANK RELATIVE TO THE POSITIONS DEFINED
+%             % BY THE LIFETIME CRITERIA ABOVE
+%             % limit intensity vector to positions defined by lifetime criteria
+%             ivec_curr = nan*ivec_all;
+%             ivec_curr(findpos_all) = ivec_all(findpos_all);
+%             % calculate intensity percentile rank vector (modified compared to
+%             % version from line 78
+%             ivec_rank_all = intensityPercentageRank(ivec_curr);
+          
+        
+        % OPTION 1: USE PERCENTAGE RANK RELATIVE TO ALL USABLE POSITIONS
+        findpos_intRank = ( (ivec_rank_all>min_pint) & (ivec_rank_all<max_pint) );
+        % modify combine positions
+        findpos_all = find(findpos_stat & findpos_lft & findpos_intRank);
+             
+    end
+    
+    if nres>1
+        posvec(r).posvec = findpos_all;
+    else
+        posvec = findpos_all;
+    end
+    
+end % of for r-loop
+
+
+end % of function
+
+
+%%=========================================================================
+%
+%                           subfunctions
+%
+%==========================================================================
+
+function [vec_intPercRank] = intensityPercentageRank(vec_int);
+
+% sorting matrix
+% first colums: intensities
+imat_sort(:,1) = vec_int;
+%second columns: original positions
+imat_sort(:,2) = [1:length(vec_int)];
+% intensity-sorted matrix
+imat_sorted1 = sortrows(imat_sort,1);
+% assign rank into additional column
+imat_sorted1(:,3) = [1:length(vec_int)];
+% maximum defined position
+imaxpos = max(find(isfinite(imat_sorted1(:,1))));
+% convert absolute rank into percentile rank
+imat_sorted1(:,3) = imat_sorted1(:,3)/imaxpos;
+% fill non-defined positions from intensity vector with nans
+imat_sorted1(imaxpos:length(vec_int),3) = nan;
+% sort matrix back to original position (in column 2)
+imat_sorted2 = sortrows(imat_sorted1,2);
+% extract re-sorted percentage rank
+vec_intPercRank = imat_sorted2(:,3);
+
+end % of subfunction
+
