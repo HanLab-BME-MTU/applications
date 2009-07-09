2ccfb8233349d69dd4e081c95711653162a6e6fc
Kathryn Applegate <kathryn.applegate@gmail.com>
2009-07-09 22:37:29 +0000

    changed so you can load projList
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@3512 0bcb941b-74d1-0310-bb26-e6636707f451

--- EB3Tracker/GUIs/plusTipGuiSwitch.m
+++ EB3Tracker/GUIs/plusTipGuiSwitch.m
@@ -3,18 +3,54 @@ function [handles]=plusTipGuiSwitch(hObject,eventdata,handles,callbackName)
 % GUIs that use this function:
 % plusTipTrackViz - for track plotting and movie making
 
+
+
 switch callbackName
-    
+
     case 'getProjPush'
-        if handles.getStr==1
-            handles.strList=inputGUI;
-            [projList,projPathParsed]=getProj(handles.strList);
-        else
-            handles.strList=[];
-            [projList,projPathParsed]=getProj;
+        if ~isfield(handles,'loadProjList')
+            handles.loadProjList=0;
         end
-        handles.projList=projList;
-        
+
+        if handles.loadProjList==0
+
+            if handles.getStr==1
+                handles.strList=inputGUI;
+                [projList,projPathParsed]=getProj(handles.strList);
+            else
+                handles.strList=[];
+                [projList,projPathParsed]=getProj;
+            end
+            handles.projList=projList;
+
+        else
+            % select one or more projList files
+            [handles.projList]=combineProjListFiles(0);
+            
+            % if "select subset" is checked, ask user for search string(s)
+            % and find matches from projList
+            if handles.getStr==1
+                handles.strList=inputGUI;
+                roiDirList=struct2cell(handles.projList);
+                roiDirList=roiDirList(2,:)';
+                nStr=length(handles.strList);
+
+                projCount=0;
+                if ~isempty(roiDirList)
+                    tempROI=ones(length(roiDirList),1);
+                    for i=1:nStr
+                        testStr = handles.strList{i};
+                        tempROI=tempROI & cellfun(@(y) ~isempty(strfind(y,lower(testStr))),lower(roiDirList));
+                    end
+
+                    matches=find(tempROI);
+
+                    handles.projList=handles.projList(matches);
+                end
+            end
+
+        end
+
     case 'selectSavedRoiPushbutton'
         [FileName,PathName] = uigetfile({'*.*'},'Select roiYX.mat or roiMask.tif');
         if ~isequal(FileName,0)
@@ -27,7 +63,7 @@ switch callbackName
                 errordlg('File chosen was not a roiYX.mat or roiMask.tif file. Please try again.','File Error');
             end
         end
-        
+
     case 'startFrame'
         sFVal=get(hObject,'String');
         if isequal(lower(sFVal),'min')
@@ -35,7 +71,7 @@ switch callbackName
         else
             handles.timeRange(1)=str2double(sFVal);
         end
-        
+
     case 'endFrame'
         eFVal=get(hObject,'String');
         if isequal(lower(eFVal),'max')
@@ -43,13 +79,13 @@ switch callbackName
         else
             handles.timeRange(2)=str2double(eFVal);
         end
-        
+
     case 'selectTracksCheck'
         handles.ask4select=get(hObject,'Value');
-        
+
     case 'showTracksCheck'
         handles.showTracks=get(hObject,'Value');
-        
+
     case 'speedLimitEdit'
         velLimVal=get(hObject,'String');
         if isequal(lower(velLimVal),'max')
@@ -57,11 +93,11 @@ switch callbackName
         else
             handles.velLimit=str2double(velLimVal);
         end
-        
+
     case 'indivTrackNumbersEdit'
         userInput=get(hObject,'String');
         handles.indivTrack=str2num(userInput)';
-    
+
     case 'plotTracksPush'
         [handles.selectedTracks] = plusTipPlotTracks(handles.projData,[],...
             handles.timeRange,handles.img,handles.ask4select,...
@@ -78,10 +114,10 @@ switch callbackName
             end
 
         end
-        
+
     case 'aviCheckTrackMov'
         handles.doAvi=get(hObject,'Value');
-        
+
     case 'selectedTracksDisplay'
         hObject=handles.selectedTracksDisplay;
         if isempty(handles.selectedTracks)
@@ -90,17 +126,17 @@ switch callbackName
             set(hObject,'Enable','On');
         end
         set(hObject,'String',num2str(handles.selectedTracks));
-        
+
     case 'speedMovieButton'
         plusTipSpeedMovie(handles.projData,handles.timeRange,handles.velLimit,handles.roi,handles.doAvi);
-        
+
     case 'trackMovieButton'
         plusTipTrackMovie(handles.projData,handles.indivTrack,handles.timeRange,...
             handles.roi,handles.magCoef,handles.showTracks,handles.showDetect,handles.doAvi);
-        
+
     case 'aviCheckSpeedMov'
         handles.doAvi=get(hObject,'Value');
-        
+
     case 'resetButton'
         closeGUI = handles.figure1; %handles.figure1 is the GUI figure
         guiPosition = get(handles.figure1,'Position'); %get the position of the GUI
@@ -108,7 +144,7 @@ switch callbackName
         eval(guiName) %call the GUI again
         close(closeGUI); %close the old GUI
         set(gcf,'Position',guiPosition); %set the position for the new GUI
-        
+
     otherwise
         error('???')
 
