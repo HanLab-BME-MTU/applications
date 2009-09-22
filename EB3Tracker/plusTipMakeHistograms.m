cb1c4180aad0acd0efd2418c7aa5b1130e58a037
Kathryn Applegate <kathryn.applegate@gmail.com>
2009-09-22 10:40:18 +0000

    revamped to take in generic dataMatCrpMinMic matrix from plusTipMergeSubtracks or concatenated version from multiple movies. now makes histograms for speed, lifetime, and displacements (growth/fgap/bgap/stacked)
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@3609 0bcb941b-74d1-0310-bb26-e6636707f451

--- EB3Tracker/plusTipMakeHistograms.m
+++ EB3Tracker/plusTipMakeHistograms.m
@@ -1,96 +1,92 @@
-function plusTipMakeHistograms(runInfo,reclassMatrix)
-% plusTipMakeHistograms saves growth, fgap, and bgap speed histograms, as well as
-% a txt file containing the raw values for growth, fgap, and bgap.  
-%
-% reclassMatrix is the matrix where fgaps identified as continuation of growth are
-% closed; that is, where trackType=5, the growth phase before, the fgap,
-% and the growth phase after are merged into one subtrack, so that the
-% average velocity values stored here are slightly different from that of
-% projData.nTrack_sF_eF_vMicPerMin_trackType_lifetime_totalDispPix, where
-% the rows are not merged.
+function plusTipMakeHistograms(speedLifeDispMat,saveDir)
+% plusTipMakeHistograms saves speed, lifetime, and displacement histograms
+% for growth, fgap, and bgap populations
 
-homeDir=pwd;
-cd(runInfo.metaDir)
-
-histDir=[runInfo.metaDir filesep 'histograms'];
-mkdir(histDir)
-
-
-% here are the actual speeds according to their kinds
-pop1=    reclassMatrix(reclassMatrix(:,5)==1,4);  % growth
-pop2=    reclassMatrix(reclassMatrix(:,5)==2,4);  % fgap
-pop3=abs(reclassMatrix(reclassMatrix(:,5)==3,4)); % bgap
-
-% put populations into a matrix and write them into a text file
-M=nan(max([length(pop1) length(pop2) length(pop3)]),3);
-M(1:length(pop1),1)=pop1;
-M(1:length(pop2),2)=pop2;
-M(1:length(pop3),3)=pop3;
-dlmwrite([runInfo.metaDir filesep 'growth_Fgap_Bgap_Distrib.txt'], M, 'precision', 3,'delimiter', '\t','newline', 'pc');
-
-% create x-axis bins spanning all values
-n=linspace(min([pop1;pop2;pop3]),max([pop1;pop2;pop3]),25);
-
-% bin the samples
-[x1,dummy] = histc(pop1,n); % growth
-[x2,dummy] = histc(pop2,n); % fgap
-[x3,dummy] = histc(pop3,n); % bgap
-
-% put the binned values into a matrix for the stacked plot
-M=nan(max([length(x1) length(x2) length(x3)]),3);
-M(1:length(x1),1)=x1;
-M(1:length(x2),2)=x2;
-M(1:length(x3),3)=x3;
-
-% make the plot
-figure
-bar(n,M,'stack')
-colormap([1 0 0; 0 0 1; 0 1 0])
-legend('growth','fgap','bgap','Location','best')
-title('Stacked Speed Distributions')
-xlabel('speed (um/min)');
-ylabel('frequency of tracks');
-saveas(gcf,[histDir filesep 'stackedHist.fig'])
-saveas(gcf,[histDir filesep 'stackedHist.tif'])
-close(gcf)
-
-figure;
-% growth
-if ~isempty(x1)
-    bar(n,x1,'r')
-    title('growth speed distribution')
-    xlabel('speed (um/min)');
-    ylabel('frequency of tracks');
-
-    saveas(gcf,[histDir filesep 'growthHist.fig'])
-    saveas(gcf,[histDir filesep 'growthHist.tif'])
+if ~isdir(saveDir)
+    mkdir(saveDir)
 end
-close(gcf)
 
-figure
-% fgap
-if ~isempty(x2)
-    bar(n,x2,'b')
-    title('fgap speed distribution')
-    xlabel('speed (um/min)');
+
+for iParam=1:3
+    switch iParam
+        case 1
+            data=speedLifeDispMat(:,1:3); % speed
+            titleStr='speed';
+            xStr='speed (microns/min)';
+        case 2
+            data=speedLifeDispMat(:,4:6); % lifetime
+            titleStr='lifetime';
+            xStr='lifetime (min)';
+        case 3
+            data=speedLifeDispMat(:,7:9); % displacement
+            titleStr='displacement';
+            xStr='displacement (microns)';
+    end
+
+
+    % create x-axis bins spanning all values
+    n=linspace(nanmin(data(:)),nanmax(data(:)),25);
+
+    % bin the samples
+    [x1,dummy] = histc(data(:,1),n); % growth
+    [x2,dummy] = histc(data(:,2),n); % fgap
+    [x3,dummy] = histc(data(:,3),n); % bgap
+
+    % put the binned values into a matrix for the stacked plot
+    M=nan(max([length(x1) length(x2) length(x3)]),3);
+    M(1:length(x1),1)=x1;
+    M(1:length(x2),2)=x2;
+    M(1:length(x3),3)=x3;
+
+    % make the plot
+    figure
+    bar(n,M,'stack')
+    colormap([1 0 0; 0 0 1; 0 1 0])
+    legend('growth','fgap','bgap','Location','best')
+    title(['stacked ' titleStr ' distributions'])
+    xlabel(xStr);
     ylabel('frequency of tracks');
+    %saveas(gcf,[saveDir filesep titleStr '_stackedHist.fig'])
+    saveas(gcf,[saveDir filesep titleStr '_stackedHist.tif'])
+    close(gcf)
 
-    saveas(gcf,[histDir filesep 'fgapHist.fig'])
-    saveas(gcf,[histDir filesep 'fgapHist.tif'])
+    figure;
+    % growth
+    if ~isempty(x1)
+        bar(n,x1,'r')
+        title(['growth ' titleStr ' distribution'])
+        xlabel(xStr);
+        ylabel('frequency of tracks');
+
+        %saveas(gcf,[saveDir filesep titleStr '_growthHist.fig'])
+        saveas(gcf,[saveDir filesep titleStr '_growthHist.tif'])
+    end
+    close(gcf)
+
+    figure
+    % fgap
+    if ~isempty(x2)
+        bar(n,x2,'b')
+        title(['fgap ' titleStr ' distribution'])
+        xlabel(xStr);
+        ylabel('frequency of tracks');
+
+        %saveas(gcf,[saveDir filesep titleStr '_fgapHist.fig'])
+        saveas(gcf,[saveDir filesep titleStr '_fgapHist.tif'])
+    end
+    close(gcf)
+
+    figure
+    % bgap
+    if ~isempty(x3)
+        bar(n,x3,'g')
+        title(['bgap ' titleStr ' distribution'])
+        xlabel(xStr);
+        ylabel('frequency of tracks');
+
+        %saveas(gcf,[saveDir filesep titleStr '_bgapHist.fig'])
+        saveas(gcf,[saveDir filesep titleStr '_bgapHist.tif'])
+    end
+    close(gcf)
+
 end
-close(gcf)
-
-figure
-% bgap
-if ~isempty(x3)
-    bar(n,x3,'g')
-    title('bgap speed distribution')
-    xlabel('speed (um/min)');
-    ylabel('frequency of tracks');
-
-    saveas(gcf,[histDir filesep 'bgapHist.fig'])
-    saveas(gcf,[histDir filesep 'bgapHist.tif'])
-end
-close(gcf)
-
-cd(homeDir)
