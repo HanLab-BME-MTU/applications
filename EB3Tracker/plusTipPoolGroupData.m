f091772702486b9ce51765b5aa29433e27457708
Kathryn Applegate <kathryn.applegate@gmail.com>
2009-10-06 15:31:56 +0000

    plusTipPoolGroupData.m now has options for pooling data and making plots within vs. between groups
    plusTipOutlierPlot.m is a new function to plot data by group, with the option of filtering out outliers in groupList using the Rousseeuw criterion
    
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@3633 0bcb941b-74d1-0310-bb26-e6636707f451

--- EB3Tracker/plusTipPoolGroupData.m
+++ EB3Tracker/plusTipPoolGroupData.m
@@ -1,12 +1,52 @@
-function plusTipPoolGroupData(groupList)
+function plusTipPoolGroupData(groupList,saveDir,doBtw,doWtn,doPlot)
+% plusTipPoolGroupData pools plus tip data from multiple projects in groups
+%
+% SYNOPSIS:  plusTipPoolGroupData(groupList,saveDir,doBtw,doWtn,doPlot)
+%
+% INPUT:
+% groupList : output of plusTipPickGroups, nProj x 2 cell array where the
+%             first column contains the group identifier for each group,
+%             and the second column contains the project path
+% saveDir   : path to output directory
+% doBtw     : 1 to pool data from projects in the same group, 0 to skip
+% doWtn     : 1 to save text file of distributions from all projects in
+%             each group in a "withinGroupComparisons" folder
+% doPlot    : 1 to make histograms and boxplots for within and/or between
+%             group data
 
 
-% get output directory and load groupLIst
-saveDir=uigetdir(pwd,'Select output directory for pooled group data.');
+homeDir=pwd;
+
+if nargin<1 || isempty(groupList)
+    [fileName,pathName] = uigetfile('*.mat','Select groupList.mat file');
+    if fileName==0
+        display('no file selected')
+        return
+    end
+    load([pathName filesep fileName]);
+    cd(pathName)
+end
+
+if nargin<2 || isempty(saveDir)
+    saveDir=uigetdir(pwd,'Select output directory for pooled group data.');
+end
+
+if nargin<3 || isempty(doBtw)
+    doBtw=1;
+end
+
+if nargin<4 || isempty(doWtn)
+    doWtn=0;
+end
+
+if nargin<5 || isempty(doPlot)
+    doPlot=1;
+end
+
 
 
 projGroupName=groupList(:,1);
-projGroupDir=groupList(:,2);
+projGroupDir=cellfun(@(x) formatPath(x),groupList(:,2),'uniformoutput',0);
 
 
 % fix the names if there are spaces or hyphens and append prefix 'grp'
@@ -16,20 +56,25 @@ projGroupName=cellfun(@(x) ['grp_' x],projGroupName,'uniformoutput',0);
 
 
 % count unique groups and keep them in order of the original list
-[grpNames,m,projGroupIdx] = unique(projGroupName);
+[btwGrpNames,m,projGroupIdx] = unique(projGroupName);
 [b,idx]=sort(m);
-grpNames=grpNames(idx);
+btwGrpNames=btwGrpNames(idx);
 
 
 projCount=1; % all-project counter
 projNum=cell(length(projGroupName),1); % within-group counter
-allDataCell=cell(length(grpNames),1); % cell-array for data matrix
-for iGroup = 1:length(grpNames)
+allDataCell=cell(length(btwGrpNames),1); % cell-array for data matrix
+groupData=struct('info',{},...
+    'gs',{},'fs',{},'bs',{},...
+    'gl',{},'fl',{},'bl',{},...
+    'gd',{},'fd',{},'bd',{});
+
+for iGroup = 1:length(btwGrpNames)
 
     % indices of projects in iGroup
-    tempIdx=strmatch(grpNames(iGroup),projGroupName,'exact');
+    tempIdx=strmatch(btwGrpNames(iGroup),projGroupName,'exact');
 
-    data=cell(length(tempIdx),1);
+    dataByProject=cell(length(tempIdx),1);
     trkCount=1;
     for iProj = 1:length(tempIdx)
 
@@ -40,9 +85,9 @@ for iGroup = 1:length(grpNames)
         trkIdx=unique(dataMatCrpSecMic(:,1));
         dataMatCrpSecMic(:,1)=swapMaskValues(dataMatCrpSecMic(:,1),trkIdx,[trkCount:trkCount+length(trkIdx)-1]);
         trkCount=trkCount+length(trkIdx);
-        
+
         % assign matrix to cell array
-        data{iProj,1}=dataMatCrpSecMic;
+        dataByProject{iProj,1}=dataMatCrpSecMic;
 
         projNum{projCount,1}=iProj;
         projNum{projCount,2}=formatPath(temp.projData.anDir);
@@ -51,55 +96,77 @@ for iGroup = 1:length(grpNames)
     end
 
     % concat all the data
-    allData=cell2mat(data);
-    allDataCell{iGroup,1}=allData;
+    allData=cell2mat(dataByProject);
+    [stats,M]=plusTipDynamParam(allData);
 
-    % get stats for the whole group
-    [groupData.(grpNames{iGroup,1}).stats,M]=plusTipDynamParam(allData);
+    if doBtw==1
+        % put data in cell array for bwt group box plot
+        allDataCell{iGroup,1}=allData;
 
-    % store speed/lifetime/displacement lists for growth/fgap/bgap
-    groupData.(grpNames{iGroup,1}).gs=M(~isnan(M(:,1)),1);
-    groupData.(grpNames{iGroup,1}).fs=M(~isnan(M(:,2)),2);
-    groupData.(grpNames{iGroup,1}).bs=M(~isnan(M(:,3)),3);
-    groupData.(grpNames{iGroup,1}).gl=M(~isnan(M(:,4)),4);
-    groupData.(grpNames{iGroup,1}).fl=M(~isnan(M(:,5)),5);
-    groupData.(grpNames{iGroup,1}).bl=M(~isnan(M(:,6)),6);
-    groupData.(grpNames{iGroup,1}).gd=M(~isnan(M(:,7)),7);
-    groupData.(grpNames{iGroup,1}).fd=M(~isnan(M(:,8)),8);
-    groupData.(grpNames{iGroup,1}).bd=M(~isnan(M(:,9)),9);
-   
-    
-    tempDir=[saveDir filesep 'withinGroupComparisons' filesep grpNames{iGroup,1}];
-    if ~isdir(tempDir)
+        % make structure containing the concatenated distributions
+        groupData(iGroup,1).info.name=btwGrpNames{iGroup,1};
+        groupData(iGroup,1).info.groupListIdx=tempIdx;
+        groupData(iGroup,1).info.stats=stats;
+        groupData(iGroup,1).gs=M(~isnan(M(:,1)),1);
+        groupData(iGroup,1).fs=M(~isnan(M(:,2)),2);
+        groupData(iGroup,1).bs=M(~isnan(M(:,3)),3);
+        groupData(iGroup,1).gl=M(~isnan(M(:,4)),4);
+        groupData(iGroup,1).fl=M(~isnan(M(:,5)),5);
+        groupData(iGroup,1).bl=M(~isnan(M(:,6)),6);
+        groupData(iGroup,1).gd=M(~isnan(M(:,7)),7);
+        groupData(iGroup,1).fd=M(~isnan(M(:,8)),8);
+        groupData(iGroup,1).bd=M(~isnan(M(:,9)),9);
+    end
+
+    if doWtn==1
+        tempDir=[saveDir filesep 'withinGroupComparisons' filesep btwGrpNames{iGroup,1}];
+        if isdir(tempDir)
+            rmdir(tempDir,'s')
+        end
         mkdir(tempDir);
+
+        % write out speed/lifetime/displacement distributions into a text file
+        dlmwrite([tempDir filesep 'gs_fs_bs_gl_fl_bl_gd_fd_bd_' btwGrpNames{iGroup,1} '.txt'], M, 'precision', 3,'delimiter', '\t','newline', 'pc');
+
+        if doPlot==1
+            % save histograms of pooled distributions from iGroup
+            plusTipMakeHistograms(M,tempDir);
+
+            % here are the names for each movie in iGroup
+            wtnGrpNames=repmat(btwGrpNames(iGroup,1),[length(dataByProject) 1]);
+            for iName=1:length(wtnGrpNames)
+                wtnGrpNames{iName}=[wtnGrpNames{iName} '_' num2str(iName)];
+            end
+
+            % make within-group boxplots (show each movie in iGroup)
+            plusTipMakeBoxplots(dataByProject,wtnGrpNames,tempDir);
+        end
+
     end
-    plusTipMakeHistograms(M,tempDir);
 
-    % write out speed/lifetime/displacement distributions into a text file
-    dlmwrite([tempDir filesep 'gs_fs_bs_gl_fl_bl_gd_fd_bd_' grpNames{iGroup,1} '.txt'], M, 'precision', 3,'delimiter', '\t','newline', 'pc');
-
-    % make within-group boxplots
-    setName=repmat(grpNames(iGroup,1),[length(data) 1]);
-    for iName=1:length(setName)
-        setName{iName}=[setName{iName} '_' num2str(iName)];
-    end
-    plusTipMakeBoxplots(data,setName,tempDir);
-
-
-    clear data allData
+    clear dataByProject allData
 
 end
-
-% make between-group boxplots
-tempDir=[saveDir filesep 'btwGroupComparisons'];
-if ~isdir(tempDir)
+if doBtw==1
+    
+    tempDir=[saveDir filesep 'btwGroupComparisons'];
+    if isdir(tempDir)
+        rmdir(tempDir,'s')
+    end
     mkdir(tempDir);
+
+    if doPlot==1
+        % make between-group boxplots (show pooled data)
+        plusTipMakeBoxplots(allDataCell,btwGrpNames,tempDir);
+    end
+
+%     % save movie reference list
+%     groupData.projIdx=cell(length(projGroupName),3);
+%     groupData.projIdx(:,1)=projGroupName;
+%     groupData.projIdx(:,2:3)=projNum(:,1:2);
+
+    save([tempDir filesep 'groupData'],'groupData');
 end
-plusTipMakeBoxplots(allDataCell,grpNames,tempDir);
 
-% save movie reference list
-groupData.projIdx=cell(length(projGroupName),3);
-groupData.projIdx(:,1)=projGroupName;
-groupData.projIdx(:,2:3)=projNum(:,1:2);
 
-save([tempDir filesep 'groupData'],'groupData');
+cd(homeDir)
