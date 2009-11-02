6ed385fd09b5d5ec5934d5108b1a44c0fd31479a
Francois Aguet <francois_aguet@hms.harvard.edu>
2009-11-02 15:55:47 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@3678 0bcb941b-74d1-0310-bb26-e6636707f451

--- runDetection.m
+++ runDetection.m
@@ -1,4 +1,4 @@
-function [exp] = runDetection(exp);
+function [exp] = runDetection(exp)
 % runDetection automatically runs Henry's detection software - with iclean
 % 1 on all the movies specified in the experiment structure
 % SYNOPSIS [exp] = loadAndSaveDetection(exp);
@@ -8,57 +8,37 @@ function [exp] = runDetection(exp);
 %                       which is the path to the data location
 % OUTPUT               
 % REMARKS   The function only performs the detection for a given movie if 
-%           there are no detection data (folder called maxdata) already 
+%           there are no detection data (folder called maxdata283) already 
 %           present in the specified directory; if you want a partial or
 %           faulty detection to be replaced, you need to DELETE the folders
 %           first!!!!
 %
 % Dinah Loerke, last modified Feb 2008
+% Francois Aguet, last modified Oct 2009
 
-od = cd;
+% loop over all entries in the structure to enter the image data necessary for the detection input
+nExp = length(exp);
+imData(1:nExp) = struct('origImageName', [], 'origImagePath', []);
 
-% loop over all entries in the structure to enter the image data necessary
-% for the detection input
-for i=1:length(exp)
+for i=1:nExp
+    [origImageName, origImagePath] = uigetfile('.tif', ['Select first original image for movie #' num2str(i)], exp(i).source);
     
-    path = exp(i).source;
-    % change to source directory
-    cd(path);
-    
-    [oriImageName, oriImagePath] = uigetfile('.tif',['Select first original image for movie #',num2str(i)]); %HRJ'.tif'
-    
-    imData(i).oriImageName = oriImageName;
-    imData(i).oriImagePath = oriImagePath;
-    exp(i).firstImageName = oriImageName;
-    
-    cd(od);
-end
-pause(0.1);
+    if (origImageName ~= 0)
+        imData(i).origImageName = origImageName;
+        imData(i).origImagePath = origImagePath;
+        exp(i).firstImageName = origImageName;
 
-
-for i = 1:length(exp)
-    
-    path = exp(i).source;
-    % change to source directory
-    cd(path);
-    
-    
-    oriImageName = imData(i).oriImageName;
-    oriImagePath = imData(i).oriImagePath;
-    
-    runvar = 0;
-    % if detection results folder already exist, skip this entry 
-    if  ~ (exist('maxdata283')==7) 
-        runvar = 1;
-    end
-    
-    if runvar==1
-        main283AUTO(1,oriImageName,oriImagePath);
-    end
-    
-    cd(od);
-    
-    
-end % of for i
-
-end % of function
+        if ~(exist([exp(i).source filesep 'maxdata283'], 'dir') == 7)
+            fprintf('Running detection subroutine "main238AUTO"...\n');
+            main283AUTO(1, imData(i).origImageName, imData(i).origImagePath);
+            fprintf('...done\n');
+        else
+            overwrite = input('Detection results already exist. Overwrite? (y/n): ', 's');
+            if strcmp(overwrite, 'y')
+                fprintf('Running detection subroutine "main238AUTO"...\n');
+                main283AUTO(1, imData(i).origImageName, imData(i).origImagePath);
+                fprintf('...done\n');
+            end;
+        end;
+    end;
+end;
