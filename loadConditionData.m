33ff6902d615efc040d5446f78862cbc05901367
Francois Aguet <francois_aguet@hms.harvard.edu>
2009-11-02 23:30:10 +0000

    Cleaned up, uses paths instead of changing directories
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@3699 0bcb941b-74d1-0310-bb26-e6636707f451

--- loadConditionData.m
+++ loadConditionData.m
@@ -15,123 +15,54 @@ function [experiment] = loadConditionData()
 % 
 %
 % Dinah Loerke, January 24th, 2008
+% Francois Aguet, 11/02/2009
 
 
 % select directory where all data for this condition are located
+condDir = uigetdir(pwd, 'Please select the folder for this condition');
 
-directory_or = cd;
-[directory_cond] = uigetdir(directory_or,'Please select the folder for this condition'); 
-cd(directory_cond);
+% get directories for experiments under this condition
+expDir = dirList(condDir);
 
-% get files in specified directory
-files_cond = dir(directory_cond);
-% files(i).name contains the string name of the file
-% files(i).isdir contains the true/false information about whether the file
-
-
-% identify the usable directories that aren't '.' or '..'
-goodlist_cond = zeros(length(files_cond),1);
-for g=1:length(files_cond)
-    if ( length(files_cond(g).name)>2 ) & ( files_cond(g).isdir == true )
-        goodlist_cond(g) = 1;
-    end
-end
-usedir_cond = find(goodlist_cond);
-
-% initialize counter
 ct = 1;
-
-if length(usedir_cond)>0
-    
-    % loop over approriate directories
-    for i=1:length(usedir_cond)
-        
-        % current folder name
-        dirname_date = files_cond(usedir_cond(i)).name;
-        % search for date numbers, either at right or left margin of the
-        % folder name
-        idxs=length(dirname_date);
-        % if last (or first) char is a number
-        if (uint8(dirname_date(idxs))>47 & uint8(dirname_date(idxs))<58)
-            idc = idxs;
-            while(uint8(dirname_date(idc))>47 & uint8(dirname_date(idc))<58)
-                idc = idc-1;
-            end
-            currDate = dirname_date(idc+1:idxs);
-            
-        elseif (uint8(dirname_date(1))>47 & uint8(dirname_date(1))<58)
-            idc = 1;
-            while(uint8(dirname_date(idc))>47 & uint8(dirname_date(idc))<58)
-                idc = idc+1;
-            end
-            currDate = dirname_date(1:idc-1);
-        
-        else
-            % default for date = 010101
-            currDate = '010101';
-        end
-        
-        
-        % move to directory
-        cd(dirname_date);
-        directory_date = cd;
+if ~isempty(expDir)
+    for i = 1:length(expDir)
+                
+        % extract date from file name
+        currDate = regexp(expDir(i).name, '\d+', 'match');
+        if isempty(currDate)
+            currDate = {'010101'}; % arbitrary default
+        elseif (length(currDate) > 1)
+            lengths = cellfun(@length, currDate);
+            currDate = currDate(lengths == max(lengths(:)));
+        end;
+        currDate = currDate{1};
+        expPath = [condDir filesep expDir(i).name];
 
         % look for the individual cell data in this folder
-        files_cell = dir(cd);
-
+        cellDir = dirList(expPath);
         
-        % identify the directories that aren't '.' or '..'
-        goodlist_cell = zeros(length(files_cell),1);
-        for g=1:length(files_cell)
-            if ( length(files_cell(g).name)>2 ) & ( files_cell(g).isdir == true )
-                goodlist_cell(g) = 1;
-            end
-        end
-        usedir_cell = find(goodlist_cell);
-
         % loop over all cells
-
-        if length(usedir_cell)>0
-            
-            for k=1:length(usedir_cell)
-
-                % current cell directory
-                dirname_cell = files_cell(usedir_cell(k)).name;
+        if ~isempty(cellDir)
+            for k = 1:length(cellDir)
 
                 % extract framerate from the name of the cell folder
-                % NOTE: if there's no specific identification for fast, then
-                % the default is slow
-
-                inFast1 = findstr(dirname_cell, 'fast');
-                inFast2 = findstr(dirname_cell, '400ms');
-
-                if ( (length(inFast1)>0) | (length(inFast2)>0) )
+                % NOTE: if there's no specific identification for fast, default to slow
+                if ( ~isempty(findstr(cellDir(k).name, 'fast')) || ~isempty(findstr(cellDir(k).name, '400ms')) )
                     currFramerate = 0.4;
                 else 
                     currFramerate = 2;
                 end
 
-                cd(dirname_cell);
-
                 % enter data
-                experiment(ct).source = cd;
+                experiment(ct).source = [expPath filesep cellDir(k).name];
                 experiment(ct).date = currDate;
                 experiment(ct).framerate = currFramerate;
 
                 ct = ct+1;
-
-                cd(directory_date);
-            end % of for k-loop
-            
-        end % of if length(usedir_cell)>0
-        
-        cd(directory_cond);
-        
-    end % of for i-loop
-
+            end
+        end   
+    end
 else
     error('no usable data in directory');
-end % of if
-
-end % of function
-            
+end      
