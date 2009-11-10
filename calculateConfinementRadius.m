3a1696749a3c8dc96fd7172ff74ba3374efbb281
Daniel Nunez <daniel_nunez@hms.harvard.edu>
2009-11-10 20:45:46 +0000

    made changes to calculateConfinementRadius.m and changePathUsingEndocytosis.m
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@3741 0bcb941b-74d1-0310-bb26-e6636707f451

--- calculateConfinementRadius.m
+++ calculateConfinementRadius.m
@@ -1,75 +1,94 @@
-function [confRadius,trackCenter] = calculateConfinementRadius(tracks,probDim)
+function [experiment] = calculateConfinementRadius(experiment)
 %calculateConfinementRadius outputs confinement radii for all tracks
 %specified in input
 %
 %SYNOPSIS confinementRadius = calculateConfinementRadius(tracks,probDim);
 %
-%INPUT      tracks:            -- EITHER -- 
-%                           Output of trackWithGapClosing:
-%                           Matrix indicating the positions and amplitudes 
-%                           of the tracked features to be plotted. Number 
-%                           of rows = number of tracks, while number of 
-%                           columns = 8*number of time points. Each row 
-%                           consists of [x1 y1 z1 a1 dx1 dy1 dz1 da1 x2 y2 z2 a2 dx2 dy2 dz2 da2 ...]
-%                           in image coordinate system (coordinates in
-%                           pixels). NaN is used to indicate time points 
-%                           where the track does not exist.
-%                           -- OR -- 
-%                           Output of trackCloseGapsKalman:
-%                           Structure array with number of entries equal to
-%                           the number of tracks (or compound tracks when
-%                           merging/splitting are considered). Contains the
-%                           fields:
-%           .tracksCoordAmpCG: The positions and amplitudes of the tracked
-%                              features, after gap closing. Number of rows
-%                              = number of track segments in compound
-%                              track. Number of columns = 8 * number of 
-%                              frames the compound track spans. Each row
-%                              consists of 
-%                              [x1 y1 z1 a1 dx1 dy1 dz1 da1 x2 y2 z2 a2 dx2 dy2 dz2 da2 ...]
-%                              NaN indicates frames where track segments do
-%                              not exist.
-%           .seqOfEvents     : Matrix with number of rows equal to number
-%                              of events happening in a track and 4
-%                              columns:
-%                              1st: Frame where event happens;
-%                              2nd: 1 - start of track, 2 - end of track;
-%                              3rd: Index of track segment that ends or starts;
-%                              4th: NaN - start is a birth and end is a death,
-%                                   number - start is due to a split, end
-%                                   is due to a merge, number is the index
-%                                   of track segment for the merge/split.
+%INPUT      experiment=   data structure pointing to all the
+%                       image/detection/tracking data for a given condition;
+%                       for e.g. 10 cell movies, the structure should have 10
+%                       entries; the data structure can be created with the
+%                       function loadConditionData, and it needs to contain
+%                       at least the field
+%                       .source, which is the (path) location of the
+%                       lifetime information folder
 %
-%OUTPUT     confinementRadius:  Calculated the mean of the eigen vectors of 
-%                               the variance-covariance matrix of this 
-%                               track's positions 
+%OUTPUT     confinementRadius:  Calculated the mean of the eigen vectors of
+%                               the variance-covariance matrix of this
+%                               track's positions
 %           trackCenter:        calculate the track's center
 %
 %USES       none
 %
-%created by DA Nunez April 2, 2009 
+%created by DA Nunez April 2, 2009
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%estimate the confinement radius
-for iTrack = 1:size(tracks,1)
+%for endocytosis, always 2D
+probDim = 2;
+if nargin < 2 || isempty(rest)
+   rest = [1 1 4 1 300]; 
+end
 
-    %get track coordinates
-    xCoord = (tracks(iTrack,1:8:end))';
-    yCoord = (tracks(iTrack,2:8:end))';
-    zCoord = (tracks(iTrack,3:8:end))';
-    xyzCoord = [xCoord yCoord zCoord];
+%for each experiment
+for iexp = 1:length(experiment)
+    
+    waitHandle = waitbar(iexp/length(experiment),['processing movie ' num2str(iexp) ' out of ' num2str(length(experiment))]);
 
-    %find the eignevalues and eigenvectors of the variance-covariance
-    %matrix of this track's positions
-    eigenVal = eig(nancov(xyzCoord(:,1:probDim)));
+    
+    %Load Lifetime Information
+    cd([experiment(iexp).source filesep 'LifetimeInfo'])
+    load('lftInfo')
+    % status matrix
+    statMat = full(lftInfo.Mat_status);
+    % lifetime matrix
+    lftMat = full(lftInfo.Mat_lifetime);
+    % x-coordinate matrix
+    matX = full(lftInfo.Mat_xcoord);
+    matX(matX == 0) =nan;
+    matX(statMat == 4) = nan;
+    matX(statMat == 5) = nan;
+    % y-coordinate matrix
+    matY = full(lftInfo.Mat_ycoord);
+    matY(matY == 0) =nan;
+    matY(statMat == 4) = nan;
+    matY(statMat == 5) = nan;
+    % disapp status matrix
+    daMat = (lftInfo.Mat_disapp);
+    % framerate
+    framerate = experiment(iexp).framerate;
 
-    %calculate the track's confinement radius
-    confRadius(iTrack,1) = sqrt( min(eigenVal) * (probDim + 2) );
-
-
-    %calculate the track's center
-    trackCenter(iTrack,:) = nanmean(xyzCoord(:,1:probDim));
-
-end %for each track
+    %find all pits in movie that meet requirements specified by restriction
+    %vector
+    [findTrack,findFrame] = find((statMat==rest(1,1)) & (daMat==rest(1,2)) &...
+        (lftMat>rest(1,3)) & (lftMat>round(rest(1,4)/framerate)) & (lftMat<round(rest(1,5)/framerate)));
+    
+    %estimate the confinement radius
+    for iTrack = 1:size(findTrack,1)
+        
+        %get track coordinates
+        xCoord = (matX(findTrack(iTrack),:))';
+        yCoord = (matY(findTrack(iTrack),:))';
+        xyzCoord = [xCoord yCoord];
+        
+        %find the eignevalues and eigenvectors of the variance-covariance
+        %matrix of this track's positions
+        eigenVal = eig(nancov(xyzCoord(:,1:probDim)));
+        
+        %calculate the track's confinement radius
+        confRadius(iTrack,1) = sqrt( mean(eigenVal) * (probDim + 2) );
+         if ~isreal(confRadius(iTrack,1))
+             keyboard
+         end
+        
+        %calculate the track's center
+        trackCenter(iTrack,:) = nanmean(xyzCoord(:,1:probDim));
+        
+    end %for each track
+    %store track confinement radii for each movie
+    experiment(iexp).trackCenter = trackCenter;
+    experiment(iexp).confRadius = confRadius;
+    
+    close(waitHandle)
+end %of for each experiment
 
 end %of function
--- changePathUsingEndocytosis.m
+++ changePathUsingEndocytosis.m
@@ -1,8 +1,8 @@
 function experiment = changePathUsingEndocytosis(experiment)
 
-% changePathUsingEndocytosis changes the sourc path of a given movie by
+% changePathUsingEndocytosis changes the source path of a given movie by
 % looking for the word endocytosis in the current working directory and
-% substituting everything up to this word
+% substituting everything up to this word on movie directory
 %
 % INPUT     experiment=   data structure pointing to all the
 %                       image/detection/tracking data for a given condition;
@@ -22,16 +22,31 @@ function experiment = changePathUsingEndocytosis(experiment)
 % Daniel Nunez, October 23, 2009
 
 %find endocytosis on cwd
+%remember current working dir
 od = pwd;
+%find the word 'endocytosis' on current working dir
 findE = strfind(od,'endocytosis');
-directoryString = od(1:findE-1);
+%grab dir structure up to this word
+upstreamString = od(1:findE-1);
+%find directory separators in source
+findSeparators = strfind(upstreamString,'/');
+findSeparators = [findSeparators strfind(upstreamString,'\')];
+%change separators to filesep to make system os independent
+upstreamString(findSeparators) = filesep;
 
 %for each experiment
 for iexp = 1:length(experiment)
 %find endocytosis on on source path
 findE = strfind(experiment(iexp).source,'endocytosis');
-%change begining of source path with current path up to endocytosis
-experiment(iexp).source = strcat(directoryString,experiment(iexp).source(findE:end));
+%grab string that denotes directory structure downstream of endocytosis
+downstreamString = experiment(iexp).source(findE:end);
+%find directory separators in source
+findSeparators = strfind(downstreamString,'/');
+findSeparators = [findSeparators strfind(downstreamString,'\')];
+%change separators to filesep to make system os independent
+downstreamString(findSeparators) = filesep;
+%put paths together
+experiment(iexp).source = strcat(upstreamString,downstreamString);
 
 
 end %of for each experiment
