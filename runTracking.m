59feac6b9685dceb35a12b5b934077484be0156b
Daniel Nunez <daniel_nunez@hms.harvard.edu>
2009-11-10 00:24:14 +0000

    changed runTracking.m so that it will not track a movie if DetectionSturctures directory exists
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@3735 0bcb941b-74d1-0310-bb26-e6636707f451

--- runTracking.m
+++ runTracking.m
@@ -3,22 +3,26 @@ function [] = runTracking(experiment,tracksettings,force)
 % the TrackInfo and lftInfo data structures necessary for lifetime
 % analysis.
 %
-% SYNOPSIS [experiment] = runTracking()
+% SYNOPSIS [experiment] = runTracking(experiment,tracksettings,force)
 %
-% INPUT
+% INPUT     experiment(optional): structure containing the field source,
+%               which specifies the directory for each movie (default is to
+%               ask you to load them via gui)
+%           tracksettings(optional):
+%           force(optional): 1 to force to rerun tracking, 0 otherwise
+%               (default is 0)
 %
 % OUTPUT
 %
 % REMARKS   The function only performs the tracking for a given movie if
-%           there is no tracking data (two folders called LifetimeInfo
-%           and TrackInfoMatrices) already present in the specified
-%           directory; if you want a partial or faulty tracking to be
-%           replaced, you need to DELETE these folders first.
+%           the folder DetectionStructures is not already present in the
+%           specified directory; if you want a partial or faulty tracking
+%           to be replaced, you need to DELETE these folders first.
 %
 % Dependencies  loadIndividualMovies
 %               loadAndSaveDetection
 %               trackMissingFields
-%               
+%
 % Daniel Nunez, March 5, 2008
 
 %INPUTS
@@ -26,30 +30,38 @@ if nargin < 3 || isempty(force)
     force = 0;
 end
 if nargin < 1 || isempty(experiment)
-% trim experiment structure as specified by user input
-[experiment] = loadIndividualMovies();
+    % trim experiment structure as specified by user input
+    [experiment] = loadIndividualMovies();
 end
 if nargin < 2 || isempty(tracksettings)
-%load track settings required for tracking
-[fileName filePath] = uigetfile('.mat','choose track settings mat file for tracking');
-load([filePath filesep fileName]);
+    %load track settings required for tracking
+    [fileName filePath] = uigetfile('.mat','choose track settings mat file for tracking');
+    load([filePath filesep fileName]);
 end
 
-% CONVERT
-% convert data from Henry's format to a format that is taken by the tracker
-% if this is not necessary (a detection.mat file exists under a folder
-% DetectionStructures) the function loadAndSaveDetection will not do
-% anything to that movie
-[experiment] = loadAndSaveDetection(experiment);
-
-% add track settings to each experiment
-for iexp = 1:length(experiment)
-    experiment(iexp).tracksettings = tracksettings;
-end
-
-% TRACK
-% this function creates the TrackInfo and lftInfo data structures if
-% necessary
-trackMissingFields(experiment,force);
-
+%ONLY TRACK IF DETECTIONSTRUCTURES DOES NOT EXIST. OTHERWISE MUST FORCE TO
+%GET TO TRACK
+parfor iexp = 1:length(experiment)
+    %select experiment of interest
+    exp = experiment(iexp);
+    %go to experiment folder
+    cd(exp.source);
+    if ~ ( exist('DetectionStructures')==7 ) || force
+        % CONVERT
+        % convert data from Henry's format to a format that is taken by the tracker
+        % if this is not necessary (a detection.mat file exists under a folder
+        % DetectionStructures) the function loadAndSaveDetection will not do
+        % anything to that movie
+        [exp] = loadAndSaveDetection(exp);
+        
+        % add track settings to each experiment
+        exp.tracksettings = tracksettings;
+        
+        % TRACK
+        % this function creates the TrackInfo and lftInfo data structures if
+        % necessary
+        trackMissingFields(exp,force);
+        
+    end %of if DetectionSturctures exists
+end %of for each experiment
 end %of function
