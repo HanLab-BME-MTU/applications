93fab89241dcd94b00042fd7894fa8cca968c193
Kathryn Applegate <kathryn.applegate@gmail.com>
2009-11-20 18:21:21 +0000

    plusTipGroupListSummary.m - all tracks were used before; now only growth tracks are used.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@3771 0bcb941b-74d1-0310-bb26-e6636707f451

--- EB3Tracker/plusTipGroupListSummary.m
+++ EB3Tracker/plusTipGroupListSummary.m
@@ -1,5 +1,5 @@
 function [groupListSpeedLifeDisp,grpData]=plusTipGroupListSummary(groupList)
-% returns cell array with grouped projects and speed, lifetime, and displacement
+% returns cell array with grouped projects and GROWTH speed, lifetime, and displacement
 % data for each
 
 groupListSpeedLifeDisp=cell(size(groupList,1),3);
@@ -10,16 +10,17 @@ for i=1:length(groupList)
     projDir=[groupList{i,2} filesep 'meta'];
     cd(projDir)
     load projData
-
-    aT=projData.nTrack_sF_eF_vMicPerMin_trackType_lifetime_totalDispPix;
+    aT=plusTipMergeSubtracks(projData); % use merged data, don't crop beginning/end
+    aT(aT(:,5)~=1,:)=[]; % use growths only
     aT(:,6)=aT(:,6).*projData.secPerFrame;
     aT(:,7)=aT(:,7).*(projData.pixSizeNm/1000);
     
     groupListSpeedLifeDisp{i,3}=aT(:,[4,6:7]);
 end
 
-
 [grpNames,b,m]=unique(groupList(:,1));
+grpNames=cellfun(@(x) ['grp_' x],grpNames,'uniformoutput',0);
+
 grpData.allGroups=cell2mat(groupListSpeedLifeDisp(:,3));
 for iGroup=1:length(grpNames)
     grpIdx=find(m==iGroup);
