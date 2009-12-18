0d70f6867e0c84eaffdf8f2a5786539c75ebaf4b
Francois Aguet <francois_aguet@hms.harvard.edu>
2009-12-18 23:15:44 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@3824 0bcb941b-74d1-0310-bb26-e6636707f451

--- runDetection.m
+++ runDetection.m
@@ -14,13 +14,15 @@ function [exp] = runDetection(exp)
 %           first!!!!
 %
 % Dinah Loerke, last modified Feb 2008
-% Francois Aguet, last modified Oct 2009
+% Francois Aguet, last modified Dec 2009
 
 % loop over all entries in the structure to enter the image data necessary for the detection input
 nExp = length(exp);
 imData(1:nExp) = struct('origImageName', [], 'origImagePath', []);
 
-for i=1:nExp
+runStatus = zeros(1,nExp);
+
+for i = 1:nExp
     [origImageName, origImagePath] = uigetfile('.tif', ['Select first original image for movie #' num2str(i)], exp(i).source);
     
     if (origImageName ~= 0)
@@ -29,12 +31,18 @@ for i=1:nExp
         exp(i).firstImageName = origImageName;
 
         if ~(exist([exp(i).source filesep 'maxdata283'], 'dir') == 7)
-            main283AUTO(1, imData(i).origImageName, imData(i).origImagePath);
+            runStatus(i) = 1;
         else
             overwrite = input('Detection results already exist. Overwrite? (y/n): ', 's');
             if strcmp(overwrite, 'y')
-                main283AUTO(1, imData(i).origImageName, imData(i).origImagePath);
+                runStatus(i) = 1;
             end;
         end;
+    end;
+end;
+
+for i = 1:nExp
+    if runStatus(i)
+        main283AUTO(1, imData(i).origImageName, imData(i).origImagePath);
     end;
 end;
