73b148e3d03fb39c4f2b1db8cf8a98a2884fd88d
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-01-20 23:51:35 +0000

    Obsolete. Image size is now added by the 'loadConditionData' functions.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@3917 0bcb941b-74d1-0310-bb26-e6636707f451

--- determineImagesize.m
+++
@@ -1,6 +0,0 @@
-function [data] = determineImagesize(data)
-
-for i = 1:length(data)
-    tifFiles = dir([data(i).source filesep '*.tif']);
-    data(i).imagesize = size(imread([data(i).source filesep tifFiles(1).name]));
-end
