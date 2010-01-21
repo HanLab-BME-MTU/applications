40c17f46f9a03b9a7375a9f37181bebb64c5ea8f
Kathryn Applegate <kathryn.applegate@gmail.com>
2010-01-21 15:06:05 +0000

    deleting EB3Tracker to replace with plusTipTracker
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@3925 0bcb941b-74d1-0310-bb26-e6636707f451

--- EB3Tracker/pixPerFrame2umPerMin.m
+++
@@ -1,5 +0,0 @@
-function [umPerMin] = pixPerFrame2umPerMin(pixPerFrame,secPerFrame,pixSizeNm)
-% converts pixels/frame -> microns/min given sec/frame and nm pixel size
-%
-% [umPerMin] = pixPerFrame2umPerMin(pixPerFrame,secPerFrame,pixSizeNm)
-umPerMin = (60/1000) * pixPerFrame * (pixSizeNm/secPerFrame);
--- EB3Tracker/umPerMin2pixPerFrame.m
+++
@@ -1,5 +0,0 @@
-function [pixPerFrame] = umPerMin2pixPerFrame(umPerMin,secPerFrame,pixSizeNm)
-% converts microns/min -> pixels/frame given sec/frame and nm pixel size
-%
-% [pixPerFrame] = umPerMin2pixPerFrame(umPerMin,secPerFrame,pixSizeNm)
-pixPerFrame = (1000/60) * umPerMin .* (secPerFrame/pixSizeNm);
