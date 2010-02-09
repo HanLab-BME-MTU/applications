03ee5b63817378f0d6008dbbdb4ef0862a0540ab
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-02-09 23:04:29 +0000

    Now excludes data sets with 'fast', '400ms', or '1channel' in the path and loads only true dual-channel movies
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@4097 0bcb941b-74d1-0310-bb26-e6636707f451

--- loadConditionDataDualChannel.m
+++ loadConditionDataDualChannel.m
@@ -20,7 +20,7 @@ function [experiment] = loadConditionDataDualChannel(condDir)
 % select directory where all data for this condition are located
 if (nargin == 0)
     condDir = [uigetdir(pwd, 'Select the ''condition'' folder') filesep];
-end;
+end
 fprintf('Condition selected: %s\n', condDir);
     
 % get directories where all data for this condition are located
@@ -41,7 +41,7 @@ if ~isempty(expDir)
         elseif (length(currDate) > 1)
             lengths = cellfun(@length, currDate);
             currDate = currDate(lengths == max(lengths(:)));
-        end;
+        end
         dateList(i).date = currDate{1};
         
         % look for the individual cell data in this folder
@@ -52,54 +52,51 @@ if ~isempty(expDir)
         if ~isempty(cellDir)
             for k = 1:length(cellDir)
                 
-                % extract framerate from directory name. Default to slow                
-                if ( ~isempty(findstr(cellDir(k).name, 'fast')) || (~isempty(findstr(cellDir(k).name, '400ms'))) )
-                    framerate = 0.4;
-                else
+                % only load slow movies that have two channels                
+                if isempty(findstr(cellDir(k).name, 'fast')) && isempty(findstr(cellDir(k).name, '400ms')) && isempty(findstr(expPath, '1channel'))
                     framerate = 2;
-                end
-                
-                if (ct == 1)
-                    % get directories for individual channels
-                    cellPath = [expPath cellDir(k).name filesep];
-                    channel1Path = [uigetdir(cellPath, 'select first (master) channel (e.g. CCP channel)') filesep];
-                    channel2Path = [uigetdir(cellPath, 'select second (slave) channel (e.g. other protein)') filesep];
-                    channel1Name = channel1Path(length(cellPath)+1:end-1);
-                    channel2Name = channel2Path(length(cellPath)+1:end-1);
-                    fprintf('Channel 1 name: "%s"\n', channel1Name);
-                    fprintf('Channel 2 name: "%s"\n', channel2Name);
-                else
                     
-                    % look for the individual channels in cell folder
-                    if ~isempty(channel1Name)
-                        channel1Path = [expPath cellDir(k).name filesep channel1Name filesep];
+                    if (ct == 1)
+                        % get directories for individual channels
+                        cellPath = [expPath cellDir(k).name filesep];
+                        channel1Path = [uigetdir(cellPath, 'select first (master) channel (e.g. CCP channel)') filesep];
+                        channel2Path = [uigetdir(cellPath, 'select second (slave) channel (e.g. other protein)') filesep];
+                        channel1Name = channel1Path(length(cellPath)+1:end-1);
+                        channel2Name = channel2Path(length(cellPath)+1:end-1);
+                        fprintf('Channel 1 name: "%s"\n', channel1Name);
+                        fprintf('Channel 2 name: "%s"\n', channel2Name);
                     else
-                        channel1Path = [expPath cellDir(k).name filesep];
+                        % look for the individual channels in cell folder
+                        if ~isempty(channel1Name)
+                            channel1Path = [expPath cellDir(k).name filesep channel1Name filesep];
+                        else
+                            channel1Path = [expPath cellDir(k).name filesep];
+                        end
+                        if ~(exist(channel1Path, 'dir')==7)
+                            channel1Path = [uigetdir(cellDir(k).name, 'select first (master) channel (e.g. CCP channel)') filesep];
+                        end
+                        channel2Path = [expPath cellDir(k).name filesep channel2Name filesep];
+                        if ~(exist(channel2Path, 'dir')==7)
+                            channel2Path = [uigetdir(cellDir(k).name, 'select second (slave) channel (e.g. other protein)') filesep];
+                        end
                     end
-                    if ~(exist(channel1Path, 'dir')==7)
-                        channel1Path = [uigetdir(cellDir(k).name, 'select first (master) channel (e.g. CCP channel)') filesep];
-                    end
-                    channel2Path = [expPath cellDir(k).name filesep channel2Name filesep];
-                    if ~(exist(channel2Path, 'dir')==7)
-                        channel2Path = [uigetdir(cellDir(k).name, 'select second (slave) channel (e.g. other protein)') filesep];
-                    end
+                    
+                    fprintf('Loading: %s\n', channel1Path);
+                    
+                    experiment(ct).source = channel1Path;
+                    experiment(ct).channel1 = channel1Path;
+                    experiment(ct).channel2 = channel2Path;
+                    experiment(ct).date = currDate{1};
+                    experiment(ct).framerate = framerate;
+                    tifFiles = dir([experiment(ct).channel1 '*.tif']);
+                    experiment(ct).imagesize = size(imread([experiment(ct).channel1 tifFiles(1).name]));
+                    experiment(ct).movieLength = length(tifFiles);
+                    
+                    ct = ct+1;
                 end
-                
-                fprintf('Loading: %s\n', channel1Path);
-
-                experiment(ct).source = channel1Path;
-                experiment(ct).channel1 = channel1Path;
-                experiment(ct).channel2 = channel2Path;
-                experiment(ct).date = currDate{1};
-                experiment(ct).framerate = framerate;
-                tifFiles = dir([experiment(ct).channel1 '*.tif']);
-                experiment(ct).imagesize = size(imread([experiment(ct).channel1 tifFiles(1).name]));
-                experiment(ct).movieLength = length(tifFiles);
-
-                ct = ct+1;
-            end;
-        end;
-    end;
+            end
+        end
+    end
 else
     error('No data found in directory.');
-end;
+end
