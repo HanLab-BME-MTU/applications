57c5d34a7797bf3fa0333716792e44a703bea910
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-02-10 20:38:47 +0000

    Compacted.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@4128 0bcb941b-74d1-0310-bb26-e6636707f451

--- fillStructLifetimeHist_segment.m
+++ fillStructLifetimeHist_segment.m
@@ -1,4 +1,4 @@
-function [data] = fillStructLifetimeHist_segment(data,censor);
+function [data] = fillStructLifetimeHist_segment(data, censor)
 % fill experiment structure with lifetime histogram based on the lftInfo
 % file saved at the specified directory; separate lftHist vectors are
 % calculated based on the image segmentation in the specified folders
@@ -24,73 +24,40 @@ function [data] = fillStructLifetimeHist_segment(data,censor);
 % REMARKS 
 %
 % Dinah Loerke, last modified June 25, 2008
+% Francois Aguet, 01/22/2010
 
-
-censoring = 1;
-if nargin>1
-    censoring = censor;
+if (nargin < 2)
+    censor = 1;
 end
 
-% number of entries in data structure
-lens = length(data);
-
-od = cd;
-
-for i=1:lens
+for i = 1:length(data)
     
-    fprintf('movie #%02d',i);
-        
-    % current path
-    path = data(i).source;
-
-    % number of frames for this exp
-    lenf = data(i).movieLength;
-
-    % load lifetime inof data file
-    lftpath = [path,'/LifetimeInfo'];
-    cd(lftpath);
-    lftname = 'lftInfo.mat';
-    loadfile = load(lftname);
-    cd(od);
-
-    lftInfo = loadfile.lftInfo;
+    fprintf('Movie no. %d\n', i);    
+    load([data(i).source 'LifetimeInfo' filesep 'lftInfo.mat']);
     
     % check if segmentation status already exists
-    if isfield(data,'segmentStatus')
-        
+    if isfield(data(i), 'segmentStatus') && ~isempty(data(i).segmentStatus)
         segmentStatusVector = data(i).segmentStatus;
-    % else determine the segmentation status here and fill in the value    
     else
-              
-        % load segmentation image from specified location
-        SegmFileName = data(i).segmentDataFileName;
-        SegmFilePath = data(i).segmentDataFilePath;
-
-        cd(SegmFilePath);
-        SegmentMask = imread(SegmFileName);    
-
-        cd(od);
-
+        SegmentMask = imread([data(i).segmentDataFilePath data(i).segmentDataFileName]);
         % calculate segmentation status (1=Inside, 0=oustide segmented region)
         [segmentStatusVector] = calcIORegionLfthistSimple(lftInfo, SegmentMask);
-        
         data(i).segmentStatus = segmentStatusVector';
-        
     end
     
     lftMat = lftInfo.Mat_lifetime;
     statMat =  lftInfo.Mat_status;
 
-    [sx,sy] = size(lftMat);
+    sx = size(lftMat, 1);
 
-    lftVec = nan*zeros(sx,1);
-    lftVecIN = nan*zeros(sx,1);
-    lftVecOUT = nan*zeros(sx,1);
+    lftVec = NaN(sx,1);
+    lftVecIN = NaN(sx,1);
+    lftVecOUT = NaN(sx,1);
 
     % DEFAULT:
     % a trajectory is counted for the lifetime analysis if the status of 
     % the trajectory is ==1 and the value of the gaps is ==4
-    % IF censoring==0
+    % IF censor==0
     % count all status trajectories
     for k=1:sx
         % current status vector
@@ -99,38 +66,28 @@ for i=1:lens
         clft = lftMat(k,:);
         
         countStat = ( (min(cstat)==1) & (max(cstat)<5) );
-        if censoring==0
+        if censor==0
             countStat = (max(cstat)<5);
         end
         
         if countStat==1
             lftVec(k) = max(clft);
-            % count into either IN or OUT vector depending on
-            % segmentation status
+            % count into either IN or OUT vector depending on segmentation status
             if segmentStatusVector(k)==1
                 lftVecIN(k) = max(clft);
             else
                 lftVecOUT(k) = max(clft);
             end
-
         end
-            
-    end % of for k-loop    
+    end   
     
     % lifetime vectors containing all counted lifetime lengths
-    lftVec = lftVec(find(isfinite(lftVec)));
-    lftVecIN = lftVecIN(find(isfinite(lftVecIN)));
-    lftVecOUT = lftVecOUT(find(isfinite(lftVecOUT)));
+    lftVec = lftVec(isfinite(lftVec));
+    lftVecIN = lftVecIN(isfinite(lftVecIN));
+    lftVecOUT = lftVecOUT(isfinite(lftVecOUT));
 
     % lifetime histograms
-    data(i).lftHist = hist(lftVec,[1:lenf]);
-    data(i).lftHist_InRegion = hist(lftVecIN,[1:lenf]);
-    data(i).lftHist_OutRegion = hist(lftVecOUT,[1:lenf]);
-        
-    fprintf('\b\b\b\b\b\b\b\b\b');       
-
-end % of for
-
-fprintf('\n'); 
-
-end % of function
+    data(i).lftHist = hist(lftVec, 1:data(i).movieLength);
+    data(i).lftHist_InRegion = hist(lftVecIN, 1:data(i).movieLength);
+    data(i).lftHist_OutRegion = hist(lftVecOUT, 1:data(i).movieLength);
+end
