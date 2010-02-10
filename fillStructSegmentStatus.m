c7f4a4d70d3180d767bd3f8f0488135807369121
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-02-10 20:11:11 +0000

    Compacted.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@4125 0bcb941b-74d1-0310-bb26-e6636707f451

--- fillStructSegmentStatus.m
+++ fillStructSegmentStatus.m
@@ -1,4 +1,4 @@
-function [data] = fillStructSegmentStatus(data, fieldname);
+function [data] = fillStructSegmentStatus(data, fieldname)
 % fill experiment structure with vectors designating the segmentation
 % status of each trajectory (inside vs. outside pattern) 
 % based on the image segmentation in the specified folders
@@ -23,38 +23,17 @@ function [data] = fillStructSegmentStatus(data, fieldname);
 %           
 %
 % Dinah Loerke, modified July 30, 2008
+% Francois Aguet, modified Feb 2010
 
-segmDefault = 0;
-if nargin>1
-    segmDefault = 1;
-end
-
-% number of entries in data structure
-lens = length(data);
-
-od = cd;
-
-for i=1:lens
+for i = 1:length(data)
     
     fprintf('movie #%02d',i);
     
-    % current path
-    path = data(i).source;
-    
-    % number of frames for this exp
-    lenf = data(i).movieLength;
-    
-    if segmDefault==0
-        % load segmentation image from specified location
-        SegmFileName = data(i).segmentDataFileName;
-        SegmFilePath = data(i).segmentDataFilePath;
-
-        cd(SegmFilePath);
-        SegmentMask = imread(SegmFileName);    
-        
+    if (nargin < 2) % use default field names
+        SegmentMask = imread([data(i).segmentDataFilePath data(i).segmentDataFileName]);    
     else
-        segmentation = getfield(data(i), fieldname);
-        if isstr(segmentation)
+        segmentation = data(i).(fieldname);
+        if ischar(segmentation)
             SegmentMask = imread(segmentation);
         else
             SegmentMask = segmentation;
@@ -65,28 +44,12 @@ for i=1:lens
         SegmentMask = logical(SegmentMask/max(SegmentMask(:)));
     end    
         
-    % load lifetime info data file
-    lftpath = [path,'/LifetimeInfo'];
-    cd(lftpath);
-    lftname = 'lftInfo.mat';
-    loadfile = load(lftname);
-    cd(od);
-    
-    lftInfo = loadfile.lftInfo;
+    load([data(i).source 'LifetimeInfo' filesep 'lftInfo.mat']);
 
     % calculate segmentation status (1=Inside, 0=oustide segmented region)
-    [segmentStatusVector,segmentEUdistVector] = calcIORegionLfthistSimple(lftInfo, SegmentMask);
-    
+    [segmentStatusVector, segmentEUdistVector] = calcIORegionLfthistSimple(lftInfo, SegmentMask);
     
     % lifetime histograms
     data(i).segmentStatus = segmentStatusVector';
     data(i).segmentEUdist = segmentEUdistVector';
-    
-        
-    fprintf('\b\b\b\b\b\b\b\b\b');       
-
-end % of for
-
-fprintf('\n'); 
-
-end % of function
+end
