f9c06f182c47a9b3d694878cf5420cd38f19722d
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-02-11 04:53:56 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@4144 0bcb941b-74d1-0310-bb26-e6636707f451

--- MultiUniversalEventCorrelation/extractAllPosSideBuffer.m
+++ MultiUniversalEventCorrelation/extractAllPosSideBuffer.m
@@ -1,4 +1,4 @@
-function [MPMallpos] = extractAllPosSideBuffer(lftinfopath, tbuffer);
+function [MPMallpos] = extractAllPosSideBuffer(lftinfopath, tbuffer)
 % this function extracts an MPM of points of interest from trackInfo using
 % a side buffer on both sides of the trajectory, i.e. with a specified
 % numver of points before appearance and after disappearance of the object
@@ -17,21 +17,16 @@ function [MPMallpos] = extractAllPosSideBuffer(lftinfopath, tbuffer);
 %                           third layer: time relative to disappearance
 %                       
 %
-% last modified: Dinah Loerke   01/30/2009
+% Dinah Loerke   01/30/2009
+% Francois Aguet, Jan 2010
 
+% extract lft matrices
+load([lftinfopath filesep 'lftInfo.mat']);
 
-
-
-%% extract lft matrices
-cd(lftinfopath);
-loadfile = open('lftInfo.mat');
-lmat = loadfile.lftInfo;
-
-mat_lft   = full(lmat.Mat_lifetime);
-mat_stat  = full(lmat.Mat_status);
-mat_x     = full(lmat.Mat_xcoord);
-mat_y     = full(lmat.Mat_ycoord);
-mat_da    = full(lmat.Mat_disapp);
+mat_lft   = full(lftInfo.Mat_lifetime);
+mat_x     = full(lftInfo.Mat_xcoord);
+mat_y     = full(lftInfo.Mat_ycoord);
+mat_da    = full(lftInfo.Mat_disapp);
 vec_lft   = max(mat_lft,[],2);
 
 % frame number
@@ -43,9 +38,7 @@ mpm_y = mat_y;
 mpm_t1 = nan*mpm_x;
 mpm_t2 = nan*mpm_x;
 
-
-
-%% fill in time shift values
+% fill in time shift values
 % for each trajectory with lifetime > 4 frames, add the specified number of
 % frames to the left and right; the position of the first or last visible
 % frame is filled in
@@ -57,8 +50,6 @@ for i=1:length(gtpos)
     k = gtpos(i);
      
     % current traj
-    ct_x = mat_x(k,:);
-    ct_y = mat_y(k,:);
     ct_da = mat_da(k,:);
     
     % appearance and disappearance positions
@@ -86,9 +77,9 @@ for i=1:length(gtpos)
         mpm_x(k,startpos:pos_a-1) =  mpm_x(k,pos_a);
         mpm_y(k,startpos:pos_a-1) =  mpm_y(k,pos_a);
         % fill in pre-appearance time
-        mpm_t1(k,startpos:pos_a-1) = -pos_a + [startpos:pos_a-1];
+        mpm_t1(k,startpos:pos_a-1) = startpos-pos_a:-1;
         % fill in trajectory times
-        mpm_t1(k,pos_a:endpos) = [pos_a:endpos] - pos_a; 
+        mpm_t1(k,pos_a:endpos) = 0:endpos-pos_a; 
     end
     
     % is trajectory is seen to disappear (i.e. has post-disappearance
@@ -98,17 +89,13 @@ for i=1:length(gtpos)
         mpm_x(k,pos_da+1:endpos) =  mpm_x(k,pos_da);
         mpm_y(k,pos_da+1:endpos) =  mpm_y(k,pos_da);
         % fill in post-disappearance time
-        mpm_t2(k,pos_da+1:endpos) = [pos_da+1:endpos] - pos_da;
+        mpm_t2(k,pos_da+1:endpos) = 1:endpos-pos_da;
         % fill in trajectory times
-        mpm_t2(k,startpos:pos_da) = -pos_da + [startpos:pos_da];
-    end 
-    
-         
-end % of for i-loop
+        mpm_t2(k,startpos:pos_da) = startpos-pos_da:0;
+    end      
+end
 
-%fprintf('\n');
-
-MPMallpos = nan*zeros(nx,2*nf,3);
+MPMallpos = NaN(nx,2*nf,3);
 
 % merge information into one big MPM file
 MPMallpos(:,1:2:2*nf,1) = mpm_x;
@@ -117,6 +104,3 @@ MPMallpos(:,1:2:2*nf,2) = mpm_t1;
 MPMallpos(:,2:2:2*nf,2) = mpm_t1;
 MPMallpos(:,1:2:2*nf,3) = mpm_t2;
 MPMallpos(:,2:2:2*nf,3) = mpm_t2;
-    
-end % of function
-
