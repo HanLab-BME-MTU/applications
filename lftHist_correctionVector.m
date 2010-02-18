81a756f6b86826c3c7acf89450ec5750339933df
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-02-18 17:22:44 +0000

    Simplified
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@4177 0bcb941b-74d1-0310-bb26-e6636707f451

--- lftHist_correctionVector.m
+++ lftHist_correctionVector.m
@@ -1,16 +1,10 @@
-function[bincorvec]=lftHist_correctionVector(length)
-% calculates the correction vector for the lifetime histogram, based on the
-% fact that longer lifetimes are more likely to be cut off at the beginning
-% or end which will result in them not being counted
+function [corrVect] = lftHist_correctionVector(N)
+% Correction vector for the lifetime histogram. Longer lifetimes are more likely
+% to be cut off at the beginning or end which will result in them not being counted
+%
+% The weighting function is N/(N-x), where N is the movie length.
+% To do: current function truncates the sequence, should be fixed.
+% 
+% Last modified by Francois Aguet, 02/18/2010
 
-% vector of possible lifetimes
-redlen = length-2;
-taus = [1:redlen];
-
-% detection probability of a given lifetime
-dp_tau = (redlen-taus+1) / length;
-
-% bincorvec
-bincorvec = 1./dp_tau;
-
-end % of function
+corrVect = N./(N-2:-1:1);
