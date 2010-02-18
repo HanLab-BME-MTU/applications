83c6357df2b869120a9016339a6e1255c6654815
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-02-18 06:00:52 +0000

    Fixed a bug that made previous version crash.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@4174 0bcb941b-74d1-0310-bb26-e6636707f451

--- numPersistentField.m
+++ numPersistentField.m
@@ -1,46 +1,49 @@
-function [numPers]=numPersistentField(exp, threshold)
-% calculate the number of objects that have a lifetime
+function [numPers]=numPersistentField(data, threshold)
+% calculate the number of objects that have a lifetime 
 % exceeding a specified threshold value, so-called 'persistent' objects
-% INPUT: experFiedld      = experiment structure field
+% INPUT: data      = experiment structure field
 %        threshold  = threshold in seconds
 % OUTPUT: numPers     = fraction of long-lived objects in every frame
-%
+%         
 % NOTE: only movies with framerate 2 are used for this analysis
-% NOTE: in a previous version of this function, the fraction of persistent
+% NOTE: in a previous version of this function, the fraction of persistent 
 % objects in each frame was calculated, this function looks for the
 % fraction of the whole pool of objects - because of the higher frequency
 % of short-lived objects, the contribution of persistent objects per whole
 % will be lower!
 %
-% Dinah Loerke, July 11, 2007
-% Last modified: Francois Aguet, 01/21/2010
+% last modified : July 11, 2007 (Dinah)
+% Last modified: Francois Aguet, 02/18/2010
 
-
+numPers = [];
+    
 % proceed if the movie is long enough for the specified threshold
 % and has specified framerate
 % AND to prevent multiple copies of resampled movies skewing the
 % results, use only one of multiple resample copies
-if ((exp.framerate*exp.movieLength)>min(threshold)) %& (framerate>=2)
-        
-    if ~isfield(exp,'lftInfo') || isempty(exp.lftInfo)
+if ((data.framerate*data.movieLength)>min(threshold)) %& (data.framerate>=2)   
     
-        lftPath = [exp.source filesep 'LifetimeInfo' filesep 'lftInfo.mat'];
+    if isfield(data, 'lftInfo') && ~isempty(data.lftInfo)
+        lftInfo = data.lftInfo;
+    else % load it
+        lftPath = [data.source 'LifetimeInfo' filesep 'lftInfo.mat'];
         if (exist(lftPath, 'file')==2)
             load(lftPath);
-        end;
+        end
     end;
-            
+
     if ~isempty(lftInfo)
         % convert the threshold (which is in seconds) into number of frames,
         % which depends on the framerate of this movies
-        thresh_frames = round(threshold/exp.framerate);
-        
-        if ( exp.framerate*exp.movieLength>threshold )
+        thresh_frames = round(threshold/data.framerate);
+         
+        if ( data.framerate*data.movieLength>threshold )
             numPers = numAboveThresh(lftInfo, thresh_frames);
         end
     end
 end
 end
+
 
 
 function [numPers]=numAboveThresh(lftInfo, threshold)
@@ -59,14 +62,14 @@ numCat = zeros(4,length(threshold));
 % loop over all specified threshold values
 for t = 1:length(threshold)
     
-    for b = 1:size(Mlft,1)
+    for b = 1:size(Mlft, 1)
         % objects are divided into several categories:
-        % 1. persistent (lifetime > th, regardless of status)
+        % 1. persistent (lifetime > threshold(t), regardless of status)
         % 2. analysis pool (full lifetime is known, i.e. status =1, and
-        %    lifetime is longer than 4 frames and less than th)
+        %    lifetime is longer than 4 frames and less than threshold(t))
         % 3. artifactual (lifetime is >=4 frames, therefore not used for
         %    the analysis of the different populations)
-        % 4. unknown (full lifetime isn't known AND >th)
+        % 4. unknown (full lifetime isn't known AND >threshold(t))
         %
         % The final fraction of persistent pits is the contributions of
         % cat1/(cat1+cat2)
@@ -76,18 +79,18 @@ for t = 1:length(threshold)
         % current object status
         currStatus = min(nonzeros(Mstat(b,:)));
         
-        % if lifetime > th, then objevcts is persistent
+        % if lifetime > threshold(t), then objevcts is persistent
         if (currLifetime > threshold(t))
             cat = 1;
         else
             % else if the trajectory is partial, the lifetime is unknown
-            if (currStatus>1)
+            if (currStatus > 1)
                 cat = 4;
                 % else the trajectory is known, and the category depends only on
                 % whether the lifetime is above or below the artifact threshold of
                 % 4 (frames)
             else
-                if (currLifetime>4)
+                if (currLifetime > 4)
                     cat = 2;
                 else
                     cat = 3;
@@ -96,9 +99,8 @@ for t = 1:length(threshold)
         end
         
         % add to the appropriate category
-        numCat(cat,t) = numCat(cat,t)+1; 
+        numCat(cat,t) = numCat(cat,t)+1;
     end
-    
     % calculate fraction of category 1 out of the 'usable' trajectories
     numPers(t) = numCat(1,t);
 end
