26312f1f284136913618aaa4ddd0bef49e23a656
Daniel Nunez <daniel_nunez@hms.harvard.edu>
2010-02-19 16:39:28 +0000

    added pitInfo GUI window
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@4184 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ pitInfo.m
@@ -0,0 +1,73 @@
+function varargout = pitInfo(varargin)
+% PITINFO M-file for pitInfo.fig
+%      PITINFO, by itself, creates a new PITINFO or raises the existing
+%      singleton*.
+%
+%      H = PITINFO returns the handle to a new PITINFO or the handle to
+%      the existing singleton*.
+%
+%      PITINFO('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in PITINFO.M with the given input arguments.
+%
+%      PITINFO('Property','Value',...) creates a new PITINFO or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before pitInfo_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to pitInfo_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help pitInfo
+
+% Last Modified by GUIDE v2.5 18-Feb-2010 20:54:06
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @pitInfo_OpeningFcn, ...
+                   'gui_OutputFcn',  @pitInfo_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before pitInfo is made visible.
+function pitInfo_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to pitInfo (see VARARGIN)
+
+% Choose default command line output for pitInfo
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes pitInfo wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = pitInfo_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
