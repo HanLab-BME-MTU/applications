aae1a69b35c6e9501266fe34e547daf849793bcd
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-03-01 20:06:44 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@4214 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ MultiChannelAnalysis/lifetimeAnalysisDualChannel.m
@@ -0,0 +1,61 @@
+function lifetimeAnalysisDualChannel(data, kWeibull, cutoffIdx)
+
+if nargin < 2
+    kWeibull = [1 2];
+end
+if nargin < 3
+    cutoffIdx = 5;
+end
+
+
+nExp = length(data);
+for k = 1:nExp
+
+    load([data(k).source 'LifetimeInfo' filesep 'lftInfo.mat']);
+    lftMat = full(lftInfo.Mat_lifetime);
+    statMat =  full(lftInfo.Mat_status);
+        
+    cargoIdx = find(data(k).cargoStatus == 1 & data(k).csValid == 1);
+    nocargoIdx = find(data(k).cargoStatus == 0 & data(k).csValid == 1);
+    
+    cargoLifetimeVect = NaN(1, length(cargoIdx));
+    nocargoLifetimeVect = NaN(1, length(nocargoIdx));
+    
+    lifetimeVect = NaN(size(lftMat,1),1);
+    for t = 1:size(lftMat,1)
+        cstat = nonzeros(statMat(t,:));
+        if ( (min(cstat)==1) && (max(cstat)<5) )
+            lifetimeVect(t) = max(lftMat(t,:));
+        end
+    end
+    lifetimeVect = lifetimeVect(isfinite(lifetimeVect));
+    data(k).lftHist = hist(lifetimeVect, 1:data(k).movieLength);
+    
+    for t = 1:length(cargoIdx)
+        cstat = nonzeros(statMat(cargoIdx(t),:));
+        if ( (min(cstat)==1) && (max(cstat)<5) )
+            cargoLifetimeVect(t) = max(lftMat(cargoIdx(t),:));
+        end
+    end
+    cargoLifetimeVect = cargoLifetimeVect(isfinite(cargoLifetimeVect));
+    
+    for t = 1:length(nocargoLifetimeVect)
+        cstat = nonzeros(statMat(nocargoIdx(t),:));
+        if ( (min(cstat)==1) && (max(cstat)<5) )
+            nocargoLifetimeVect(t) = max(lftMat(nocargoIdx(t),:));
+        end
+    end
+    nocargoLifetimeVect = nocargoLifetimeVect(isfinite(nocargoLifetimeVect));
+    
+    data(k).cargoLftHist = hist(cargoLifetimeVect, 1:data(k).movieLength);
+    data(k).nocargoLftHist = hist(nocargoLifetimeVect, 1:data(k).movieLength);    
+end
+
+% create mean histograms
+lftHistMean = stageFitLifetimes(data, 'lftHist');
+cargoLftHistMean = stageFitLifetimes(data, 'cargoLftHist');
+nocargoLftHistMean = stageFitLifetimes(data, 'nocargoLftHist');
+
+lifetimeAnalysis_2p(lftHistMean, kWeibull, cutoffIdx);
+lifetimeAnalysis_2p(cargoLftHistMean, kWeibull, cutoffIdx);
+lifetimeAnalysis_2p(nocargoLftHistMean, kWeibull, cutoffIdx);
