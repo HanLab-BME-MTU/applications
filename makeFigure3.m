349a486fe5c0ccd86361f53fae21c962e5293a48
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2010-03-04 22:23:35 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/tropo@4263 0bcb941b-74d1-0310-bb26-e6636707f451

--- makeFigure3.m
+++ makeFigure3.m
@@ -268,7 +268,7 @@ for iTM = 1:3
         'UniformOutput', false));
     xlabel('Time (s)');
     if iTM == 1
-        ylabel('Distance to edge (nm)');
+        ylabel('Distance to Edge (nm)');
     end
     legend(names);
     print(hFig, '-depsc' , [outputDirectory filesep 'fig3_C' ...
@@ -281,7 +281,7 @@ for iTM = 1:3
     %                                                                 %
     %-----------------------------------------------------------------%
 
-    data = zeros(2, nFrames-1);
+    data = zeros(1, nFrames-1);
     timeScale = 0:timeInterval:(nFrames-2)*timeInterval;
     
     for iFrame = 1:nFrames-1
@@ -306,25 +306,23 @@ for iTM = 1:3
         BW = imread([maskPath filesep maskFiles(iFrame).name]);
         distToEdge = double(bwdist(1 - BW)) * (pixelSize / 1000); % in microns
     
-        data(1,iFrame) = mean(distToEdge(idxS1));
-        data(2,iFrame) = mean(distToEdge(idxS2));
+        data(1,iFrame) = mean(distToEdge(idxS1)) - mean(distToEdge(idxS2));
     end
     
     hFig = figure('Visible', 'off');
     set(gca, 'FontName', 'Helvetica', 'FontSize', 20);
     set(gcf, 'Position', [ 680 678 560 400], 'PaperPositionMode', 'auto');
-    plot(gca, timeScale, data(1,:), 'g-', 'LineWidth', 1); hold on;
-    plot(gca, timeScale, data(2,:), 'r-', 'LineWidth', 1); hold off;
+    plot(gca, timeScale, data(1,:), 'b-', 'LineWidth', 1); hold on;
     % These settings are adapted to the 3 movies. Change this when you
     % change to other movies.
-    yRange  = 1.8:.2:3.4;
+    yRange  = -1.0:.1:1.0;
     axis([0, max(timeScale), yRange(1), yRange(end)]);
     set(gca,'YTick', yRange);
     set(gca,'YTickLabel',arrayfun(@(x) num2str(x, '%3.1f'), yRange, ...
         'UniformOutput', false));
     xlabel('Time (s)');
     if iTM == 1
-        ylabel('Distance to edge (nm)');
+        ylabel('Distance to Actin Front (nm)');
     end
     legend(names);
     print(hFig, '-depsc' , [outputDirectory filesep 'fig3_D' ...
