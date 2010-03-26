fe1fa0154ea4411b3684f4e3608dc4b810f428f2
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-03-26 16:14:03 +0000

    Overwriting of previous detections is now handled differently, via an input parameter. By default, existing detections are not overwritten.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@4410 0bcb941b-74d1-0310-bb26-e6636707f451

--- runDetection.m
+++ runDetection.m
@@ -1,11 +1,11 @@
-function [exp] = runDetection(exp)
+function [exp] = runDetection(exp, overwrite)
 % runDetection automatically runs Henry's detection software - with iclean
 % 1 on all the movies specified in the experiment structure
 % SYNOPSIS [exp] = loadAndSaveDetection(exp);
 %
-% INPUT     exp    :    experiment structure, which has to contain a field
-%                       .source
-%                       which is the path to the data location
+% INPUT     exp       : experiment structure, containing a '.source' field (path to data location)
+%           overwrite : optional, 1 to overwrite previous detection results (default 0).
+%                       
 % OUTPUT               
 % REMARKS   The function only performs the detection for a given movie if 
 %           there are no detection data (folder called maxdata283) already 
@@ -14,7 +14,11 @@ function [exp] = runDetection(exp)
 %           first!!!!
 %
 % Dinah Loerke, last modified Feb 2008
-% Francois Aguet, last modified Dec 2009
+% Francois Aguet, last modified Mar 2010
+
+if nargin<2
+    overwrite = 0;
+end
 
 % loop over all entries in the structure to enter the image data necessary for the detection input
 nExp = length(exp);
@@ -33,8 +37,8 @@ for i = 1:nExp
         if ~(exist([exp(i).source filesep 'maxdata283'], 'dir') == 7)
             runStatus(i) = 1;
         else
-            overwrite = input('Detection results already exist. Overwrite? (y/n): ', 's');
-            if strcmp(overwrite, 'y')
+            if (overwrite)
+                fprintf('Overwriting detecting results for movie %d.\n', i);
                 runStatus(i) = 1;
             end;
         end;
