024d2404bd6109c990ece6925bf2dafb181ceced
Chuangang Ren <chuangang_ren@hms.harvard.edu>
2010-04-29 02:38:31 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/biosensors/main@4531 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ BioSensorsPackage.m
@@ -0,0 +1,37 @@
+classdef BioSensorsPackage < Package
+% A concrete process for BioSensor Package
+    
+    methods
+        function obj = BioSensorsPackage (owner)
+           % Construntor of class MaskProcess
+           if nargin == 0
+              super_args = {};
+           else
+               % Owner: MovieData object
+               super_args{1} = owner;
+               super_args{2} = 'BioSensorsPackage'; 
+               % Dependency Matrix (same length as process class name string)
+               super_args{3} = [0 0 0 0
+                                1 0 0 0
+                                0 1 0 0
+                                0 0 1 0];
+               % Process class name string
+               super_args{4} = {'Mask', ... 
+                                'Protrusion', ...
+                                'FinalProcess'};
+           end
+           % Call the supercalss constructor with empty cell array (no
+           % argument) if nargin == 0
+           obj = obj@Package(super_args{:});
+        end
+        function sanityCheck(obj,full) % throws Exception Cell Array
+            % Sanity Check
+            % full: true or false
+            if nargin < 2
+                full = false;
+            end
+            obj.checkProcesses(full)  % throws Exception Cell Array
+        end
+    end
+end
+
