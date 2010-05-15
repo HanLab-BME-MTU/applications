cae1064d24aac50b3e79b58855ceac174471d417
Hunter Elliott <hunter_elliott@hms.harvard.edu>
2010-05-15 00:30:12 +0000

    Converted the creatBackgroundMasks.m function to the new movieData format, and added its constructor
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/biosensors/main@4655 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ BackgroundMaskProcess.m
@@ -0,0 +1,31 @@
+classdef BackgroundMaskProcess < SegmentationProcess
+    
+    methods(Access = public)
+        
+        function obj = BackgroundMaskProcess(owner,funParams)
+            if nargin == 0
+                super_args = {};
+            else
+                nChan = numel(owner.channelPath_);
+                
+                super_args{1} = owner;
+                super_args{2} = @createMovieBackgroundMasks;                               
+                
+                if nargin < 2 || isempty(funParams)                                       
+                    
+                    %----Defaults----%
+                    funParams.OutputDirectory = [owner.movieDataPath_ filesep 'BackgroundMasks'];                          
+                    funParams.ChannelIndex = 1:nChan; %Default is to attempt to creat background masks for all channels
+                    funParams.GrowthRadius = 20;
+                    funParams.BatchMode = false;                                              
+                end
+                %Make sure the input parameters are legit??
+                super_args{3} = funParams;                    
+            end
+            
+            obj = obj@SegmentationProcess(super_args{:});
+            obj.name_ = mfilename('class');
+            
+        end        
+    end
+end
