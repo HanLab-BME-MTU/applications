a56662bdb9043b6bc1027e318bc9f187a9ad3b74
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2010-05-20 14:49:11 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/tropo@4679 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ checkMovieDensityMap.m
@@ -0,0 +1,18 @@
+function status = checkMovieDensityMap(movieData)
+%
+% status = checkMovieDensityMap(movieData)
+% Returns true if the movie specified by the input movieData has been
+% successfully compute density maps using getMovieDensityMap.m and false
+% otherwise.
+%
+% Sylvain Berlemont, 12/2009
+%
+
+%Ensure correct OS for filenames
+movieData = setupMovieData(movieData);
+
+status = isfield(movieData,'density') && ...
+    isfield(movieData.density,'status') && ...
+    movieData.density.status == 1 && ...
+    isfield(movieData.density,'directory') && ...
+    exist([movieData.density.directory],'dir');
---
+++ getMovieDensityMap.m
@@ -0,0 +1,106 @@
+function movieData = getMovieDensityMap(movieData, batchMode)
+
+%Indicate that labeling was started
+movieData.density.status = 0;
+
+%Verify that the labels has been performed
+if checkMovieLabels(movieData) && ~strcmp(movieData.labels.method,'window')
+    error('Must create per-window label before create density maps.');
+end
+
+movieData.density.directory = fullfile(movieData.analysisDirectory, 'density');
+
+if ~exist(movieData.density.directory, 'dir')
+    mkdir(movieData.density.directory);
+end
+
+%Determine number of windows/bands
+nBands = movieData.labels.nBands;
+nSectors = movieData.labels.nSectors;
+nFrames = movieData.labels.nFrames;
+
+movieData.density.distanceValues = ...
+    movieData.contours.parameters.distanceValues(1:nBands+1);
+
+movieData.density.channels(1:2) = struct('averageMap', [], 'minMaxMap',  []);
+
+% Read the list of label files
+labelPath = movieData.labels.directory;
+labelFiles = dir([labelPath filesep '*.tif']);
+
+% Read the list of TMs speckles (channel 1)
+s1Path = [movieData.fsmDirectory{1} filesep 'tack' filesep 'locMax'];
+s1Files = dir([s1Path filesep '*.mat']);
+
+% Read the list of Actin speckles (channel 2)
+%s2Path = [movieData.fsmDirectory{2} filesep 'tack' filesep 'locMax'];
+%s2Files = dir([s2Path filesep '*.mat']);
+
+if ~batchMode
+    h = waitbar(0,'Please wait, labeling window frames...');
+end
+
+averageMap1 = nan(nSectors,nFrames-1,nBands);
+minMaxMap1 = nan(nSectors,nFrames-1,nBands);
+    
+for iFrame = 1:nFrames-1
+    % Load label
+    L = imread([labelPath filesep labelFiles(iFrame).name]);
+    
+    % Load channel 1 speckles
+    load(fullfile(s1Path, s1Files(iFrame).name));
+    ind = find(locMax);
+    [I J] = ind2sub(size(locMax), ind);
+    
+    % Compute delaunay triangulation
+    dt = DelaunayTri(I, J);
+    % Find every index of triangle surrounding every point
+    tIndices = vertexAttachments(dt);
+    % Find every vertex index composing neighbors triangles (it includes itself)
+    vIndices = cellfun(@(iTriangles) unique(dt.Triangulation(iTriangles,:)), ...
+        tIndices, 'UniformOutput', false);
+    % Compute distances
+    dist = arrayfun(@(i) sqrt((I(vIndices{i}) - repmat(I(i), numel(vIndices{i}), 1)).^2 + ...
+        (J(vIndices{i}) - repmat(J(i), numel(vIndices{i}), 1)).^2), (1:numel(vIndices))', ...
+        'UniformOutput', false);
+    % remove 0 distance
+    dist = cellfun(@nonzeros, dist, 'UniformOutput', false);
+
+    for iBand = 1:nBands
+        % Discard first and last sector
+        for iSector = 2:nSectors-1
+            iWin = sub2ind([nBands, nSectors], iBand, iSector);
+            
+            isSpeckleInside = L(ind) == iWin;
+            
+            if any(isSpeckleInside)
+                distInside = dist(isSpeckleInside);
+                
+                averageMap1(iSector,iFrame,iBand) = mean(cellfun(@mean, distInside));
+                minMaxMap1(iSector,iFrame,iBand) = mean(cellfun(@(x) (max(x) - min(x)) / (max(x) + min(x)), distInside));
+            end
+        end
+    end
+    
+    if ~batchMode && ishandle(h)
+        waitbar(iFrame/nFrames,h)
+    end
+end
+
+movieData.density.channels(1).averageMap = averageMap1;
+movieData.density.channels(1).minMaxMap = minMaxMap1;
+
+if ~batchMode && ishandle(h)
+    close(h);
+end
+
+if ~checkMovieProtrusionSamples(movieData)
+    error('Must sample protrusion before labeling windows.');
+end
+
+movieData.density.dateTime = datestr(now);
+movieData.density.status = 1;
+
+updateMovieData(movieData);
+
+end
