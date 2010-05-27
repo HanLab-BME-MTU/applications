49636acae0275ee69a37995de1caa96fa81f854b
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-05-27 19:05:15 +0000

    Cosmetic changes.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@4756 0bcb941b-74d1-0310-bb26-e6636707f451

--- stageFitLifetimes.m
+++ stageFitLifetimes.m
@@ -1,4 +1,4 @@
-function [results] = stageFitLifetimes(data, histName)
+function [results] = stageFitLifetimesNEW2(data, histName, cutoff_start)
 % STAGEFITLIFETIMES(DATA) fits the lifetimes contained in the structure in
 % several stages, using data with slow and fast framerates. Globally averaged
 % and leave-one-out histograms are returned for later jackknife estimation.
@@ -26,75 +26,75 @@ function [results] = stageFitLifetimes(data, histName)
 % last modified DATE: 31-Jul-2007 (Dinah)
 % Francois Aguet, Feb 2010
 
-if nargin < 2
+if nargin<2 || isempty(histName)
     histName = 'lftHist';
 end
 
-%========================================================================
-% 1. average all data to identify initial detection artifact
-%=========================================================================
+if nargin<3
 
-% NOTE: since we're interested in the initial decay of the detection
-% artifact, it should be sufficient to fit only the 100 or so frames; the
-% cutoff is defined as cutoff_art
-
-cutoff_art = 100;
-tvec = 1:cutoff_art;
-
-histMatrix_all = zeros(length(data), cutoff_art);
-idx = 1:cutoff_art;
-for i=1:length(data)
-    if ~isempty(data(i).(histName))
-        % normalize over area betwen 0 and cutoff_art frames. FA: cmp normalization over full domain?
-        histMatrix_all(i,:) = data(i).(histName)(idx)/sum(data(i).(histName)(idx));
+    %========================================================================
+    % 1. Detection artifact based on average of all data
+    %=========================================================================
+    
+    % The detection artifact consists only of a small number of frames;
+    % 100 frames are sufficient for its estimation.
+    cutoff_art = 100;
+    tvec = 1:cutoff_art;
+    
+    histMatrix_all = zeros(length(data), cutoff_art);
+    idx = 1:cutoff_art;
+    for i=1:length(data)
+        if ~isempty(data(i).(histName))
+            % normalize
+            histMatrix_all(i,:) = data(i).(histName)(idx)/sum(data(i).(histName)(idx));
+        end
     end
+    
+    % combined/averaged histograms, normalized
+    cHistogramVector = nanmean(histMatrix_all,1);
+    cHistogramVector = cHistogramVector/nansum(cHistogramVector);
+    
+    
+    % plot results
+    figure;
+    plot(tvec, cHistogramVector, 'k.'); hold on;
+    xlabel('frames');
+    ylabel('average frequency');
+    axis([0 cutoff_art 0 1.1*max(cHistogramVector(:))]);
+    
+    
+    % fit averaged results with multiple exponentials (k = 1)
+    prmVect = [0  0.3 0.3 1  0.3 20 1];
+    estVect = [1  1 1 0  1 1 0];
+    prmVect = fitNWeibull(tvec, cHistogramVector, prmVect, estVect, 'PDF');
+    
+    % sort parameter vector according to population mean
+    N = 2;
+    [dummy order] = sort(prmVect(3:3:end));
+    idx = reshape(2:3*N+1, [3 N]);
+    idx = reshape(idx(:,order), [1 3*N]);
+    prmVect(2:end) = prmVect(idx);
+    [w W] = nWeibull(tvec, prmVect, 'PDF');
+    
+    % plot first component, which is the detection artifact
+    plot(tvec, W(1,:), 'r-');
+    title('detection artifact');
+    
+    
+    %=====================
+    % Detection artifact
+    %=======================
+    % A detection artifact in form of a large number of short tracks (~1-4 frames)
+    % is part of the liftime distributions. A cutoff to exclude this part of the
+    % histogram is defined below. The cutoff is in principle independent of frame rate.
+    
+    % distance at which the artifact exponential decreases past 0.1% of its amplitude at 1 frame
+    % prmVect(3)*log((1-exp(-1/prmVect(3)))/0.001) + 1
+    cutoff_start = ceil(-prmVect(3)*log(0.001)+1) + 1; %%%%%%%%%%%% + 1 remove
+    fprintf('Detection artifact: cutoff at %d frames.\n', cutoff_start);
 end
+results.detectionCutoff = cutoff_start;
 
-% combined/averaged histograms, normalized
-cHistogramVector = nanmean(histMatrix_all,1);
-cHistogramVector = cHistogramVector/nansum(cHistogramVector);
-
-
-% plot results
-figure;
-plot(tvec, cHistogramVector, 'b.-'); hold on;
-xlabel('frames');
-ylabel('average frequency');
-axis([0 cutoff_art 0 1.1*max(cHistogramVector(:))]);
-
-
-% fit averaged results with multiple exponentials (weibulls with k==1)
-guessvector = [0    0.3 0.3 1   0.3 5   1   0.3 20  1];
-fixvector =   [0    0   0   1   0   0   1   0   0   1];
-estAll = fitcurveMultiWeibullODF_lsq(tvec, cHistogramVector, guessvector, fixvector);
-
-estAll(2:end) = abs(estAll(2:end)); % abs of all except offset
-
-
-% plot first component, which is the detection artifact
-plot(tvec, abs(estAll(2))*(1/estAll(3))*exp(-tvec/estAll(3)), 'g-');
-title('detection artifact');
-
-% extract value of sigma for initial detection artifact
-sigArt = estAll(3);
-% distance at which the artifact exponential decreases past 0.1% of its amplitude at 1 frame
-d01percent = -sigArt*log(0.001)+1;
-disp(['0.1% distance: ', num2str(d01percent), 'frames']);
-
-
-% ========================================================================
-%                   define following data cutoffs
-% ========================================================================
-
-% initial cutoff (fast lifetimes that are cut off because they may
-% represent noisy detection artifact) - in frames
-cutoff_start = ceil(d01percent);
-% NOTE: I allow the option of using DIFFERENT values for cutoff_start in
-% the fast and slow movies further below
-
-% end cutoff (long lifetimes close to the length of the movie have noisy
-% frequencies because of bad statistics) - in percent, e.g. 0.1 for 10%
-cutoff_slow = 0.1;
 
 % ========================================================================
 %       determine which entries in the data structure represent fast or
@@ -108,10 +108,6 @@ cutoff_slow = 0.1;
 % input movies with the same framerate (2s slow, 0.4s fast), the knowledge
 % of the last frame suffices
 
-frThreshold = 1;
-slowIdx = find([data.framerate] >  frThreshold & arrayfun(@(x)~isempty(x.(histName)), data));
-fastIdx = find([data.framerate] <= frThreshold & arrayfun(@(x)~isempty(x.(histName)), data));
-timespan = [data.framerate].*[data.movieLength];
 
 % ========================================================================
 % Average all fast/slow data
@@ -120,189 +116,150 @@ timespan = [data.framerate].*[data.movieLength];
 % the following procedure is performed twice, once for fast, once for slow
 % movies; the respective interpolation time vectors were defined above
 
+timespan = [data.framerate].*[data.movieLength];
 nTracks = zeros(1, length(data));
+frameRates = unique([data.framerate]);
+histStruct = struct('t', [], 'hist', []);
 
-% =========================================================================
-% Process fast data
-% =========================================================================
-if ~isempty(fastIdx)
-    nMovies = length(fastIdx);
-    ipFramerate = 0.4;
+for r = 1:length(frameRates)
+    idx = find([data.framerate]==frameRates(r) & arrayfun(@(x)~isempty(x.(histName)), data));
     
-    %cutpoint = round(floor(min(timespan(fastIdx))/0.4)*0.9); % cut at 90% of total length
-    % cutpoint: shortest movie length - 2 (see lftHist_correctionVector)
-    cutpoint = min([data(fastIdx).movieLength]) - 2;
-    
-    histMatrix = NaN(nMovies, 1000);
-    
-    figure;
-    for p = 1:nMovies
-        i = fastIdx(p);
-
-        % time vector, cut off at beginning and end
-        tvec_curr = data(i).framerate*(cutoff_start+1:cutpoint-1);
-                
-        % Number of trajectories in movie
-        nTracks(i) = sum(data(i).(histName)(cutoff_start:end));
-        
-        % correct histogram for movie length
-        currCorrVec = lftHist_correctionVector(data(i).movieLength); % length: movielength-2
-        %currHistCorr = data(i).(histName)(1:data(i).movieLength-2).*currCorrVec;
-        currHistCorr = data(i).(histName)(cutoff_start+1:cutpoint-1).*currCorrVec(cutoff_start+1:cutpoint-1);
-
-        % pre-normalize first by remaining sum
-        currHistNorm = currHistCorr/sum(currHistCorr);
-        
-        % enter interpolated/renormalized histogram into summarizing matrix
-        histMatrix(p,:) = NaN;
-        histMatrix(p,1:length(currHistNorm)) = currHistNorm;
-        
-        % plot results
-        plot(tvec_curr, cumsum(currHistNorm), 'b.-'); hold on;
-        axis([0 120 0 1.05]);
-    end
-    
-    % when the averaging takes place, that's a possible source for
-    % artifacts for the first rayleigh population -
-    % e.g. when the first defined point is averaged over 1 entry
-    % and then the next one over 6 points - the first point is only if
-    % the number of entries is >1 for more than 2 rows
-    for ci = 1:20
-        col = histMatrix(:,ci);
-        n_nan = sum(isnan(col));
-        n_def = nMovies - n_nan;
-        if (n_nan>n_def) && (n_def>0)
-            histMatrix(:,ci) = NaN;
-        end
-    end
-    
-    histVectorAve = nanmean(histMatrix,1);
-    finitepos = find(isfinite(histVectorAve));
-    
-    % define appropriate restricted time vector
-    tvecComp = tvec_curr(1)+ipFramerate*(1:length(histVectorAve));
-    tvecRes = tvecComp(finitepos);
-    % define appropriate restricted lft vector
-    histVectorAve = histVectorAve(finitepos);
-        
-    % leave-one-out averages for later jackknife
-    if nMovies > 1
-        histVectorAveN1 = zeros(nMovies, length(finitepos));
-        for p = 1:nMovies
-            histVectorAveN1(p,:) = nanmean(histMatrix([1:p-1 p+1:nMovies], finitepos), 1);
-        end
-        results.hist_fast = [round(tvecRes*10)/10; histVectorAve; histVectorAveN1];
-    else
-        results.hist_fast = [round(tvecRes*10)/10; histVectorAve];
-    end
-    
-    results.numcells_fast = sum(nTracks(fastIdx));
-    plot(tvecRes, cumsum(histVectorAve),'r.-');
-    title('Cumulative average histogram');
-    
-    figure;
-    plot(tvecRes, histVectorAve,'r.-');
-    axis([0 120 0 1.05*max(histVectorAve)]);
-    title('Average histogram');
-end
-
-% =========================================================================
-% Process slow data
-% =========================================================================
-if ~isempty(slowIdx)
-    nMovies = length(slowIdx);
-    ipFramerate = 2;
-    
-    %cutpoint = round(floor(min(timespan(slowIdx))/2)*(1-cutoff_slow)); % cut at 90%
-    cutpoint = min([data(slowIdx).movieLength]) - 2;
-    histMatrix  = NaN(nMovies,1000);
-    
-    figure;
-    for p = 1:nMovies
-        i = slowIdx(p);
-
-        % time vector, cut off at beginning and end
-        tvec_curr = data(i).framerate*(cutoff_start+1:cutpoint-1);
-
-        % Number of trajectories in movie
-        nTracks(i) = sum(data(i).(histName)(cutoff_start:end));
-
-        % correct histogram for movie length
-        currCorrVec = lftHist_correctionVector(data(i).movieLength); % length: movielength-2
-        %currHistCorr = data(i).lftHist(1:data(i).movieLength-2).*currCorrVec;
-        currHistCorr = data(i).(histName)(cutoff_start+1:cutpoint-1).*currCorrVec(cutoff_start+1:cutpoint-1);
-
-        % number of persistent trajectories: trajectories longer than cutpoint
-        if isfield(data(i), 'lftHistCut') && ~isempty(data(i).lftHistCut)
-            numPers = sum(data(i).lftHistCut(cutpoint:end));
+    if ~isempty(idx)
+        nMovies = length(idx);
+        if frameRates(r) >= 1
+            frString = [num2str(frameRates(r)) 's'];
         else
-            % threshold is in seconds, cutoff must be multiplied by framerate
-            numPers = numPersistentField(data(i), data(i).framerate*cutpoint);
+            frString = [num2str(frameRates(r)*1000) 'ms'];
         end
-        % normalize by the total sum, where the sum of the histogram plus
-        % the number of persistent objects equals 100%
-        nTracks(i) = nTracks(i) + numPers;
-
-        % pre-normalize first by remaining sum
-        currHistNorm = currHistCorr/(sum(currHistCorr)+numPers);
         
-        % enter interpolated/renormalized histogram into summarizing matrix
-        histMatrix(p,:) = NaN;
-        histMatrix(p,1:length(currHistNorm)) = currHistNorm;
         
-        % plot results
-        plot(tvec_curr,cumsum(currHistNorm), 'b.-'); hold on;        
-        axis([0 tvec_curr(end) 0 1.05]);
-    end
-    
-    % when the averaging takes place, that's a possible source for
-    % artifacts for the first rayleigh population -
-    % e.g. when the first defined point is averaged over 1 entry
-    % and then the next one over 6 points - the first point is only if
-    % the number of entries is >1 for more than 2 rows
-    for ci = 1:20
-        col = histMatrix(:,ci);
-        n_nan = sum(isnan(col));
-        n_def = nMovies - n_nan;
-        if (n_nan>n_def) && (n_def>0)
-            histMatrix(:,ci) = NaN;
-        end
-    end
-    
-    histVectorAve = nanmean(histMatrix,1);
-    finitepos = find(isfinite(histVectorAve));
-    
-    % define appropriate restricted time vector
-    tvecComp = tvec_curr(1)+ipFramerate*(1:length(histVectorAve));
-    tvecRes = tvecComp(finitepos);
-    % define appropriate restricted lft vector
-    histVectorAve = histVectorAve(finitepos);
-    
-    % leave-one-out averages for later jackknife
-    if nMovies > 1
-        histVectorAveN1 = zeros(nMovies, length(finitepos));
+        % end cutoff (long lifetimes close to the length of the movie have noisy
+        % frequencies because of bad statistics) - in percent, e.g. 0.1 for 10%
+        cutpoint = round(floor(min(timespan(idx))/frameRates(r))*0.9); % cut at 90% of total length
+        %cutpoint = min([data(idx).movieLength]); % no cutoff
+        
+        %figure;
         for p = 1:nMovies
-            histVectorAveN1(p,:) = nanmean(histMatrix([1:p-1 p+1:nMovies], finitepos), 1);
+            i = idx(p);
+            
+            % time vector, cut off at beginning and end
+            tvec_curr = data(i).framerate*(cutoff_start+1:cutpoint-1);
+            
+            % Number of trajectories in movie
+            nTracks(i) = sum(data(i).(histName)(cutoff_start:end));
+            
+            % correct histogram for movie length
+            currHistCorr = data(i).(histName) .* lftHist_correctionVector(data(i).movieLength);
+            currHistCorr = currHistCorr(cutoff_start+1:cutpoint-1);
+            
+            if frameRates(r)==2
+                % number of persistent trajectories: trajectories longer than cutpoint
+                if isfield(data(i), 'lftHistCut') && ~isempty(data(i).lftHistCut)
+                    numPers = sum(data(i).lftHistCut(cutpoint:end));
+                else
+                    % threshold is in seconds, cutoff must be multiplied by framerate
+                    numPers = numPersistentField(data(i), data(i).framerate*cutpoint);
+                end
+                % normalize by the total sum, where the sum of the histogram plus
+                % the number of persistent objects equals 100%
+                nTracks(i) = nTracks(i) + numPers;
+                
+                % pre-normalize first by remaining sum
+                currHistNorm = currHistCorr/(sum(currHistCorr)+numPers);
+            else
+                % pre-normalize first by remaining sum
+                currHistNorm = currHistCorr/sum(currHistCorr);
+            end
+            
+            % enter interpolated/renormalized histogram into summarizing matrix
+            histStruct(p).t = tvec_curr;
+            histStruct(p).hist = currHistNorm;
         end
-        results.hist_slow = [round(tvecRes*10)/10; histVectorAve; histVectorAveN1];
-    else
-        results.hist_slow = [round(tvecRes*10)/10; histVectorAve];
+        
+        tmax = max([histStruct.t]);
+        histMatrix = NaN(nMovies, tmax/frameRates(r)+1);
+        for p = 1:nMovies
+            histMatrix(p,1:length(histStruct(p).t)) = histStruct(p).hist;
+        end
+        
+        % when the averaging takes place, that's a possible source for
+        % artifacts for the first rayleigh population -
+        % e.g. when the first defined point is averaged over 1 entry
+        % and then the next one over 6 points - the first point is only if
+        % the number of entries is >1 for more than 2 rows
+        for ci = 1:20
+            col = histMatrix(:,ci);
+            n_nan = sum(isnan(col));
+            n_def = nMovies - n_nan;
+            if (n_nan>n_def) && (n_def>0)
+                histMatrix(:,ci) = NaN;
+            end
+        end
+        
+        histVectorAve = nanmean(histMatrix,1);
+        finitepos = find(isfinite(histVectorAve));
+        
+        % define appropriate restricted time vector
+        tvecComp = tvec_curr(1)+frameRates(r)*(1:length(histVectorAve));
+        tvecRes = tvecComp(finitepos);
+        % define appropriate restricted lft vector
+        histVectorAve = histVectorAve(finitepos);
+        
+        % leave-one-out averages for later jackknife
+        fieldName = ['hist_' frString];
+        if nMovies > 1
+            histVectorAveN1 = zeros(nMovies, length(finitepos));
+            for p = 1:nMovies
+                histVectorAveN1(p,:) = nanmean(histMatrix([1:p-1 p+1:nMovies], finitepos), 1);
+            end
+            results.(fieldName) = [round(tvecRes*10)/10; histVectorAve; histVectorAveN1];
+        else
+            results.(fieldName) = [round(tvecRes*10)/10; histVectorAve];
+        end
+        
+        results.(['numtracks_' frString]) = sum(nTracks(idx));
+        results.histMatrix = histMatrix;
+        
+        
+        % Plot cumulative lifetime distributions
+        figure
+        for p = 1:nMovies
+            plot(histStruct(p).t, cumsum(histStruct(p).hist), 'b.-'); hold on;
+        end
+        plot(tvecRes, cumsum(histVectorAve),'r.-');
+        axis([0 tvec_curr(end) 0 1.05]);
+        xlabel('Time (s)', 'FontName', 'Helvetica', 'FontSize', 14);
+        title(['Mean cumulative histogram ' frString]);
+        
+        % Plot lifetime distributions
+        figure;
+        %for p = 1:nMovies
+        %    plot(histStruct(p).t, histStruct(p).hist, 'b.-'); hold on;
+        %end        
+        plot(tvecRes, histVectorAve, 'r.-');
+        axis([0 tvecRes(end) 0 1.05*max(histVectorAve)]);
+        xlabel('Time (s)', 'FontName', 'Helvetica', 'FontSize', 14);
+        title(['Mean histogram ' frString]);
+        
+        if frameRates(r) == 2
+            % Determine fraction of permanent objects
+            prmVect = [0 0.3 4 2  0.3 8 2  0.3 90 1];
+            estVect = [1 1 1 1 1 1 1 1 1 1];
+            prmVect = fitNWeibull(tvecRes, 1-cumsum(histVectorAve), prmVect, estVect, 'PDF');
+            
+            figure;
+            plot(tvecRes, 1-cumsum(histVectorAve),'k.-');
+            hold on;
+            np = (length(prmVect)-1)/3;
+            colorV = ones(np,3);
+            colorV(:,1) = (0:1/np:1-1/np);
+            set(gca, 'ColorOrder', hsv2rgb(colorV), 'FontName', 'Helvetica', 'FontSize', 14, 'LineWidth', 1.5);
+            [w W] = nWeibull(tvecRes , prmVect, 'PDF');
+            plot(tvecRes, W);
+            plot(tvecRes, w, 'r');
+            
+            title('Immobile fraction offset fit');
+            fprintf('Immobile fraction offset: %f\n', prmVect(1));
+        end
     end
-    
-    results.numcells_slow = sum(nTracks(slowIdx));
-    plot(tvecRes, cumsum(histVectorAve),'r.-');
-    title('Cumulative average histogram (slow)');
-    
-    figure;
-    plot(tvecRes, 1-cumsum(histVectorAve),'r.-');
-    guessvec = [0 0.3 4 2 0.3 8 2 0.3 90 1];
-    fixvec = [0 0 0 0 0 0 0 0 0 0];
-    estSlowInv = fitcurveMultiWeibullODF_lsq(tvecRes, 1-cumsum(histVectorAve), guessvec, fixvec);
-    disp(['immobile fraction offset = ', num2str(estSlowInv(1))]);
-    title('Population fit');
-    
-    figure;
-    plot(tvecRes, histVectorAve,'r.-');
-    axis([0 tvecRes(end) 0 1.05*max(histVectorAve)]);
-    title('Average histogram (slow)');
 end
