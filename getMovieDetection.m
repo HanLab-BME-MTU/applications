97bc51a430b74beee533769d7385b4866c70d230
sb234 <sb234@0bcb941b-74d1-0310-bb26-e6636707f451>
2010-06-09 20:41:43 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/vinculinFA/main@4890 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ getMovieDetection.m
@@ -0,0 +1,53 @@
+function movieData = getMovieDetection(movieData,batchMode)
+
+%Indicate that detection was started
+movieData.detection.status = 0;
+
+if ~batchMode
+    h = waitbar(0,'Please wait, focal adhesion detection....');
+end
+
+imagePath = movieData.channels(1).roiDirectory;
+imageFiles = dir([imagePath filesep '*.tif']);
+
+movieData.detection.directory = [movieData.channels(1).analysisDirectory ...
+    filesep 'detection'];
+
+if ~exist(movieData.detection.directory, 'dir')
+    mkdir(movieData.detection.directory);
+end
+
+movieData.detection.filename = 'segmentParams.mat';
+
+nFrames = numel(imageFiles);
+
+% !!!! LINK THIS WITH THE REAL PARAMS:
+% sigmaPSF = vectorialPSFSigma(1.4, 509, 67)
+sigmaPSF = 1.6255;
+minSize = 2;
+
+segmentParams = cell(nFrames, 1);
+
+for i = 1:nFrames
+    % Read images
+    I = imread([imagePath filesep imageFiles(i).name]);
+
+    % Get initial segment parameters
+    segmentParams{i} = subResSegment2DInit(I,sigmaPSF,minSize);
+    
+    if ~batchMode && ishandle(h)
+        waitbar(i/nFrames, h)
+    end
+end
+
+save([movieData.detection.directory filesep movieData.detection.filename], ...
+    'segmentParams');
+
+movieData.detection.dateTime = datestr(now);
+movieData.detection.status = 1;
+
+if ~batchMode && ishandle(h)
+    close(h);
+end
+
+updateMovieData(movieData);
