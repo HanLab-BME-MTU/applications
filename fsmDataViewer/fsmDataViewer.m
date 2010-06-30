4de60608b4e6e73b052bde3021f809546b8ae7ec
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2010-06-30 15:23:05 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@5017 0bcb941b-74d1-0310-bb26-e6636707f451

--- fsmDataViewer/fsmDataViewer.m
+++ fsmDataViewer/fsmDataViewer.m
@@ -20,8 +20,8 @@ end
 hFig = imtool(loadChannels(settings, 1), []);
 
 % Change 'save as...' menu to custom function
-% hSaveMenu = findobj(hFig, '-regexp', 'Tag', 'save as menu item');
-% set(hSaveMenu, 'Callback', @saveFrame);
+hSaveMenu = findobj(hFig, '-regexp', 'Tag', 'save as menu item');
+set(hSaveMenu, 'Callback', @saveMovie);
 
 % Unlock imtool axes children.
 set(hFig, 'HandleVisibility', 'on');
@@ -53,7 +53,70 @@ displayLayers(hFig, 1);
 % Relock imtool axes children.
 set(hFig, 'HandleVisibility', 'callback');
 
-% function saveFrame(varargin)
-% 
-% uiputfile({'*.jpg;*.tif;*.png;*.gif','All Image Files'}, 'Save As', 'Untitled.tif')
-% 
+function saveMovie(varargin)
+
+% Get figure handler
+h = findall(0, '-regexp', 'Name', 'fsmDataViewer');
+settings = get(h, 'UserData');
+nFrames = settings.numFrames;
+fString = strcat('%0',num2str(ceil(log10(nFrames)+1)),'.f');
+
+filterSpec = {...
+    '*.eps', 'EPS file (*.eps)'; ...
+    '*.tif','TIFF image (*.tif)'; ...
+    '*.mov','QuickTime movie (*.mov)'};
+
+[fileName,pathName,filterIndex] = uiputfile(filterSpec, 'Save As', 'Untitled.eps');
+
+if ischar(fileName) && ischar(pathName)
+    isQuickTime = strcmp(filterSpec{filterIndex,1}, '*.mov');
+
+    if ~isQuickTime
+        [path,body] = getFilenameBody(fullfile(pathName,fileName));
+        fString = strcat('%0',num2str(ceil(log10(nFrames)+1)),'.f');
+        ext = filterSpec{filterIndex,1}(2:end);
+        switch filterIndex
+            case 1
+                driver = '-depsc2';
+            case 2
+                driver = '-dtiffn';            
+        end
+    end
+    
+    [iFirst,iLast]=fsmTrackSelectFramesGUI(1,nFrames,0,'Select frames to be processed:');
+    
+    scrsz = get(0,'ScreenSize');
+    hFig = figure('Position',scrsz);
+    set(hFig,'UserData',settings);
+    
+    if isQuickTime
+        evalString = ['MakeQTMovie start ''' fullfile(pathName,fileName) ''''];
+        eval(evalString);
+    end
+    
+    if settings.numChannels == 1
+        colormap 'gray';
+    end
+        
+    for iFrame = iFirst:iLast  
+        % Display channels
+        I = loadChannels(settings, iFrame);
+        
+        imagesc(I,'Parent', gca),axis image,axis off;
+        % Display layers
+        displayLayers(hFig, iFrame);
+        
+        if isQuickTime
+            MakeQTMovie addaxes
+        else
+            print(hFig, driver, [path filesep body num2str(iFrame,fString) ext]);
+        end
+    end
+    
+    if isQuickTime
+         MakeQTMovie finish
+    end
+    
+    close(hFig);
+end
+
