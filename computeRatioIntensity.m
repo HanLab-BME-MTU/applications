5ace56cc6f7423249c765d50c44ca37d4e41d899
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2010-07-20 16:03:24 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/tropo@5060 0bcb941b-74d1-0310-bb26-e6636707f451

--- computeRatioIntensity.m
+++ computeRatioIntensity.m
@@ -1,7 +1,16 @@
-function ratios = computeRatioIntensity(nChannels, bandWidth, normalized)
+function [ratios proportions] = computeRatioIntensity(nChannels, bandWidth, normalized)
+
+if nChannels <= 1
+    error('nChannels needs to be greater than 1.');
+end
 
 % Ask for mask
 [fileName,pathName] = uigetfile({'*.tif'},'Select mask file');
+
+if ~ischar(fileName) || ~ischar(pathName)
+    disp('Abord');
+    return;
+end
 
 mask = imread(fullfile(pathName, fileName));
 
@@ -11,6 +20,11 @@ pathNames = cell(nChannels,1);
 for iChannel = 1:nChannels
     [fileName,pathName] = uigetfile({'*.tif'},['Select image file for channel ' num2str(iChannel)]);
     
+    if ~ischar(fileName) || ~ischar(pathName)
+        disp('Abord');
+        return;
+    end
+
     fileNames{iChannel} = fileName;
     pathNames{iChannel} = pathName;
 end
@@ -21,20 +35,49 @@ maxDist = max(dist(:));
 
 bands = 0:bandWidth:maxDist;
 
-ratios = zeros(numel(bands) - 1, nChannels);
+% Compute ratios
+
+ratios = zeros(numel(bands) - 1, nChannels * (nChannels - 1) / 2);
+
+iRatio = 1;
+
+for iChannel = 1:nChannels-1    
+    I = imread(fullfile(pathNames{iChannel}, fileNames{iChannel}));
+    for jChannel = iChannel+1:nChannels
+        J = imread(fullfile(pathNames{jChannel}, fileNames{jChannel}));
+        
+        for iBand = 1:numel(bands)-1
+            ratios(iBand, iRatio) = ...
+                mean(I(dist > bands(iBand) & dist <= bands(iBand+1))) / ...
+                mean(J(dist > bands(iBand) & dist <= bands(iBand+1)));
+        end
+        
+        iRatio = iRatio + 1;
+    end
+end
+
+% Compute proportions
+
+proportions = zeros(numel(bands) - 1, nChannels);
 
 for iChannel = 1:nChannels
     I = imread(fullfile(pathNames{iChannel}, fileNames{iChannel}));
 
     for iBand = 1:numel(bands)-1
-        ratios(iBand, iChannel) = mean(I(dist > bands(iBand) & dist <= bands(iBand+1)));
+        proportions(iBand, iChannel) = mean(I(dist > bands(iBand) & dist <= bands(iBand+1)));
     end
 end
 
-if normalized
-    ratios = bsxfun(@rdivide,ratios,sum(ratios,2));
-end
-
-figure, bar(ratios,'stack');
+figure, bar(ratios);
 xlabel('Bands (pixel)');
 ylabel('Average intensity');
+title('Ratios');
+
+if normalized
+    proportions = bsxfun(@rdivide,proportions,sum(proportions,2));
+end
+
+figure, bar(proportions,'stack');
+xlabel('Bands (pixel)');
+ylabel('Average intensity');
+title('Proportions');
