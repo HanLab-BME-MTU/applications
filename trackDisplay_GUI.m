52a2d5f7701c8a1305787a82b653b5b1b1ffbd0c
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-09-27 01:43:46 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@5228 0bcb941b-74d1-0310-bb26-e6636707f451

--- trackDisplay_GUI.m
+++ trackDisplay_GUI.m
@@ -59,6 +59,7 @@ if isfield(data, 'channel1')
     frameListCh1 = cellfun(@(x,y) [x y], repmat({data.channel1}, [1 data.movieLength]), {framesCh1.name}, 'UniformOutput', false);
     framesCh2 = dir([data.channel2 '*.tif']);
     frameListCh2 = cellfun(@(x,y) [x y], repmat({data.channel2}, [1 data.movieLength]), {framesCh2.name}, 'UniformOutput', false);
+    handles.nCh = 2;
 else
     framesCh1 = dir([data.source '*.tif']);
     frameListCh1 = cellfun(@(x,y) [x y], repmat({data.source}, [1 data.movieLength]), {framesCh1.name}, 'UniformOutput', false);
@@ -66,15 +67,14 @@ else
     mpath = [data.source 'Detection' filesep 'Masks' filesep];
     framesCh2 = dir([mpath '*.tif']);
     frameListCh2 = cellfun(@(x,y) [x y], repmat({mpath}, [1 data.movieLength]), {framesCh2.name}, 'UniformOutput', false);
+    handles.nCh = 1;
 end
 
 frameCh1 = imread(frameListCh1{1});
 frameCh2 = imread(frameListCh2{1});
 
-
 % Choose default command line output for trackDisplay_GUI
 handles.output = hObject;
-
 
 % initialize handles
 handles.frameListCh1 = frameListCh1;
@@ -82,13 +82,16 @@ handles.frameListCh2 = frameListCh2;
 
 
 handles.data = data;
-handles.tracks = varargin{2};
+handles.tracks1 = varargin{2};
 if numel(varargin)>2
     handles.tracks2 = varargin{3};
 end
 
 if isfield(data, 'channel1')
-
+    load([data.channel1 'Detection' filesep 'detectionResults.mat']);
+    handles.detection1 = frameInfo;
+    load([data.channel2 'Detection' filesep 'detectionResults.mat']);
+    handles.detection2 = frameInfo;
 else
     load([data.source 'Detection' filesep 'detectionResults.mat']);
     handles.detection = frameInfo;
@@ -111,7 +114,6 @@ set(h, 'Max', data.movieLength);
 set(h, 'SliderStep', [1/(data.movieLength-1) 0.05]);
 
 
-
 imagesc(frameCh1, 'Parent', handles.('axes1'));
 imagesc(frameCh2, 'Parent', handles.('axes2'));
 colormap(gray(256));
@@ -125,6 +127,7 @@ axis(handles.('axes3'), [0 handles.data.movieLength 0 1]);
 % uiwait(handles.figure1);
 
 
+
 % --- Outputs from this function are returned to the command line.
 function varargout = trackDisplay_GUI_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
@@ -134,6 +137,7 @@ function varargout = trackDisplay_GUI_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
+
 
 
 % --- Executes on slider movement.
@@ -150,12 +154,15 @@ function slider1_Callback(hObject, eventdata, handles)
 f = round(get(hObject, 'value'));
 set(hObject, 'Value', f);
 set(handles.('text1'), 'String', ['Frame ' num2str(f)]);
-refreshDisplay(hObject, handles);
+refreshFrameDisplay(hObject, handles);
+
+refreshTrackDisplay(handles);
 
 
 
-function refreshDisplay(hObject, handles)
+function refreshFrameDisplay(hObject, handles)
 
+cmap = jet(handles.data.movieLength);
 f = get(handles.('slider1'), 'value');
 
 % save zoom properties
@@ -163,60 +170,44 @@ haxes = handles.('axes1');
 XLim = get(haxes, 'XLim');
 YLim = get(haxes, 'YLim');
 
-fr = handles.('frameListCh1');
-frameCh1 = imread(fr{f});
-fr = handles.('frameListCh2');
-frameCh2 = imread(fr{f});
-tracks = handles.('tracks');
+frameCh1 = imread(handles.frameListCh1{f});
+frameCh2 = imread(handles.frameListCh2{f});
 
-startV = [tracks.start];
-endV = [tracks.end];
-
-idx = find(startV <= f & f <= endV);
-handles.visibleIdx = idx;
-
-% nActive = sum(idx);
-% maxLifetime = 299; % change to read from input
-
-% lft = handles.('lifetimes');
-% 
-% activeIdx = ~isnan(x(:,f));
-% lifetimes = lft(activeIdx, 1:f);
-% maxLifetime = max(lifetimes, [], 2);
-% xs = x(activeIdx,1:f);
-% ys = y(activeIdx,1:f);
-cmap = jet(301);
-% 
-% 
+idx1 = find([handles.tracks1.start] <= f & f <= [handles.tracks1.end]);
+handles.visibleIdx1 = idx1;
+idx2 = find([handles.tracks2.start] <= f & f <= [handles.tracks2.end]);
+handles.visibleIdx2 = idx2;
 
 
 imagesc(frameCh1, 'Parent', handles.('axes1'));
 axis(handles.('axes1'),'image');
- 
-
-% hold(handles.('axes1'), 'on');
-% for k = idx
-%     fi = 1:f-tracks(k).start+1;
-%     plot(tracks(k).x(fi), tracks(k).y(fi), '-', 'Color', cmap(tracks(k).lifetime,:));
-% end
-% hold(handles.('axes1'), 'off');
+% overlay tracks only if 2-channel input
+if isfield(handles, 'tracks2')
+    hold(handles.('axes1'), 'on');
+    for k = idx1
+        fi = 1:f-handles.tracks1(k).start+1;
+        %plot(handles.tracks1(k).x(fi), handles.tracks1(k).y(fi), '-', 'Color', cmap(handles.tracks1(k).lifetime,:));
+        plot(handles.axes1, handles.tracks1(k).x(1), handles.tracks1(k).y(1), '*', 'Color', 'r');
+        plot(handles.axes1, handles.tracks1(k).x(fi), handles.tracks1(k).y(fi), '-', 'Color', 'r');
+    end
+%     hold(handles.('axes1'), 'off');
+end
 
 
 imagesc(frameCh2, 'Parent', handles.('axes2'));
 axis(handles.('axes2'),'image');
-
 hold(handles.('axes2'), 'on');
-for k = idx
-    fi = 1:f-tracks(k).start+1;
-    plot(handles.('axes2'), tracks(k).x(1), tracks(k).y(1), '*', 'Color', cmap(tracks(k).lifetime,:));
-    plot(handles.('axes2'), tracks(k).x(fi), tracks(k).y(fi), '-', 'Color', cmap(tracks(k).lifetime,:));
-end
+% for k = idx
+%     fi = 1:f-tracks(k).start+1;
+%     plot(handles.('axes2'), handles.tracks1(k).x(1), handles.tracks1(k).y(1), '*', 'Color', cmap(tracks(k).lifetime,:));
+%     plot(handles.('axes2'), handles.tracks1(k).x(fi), handles.tracks1(k).y(fi), '-', 'Color', cmap(tracks(k).lifetime,:));
+% end
 if isfield(handles, 'tracks2')
     tracks2 = handles.('tracks2');
-    for k = idx
+    for k = idx2
         fi = 1:f-tracks2(k).start+1;
-        plot(tracks2(k).x(1), tracks2(k).y(1), 'o', 'Color', 'r');
-        plot(tracks2(k).x(fi), tracks2(k).y(fi), '--', 'Color', 'r');
+        plot(handles.axes2, handles.tracks2(k).x(1), handles.tracks2(k).y(1), '*', 'Color', 'b');
+        plot(handles.axes2, handles.tracks2(k).x(fi), handles.tracks2(k).y(fi), '-', 'Color', 'b');
     end
 end
 
@@ -226,25 +217,51 @@ if ~isempty(handles.selectedTrack)
     ci = f-t.start+1;
     if 1 <= ci && ci <= t.lifetime
         plot(handles.('axes2'), t.x(ci), t.y(ci), 'ro', 'MarkerSize', 15);
+        text(t.x(ci), t.y(ci), num2str(handles.selectedTrack), 'Color', [1 0 0], 'Parent', handles.('axes2'));
     end
 end
 
 % show detection COM values
 if get(handles.('checkbox1'), 'Value')
-    detection = handles.('detection');
-    plot(handles.('axes2'), detection(f).xcom, detection(f).ycom, 'x', 'Color', hsv2rgb([120/360 0.5 0.5]));
+    if handles.nCh == 1
+        plot(handles.('axes2'), handles.detection(f).xcom, handles.detection(f).ycom, 'x', 'Color', hsv2rgb([120/360 0.5 0.5]));
+    else
+        plot(handles.('axes1'), handles.detection1(f).xcom, handles.detection1(f).ycom, 'x', 'Color', hsv2rgb([0/360 0.5 0.5]));
+        plot(handles.('axes2'), handles.detection2(f).xcom, handles.detection2(f).ycom, 'x', 'Color', hsv2rgb([0/360 0.5 0.5]));
+    end
 end
 hold(handles.('axes2'), 'off');
-
-% frame indicator
-% ybounds = get(handles.('axes3'), 'YLim');
-% axis(handles.('axes3'), [0 handles.data.movieLength ybounds(1) ybounds(2)]);
-% plot(handles.('axes3'), [f ybounds(1)], [f ybounds(2)], 'k--');
 
 % write zoom level
 set(haxes, 'XLim', XLim);
 set(haxes, 'YLim', YLim);
 guidata(hObject,handles);
+
+
+
+function refreshTrackDisplay(handles)
+
+if ~isempty(handles.selectedTrack)
+
+    h = handles.('axes3');
+    sTrack = handles.tracks(handles.selectedTrack);
+    % frame indicator
+    f = get(handles.('slider1'), 'value');
+    
+    hold(h, 'off');
+   
+    plot(h, sTrack.t, sTrack.A + sTrack.c, 'r');
+    hold(h, 'on');
+    plot(h, sTrack.t, sTrack.c, 'k');
+    plot(h, sTrack.t, sTrack.c + 3*sTrack.cStd, 'k--');
+    
+    ybounds = get(h, 'YLim');
+    plot(h, [f f], ybounds, '--', 'Color', 0.7*[1 1 1]);
+    
+    xlim(h, [0 handles.data.movieLength]);
+    legend(h, 'Amplitude', 'Background');
+end
+
 
 
 
@@ -260,11 +277,13 @@ if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColo
 end
 
 
+
 % --- Executes when figure1 is resized.
 function figure1_ResizeFcn(hObject, eventdata, handles)
 % hObject    handle to figure1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
 
 
 % --- Executes on button press in checkbox1.
@@ -275,7 +294,8 @@ function checkbox1_Callback(hObject, eventdata, handles)
 
 % Hint: get(hObject,'Value') returns toggle state of checkbox1
 
-refreshDisplay(hObject, handles);
+refreshFrameDisplay(hObject, handles);
+
 
 
 % --- Executes on button press in pushbutton1.
@@ -293,8 +313,6 @@ axes(handles.('axes1')); % linked to axes2, selection possible in both
 % mean position of visible tracks
 idx = handles.visibleIdx;
 
-% idx = idx(1:10);
-
 np = length(idx);
 
 mu_x = zeros(1,length(np));
@@ -302,11 +320,10 @@ mu_y = zeros(1,length(np));
 
 f = get(handles.('slider1'), 'value');
 
-tracks = handles.('tracks');
 for k = 1:np
-    fi = 1:f-tracks(idx(k)).start+1; % was tracks(k)
-    mu_x(k) = mean(tracks(idx(k)).x(fi));
-    mu_y(k) = mean(tracks(idx(k)).y(fi));
+    fi = 1:f-handles.tracks1(idx(k)).start+1;
+    mu_x(k) = mean(handles.tracks1(idx(k)).x(fi));
+    mu_y(k) = mean(handles.tracks1(idx(k)).y(fi));
 end
 
 % mu_x = tracks(idx(k)).x(fi);
@@ -316,36 +333,12 @@ end
 d = sqrt((x-mu_x).^2 + (y-mu_y).^2);
 minIdx = d==min(d);
 
-% mu_x(idx)
-% mu_y(idx)
-
 handles.selectedTrack = idx(minIdx);
 guidata(hObject,handles);
-refreshDisplay(hObject, handles);
 
+refreshFrameDisplay(hObject, handles);
+refreshTrackDisplay(handles)
 
-% % plot selected tracks
-% h = handles.('axes2');
-% hold(h, 'on');
-% % plot(h, mu_x, mu_y, 'yo');
-% plot(h, tracks(idx(minIdx)).x(fi), tracks(idx(minIdx)).y(fi), 'ro', 'MarkerSize', 15);
-% hold(h, 'off');
-
-sigma = 1.628;
-h = handles.('axes3');
-hold(h, 'off');
-plot(h, tracks(idx(minIdx)).t, tracks(idx(minIdx)).A + tracks(idx(minIdx)).c, 'r');
-hold(h, 'on');
-plot(h, tracks(idx(minIdx)).t, tracks(idx(minIdx)).I/(2*pi*sigma^2) + tracks(idx(minIdx)).c, 'b--');
-plot(h, tracks(idx(minIdx)).t, tracks(idx(minIdx)).c, 'k');
-plot(h, tracks(idx(minIdx)).t, tracks(idx(minIdx)).c + 3*tracks(idx(minIdx)).cStd, 'k--');
-
-
-% tracks(idx).I
-% tracks(idx).t
-
-xlim(handles.('axes3'), [0 handles.data.movieLength]);
-legend(handles.('axes3'), 'Amplitude', 'Background');
 
 
 % --- Executes on button press in montageButton.
@@ -384,6 +377,7 @@ if ~isempty(handles.selectedTrack)
 end
 
 
+
 % --- Executes on selection change in popupmenu1.
 function popupmenu1_Callback(hObject, eventdata, handles)
 % hObject    handle to popupmenu1 (see GCBO)
@@ -392,6 +386,7 @@ function popupmenu1_Callback(hObject, eventdata, handles)
 
 % Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
 
 
 % --- Executes during object creation, after setting all properties.
