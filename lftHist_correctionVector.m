47f9eb8d73ed91e88d2eec54f7e5bd80819ccea2
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-09-30 21:41:29 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@5244 0bcb941b-74d1-0310-bb26-e6636707f451

--- lftHist_correctionVector.m
+++ lftHist_correctionVector.m
@@ -3,9 +3,8 @@ function weights = lftHist_correctionVector(N)
 % observed in their entirety over the movie duration. This introduces a bias in the
 % lifetime histogram, which can be corrected by a weighting function, given by
 % N/(N-t-1), where N is the movie length.
-% The time vector is defined as t in [-1,0,...,N-2]. The first frame yields no
-% useable events, time t=0 corresponds to the second frame.
+% Single frame events are observable in N-2/N frames, two-frame events in N-3/N frames etc.
+% Events of duration N-1 and N cannot be observed, and are set to zero.
 % 
-% Last modified by Francois Aguet, 05/11/2010
-
-weights = N./(N:-1:1);
+% Last modified by Francois Aguet, 09/30/2010
+weights = [N./(N-2:-1:1) 0 0];
