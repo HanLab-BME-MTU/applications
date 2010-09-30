9782016c1158a2ad210786981ce8e8789dac79f6
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-09-30 22:21:27 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@5245 0bcb941b-74d1-0310-bb26-e6636707f451

--- lftHist_correctionVector.m
+++ lftHist_correctionVector.m
@@ -1,10 +1,10 @@
 function weights = lftHist_correctionVector(N)
 % Weighting vector for lifetime histogram. Longer lifetimes are less likely to be 
 % observed in their entirety over the movie duration. This introduces a bias in the
-% lifetime histogram, which can be corrected by a weighting function, given by
-% N/(N-t-1), where N is the movie length.
-% Single frame events are observable in N-2/N frames, two-frame events in N-3/N frames etc.
-% Events of duration N-1 and N cannot be observed, and are set to zero.
+% lifetime histogram, which can be corrected by the weighting function N/(N-t-1),
+% where N is the longest observable lifetime, i.e., the lifetime histogram extends
+% from 1 to MovieLength-2 frames.
+% Single frame events are thus observable in all N frames, two-frame events in N-1/N frames etc.
 % 
 % Last modified by Francois Aguet, 09/30/2010
-weights = [N./(N-2:-1:1) 0 0];
+weights = N./(N:-1:1);
