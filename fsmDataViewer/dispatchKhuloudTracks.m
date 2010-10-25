6aebdfe23f0297a042e8b2c68e0a9afa736d66e2
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2010-10-25 18:27:38 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@5324 0bcb941b-74d1-0310-bb26-e6636707f451

--- fsmDataViewer/dispatchKhuloudTracks.m
+++ fsmDataViewer/dispatchKhuloudTracks.m
@@ -10,9 +10,15 @@ if ~exist('tracksFinal', 'var')
     error('Unable to find track feature info.');
 end
 
-trackSEL = getTrackSEL(tracksFinal);
+trackSEL = getTrackSEL(tracksFinal); %#ok<NODEF>
 
 dataLayer = cell(nFrames,1);
+
+% Find the non-compound tracks
+isCompoundTrack = cellfun(@(x) size(x,1) ~= 1, {tracksFinal(:).tracksCoordAmpCG})';
+
+tracksFinal = tracksFinal(~isCompoundTrack);
+trackSEL = trackSEL(~isCompoundTrack,:);
 
 for iFrame = 1:nFrames
     % Find which tracks live in iFrame
@@ -21,15 +27,17 @@ for iFrame = 1:nFrames
     tracks = cell(numel(inFrameInd),1);
     
     indX = 8*(iFrame-trackSEL(inFrameInd,1)) + 1;
-    indY = 8*(iFrame-trackSEL(inFrameInd,1)) + 2;
+    indY = indX+1;
     
     for iiTrack = 1:numel(inFrameInd)
         iTrack = inFrameInd(iiTrack);
        
         x = tracksFinal(iTrack).tracksCoordAmpCG(1:8:indX(iiTrack));
         y = tracksFinal(iTrack).tracksCoordAmpCG(2:8:indY(iiTrack));
+
+        isValid = ~isnan(x);
         
-        tracks{iiTrack} = [x', y'];
+        tracks{iiTrack} = [x(isValid)', y(isValid)'];
     end
     
     dataLayer{iFrame} = tracks;
--- fsmDataViewer/displayKhuloudTracks.m
+++ fsmDataViewer/displayKhuloudTracks.m
@@ -4,7 +4,9 @@ nTracks = numel(tracks);
 
 for iTrack = 1:nTracks
     pts = tracks{iTrack};
-    isValid = ~isnan(pts(:,1));
-    line(pts(isValid,1), pts(isValid,2), 'Marker', 'none', 'Color', layerColor, 'Parent', hAxes, 'Tag', tag);
+    if any(pts <= 1)
+        pts
+    end
+    line(pts(:,1), pts(:,2), 'Marker', 'none', 'Color', layerColor, 'Parent', hAxes, 'Tag', tag);
     line(pts(end,1),pts(end,2),'Marker','.','Color', layerColor, 'Parent', hAxes, 'Tag', tag);
 end
