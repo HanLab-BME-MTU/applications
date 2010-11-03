fb87eb565af6b155b8d2b652c49ce242d234bcc2
sb234 <sb234@0bcb941b-74d1-0310-bb26-e6636707f451>
2010-11-03 17:22:08 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/vinculinFA/main@5363 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ runBatchProcessVinculinMovies.m
@@ -0,0 +1,42 @@
+function runBatchProcessVinculinMovies
+
+% parent directory of every movie to be analyzed
+params.rootDirectory = '/home/sb234/Projects/VinculinFA/completed/';
+
+% name of the channel directory subfolders
+params.channelDirectory = {'ch488', 'ch560'};
+
+% name of the processes to be run
+params.procNames = {...
+    'particleDetection',...
+    'particleTracking',...
+    'pairTracks'};
+params.runSteps = [1 -1 -1];
+params.batchMode = 1;
+
+% Physical parameters
+params.pixelSize = 67; %pixel size in nanometers
+params.timeInterval = 10;
+% N.A, magnification, camera cell size (meter), emission wavelength (meter)
+sigmaPSF = getGaussianPSFsigma(1.45,100, 6.7 * 1e-6, 509 * 1e-9);
+
+% INIT
+params.setupMovieDataFunc = @setupViculinMovieData;
+
+% PROC 1: particle detection
+params.particleDetection.iChannel = 1;
+params.particleDetection.detectFunc = @detectPaxilinParticles;
+params.particleDetection.sigmaPSF = sigmaPSF;
+params.particleDetection.kSigma = 2;
+
+% PROC 2: particle tracking
+params.particleTracking = struct([]); % no parameter
+
+% PROC 3: vimentin track pairing
+params.pairTracks.iChannel = 1;
+params.pairTracks.sigmaPSF = sigmaPSF;
+params.pairTracks.thetaTh = pi/16;
+params.pairTracks.alpha = 0.05;
+
+% Run all processes
+batchProcessMyMovies(rootDirectory,params);
---
+++ setupViculinMovieData.m
@@ -0,0 +1,47 @@
+function movieData = setupViculinMovieData(path)
+
+% Analysis directory
+movieData.analysisDirectory = fullfile(path, 'ch488', 'analysis');
+
+% Image directory
+movieData.imageDirectory = path;
+movieData.channelDirectory = cellfun(@(x) fullfile(x, 'roi'), {'ch488', 'ch560'}, 'UniformOutput', false);
+
+% Get the number of images
+nImages = cellfun(@(channelPath) numel(dir([movieData.imageDirectory filesep channelPath filesep '*.tif'])), movieData.channelDirectory);
+assert(all(nImages(:) == nImages(1)));
+movieData.nImages = nImages(1);
+
+% Load physical parameter from
+filename = fullfile(movieData.imageDirectory, 'ch560', 'analysis', 'fsmPhysiParam.mat');
+
+if exist(filename, 'file')
+    load(filename);
+    
+    if fsmPhysiParam.pixelSize ~= params.pixelSize
+        errMsg = MException(['pixel size in qFSM differs from value in '...
+            'batch params (SKIPPING).']);
+        throw(errMsg);
+    end
+    
+    if fsmPhysiParam.frameInterval ~= params.timeInterval
+        errMsg = MException(['time interval in qFSM differs from value '...
+            'in batch params (SKIPPING).']);
+        throw(errMsg);
+    end
+    
+    clear fsmPhysiParam;
+end
+
+movieData.pixelSize_nm = params.pixelSize;
+movieData.timeInterval_s = params.timeInterval;
+
+% Get the mask directory
+movieData.masks.channelDirectory = {''};
+movieData.masks.directory = fullfile(movieData.imageDirectory, 'ch560', 'analysis', 'edge', 'cell_mask');
+if exist(movieData.masks.directory, 'dir')
+    movieData.masks.n = numel(dir([movieData.masks.directory filesep '*.tif']));
+    movieData.masks.status = 1;
+else
+    movieData.masks.status = 0;
+end
