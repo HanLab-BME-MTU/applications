12c1f29f488557bdaaf45d9563f20d001da3cb00
sb234 <sb234@0bcb941b-74d1-0310-bb26-e6636707f451>
2010-11-05 15:51:13 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/vinculinFA/main@5373 0bcb941b-74d1-0310-bb26-e6636707f451

--- detectFocalAdhesionParticles.m
+++ detectFocalAdhesionParticles.m
@@ -8,8 +8,8 @@ bandPassIso = filterLoG(ima,sigmaPSF);
 bandPassIso(~mask) = 0;
 
 % Filter image with ridge detector
-[bandPassAniso, theta, nms] = steerableFiltering(ima,2,sigmaPSF);
-bandPassAniso(~mask) = 0;
+[bandPassAni, theta, nms] = steerableFiltering(ima,2,sigmaPSF);
+bandPassAni(~mask) = 0;
 nms(~mask) = 0;
 
 % Filter image with a Gaussian filter with large support to compute bkg
@@ -17,44 +17,61 @@ nms(~mask) = 0;
 lowPass = Gauss2D(ima,4 * sigmaPSF);
 lowPass(~mask) = 0;
 
-% Detection of isolated particles
-hside1 = 2 * ceil(3 * sigmaPSF) + 1;
-hside2 = 2 * ceil(sigmaPSF) + 1;
-locMax = locmax2d(bandPassIso, [hside1 hside1]);
-% Remove local maxima lying on ridges since these locations will be
-% evaluated later anyways.
-locMax(nms ~= 0) = 0;
-% Get local minima from the low pass filtering image
-locMin = locmin2d(lowPass, [hside2 hside2]);
+% test
+locMaxIso = locmax2d(bandPassIso, [5 5]);
+locMaxAni = locmax2d(nms, [5 5]);
+ind = find(locMaxIso ~= 0 | locMaxAni ~= 0);
+[y x] = ind2sub(size(ima), ind);
+amp = ima(ind);
 
-indMin = find(locMin);
-indMax = find(locMax);
-[yMin xMin] = ind2sub(size(locMin), indMin);
-[yMax xMax] = ind2sub(size(locMax), indMax);
-dt = DelaunayTri(xMin,yMin);
-triangles = pointLocation(dt,[xMax, yMax]);
-nnzIdx = ~isnan(triangles);
-tri = dt(triangles(nnzIdx),:);
+% imshow(ima,[]); hold on;
+% [y x] = ind2sub(size(ima), indIso);
+% plot(x,y,'g.');
+% [y x] = ind2sub(size(ima), indAni);
+% plot(x,y,'b.');
+% [y x] = ind2sub(size(ima), indBoth);
+% plot(x,y,'r.');
 
-% Compute the adaptative mean
-avgBkg = inf(size(indMax));
-avgBkg(nnzIdx) = mean(reshape(lowPass(indMin(tri(:))), size(tri)),2);
 
-% Compute the adaptive standard deviation (over 27 points)
-stdBkg = inf(size(indMax));
 
-ind3x3 = bsxfun(@plus,[-nrows 0 nrows],(-1:1)');
-ind3x3b1 = bsxfun(@plus,indMin(tri(:,1)),ind3x3(:)');
-ind3x3b2 = bsxfun(@plus,indMin(tri(:,2)),ind3x3(:)');
-ind3x3b3 = bsxfun(@plus,indMin(tri(:,3)),ind3x3(:)');
-ind3x3b = [ind3x3b1, ind3x3b2, ind3x3b3];
-
-stdBkg(nnzIdx) = std(ima(ind3x3b),[],2);
-
-finalIndMax = indMax(ima(indMax) > avgBkg + kSigma * stdBkg);
-
-[y x] = ind2sub(size(ima), finalIndMax);
-
-amp = ima(finalIndMax);
-
-% Detection of particles on ridges
+% % Detection of isolated particles
+% hside1 = 2 * ceil(3 * sigmaPSF) + 1;
+% hside2 = 2 * ceil(sigmaPSF) + 1;
+% locMax = locmax2d(bandPassIso, [hside1 hside1]);
+% % Remove local maxima lying on ridges since these locations will be
+% % evaluated later anyways.
+% locMax(nms ~= 0) = 0;
+% % Get local minima from the low pass filtering image
+% locMin = locmin2d(lowPass, [hside2 hside2]);
+% 
+% indMin = find(locMin);
+% indMax = find(locMax);
+% [yMin xMin] = ind2sub(size(locMin), indMin);
+% [yMax xMax] = ind2sub(size(locMax), indMax);
+% dt = DelaunayTri(xMin,yMin);
+% triangles = pointLocation(dt,[xMax, yMax]);
+% nnzIdx = ~isnan(triangles);
+% tri = dt(triangles(nnzIdx),:);
+% 
+% % Compute the adaptative mean
+% avgBkg = inf(size(indMax));
+% avgBkg(nnzIdx) = mean(reshape(lowPass(indMin(tri(:))), size(tri)),2);
+% 
+% % Compute the adaptive standard deviation (over 27 points)
+% stdBkg = inf(size(indMax));
+% 
+% ind3x3 = bsxfun(@plus,[-nrows 0 nrows],(-1:1)');
+% ind3x3b1 = bsxfun(@plus,indMin(tri(:,1)),ind3x3(:)');
+% ind3x3b2 = bsxfun(@plus,indMin(tri(:,2)),ind3x3(:)');
+% ind3x3b3 = bsxfun(@plus,indMin(tri(:,3)),ind3x3(:)');
+% ind3x3b = [ind3x3b1, ind3x3b2, ind3x3b3];
+% 
+% stdBkg(nnzIdx) = std(ima(ind3x3b),[],2);
+% 
+% finalIndMax = indMax(ima(indMax) > avgBkg + kSigma * stdBkg);
+% 
+% [y x] = ind2sub(size(ima), finalIndMax);
+% 
+% amp = ima(finalIndMax);
+% 
+% % Detection of particles on ridges
--- runBatchProcessVinculinMovies.m
+++ runBatchProcessVinculinMovies.m
@@ -1,7 +1,8 @@
 function runBatchProcessVinculinMovies
 
 % parent directory of every movie to be analyzed
-params.rootDirectory = '/home/sb234/Projects/VinculinFA/completed/';
+%params.rootDirectory = '/home/sb234/Projects/VinculinFA/completed/';
+params.rootDirectory = '/Users/sylvain/Documents/Work/HMS/Projects/VinculinFA/052710_con_CSUX_2';
 
 % name of the channel directory subfolders
 params.channelDirectory = {'ch488', 'ch560'};
@@ -11,7 +12,7 @@ params.procNames = {...
     'particleDetection',...
     'particleTracking',...
     'pairTracks'};
-params.runSteps = [1 1 1];
+params.runSteps = [0 1 -1];
 params.batchMode = 1;
 
 % Physical parameters
@@ -30,7 +31,7 @@ params.particleDetection.sigmaPSF = sigmaPSF;
 params.particleDetection.kSigma = 2;
 
 % PROC 2: particle tracking
-params.particleTracking = struct(); % no parameter
+params.particleTracking.searchRadius = 5;
 
 % PROC 3: vimentin track pairing
 params.pairTracks.iChannel = 1;
