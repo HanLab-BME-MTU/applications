cf80b960bb494825cc1fbf93f949c95684c4d9f4
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-11-08 22:27:28 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@5380 0bcb941b-74d1-0310-bb26-e6636707f451

--- lifetimeCompactFitData.m
+++ lifetimeCompactFitData.m
@@ -12,29 +12,28 @@ function [compactRes, data] = lifetimeCompactFitData(data, restrict, shape)
 %           present
 %
 % Dinah Loerke, last modified Mar 13, 2008
+% Francois Aguet, last modified Jun 8 2010
 
 data = fillStructLifetimeHist(data);
 
 % combine lifetime histograms of the slow and fast movies separately
 res = stageFitLifetimes(data);
 
-mer_E2E = mergeFastSlowHistogramsPlat_E2E(res, restrict, shape);
-%mer_E2E = mergeHistogramsFitWeibull(res, restrict, shape);
+if length(unique([data.framerate])) == 1
+    mer_E2E = fitWeibullToHist(res, restrict, shape);
+else
+    mer_E2E = mergeHistogramsFitWeibull(res, restrict, shape);
+end
 
 % fit results
 resmat1 = mer_E2E(1).compactFitRes;
 
 % determine cell-to-cell error with jackknife
-for s = 1:length(mer_E2E)
-    E2Emat(:,:,s) = mer_E2E(s).compactFitRes;
-end
-[mx,my,mz] = size(E2Emat);
-for inx=1:mx
-    for iny=1:my       
-        ns = length(E2Emat(inx,iny,:));
-        E2Eval(inx,iny) = sqrt(((ns-1)/ns)*sum((E2Emat(inx,iny,:)-nanmean(E2Emat(inx,iny,:))).^2));
-    end
-end
+ns = length(mer_E2E);
+E2Emat = cat(3, mer_E2E.compactFitRes);
+E2Eval = sqrt((ns-1)/ns*sum((E2Emat - repmat(nanmean(E2Emat, 3), [1 1 ns])).^2, 3));
+
+
 
 % enter output results
 compactRes.numcells = length(data);
@@ -45,13 +44,10 @@ compactRes.tau = resmat1(:,2);
 compactRes.tauError = round(100*E2Eval(:,2))/100;
 compactRes.tau50 = resmat1(:,3);
 
-data = determinePitDensities(data);
-for i = 1:length(data)
-    Dens(i) = nanmean(data(i).pitDensity);
-end
-densAV = nanmean(Dens);
-densSTD = nanstd(Dens);
+Dens = arrayfun(@(x) nanmean(x.pitDensity), data);
 
+compactRes.contr;
+compactRes.tau;
 
 matrix(:,1) = compactRes.contr;
 matrix(:,2) = compactRes.contrError;
@@ -61,7 +57,7 @@ matrix(:,5) = compactRes.tau50;
 
 matrix(1,6) = compactRes.numcells;
 matrix(2,6) = compactRes.numtraj;
-matrix(3,6) = 1000*densAV;
-matrix(4,6) = 1000*densSTD;
+matrix(3,6) = 1000*nanmean(Dens);
+matrix(4,6) = 1000*nanstd(Dens);
 
 compactRes.matrix = matrix;
