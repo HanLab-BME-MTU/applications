ec65d41a9dd7493011146c9c7579624e2e4aebc2
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-12-02 22:18:15 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@5488 0bcb941b-74d1-0310-bb26-e6636707f451

--- runTrackAnalysis.m
+++ runTrackAnalysis.m
@@ -1,26 +1,39 @@
 % runTrackAnalysis
 %
 % INPUT     data        : array of experiment structures
-%           {overwrite} : '1' to overwrite previous results
+%           {buffer}    : length of buffer before and after tracks
+%           {overwrite} : 1 to overwrite previous results
+%
+% Usage example: runTrackAnalysis(data, 'buffer', 10);
 
 % Francois Aguet, November 2010
 
-function runTrackAnalysis(data, overwrite, buffer)
+function runTrackAnalysis(data, varargin)
 
-if nargin<2 || isempty(overwrite)
-    overwrite = 0;
-end
-if nargin < 3
-    buffer = [];
-end
+% defaults:
+buffer = [];
+overwrite = 0;
 
+nv = length(varargin);
+if mod(nv,2)~=0
+    error('Incorrect format for optional inputs.');
+end
+for k = 1:nv/2
+    switch lower(varargin{2*k-1})
+        case 'buffer'
+            buffer = varargin{2*k};
+        case 'overwrite'
+            overwrite = 1;
+        otherwise
+            error('Unrecognized option');
+    end
+end
 
 nExp = length(data);
 parfor i = 1:nExp
-    
     if exist([data(i).source filesep 'Tracking' filesep 'trackAnalysis.mat'],'file') ~= 2 || overwrite
         trackAnalysis(data(i), buffer, []);
-    else 
-        display(['skipping movie ' num2str(i)])
+    else
+        fprintf('TrackAnalysis: movie %d has already been analyzed.\n', i);
     end
 end
