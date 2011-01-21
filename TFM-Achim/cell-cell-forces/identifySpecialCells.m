fe6d6a4820752b4ad72f09485d37f21db6cf28ac
Achim Besser <achim.besser@hms.harvard.edu>
2011-01-21 16:13:04 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/TFM/main@5600 0bcb941b-74d1-0310-bb26-e6636707f451

--- TFM-Achim/cell-cell-forces/identifySpecialCells.m
+++ TFM-Achim/cell-cell-forces/identifySpecialCells.m
@@ -32,23 +32,53 @@ end
 
 for frame=toDoList
     I = double(imread(imageFileList{frame}));
-    meanI=mean(I(:));
-    for cellID=1:length(constrForceField{frame}.cell)
+
+    % first determine the average intensity within the cells
+    numCells=length(constrForceField{frame}.cell);
+    for cellID=1:numCells
         % integrate the intesity within the cell:
-        iVal_raw=sum(sum(I(constrForceField{frame}.cell{cellID}.innerMask)));
-        
-        % substract the average image intensity weighted with the cell
-        % area:
-        iVal(frame,cellID)=iVal_raw-meanI*sum(constrForceField{frame}.cell{cellID}.innerMask(:));
-    end    
+        iVal_raw(cellID) =sum(sum(I(constrForceField{frame}.cell{cellID}.innerMask)));
+        cell_area(cellID)=sum(constrForceField{frame}.cell{cellID}.innerMask(:));
+    end
+    % integrate the intensity outside the cell:
+    iVal_bg =sum(sum(I(~constrForceField{frame}.segmRes.mask)));
+    area_bg =sum(~constrForceField{frame}.segmRes.mask(:));
+    
+    % 1) The most simple, and most of the time, the most effective one:
+    %meanI=mean(I(:));
+    
+    % 2) The bg in the image (wo cells), this one doesn't perform well:
+    %meanI=iVal_bg/area_bg;
+    
+    % 3) The bg in the cells:
+    meanI=sum(iVal_raw)/sum(cell_area);
+    
+    % Calculate the intensity value:
+    iVal(frame,1:numCells)=iVal_raw-meanI*cell_area;
+    
+%     meanI=mean(I(:));
+%     for cellID=1:length(constrForceField{frame}.cell)
+%         % integrate the intesity within the cell:
+%         iVal_raw=sum(sum(I(constrForceField{frame}.cell{cellID}.innerMask)));
+%         
+%         % substract the average image intensity weighted with the cell
+%         % area:
+%         iVal(frame,cellID)=iVal_raw-meanI*sum(constrForceField{frame}.cell{cellID}.innerMask(:));
+%     end
+    clear cell_area iVal_raw     
 end
 
 % -------------------------------------------------------------------------
 % Old code:
 % -------------------------------------------------------------------------
 
-% Normaliz the distribution, that all fall into [0,1]:
-iVal=iVal/max(iVal(:));
+% remove the ones that are eactly 0 (These are actually empty entries,
+% since the field got extended when a new cell entered the cluster):
+iVal(iVal==0)=NaN;
+
+
+% Normalize the distribution, that all fall into [0,1]:
+iVal=(iVal-min(iVal(:)))/(max(iVal(:))-min(iVal(:)));
 
 % sort the intensity values, this should give (in the case of n cells)
 % n-seperated distributions:
@@ -62,16 +92,21 @@ hist(iVal_sorted,linspace(0,1,101));
 xlim([-0.05 1.05])
 
 % ask the user to put in an appropriate threshold:
-threshold=input('Set the threshold value: ');
+threshold=input(['Set the threshold value (',num2str(length(toDoList)),'frames): ']);
 
 % store the values in the cell structure:
+numSpec=0;
+numNorm=0;
 for frame=toDoList
     for cellID=1:length(constrForceField{frame}.cell)
         if iVal(frame,cellID)>threshold
             constrForceField{frame}.cell{cellID}.stats.spec=1;
+            numSpec=numSpec+1;
         else
             constrForceField{frame}.cell{cellID}.stats.spec=0;
+            numNorm=numNorm+1;
         end
     end    
 end
+display(['normal cells: ',num2str(numNorm),';   myosin cells: ',num2str(numSpec)]);
 
