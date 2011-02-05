b371a78695f57c8e28f27b3b72f42e0ddc5feb14
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-02-05 18:29:55 +0000

    This now uses the mex implementation for Gaussian fitting. Localization is applied to all channels (slave channels: amplitude fit only).
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@5681 0bcb941b-74d1-0310-bb26-e6636707f451

--- psfLocalization.m
+++ psfLocalization.m
@@ -14,68 +14,113 @@ if nargin<2
     overwrite = 0;
 end
 
-sigma = getGaussianPSFsigma(data.NA, data.M, data.pixelSize, data.markers{1});
+h = load([data.source 'Detection' filesep 'detectionResults.mat']);
+frameInfo = h.frameInfo;
 
-
-load([data.source 'Detection' filesep 'detectionResults.mat']);
 
 if ~isfield(frameInfo, 'xloc') || overwrite
+
+    sigma = getGaussianPSFsigma(data.NA, data.M, data.pixelSize, data.markers{1});
+    w = ceil(3*sigma);
     
+    %=================================
+    % Identify master/slave channels
+    %=================================
+    nCh = length(data.channels);
+    masterChannel = regexp(data.source, data.channels);
+    masterChannel = find([masterChannel{:}]);
+    slaveChannels = setdiff(1:nCh, masterChannel);
+
     ny = data.imagesize(1);
     nx = data.imagesize(2);
     
-    frameList = dir([data.source '*.tif*']);
+    frameList = cell(1,nCh);
+    for k = 1:nCh
+        frameList{k} = dir([data.channels{k} '*.tif*']);
+    end
+    
     maskPath = [data.source 'Detection' filesep 'Masks' filesep];
     maskList = dir([maskPath '*.tif']);
-    
-    w = ceil(3*sigma);
-    
+        
     % loop through frames
+    frames = zeros(ny,nx,nCh);
     fprintf('Progress:     ');
     for k = 1:data.movieLength
         
-        frame = double(imread([data.source frameList(k).name]));
-        mask = double(imread([maskPath maskList(k).name]));
-        
         xi = round(frameInfo(k).xcom);
         yi = round(frameInfo(k).ycom);
-        
         np = length(xi);
-        xloc = zeros(1,np);
-        yloc = zeros(1,np);
-        A = zeros(1,np);
-        cVect = zeros(1,np);
-        cStd = zeros(1,np);
+
+        xloc = NaN(1,np);
+        yloc = NaN(1,np);
+        A = NaN(nCh,np);
+        c = NaN(nCh,np);
+        cStd = NaN(nCh,np);
+        
+        xStd = NaN(1,np);
+        yStd = NaN(1,np);
+        aStd = NaN(nCh,np);
+        
+        hval = NaN(1,np);
+        pval = NaN(1,np);
+        
+        mask = double(imread([maskPath maskList(k).name]));
+
+        for ch = 1:nCh
+            frames(:,:,ch) = double(imread([data.channels{ch} frameList{ch}(k).name]));
+        end
+        
         
         for p = 1:np
             % detections within frame bounds
-            if xi(p)<=w || xi(p)>nx-w || yi(p)<=w || yi(p)>ny-w
-                xloc(p) = NaN;
-                yloc(p) = NaN;
-                A(p) = NaN;
-                cVect(p) = NaN;
-            else
-                window = frame(yi(p)-w:yi(p)+w, xi(p)-w:xi(p)+w);
+            if (xi(p)>w && xi(p)<=nx-w && yi(p)>w && yi(p)<=ny-w)
+                ch = masterChannel;
+                window = frames(yi(p)-w:yi(p)+w, xi(p)-w:xi(p)+w, ch);
                 
                 % binary mask
                 maskWindow = ~mask(yi(p)-w:yi(p)+w, xi(p)-w:xi(p)+w);
                 maskWindow(maskWindow~=0) = 1;
                 % background estimate
-                c = mean(window(maskWindow));
-                prm = fitGaussian2D(window, [0 0 max(window(:))-c sigma c], 'xyA');
-                xloc(p) = xi(p) + prm(1);
-                yloc(p) = yi(p) + prm(2);
-                A(p) = prm(3);
-                cVect(p) = c;
-                cStd(p) = std(window(maskWindow));
+                ci = mean(window(maskWindow));
+                [prm, prmStd, ~, res] = fitGaussian2D(window, [0 0 max(window(:))-ci sigma ci], 'xyA');
+                pval(p) = res.pval;
+                xp = prm(1);
+                yp = prm(2);
+                if (xp > -w && xp < w && yp > -w && yp < w)
+                    xloc(p) = xi(p) + xp;
+                    yloc(p) = yi(p) + yp;
+                    A(ch,p) = prm(3);
+                    xStd(1,p) = prmStd(1);
+                    yStd(1,p) = prmStd(2);
+                    aStd(ch,p) = prmStd(3);
+                    c(ch,p) = ci;
+                    cStd(ch,p) = std(window(maskWindow));
+                    for ch = slaveChannels
+                        window = frames(yi(p)-w:yi(p)+w, xi(p)-w:xi(p)+w, ch);
+                        ci = mean(window(maskWindow));
+                        [prm prmStd] = fitGaussian2D(window, [xp yp max(window(:))-ci sigma ci], 'A');
+                        A(ch,p) = prm(3);
+                        aStd(ch,p) = prmStd(1);
+                        c(ch,p) = ci;
+                        cStd(ch,p) = std(window(maskWindow));
+                    end
+                end
+                
             end
         end
         frameInfo(k).xloc = xloc;
         frameInfo(k).yloc = yloc;
         frameInfo(k).A = A;
+        frameInfo(k).x_std = xStd;
+        frameInfo(k).y_std = yStd;
+        frameInfo(k).A_std = aStd;
         frameInfo(k).sigma = sigma;
-        frameInfo(k).c = cVect;
+        frameInfo(k).c = c;
         frameInfo(k).cStd = cStd;
+        frameInfo(k).pval_KS = pval;
+        valid = zeros(1,np);
+        valid(pval>=0.05) = 1;
+        frameInfo(k).valid = valid;
         fprintf('\b\b\b\b%3d%%', round(100*k/(data.movieLength)));
     end
     fprintf('\n');
