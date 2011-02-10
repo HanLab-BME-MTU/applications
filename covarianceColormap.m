a9b6bfe2843b3330ef24768e5e77826fc673edec
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-02-10 22:37:20 +0000

    Now correctly displays correlation between parameters.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@5721 0bcb941b-74d1-0310-bb26-e6636707f451

--- covarianceColormap.m
+++ covarianceColormap.m
@@ -1,15 +1,28 @@
 % Francois Aguet, October 2010
 
-function covarianceColormap(M)
+function covarianceColormap(M, paramNameList)
 
-figure; imagesc(M); colormap(getMap()); axis image; colorbar; caxis([-1 1]);
-xlabel('Rate constant index', 'FontName', 'Helvetica', 'FontSize', 14)
-ylabel('Rate constant index', 'FontName', 'Helvetica', 'FontSize', 14)
+figure; imagesc(M); colormap(getMap()); axis image; caxis([-1 1]);
+colorbar('YTick', -1:0.2:1);
+
+np = size(M,1);
+ta = 1:np;
+set(gca, 'XTick', ta, 'YTick', ta, 'FontName', 'Helvetica', 'FontSize', 14);
+if nargin>1 && length(paramNameList)==np
+    set(gca, 'XTickLabel', paramNameList, 'YTickLabel', paramNameList);
+else
+    xlabel('Parameter index', 'FontName', 'Helvetica', 'FontSize', 14)
+    ylabel('Parameter index', 'FontName', 'Helvetica', 'FontSize', 14)
+end
+
+M(eye(np)==1) = 0;
+[x,y] = ind2sub(size(M), find(abs(M)>=0.8));
+hold on;
+plot(x,y, 'xy', 'LineWidth', 3, 'MarkerSize', 20);
 
 
 
 function map = getMap()
-% values = sort(values);
 values = -1:1/100:1;
 N = length(values);
 map = zeros(N,3);
