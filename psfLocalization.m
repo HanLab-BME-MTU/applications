8a72342979abe49cf4a626559ffe62c9d3fb6eff
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-03-10 17:03:13 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@5831 0bcb941b-74d1-0310-bb26-e6636707f451

--- psfLocalization.m
+++ psfLocalization.m
@@ -7,7 +7,7 @@
 %           {overwrite} : 
 
 % Francois Aguet, September 2010
-% Last modified: February 23, 2011
+% Last modified: March 6, 2011
 
 function psfLocalization(data, overwrite)
 
@@ -20,8 +20,28 @@ frameInfo = h.frameInfo;
 
 
 if ~isfield(frameInfo, 'xloc') || overwrite
-
-    sigma = getGaussianPSFsigma(data.NA, data.M, data.pixelSize, data.markers{1});
+    
+    ny = data.imagesize(1);
+    nx = data.imagesize(2);
+    
+    %=================================
+    % Identify master/slave channels
+    %=================================
+    nCh = length(data.channels);
+    masterChannel = find(strcmp(data.source, data.channels));
+    slaveChannels = setdiff(1:nCh, masterChannel);
+    
+    sigmaV = zeros(nCh, 1);
+    frameList = cell(1,nCh);
+    for k = 1:nCh
+        sigmaV(k) = getGaussianPSFsigma(data.NA, data.M, data.pixelSize, data.markers{k});
+        frameList{k} = dir([data.channels{k} '*.tif*']);
+    end
+    
+    maskPath = [data.source 'Detection' filesep 'Masks' filesep];
+    maskList = dir([maskPath '*.tif']);
+    
+    sigma = sigmaV(masterChannel);
     w2 = ceil(2*sigma);
     w3 = ceil(3*sigma);
     w4 = ceil(4*sigma);
@@ -36,25 +56,7 @@ if ~isfield(frameInfo, 'xloc') || overwrite
     r = sqrt(x.^2+y.^2);
     diskMask = zeros(size(r));
     diskMask(r<=w3) = 1;
-    
-    %=================================
-    % Identify master/slave channels
-    %=================================
-    nCh = length(data.channels);
-    masterChannel = find(strcmp(data.source, data.channels));
-    slaveChannels = setdiff(1:nCh, masterChannel);
-
-    ny = data.imagesize(1);
-    nx = data.imagesize(2);
-    
-    frameList = cell(1,nCh);
-    for k = 1:nCh
-        frameList{k} = dir([data.channels{k} '*.tif*']);
-    end
-    
-    maskPath = [data.source 'Detection' filesep 'Masks' filesep];
-    maskList = dir([maskPath '*.tif']);
-
+   
     % indexes for cross-correlation coefficients
     n = 4;
     idx = pcombs(1:n);
@@ -63,7 +65,6 @@ if ~isfield(frameInfo, 'xloc') || overwrite
     ij = i+n*(j-1);
     ii = i+n*(i-1);
     jj = j+n*(j-1);
-    
     
     % loop through frames
     iRange = zeros(nCh,2);
@@ -81,10 +82,12 @@ if ~isfield(frameInfo, 'xloc') || overwrite
         c = NaN(nCh,np);
         cStd = NaN(nCh,np);
         cStd_mask = NaN(nCh,np);
+        cStd_res = NaN(nCh,np);
         
         xStd = NaN(1,np);
         yStd = NaN(1,np);
         aStd = NaN(nCh,np);
+        pval_A = NaN(nCh, np);
         
         pval = NaN(1,np);
         
@@ -126,6 +129,7 @@ if ~isfield(frameInfo, 'xloc') || overwrite
                 
                 % mask w/ 3*sigma disk
                 window(diskMask==0) = NaN;
+                npx = sum(isfinite(window(:)));
                 
                 % fit
                 [prm, prmStd, C, res] = fitGaussian2D(window, [0 0 max(window(:))-ci sigma ci], 'xyAc');
@@ -142,22 +146,27 @@ if ~isfield(frameInfo, 'xloc') || overwrite
                     xStd(1,p) = prmStd(1);
                     yStd(1,p) = prmStd(2);
                     aStd(ch,p) = prmStd(3);
+                    pval_A(ch,p) = 1-tcdf(prm(3)/prmStd(3), npx - 5);
                     c(ch,p) = prm(5);
                     cStd(ch,p) = prmStd(4);
                     cStd_mask(ch,p) = bgStd;
+                    cStd_res(ch,p) = res.std;
                     for ch = slaveChannels
                         window = frames(yi(p)-w4:yi(p)+w4, xi(p)-w4:xi(p)+w4, ch);
                         ci = mean(window(cmask==1));
-                        
                         window(maskWindow~=0) = NaN;
+                        bgStd = nanstd(window(bandMask==1));
                         window = window(dw+1:end-dw, dw+1:end-dw);
                         window(diskMask==0) = NaN;
 
-                        [prm prmStd] = fitGaussian2D(window, [xp yp max(window(:))-ci sigma ci], 'Ac');
+                        [prm, prmStd, ~, res] = fitGaussian2D(window, [xp yp max(window(:))-ci sigmaV(ch) ci], 'Ac');
                         A(ch,p) = prm(3);
                         aStd(ch,p) = prmStd(1);
+                        pval_A(ch,p) = 1-tcdf(prm(3)/prmStd(1), npx - 3);
                         c(ch,p) = prm(5);
-                        cStd(ch,p) = prmStd(2);%std(window(cmask==1));
+                        cStd(ch,p) = prmStd(2);
+                        cStd_mask(ch,p) = bgStd;
+                        cStd_res(ch,p) = res.std;
                     end
                 end
                 
@@ -171,22 +180,22 @@ if ~isfield(frameInfo, 'xloc') || overwrite
         frameInfo(k).y_std = yStd;
         frameInfo(k).A = A;
         frameInfo(k).A_std = aStd;
-        frameInfo(k).sigma = sigma;
+        frameInfo(k).pval_A = pval_A;
+        frameInfo(k).sigma = sigmaV;
         frameInfo(k).c = c;
         frameInfo(k).cStd = cStd;
         frameInfo(k).cStd_mask = cStd_mask;
+        frameInfo(k).cStd_res = cStd_res;
 
         % test results
         frameInfo(k).pval_KS = pval;
         frameInfo(k).valid_KS = pval>=0.05;
         frameInfo(k).iRange = iRange;
         
-        
-        % use localization results in tracker
+        % to use localization results in tracker
         %frameInfo(k).amp = zeros(np,2);
         %frameInfo(k).xCoord = zeros(np,2);
         %frameInfo(k).yCoord = zeros(np,2);
-        
         %frameInfo(k).amp(:,1) = frameInfo(k).A(masterChannel,:);
         %frameInfo(k).xCoord(:,1) = frameInfo(k).xloc;
         %frameInfo(k).yCoord(:,1) = frameInfo(k).yloc;
@@ -206,6 +215,6 @@ n = size(C,1);
 K = zeros(n,n);
 
 K(ij) = C(ij) ./ sqrt(C(ii).*C(jj));
-% remaining components are not needed
+% remaining components are redundant
 % K = K + K';
 % K(sub2ind([n n], 1:n, 1:n)) = 1;
