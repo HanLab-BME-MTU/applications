ac6df4c85bc4a6cc5f2fb84fefa99d144d4bf08c
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-03-26 04:58:19 +0000

    Resizing fixed.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@6019 0bcb941b-74d1-0310-bb26-e6636707f451

--- montagePlot.m
+++ montagePlot.m
@@ -63,7 +63,7 @@ if ~isempty(idx)
         rgbCells(k,:) = zcells;
     end
     rgbCells = arrayfun(@(x) cat(3,rgbCells{:,x}), 1:nf, 'UniformOutput', false);
-        
+    
     switch varargin{idx+1}
         case 'color'
             for c = 1:nc
@@ -82,7 +82,7 @@ if ~isempty(idx)
             inputCell = cat(1, rgbCells, inputCell);
             nc = nc + 1;
             labels = [' ', labels];
-            rgbColors = [zeros(1,3) rgbColors];    
+            rgbColors = [zeros(1,3) rgbColors];
         case 'RGB'
             inputCell = rgbCells;
             nc = 1;
@@ -119,37 +119,9 @@ else
 end
 
 
-
-% figure size
-wx = 1;
-dx = 1/15; % gap between frames, relative to frame width
-dc = 1/5; % gap between channels
-
-nr = 1:5;
-
-height = nr*(nc*wx+(nc-1)*dx) + (nr-1)*dc;
-nx = ceil(nf./nr);
-width = nx*wx + (nx-1)*dx;
-
-idx = (width./height - (1+sqrt(5))/2).^2;
-idx = idx == min(idx);
-
-% final values
-width = width(idx);
-height = height(idx);
-nx = nx(idx);
-nr = nr(idx);
-
-ratio = height/width;
-
-% initial width/height
-width = 500;
-height = ratio*width;
-wxi = width / ((nx/dx + nx-1)*dx);
-dxi = wxi*dx;
-dci = wxi*dc;
-
-
+height = 250;
+width = height * (1+sqrt(5))/2;
+[wxi, dxi, dci, nx, nr, width] = getProportions(width, height, nf, nc);
 
 
 hf = figure('Visible', 'off', 'PaperPositionMode', 'auto', 'Position', [50, 200, width, height]);
@@ -162,33 +134,31 @@ if ~isempty(labels)
     for c = 1:nc
         ht = text(0, 0, labels{c}, 'FontName', fontName, 'FontSize', fontSize);
         extent = get(ht, 'extent');
-        textWidth(c) = extent(3);  
+        textWidth(c) = extent(3);
         delete(ht);
     end
     maxTextWidth = max(textWidth);
-    offset = maxTextWidth + dci;
+    offset = maxTextWidth + 2*dci;
 else
     offset = 0;
 end
 delete(ha);
 
-
-set(hf, 'Position', [50, 200, width+offset, height], 'Visible', visible);
+ha = zeros(nc,nf);
+set(hf, 'Position', [50, 100, width+offset, height], 'Visible', visible, 'ResizeFcn', {@resizeCallback});
 for rowi = 1:nr
     for c = 1:nc
         for x = 1:nx
             fi = x+(rowi-1)*nx;
             if fi<=nf
-                axes('Units', 'pixels',...
-                    'Position', [offset+(x-1)*(wxi+dxi) height-wxi - ((rowi-1)*(nc*wxi+(nc-1)*dxi+dci)+(c-1)*(wxi+dxi)) wxi wxi],...
+                ha(c,fi) = axes('Units', 'pixels',...
+                    'Position', [offset+(x-1)*(wxi+dxi) height-wxi-((rowi-1)*(nc*wxi+(nc-1)*dxi+dci)+(c-1)*(wxi+dxi)) wxi wxi],...
                     'XLim', [0 wxi], 'YLim', [0 wxi]);
                 imagesc([0 wxi], [0 wxi], inputCell{c, fi}); axis off; caxis([0 255]);%caxis([minI(c) maxI(c)]);
-                hold on;
                 if x==1 && rowi==1 && ~isempty(labels)
-                    ht = text(-dci, wxi/2, labels{c}, 'Units', 'pixels',...
+                    ht(c) = text(-dci, wxi/2, labels{c}, 'Units', 'pixels',...
                         'HorizontalAlignment', 'right', 'VerticalAlignment', 'baseline',...
-                        'FontName', fontName, 'FontSize', fontSize, 'Color', rgbColors{c});
-                    get(ht, 'extent');
+                        'FontUnits', 'pixels', 'FontName', fontName, 'FontSize', wxi/2.5, 'Color', rgbColors{c});
                 end
             end
         end
@@ -198,125 +168,71 @@ colormap(gray(256));
 
 
 if ~isempty(printPath)
-    print(hf, '-depsc2', printPath);   
+    print(hf, '-depsc2', printPath);
 end
 
 if strcmp(visible, 'off')
     close(hf);
 end
 
+    function resizeCallback(src, ~)
+        [nc,nf] = size(inputCell);
+        pos = get(src, 'Position');
+        width = pos(3);
+        height = pos(4);
+        
+        [wxi, dxi, dci, nx, nr] = getProportions(width-offset, height, nf, nc);
+        
+        for rowi = 1:nr
+            for c = 1:nc
+                for x = 1:nx
+                    fi = x+(rowi-1)*nx;
+                    if fi<=nf
+                        set(ha(c, fi),...
+                            'Position', [offset+(x-1)*(wxi+dxi) height-wxi-((rowi-1)*(nc*wxi+(nc-1)*dxi+dci)+(c-1)*(wxi+dxi)) wxi wxi]);
+                        
+                    end
+                end
+                set(ht(c), 'Position', [-dci, wxi/2], 'FontSize', wxi/2.5);
+            end
+        end
+    end
 
-% % [nx ny wx wy dx dy offset dc] = getLayout(width, height, nf, nc);
-% 
-% 
-% 
-% 
-% figure('ResizeFcn', @resizeCallback, 'Position', [50, 200, width, height]);
-% colormap(gray(256));
-% if nc==1
-%     ha = zeros(1,nf);
-%     for yi = 1:ny
-%         for xi = 1:nx
-%             % frame index
-%             fi = xi+(yi-1)*nx;
-%             if fi<=nf
-%                 ha(fi) = axes('Position', [(xi-1)*(wx+dx) offset+(ny-yi)*(wy+dy) wx wy]);
-%                 imagesc(inputCell{1, fi}); axis off; caxis([minI(1) maxI(1)]);
-%             end
-%         end
-%     end
-% else
-%     ha = zeros(nc,nf);
-%     for yi = 1:ny
-%         for ci = 1:nc
-%             for xi = 1:nx
-%                 % axes index
-%                 fi = xi + (yi-1)*nx;
-%                 if xi+(yi-1)*nx <= nf
-%                     ha(ci,fi) = axes('Position', [(xi-1)*(wx+dx) offset+(ny-yi)*(nc*wy+(nc-1)*dy+dc) + (nc-ci)*(wy+dy) wx wy]);
-%                     frame = scaleContrast(double(inputCell{ci, xi+(yi-1)*nx}), [minI(ci) maxI(ci)]);
-%                     frame = cat(3, colorV(ci,1)*frame, colorV(ci,2)*frame, colorV(ci,3)*frame);
-%                     imagesc(uint8(frame)); axis off;
-%                 end
-%             end
-%         end
-%     end
-% end
-% 
-% 
-% 
-% %     function resizeCallback(src, ~)
-% %         pos = get(src, 'Position');
-% %         [nx ny wx wy dx dy offset dc] = getLayout(pos(3), pos(4), nf, nc);
-% %         
-% %         if nc==1
-% %             for yi2 = 1:ny
-% %                 for xi2 = 1:nx
-% %                     % frame index
-% %                     fi = xi2+(yi2-1)*nx;
-% %                     if fi<=nf
-% %                         set(ha(fi), 'Position', [(xi2-1)*(wx+dx) (ny-yi2)*(wy+dy) wx wy]);
-% %                     end
-% %                 end
-% %             end
-% %         else
-% %             for yi2 = 1:ny
-% %                 for ci2 = 1:nc
-% %                     for xi2 = 1:nx
-% %                         fi = xi2 + (yi2-1)*nx;
-% %                         if fi<=nf
-% %                             set(ha(ci2,fi), 'Position', [(xi2-1)*(wx+dx) offset+(ny-yi2)*(nc*wy + (nc-1)*dy + dc) + (nc-ci2)*(wy+dy) wx wy]);
-% %                         end
-% %                     end
-% %                 end
-% %             end
-% %         end
-% %     end
-% end
-% 
-% 
-% function [nx ny wx wy dx dy offset dc] = getLayout(width, height, nf, nc)
-% df = 1/15; % gap between frames, relative to frame width
-% dc = 3/15; % gap between channels
-% 
-% nxVect = 1:nf*nc;
-% wVect = width ./ (nxVect + df*(nxVect-1));
-% 
-% if nc==1
-%     nyVect = floor((height+df*wVect)./(wVect*(1+df)));
-%     
-% else
-%     nyVect = floor((height+dc*wVect)./(wVect*(nc + df*(nc-1) + dc)));
-% end
-% ntot = nxVect .* nyVect*nc;
-% idx = find(ntot > nf*nc, 1, 'first');
-% 
-% nx = nxVect(idx);
-% ny = nyVect(idx);
-% 
-% % width of frame
-% w = width / (nx + df*(nx-1));
-% 
-% % fraction of height used
-% if nc==1
-%     p = w*(ny + df*(ny-1))/height;
-% else
-%     p = w*(ny*nc + dc*(ny-1) + df*(nc-1)*ny)/height;
-% end
-% 
-% % proportions relative to 1
-% wx = w/width;
-% 
-% if nc==1
-%     wy = p / (ny + df*(ny-1));
-%     dc = 0;
-% else
-%     wy = p / (ny*nc + dc*(ny-1) + df*(nc-1)*ny);
-%     dc = wy*dc;
-% end
-% 
-% dx = wx*df;
-% dy = wy*df;
-% offset = 1-p;
-% 
-% end
+end
+
+
+
+function [wxi, dxi, dci, nx, nr, width] = getProportions(width, height, nf, nc)
+
+% fixed proportions
+wx = 1;
+dx = 1/15; % gap between frames, relative to frame width
+dc = 1/5; % gap between channels
+
+hwRatio = height/width;
+
+nr = 1:20;
+
+% find arrangement that comes closest to < ratio
+iheight = nr*(nc*wx+(nc-1)*dx) + (nr-1)*dc;
+nx = ceil(nf./nr);
+iwidth = nx*wx + (nx-1)*dx;
+
+% distance
+idx = (iheight./iwidth - hwRatio).^2;
+idx = idx == min(idx);
+
+% corrected width/height:
+ratio = iheight(idx)/iwidth(idx);
+nr = nr(idx);
+nx = nx(idx);
+
+% initial width/height
+width = height/ratio;
+wxi = width / ((nx/dx + nx-1)*dx);
+dxi = wxi*dx;
+dci = wxi*dc;
+
+end
+
+
