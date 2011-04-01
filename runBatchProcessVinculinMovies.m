a2138c90e976ce30f5dcdddccf5f8a909309fdd0
sb234 <sb234@0bcb941b-74d1-0310-bb26-e6636707f451>
2011-04-01 22:13:45 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/vinculinFA/main@6069 0bcb941b-74d1-0310-bb26-e6636707f451

--- runBatchProcessVinculinMovies.m
+++ runBatchProcessVinculinMovies.m
@@ -1,10 +1,10 @@
 function runBatchProcessVinculinMovies
 
 % parent directory of every movie to be analyzed
-%params.rootDirectory = '/home/sb234/Projects/VinculinFA/completed/';
+params.rootDirectory = '/home/sb234/Projects/VinculinFA/completed/con/062609_con_CSUX_1';
 %params.rootDirectory = '/home/sb234/Projects/VinculinFA/completed/con/052710_con_CSUX_2';
 %params.rootDirectory = '/home/sb234/Projects/VinculinFA/completed/cre/060910_cre_CSUX_1';
-params.rootDirectory = '/Users/sylvain/Documents/Work/HMS/Projects/VinculinFA/052710_con_CSUX_2';
+%params.rootDirectory = '/Users/sylvain/Documents/Work/HMS/Projects/VinculinFA/052710_con_CSUX_2';
 %params.rootDirectory = '/home/sb234/Projects/VinculinFA/SDC Y27632 20s';
 
 % name of the channel directory subfolders
@@ -17,7 +17,7 @@ params.procNames = {...
     'particleTracking',...
     'pairTracks'};
 
-params.runSteps = [-1 -1 -1 1];
+params.runSteps = [0 0 0 1];
 params.batchMode = 0;
 
 % Physical parameters
