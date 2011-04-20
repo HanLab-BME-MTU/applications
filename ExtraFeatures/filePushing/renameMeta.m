bf2ad10d80e0362f3bcc8818374cf1a342f158d2
Maria Bagonis <maria_bagonis@hms.harvard.edu>
2011-04-20 14:22:49 +0000

    fixed to make more generic
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@6246 0bcb941b-74d1-0310-bb26-e6636707f451

--- ExtraFeatures/filePushing/renameMeta.m
+++ ExtraFeatures/filePushing/renameMeta.m
@@ -1,11 +1,11 @@
-function [ output_args ] = renameMeta(groupList)
+function [ output_args ] = renameMeta(groupList,beforeRename,afterRename)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
 for i = 1:length(groupList)
     
-    fileNameBeforeRename = [char(groupList(i,2)) filesep 'meta'] ;
-    fileNameAfterRename = [char(groupList(i,2)) filesep 'metaOld'];
+    fileNameBeforeRename = [char(groupList(i,2)) filesep beforeRename] ;
+    fileNameAfterRename = [char(groupList(i,2)) filesep afterRename];
     
     movefile(fileNameBeforeRename, fileNameAfterRename);
     
