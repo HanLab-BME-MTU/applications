0737ed8fb359cf592429ade3a6e5c1a10b24bc60
Maria Bagonis <maria_bagonis@hms.harvard.edu>
2011-04-21 19:09:20 +0000

    small bug
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@6268 0bcb941b-74d1-0310-bb26-e6636707f451

--- plusTipTracker/plusTipSubRoiExtractTracks.m
+++ plusTipTracker/plusTipSubRoiExtractTracks.m
@@ -492,11 +492,12 @@ if projData.nTracks~=0
     projData.insideSec=insideSec(trckIdxIn); % lifetime within sub-roi (seconds)
     projData.percentLifeInside=100*(projData.insideSec./projData.lifeSec); % percent time within sub-roi
 
-    speedIn=nanmean(sqrt(diff(xMatIn.*IN,[],2).^2+diff(yMatIn.*IN,[],2).^2),2);
-    speedOut=nanmean(sqrt(diff(xMatOut.*OUT,[],2).^2+diff(yMatOut.*OUT,[],2).^2),2);
+    % FIX LATER
+    %speedIn=nanmean(sqrt(diff(xMatIn.*IN,[],2).^2+diff(yMatIn.*IN,[],2).^2),2);
+    %speedOut=nanmean(sqrt(diff(xMatOut.*OUT,[],2).^2+diff(yMatOut.*OUT,[],2).^2),2);
 
-    projData.speedInMicPerMin=pixPerFrame2umPerMin(speedIn,projData.secPerFrame,projData.pixSizeNm);
-    projData.speedOutMicPerMin=pixPerFrame2umPerMin(speedOut,projData.secPerFrame,projData.pixSizeNm);
+    %projData.speedInMicPerMin=pixPerFrame2umPerMin(speedIn,projData.secPerFrame,projData.pixSizeNm);
+    %projData.speedOutMicPerMin=pixPerFrame2umPerMin(speedOut,projData.secPerFrame,projData.pixSizeNm);
 
     %projData.startOrEnd=~isnan(xMat(:,1)) | ~isnan(xMat(:,end));
     %projData.percentAtStartOrEnd=sum(projData.startOrEnd)./projData.nTracks;
