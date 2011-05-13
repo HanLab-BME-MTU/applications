2bf2d93b08c448480f7687aef0d85f5452975bc6
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-05-13 16:30:35 +0000

    Bug fix
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@6491 0bcb941b-74d1-0310-bb26-e6636707f451

--- runTracking.m
+++ runTracking.m
@@ -31,6 +31,8 @@ ip.addParamValue('FileName', [], @ischar);
 ip.addParamValue('Frames', [], @isvector);
 ip.parse(data, varargin{:});
 overwrite = ip.Results.Overwrite;
+fileName = ip.Results.FileName;
+frames = ip.Results.frames;
 
 if isempty(settings)
     %load track settings required for tracking
