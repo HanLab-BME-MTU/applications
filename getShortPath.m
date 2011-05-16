2939a1ed55756a160e7929a25f9a5a8ec8d8d13c
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-05-16 04:50:35 +0000

    Fixed single channel bug
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@6504 0bcb941b-74d1-0310-bb26-e6636707f451

--- getShortPath.m
+++ getShortPath.m
@@ -1,11 +1,15 @@
-%spath = getShortPath(data) returns the 3 directories above cell level
+%spath = getShortPath(data) returns the truncated path of a cell directory (3 levels) 
 
 % Francois Aguet, 05/13/2011
 
 function spath = getShortPath(data)
 
-mCh = strcmp(data.channels, data.source);
-sCh = setdiff(1:length(data.channels),mCh);
-spath = data.channels{sCh(1)};
+if numel(data.channels)>1
+    mCh = strcmp(data.channels, data.source);
+    sCh = setdiff(1:length(data.channels),mCh);
+    spath = data.channels{sCh(1)};
+else
+    spath = data.channels{1};
+end
 idx = regexp(spath, filesep);
 spath = spath(idx(end-4)+1:idx(end-1));
