df579b593eb1d87d3412462d675b5a313fe058ad
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-05-24 19:33:05 +0000

    Minor updates
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@6623 0bcb941b-74d1-0310-bb26-e6636707f451

--- getTrackStack.m
+++ getTrackStack.m
@@ -4,28 +4,41 @@
 
 % Francois Aguet, Jan 26 2011
 
-function stack = getTrackStack(data, track)
+function stack = getTrackStack(data, track, varargin)
 
-nChannels = length(data.channels);
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addRequired('data', @isstruct);
+ip.addRequired('track', @isstruct);
+ip.addParamValue('Reference', 'track', @(x) strcmpi(x, 'track') | strcmpi(x, 'frame'));
+ip.addParamValue('WindowWidth', 4, @isscalar);
+ip.addParamValue('Buffer', 5, @isscalar);
+ip.parse(data, track, varargin{:});
+buffer = ip.Results.Buffer;
 
+nc = length(data.channels);
 masterChannel = find(strcmp(data(1).source, data(1).channels));
-slaveChannels = setdiff(1:nChannels, masterChannel);
-
+slaveChannels = setdiff(1:nc, masterChannel);
 
 sigma = getGaussianPSFsigma(data.NA, data.M, data.pixelSize, name2wavelength(data.markers{masterChannel}));
-w = ceil(4*sigma);
+w = ceil(ip.Results.WindowWidth*sigma);
 
 % buffer with 5 frames before and after
-buffer = 5;
 bStart = track.start - max(1, track.start-buffer);
 bEnd = min(data.movieLength, track.end+buffer) - track.end;
 idx = track.start-bStart:track.end+bEnd;
 nf = length(idx);
 
-stack = cell(nChannels,nf);
-    
-xi = round(track.x);
-yi = round(track.y);
+stack = cell(nc,nf);
+
+if strcmpi(ip.Results.Reference, 'track')
+    xi = round(track.x);
+    yi = round(track.y);
+else
+    xi = ones(1,nf)*round(mean([track.x]));
+    yi = ones(1,nf)*round(mean([track.y]));
+end
+
 xi = [xi(1)*ones(1,bStart) xi xi(end)*ones(1,bEnd)];
 yi = [yi(1)*ones(1,bStart) yi yi(end)*ones(1,bEnd)];
 % load all visible frames of this track and store
