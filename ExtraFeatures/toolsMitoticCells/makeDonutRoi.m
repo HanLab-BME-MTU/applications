7e87d0b89337699d107cafc63db68d8a9e006967
Maria Bagonis <maria_bagonis@hms.harvard.edu>
2011-06-09 20:51:29 +0000

    fixeds some stuff in figure  also asks where your dir is now
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@6765 0bcb941b-74d1-0310-bb26-e6636707f451

--- ExtraFeatures/toolsMitoticCells/makeDonutRoi.m
+++ ExtraFeatures/toolsMitoticCells/makeDonutRoi.m
@@ -1,15 +1,25 @@
-function [ roiMask ] = makeDonutRoi(projRoiAll,newProj,bitDepth,pixInt,pixArea)
+function [ roiMask ] = makeDonutRoi(dir, projRoiAll,newProj,bitDepth,pixInt,pixArea)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-roiMaskAll = double(imread([projRoiAll filesep 'roiMask.tif']));
-roiYX = load([projRoiAll filesep 'roiYX']);
+if nargin<1 || isempty(dir)
+    dir=uigetdir(pwd,'Please select the directory where your rois are located');
+end
+
+roiMaskAll = double(imread([dir filesep projRoiAll filesep 'roiMask.tif']));
+roiYX = load([dir filesep projRoiAll filesep 'roiYX']);
 roiYX = roiYX.roiYX;
 imDir = [pwd filesep 'images'];
 [listOfImages] = searchFiles('.tif',[],imDir,0);
+%if isempty(listOfImages) == 1
+ %   [listOfImages] = searchFiles('.TIF',[],imDir,0); 
+    
+%end 
 fileNameIm = [char(listOfImages(1,2)) filesep char(listOfImages(1,1))];
 img = double(imread(fileNameIm))./((2^bitDepth)-1);
-imgOrg = double(imread(fileNameIm))./((2^bitDepth-1)); 
+
+
+%imgOrg2 = double(imread(fileNameIm)).((2^bitDepth-1));
 % Make a mask from the image where normalized values above 0.7 
 % and thus considered saturated are set to 1 and all non-saturated pixels
 % are set to zero. 
@@ -26,19 +36,27 @@ bw4 = bwmorph(bw3,'bridge');
 roiMaskDonutHole = imfill(bw4,'holes');
 
 
-
+% subtrack out donut mask which is 1 
+% all to keep will be 1 
 roiMask = roiMaskAll - roiMaskDonutHole; 
 roiMask(roiMask<0) = 0;
+
+
+
+
 
 dilate = 1;
 while dilate == 1
     
+imgOrg = double(imread(fileNameIm))./((2^bitDepth-1)); % reload original image
+imgOrg(~roiMask) = 0; % set all values that are outside of mask to zero 
+
 
 figure; 
-forFig = roiMask - imgOrg; 
-forFig(forFig < 0) = 0; 
 
-imagesc(forFig); 
+
+
+imagesc(imgOrg); 
 dilateMask = questdlg('Would You Like To Dilate the Donut Mask?', 'Dilate Mask?', 'Yes', 'No', 'Cancel', 'Yes');
 
 
@@ -56,7 +74,7 @@ switch dilateMask
         roiMaskDonutHole = bwmorph( roiMaskDonutHole,'erode'); 
         roiMask = roiMaskAll - roiMaskDonutHole;
         roiMask(roiMask<0) = 0; 
-        
+        close(gcf)
             case 'No'
                 dilate = 0; 
         end 
