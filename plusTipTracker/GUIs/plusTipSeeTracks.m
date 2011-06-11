73f05da12779ec3e8235ee6bff6d95b122708740
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2011-06-11 14:56:58 +0000

    M    plusTipTracker/GUIs/plusTipSeeTracks.m - run as singleton M    plusTipTracker/GUIs/plusTipSeeTracks.fig - run as singleton
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/mtTracker/main@6781 0bcb941b-74d1-0310-bb26-e6636707f451

--- plusTipTracker/GUIs/plusTipSeeTracks.m
+++ plusTipTracker/GUIs/plusTipSeeTracks.m
@@ -23,11 +23,11 @@ function varargout = plusTipSeeTracks(varargin)
 
 % Edit the above text to modify the response to help plusTipSeeTracks
 
-% Last Modified by GUIDE v2.5 17-Dec-2009 18:19:30
+% Last Modified by GUIDE v2.5 11-Jun-2011 10:56:27
 
 
 % Begin initialization code - DO NOT EDIT
-gui_Singleton = 0;
+gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
     'gui_Singleton',  gui_Singleton, ...
     'gui_OpeningFcn', @plusTipSeeTracks_OpeningFcn, ...
@@ -1095,4 +1095,3 @@ function subroiExcludeCheck_Callback(hObject, eventdata, handles)
 % Hint: get(hObject,'Value') returns toggle state of subroiExcludeCheck
 handles.subroiExcludeRegion=get(hObject,'Value');
 guidata(hObject, handles);
-
