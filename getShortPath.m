8646ce72362b4b61dc728ded4d9b07efaee14a13
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-06-24 02:41:17 +0000

    Fixed bug for 1-channel movies
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@6842 0bcb941b-74d1-0310-bb26-e6636707f451

--- getShortPath.m
+++ getShortPath.m
@@ -8,8 +8,10 @@ if numel(data.channels)>1
     mCh = strcmp(data.channels, data.source);
     sCh = setdiff(1:length(data.channels),mCh);
     spath = data.channels{sCh(1)};
+    idx = regexp(spath, filesep);
+    spath = spath(idx(end-4)+1:idx(end-1));
 else
     spath = data.channels{1};
+    idx = regexp(spath, filesep);
+    spath = spath(idx(end-3)+1:idx(end));
 end
-idx = regexp(spath, filesep);
-spath = spath(idx(end-4)+1:idx(end-1));
