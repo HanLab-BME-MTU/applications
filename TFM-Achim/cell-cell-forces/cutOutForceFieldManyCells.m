7dbe15a50061d9be29f4f7811d0d00e899bbd4a5
Achim Besser <achim.besser@hms.harvard.edu>
2011-07-14 22:01:04 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/TFM/main@6978 0bcb941b-74d1-0310-bb26-e6636707f451

--- TFM-Achim/cell-cell-forces/cutOutForceFieldManyCells.m
+++ TFM-Achim/cell-cell-forces/cutOutForceFieldManyCells.m
@@ -89,7 +89,7 @@ while finished==false
     
     numFrames=length(toDoList);
     % saveInterval=min(5,ceil(0.1*numFrames));
-    saveInterval=10;
+    saveInterval=5;
     
     for i=toDoList
         display('Plotting constraint forceField might take some time...')
