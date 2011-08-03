bc4ee814db2b6ebdf364d9f1b54f121d00e8b6cf
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-08-03 15:56:18 +0000

    Renamed/changed to select valid vs all tracks
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@7138 0bcb941b-74d1-0310-bb26-e6636707f451

--- iofun/loadTracks.m
+++ iofun/loadTracks.m
@@ -1,6 +1,6 @@
 % Francois Aguet, July 18 2011
 
-function tracks = loadValidTracks(data, varargin)
+function tracks = loadTracks(data, varargin)
 
 ip = inputParser;
 ip.CaseSensitive = false;
@@ -8,15 +8,18 @@ ip.addRequired('data', @(x) isstruct(x) & numel(x)==1);
 ip.addParamValue('FileName', 'trackAnalysis.mat', @ischar); 
 ip.addParamValue('Cutoff', 4, @isscalar);
 ip.addParamValue('Sort', 'on', @(x) strcmpi(x, 'on') | strcmpi(x, 'off'));
-
+ip.addParamValue('Type', 'valid', @(x) strcmpi(x, 'valid') | strcmpi(x, 'all'));
 ip.parse(data, varargin{:});
 
 cutoff = ip.Results.Cutoff * data.framerate;
 
 ta = load([data.source 'Tracking' filesep ip.Results.FileName]);
 tracks = ta.tracks;
-tracks = tracks([tracks.valid]==1 & [tracks.lifetime_s] >= cutoff);
-
+if strcmpi(ip.Results.Type, 'valid')
+    tracks = tracks([tracks.valid]==1 & [tracks.lifetime_s] >= cutoff);
+else
+    tracks = tracks([tracks.lifetime_s] >= cutoff);
+end
 if strcmpi(ip.Results.Sort, 'on')
     [~, sortIdx] = sort([tracks.lifetime_s], 'descend');
     tracks = tracks(sortIdx);
