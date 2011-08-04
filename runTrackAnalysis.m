c7ce9cbf0a07a5e0b6ab304bfbd8be55524c7eb7
Daniel Nunez <daniel_nunez@hms.harvard.edu>
2011-08-04 16:14:43 +0000

    changing for into  parfor
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@7154 0bcb941b-74d1-0310-bb26-e6636707f451

--- runTrackAnalysis.m
+++ runTrackAnalysis.m
@@ -37,7 +37,7 @@ for i = 1:length(data)
     data(i).smTracks = [];
 end
 
-for i = 1:length(data)
+parfor i = 1:length(data)
     if ~(exist([data(i).source filesep 'Tracking' filesep filename],'file')==2) || overwrite
         data(i) = main(data(i), buffer, trackerOutput, filename, frameIdx{i}, bufferMode);
     else
