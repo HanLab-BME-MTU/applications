8fe94ca562a1df1729f002567cf6b0fb537ef82d
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-09-07 19:26:56 +0000

    Bug fix
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@7321 0bcb941b-74d1-0310-bb26-e6636707f451

--- assignTracksToMaster.m
+++ assignTracksToMaster.m
@@ -6,13 +6,13 @@ ip.CaseSensitive = false;
 ip.addRequired('slaveTracks', @isstruct);
 ip.addRequired('masterTracks');
 ip.addParamValue('MinOverlap', 1, @isscalar);
-ip.addParamValue('MaxDistance', 2, @isscalar);
+ip.addParamValue('MaxDistance', 5, @isscalar);
 ip.parse(slaveTracks, masterTracks, varargin{:});
 minOverlap = ip.Results.MinOverlap;
 R = ip.Results.MaxDistance;
 
 % mean positions
-mMeans = arrayfun(@(t) [mean(t.x(1,:)) mean(t.x(1,:))], masterTracks, 'UniformOutput', false);
+mMeans = arrayfun(@(t) [mean(t.x(1,:)) mean(t.y(1,:))], masterTracks, 'UniformOutput', false);
 mMeans = vertcat(mMeans{:});
 sMeans = arrayfun(@(t) [mean(t.x(1,:)) mean(t.y(1,:))], slaveTracks, 'UniformOutput', false);
 sMeans = vertcat(sMeans{:});
@@ -23,8 +23,8 @@ idx = KDTreeBallQuery(mMeans, sMeans, R);
 nm = numel(masterTracks);
 for k = 1:nm
     if ~isempty(idx{k})
-        overlap = min([slaveTracks(idx).end], masterTracks(k).end) - ...
-            max([slaveTracks(idx).start], masterTracks(k).start) + 1;
+        overlap = min([slaveTracks(idx{k}).end], masterTracks(k).end) - ...
+            max([slaveTracks(idx{k}).start], masterTracks(k).start) + 1;
         idx{k} = idx{k}(overlap>=minOverlap);
     end
 end
