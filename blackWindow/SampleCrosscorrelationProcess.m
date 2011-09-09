ff77a84b6b8ea2b55bf3b5e406bbaf3d713ba6cc
Hunter Elliott <hunter_elliott@hms.harvard.edu>
2011-09-09 20:12:02 +0000

    A    blackWindow/SampleCrosscorrelationProcess.m - added. M    blackWindow/calculateWindowSampleCrosscorrelation.m - finalized, added help etc.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/morphodynamics/main@7338 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ blackWindow/SampleCrosscorrelationProcess.m
@@ -0,0 +1,55 @@
+classdef SampleCrosscorrelationProcess < DataProcessingProcess
+%Process class definition for calculateWindowSampleAutocorrelation.m
+%     
+% Hunter Elliott
+% 8/2011
+%
+    methods (Access = public)
+        
+        function obj = SampleCrosscorrelationProcess(owner,outputDir,funParams)
+                                              
+            if nargin == 0
+                super_args = {};
+            else                
+                
+                super_args{1} = owner;
+
+                super_args{2} = SampleCrosscorrelationProcess.getName;
+                super_args{3} = @calculateWindowSampleCrosscorrelation;                               
+                
+                if nargin < 3 || isempty(funParams)                                       
+                    
+                    %----Defaults----%
+                    nChan = numel(owner.channels_);
+                    funParams.ChannelIndex = 1:nChan;%Default is all channels
+                    funParams.MaxLag = floor(owner.nFrames_/4);%Rule-of-thumb for autocorrelation                    
+                    funparams.MaxBands = Inf;%Default is to include all bands in combined correlation
+                    funParams.OutputDirectory = [outputDir  filesep 'sample_crosscorrelation'];                    
+                    funParams.BatchMode = false;                              
+
+                end
+                
+                super_args{4} = funParams;    
+                                
+            end
+            
+            obj = obj@DataProcessingProcess(super_args{:});
+        end   
+        
+        function samp = loadChannelOutput(obj,iChan)                        
+            
+        end               
+           
+        function OK = checkChannelOutput(obj,iChan)
+            
+        end
+            
+    end
+    
+    methods (Static)
+        function name =getName()
+            name = 'Window Sample Crosscorrelation';
+        end                
+    end 
+    
+end    
--- blackWindow/calculateWindowSampleCrosscorrelation.m
+++ blackWindow/calculateWindowSampleCrosscorrelation.m
@@ -1,28 +1,99 @@
-function movieData = calculateWindowSampleCrosscorrelation(movieData,paramIn)
+function movieData = calculateWindowSampleCrosscorrelation(movieData,paramsIn)
+%CALCULATEWINDOWSAMPLEAUTOCORRELATION calcualates and makes figures of the crosscorrelation of the window samples 
+% 
+% movieData = calculateWindowSampleAutocorrelation(movieData)
+% movieData = calculateWindowSampleAutocorrelation(movieData)
+% 
+% This function calculates the crosscorrelation between the window samples
+% and protrusion samples for each window of the input movie. Window
+% sampling must already have been run using sampleMovieWindows.m. The
+% averaged crosscorrelation along the cell edge, into the cell, etc are
+% also calcualted and figures created for each. Additionally, the average
+% cross-correlation of a window with its neighbors is calculated and
+% returned.
+% 
+% Input:
+% 
+%   movieData - The MovieData object describing the movie to analyze.
+% 
+%   paramsIn - A structure containing the parameters to use when
+%   calcualating crosscorrelatioins. The parameters should be stored as
+%   fields in the structure, with the field names and possible values as
+%   described below:
+% 
+%   Possible Parameter Structure Field Names:
+%       ('FieldName' -> possible values)
+%
+%       ('MaxLag'->positive integer scalar) The maximum lag, in frames, to
+%       calculate the crosscorrelation to. Optional. Default is 1/4 of the
+%       total number of frames.
+%
+%       ('MaxBands' ->Positive integer scalar) The maximum number of bands
+%       of windows, starting at the cell edge, to include in the combined
+%       cross-correlation. Because the correlations with edge velocity
+%       often fall off further from the edge, specifying a smaller number
+%       here may give a stronger combined cross-correlation. Optional.
+%       Default is Infinity - that is, all bands are included.
+%
+%       ('OutputDirectory' -> character string) Optional. A character
+%       string specifying the directory to save the results and figures to.
+%       Samples for different channels will be saved as files in this
+%       directory.
+%       If not input, the results will be saved to the same directory as the
+%       movieData, in a sub-directory called "window_sample_crosscorrelation"
+%
+%       ('ChannelIndex' -> Positive integer scalar or vector) Optional. The
+%       integer index of the channels to analyze samples from. If not input,
+%       all channels which have been sampled will be used.
+%
+%       ('BatchMode' -> True/False)
+%       If true, graphical output, figure display and user interaction is
+%       supressed (i.e. progress bars, dialog and question boxes etc.) Note
+%       that if this is set to true, the .fig versions of the figures will
+%       be saved as invisible and you will have to set the 'Visible'
+%       property to 'On' after opening them to see them.
+%
+%
+% Output:
+%
+%   movieData - The updated MovieData object with the location of the
+%   results logged in the processes_ array. Additionally, the results and
+%   figures will be saved to the specified OutputDirectory.
+%
+% Hunter Elliott
+% 8/2011
+%
 
 
-p = paramIn;
-
-%TEMP UNDER CONSTRUCTION
-p.ChannelIndex = 3;
-
-%p.BatchMode = false;
-p.MaxLag = round(movieData.nFrames_/4);%Gives approximately the maximum possible maxlag
-%p.OutputDirectory = [movieData.outputDirectory_ filesep 'sample_autocorrelation_7x3_viscoElastic_MQ10'];
-if ~isfield(p,'OutputDirectory')
-    error('No output directory specified!')
+if nargin < 1 || ~isa(movieData,'MovieData')   
+    error('The first input must be a valid MovieData object!');        
 end
 
-mkClrDir(p.OutputDirectory);
-
-iWSProc = movieData.getProcessIndex('WindowSamplingProcess',1,~p.BatchMode);
-
-if isempty(iWSProc)
-    error('The movie must have valid window samples! Please run sampleMovieWindows.m first!');
+if nargin < 2 || isempty(paramsIn)
+    paramsIn = [];  
 end
 
+%Check if crosscorrelation has been run before 
+iProc = movieData.getProcessIndex('SampleCrosscorrelationProcess',1,false);
+if isempty(iProc)
+    iProc = numel(movieData.processes_)+1;
+    movieData.addProcess(SampleCrosscorrelationProcess(movieData,movieData.outputDirectory_));
+end
+
+%Parse input, store in parameter structure
+p = parseProcessParams(movieData.processes_{iProc},paramsIn);
+
+%Make sure the movie has been sampled
+iSampProc = movieData.getProcessIndex('WindowSamplingProcess',1,false);
+if isempty(iSampProc) || ~movieData.processes_{iSampProc}.checkChannelOutput(p.ChannelIndex)
+    error('The movie must have valid window sampling for all selected channels! Please run sampleMovieWindows.m first!')   
+end
+
+%Get the windowing process for later use
+%iWinProc = movieData.getProcessIndex('WindowingProcess',1,0);
+
+%Make sure the protrusion calculation has been run
 iProtProc = movieData.getProcessIndex('ProtrusionSamplingProcess',1,~p.BatchMode);
-
 if isempty(iProtProc) || ~movieData.processes_{iProtProc}.checkChannelOutput
     disp('Cannot calculate protrusion-cross correlation: No protrusion calculation found!')
 else
@@ -30,34 +101,49 @@ else
     protSamples = movieData.processes_{iProtProc}.loadChannelOutput;    
     
 end
-    
+
+
+%% ---------------------- Init ---------------------------- %%
+
+%Set up the output directory
+mkClrDir(p.OutputDirectory);
+
 nChan = numel(p.ChannelIndex);
 
+%Print options for creating .eps files.
+pOpt = {'-r300',...% dpi = 300
+        '-depsc2',...% use eps format
+                };
+
+%Get time data if available
+if ~isempty(movieData.timeInterval_)
+    tData = 0:movieData.timeInterval_:p.MaxLag*movieData.timeInterval_;
+    tLabel = 'Time Lag, Seconds';
+else
+    tData = 0:p.MaxLag;
+    tLabel = 'Time Lag, Frames';
+end
+            
+   
+%% ----------------- Crosscorr Calc ------------------ %%
 
 for iChan = 1:nChan
     
     %Load the activity samples for this channel
-    actSamples = movieData.processes_{iWSProc}.loadChannelOutput(p.ChannelIndex(iChan));
-    
-    if ndims(actSamples.avg) == 4
-        actSamples.avg = squeeze(actSamples.avg);
-    else
-        error('Finish converting this function to single-object Hunter!')
-    end
-    
+    actSamples = movieData.processes_{iSampProc}.loadChannelOutput(p.ChannelIndex(iChan));        
     
     [nStripMax,nBandMax,~] = size(actSamples.avg);
-
-    p.MaxBands = 5;%Maximum number of bands to include in combined cross-correlation analysis. Usually the correlations fall of with distance from edge, so decreasing this number gives stronger correlations.
     
     nObs = zeros(nStripMax,nBandMax);
-    %TEMP - this initialization doesn't seem to convince mLint - whats
-    %wrong????? HLE
+    
     actTimeSeries(1:nStripMax,1:nBandMax) = struct('observations',[]);
     protTimeSeries(1:nStripMax) = struct('observations',[]);
     ccProtActPerWin = nan(nStripMax,nBandMax,2*p.MaxLag+1,2);
     ccProtActPerStrip = nan(nStripMax,2*p.MaxLag+1,2);
     ccProtActPerBand = nan(nBandMax,2*p.MaxLag+1,2);
+    
+    %Get the current maximum bands
+    currMaxBands = min(p.MaxBands,nBandMax);
     
     for k = 1:nStripMax 
         
@@ -75,15 +161,15 @@ for iChan = 1:nChan
             %non-Nan points
             nObs(k,l) = nnz(~isnan(actTimeSeries(k,l).observations));
             
+            %Calculate the cross-corr for this window
             if (nObs(k,l) - p.MaxLag) >= 3*p.MaxLag                                 
-                ccProtActPerWin(k,l,:,:) = crossCorr(actTimeSeries(k,l),protTimeSeries(k,l),p.MaxLag);                                                          
-                
+                ccProtActPerWin(k,l,:,:) = crossCorr(actTimeSeries(k,l),protTimeSeries(k,l),p.MaxLag);                                                                          
             end                                   
             
         end
         
-        if (sum(squeeze(nObs(k,nObs(k,1:p.MaxBands)>p.MaxLag))) - p.MaxLag) >= (3*p.MaxLag) %Make sure we have enough points
-            ccProtActPerStrip(k,:,:) = crossCorr(actTimeSeries(k,1:p.MaxBands),protTimeSeries(k,1:p.MaxBands),p.MaxLag);%we need at least 3 points per traj for trend removal
+        if (sum(squeeze(nObs(k,nObs(k,1:currMaxBands)>p.MaxLag))) - p.MaxLag) >= (3*p.MaxLag) %Make sure we have enough points
+            ccProtActPerStrip(k,:,:) = crossCorr(actTimeSeries(k,1:currMaxBands),protTimeSeries(k,1:currMaxBands),p.MaxLag);
         end
             
     end                
@@ -142,7 +228,7 @@ for iChan = 1:nChan
     end
     
     
-    ccProtActAll = crossCorr(actTimeSeries(:,1:p.MaxBands),protTimeSeries,p.MaxLag);       
+    ccProtActAll = crossCorr(actTimeSeries(:,1:currMaxBands),protTimeSeries,p.MaxLag);       
     
     if p.BatchMode
         ccFig = figure('Visible','off');
@@ -153,6 +239,7 @@ for iChan = 1:nChan
     plot(-p.MaxLag:p.MaxLag,ccProtActAll(:,1),'r');
     hold on    
     plotTransparent(-p.MaxLag:p.MaxLag,ccProtActAll(:,1),ccProtActAll(:,2),'r',.2,0);
+    plot([0 0],ylim,'--k')
     xlabel('Time Lag, frames'); %TEMP - get time interval and scale if available!!!!
     ylabel('cross-correlation');
     %TEMP - Change this so that if all bands are used, it says "all
@@ -208,7 +295,7 @@ for iChan = 1:nChan
     for j = 1:3
         for k = 1:3
             
-            ccActLocalMean(j,k,:) = squeeze(nanmean(nanmean(ccActLocal(:,1:p.MaxBands,j,k,:,1),1),2));        
+            ccActLocalMean(j,k,:) = squeeze(nanmean(nanmean(ccActLocal(:,1:currMaxBands,j,k,:,1),1),2));        
                         
             plotInd = sub2ind([3 3],k,j);
             subplot(3,3,plotInd);      
@@ -216,6 +303,7 @@ for iChan = 1:nChan
             plot(-p.MaxLag:p.MaxLag,squeeze(ccActLocalMean(j,k,:)),'Color',plotColors(plotInd,:))            
             title(titlesArray{j,k})
             plot(xlim,[0 0],'--k');
+            plot([0 0],ylim,'--k')
             
             %Plot them all on the center for comparison
             subplot(3,3,5);
