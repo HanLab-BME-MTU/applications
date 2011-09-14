d438e638ca7a9dbbc60d9fc4c2f0a3cfde6ac025
Achim Besser <achim.besser@hms.harvard.edu>
2011-09-14 04:48:52 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/TFM/main@7362 0bcb941b-74d1-0310-bb26-e6636707f451

--- TFM-Achim/cell-cell-forces/plotResults/plotResultsForTwoStiff.m
+++ TFM-Achim/cell-cell-forces/plotResults/plotResultsForTwoStiff.m
@@ -2,22 +2,35 @@ function []=plotResultsForTwoStiff(groupedClusters)
 
 goodCellSet_35=findCells(groupedClusters,'kPa',35,'myo',0,'myoGlb',[0],'errF',Inf,'errs',0);
 [deg_vals_35,elE_vals_35,sumFmag_vals_35,resF_vals_35,sumFi_vals_35,sumLi_vals_35]=collectCellValues(groupedClusters,goodCellSet_35,'deg','elE','sumFmag','resF','sumFi','sumLi');
+cv=isnan(deg_vals_35);
+deg_vals_35(cv)=[]; elE_vals_35(cv)=[]; sumFmag_vals_35(cv)=[]; resF_vals_35(cv,:)=[]; sumFi_vals_35(cv)=[]; sumLi_vals_35(cv)=[];
 
 goodCellSet_8=findCells(groupedClusters,'kPa',8,'myo',0,'myoGlb',0,'errF',Inf,'errs',0);
 [deg_vals_8, elE_vals_8, sumFmag_vals_8, resF_vals_8, sumFi_vals_8, sumLi_vals_8]=collectCellValues(groupedClusters,goodCellSet_8,'deg','elE','sumFmag','resF','sumFi','sumLi');
+cv=isnan(deg_vals_8);
+deg_vals_8(cv)=[]; elE_vals_8(cv)=[]; sumFmag_vals_8(cv)=[]; resF_vals_8(cv,:)=[]; sumFi_vals_8(cv)=[]; sumLi_vals_8(cv)=[];
+
+n_8 =hist(deg_vals_8 ,[1:15]);
+n_35=hist(deg_vals_35,[1:15]);
+
+for k=1:15
+    M_8{k} =['8kPa: ',num2str(n_8(k)),';'];
+    M_35{k}=['35kPa: ',num2str(n_35(k))];    
+end
 
 maxDeg=(max([deg_vals_35;deg_vals_8]));
 G8kPa    =repmat({'8kPa'} ,length(deg_vals_8) ,1);
 G35kPa   =repmat({'35kPa'},length(deg_vals_35),1);
 % Gfake =repmat({''      ,maxDeg,1);
+Gcol=vertcat(G8kPa,G35kPa);
 
 G1=vertcat(deg_vals_8,deg_vals_35);
-G2=vertcat(G8kPa,G35kPa);
+G2=[M_8(deg_vals_8),M_35(deg_vals_35)]';
 
 
 figure()
 % checked with part7
-boxplot([sumFi_vals_8;sumFi_vals_35],{G1 G2},'factorgap',[20 0],'colorgroup',G2,'color','bg','notch','on','symbol','+')
+boxplot([sumFi_vals_8;sumFi_vals_35],{G1 G2},'factorgap',[20 0],'colorgroup',Gcol,'color','bg','notch','on','symbol','+')
 title(['Sum of interface forces for degrees: ',num2str(1:maxDeg)])
 xlabel('Deg of connectivity')
 ylabel('sum interface forces [nN]')
@@ -27,7 +40,7 @@ ylim([0,275])
 
 figure()
 % checked with part7
-boxplot(sqrt(sum([resF_vals_8;resF_vals_35].^2,2)),{G1 G2},'factorgap',[20 0],'colorgroup',G2,'color','bg','notch','on','symbol','+')
+boxplot(sqrt(sum([resF_vals_8; resF_vals_35].^2,2)),{G1 G2},'factorgap',[20 0],'colorgroup',Gcol,'color','bg','notch','on','symbol','+')
 title(['Residual force for degrees: ',num2str(1:maxDeg)])
 xlabel('Deg of connectivity')
 ylabel('Residual force [nN]')
@@ -37,7 +50,7 @@ ylim([0,160])
 
 figure()
 % checked with part7
-boxplot([elE_vals_8;elE_vals_35],{G1 G2},'factorgap',[20 0],'colorgroup',G2,'color','bg','notch','on','symbol','+')
+boxplot([elE_vals_8; elE_vals_35],{G1 G2},'factorgap',[20 0],'colorgroup',Gcol,'color','bg','notch','on','symbol','+')
 title(['Contraction Energy for cells with connectivity: ',num2str(1:maxDeg)])
 xlabel('Deg of connectivity')
 ylabel('Contraction Energy [pJ]')
@@ -45,6 +58,7 @@ set(gca,'fontsize',20)
 box on
 ylim([0,0.11])
 
+clear M_8 M_35;
 
 goodEdgeSet_35=findEdges(groupedClusters,'kPa',35,'myo',0,'myoGlb',[0],'errF',Inf,'errs',0);
 [deg_vals_35,lgth_vals_35,fc1_vals_35,nVec_vals_35]=collectEdgeValues(groupedClusters,goodEdgeSet_35,'deg','lgth','fc1','nVec');
@@ -53,7 +67,7 @@ fc1_mag_35        = sqrt(sum(fc1_vals_35.^2,2));
 intfStress_35     = fc1_mag_35./lgth_vals_35;
 
 goodEdgeSet_8=findEdges(groupedClusters,'kPa',8,'myo',0,'myoGlb',0,'errF',Inf,'errs',0);
-[deg_vals_8,lgth_vals_8,fc1_vals_8,nVec_vals_35]=collectEdgeValues(groupedClusters,goodEdgeSet_8,'deg','lgth','fc1','nVec');
+[deg_vals_8,lgth_vals_8,fc1_vals_8,nVec_vals_8]=collectEdgeValues(groupedClusters,goodEdgeSet_8,'deg','lgth','fc1','nVec');
 deg_vals_sorted_8 =sort(deg_vals_8,2);
 fc1_mag_8         = sqrt(sum(fc1_vals_8.^2,2));
 intfStress_8      = fc1_mag_8./lgth_vals_8;
@@ -75,14 +89,15 @@ while ~isempty(deg_dbl_sorted_groups_35)
     checkVec_35=deg_dbl_sorted_groups_35==degGroup_35;
     degCount_35=sum(checkVec_35);
     deg_dbl_sorted_groups_35(checkVec_35)=[];
-    M{label}=['deg: ',num2str(degGroup_35),'-x; [n= ',num2str(degCount_35),']'];
+    %M_35{label}=['deg: ',num2str(degGroup_35),'-x; [n= ',num2str(degCount_35),']'];
+    M_35{label}=['35kPa: ',num2str(degCount_35)];
     label=label+1;
 end
-% boxplot(fc_mag_dbl_sorted_35,num2str(deg_dbl_sorted_35(:,1)),'labels',M,'notch','on')
+% boxplot(fc_mag_dbl_sorted_35,num2str(deg_dbl_sorted_35(:,1)),'labels',M_35,'notch','on')
 % title(['Interface force for edges with connectivity: ',num2str(1:max(deg_vals_35))])
 % xlabel('Minimal deg. of connectivity')
 % ylabel('Interface force [nN]')
-% clear M
+%% clear M_35
 
 
 % figure()
@@ -102,14 +117,14 @@ while ~isempty(deg_dbl_sorted_groups_8)
     checkVec_8=deg_dbl_sorted_groups_8==degGroup_8;
     degCount_8=sum(checkVec_8);
     deg_dbl_sorted_groups_8(checkVec_8)=[];
-    M{label}=['deg: ',num2str(degGroup_8),'-x; [n= ',num2str(degCount_8),']'];
+    M_8{label}=['8kPa: ',num2str(degCount_8),';'];
     label=label+1;
 end
-% boxplot(fc_mag_dbl_sorted_8,num2str(deg_dbl_sorted_8(:,1)),'labels',M,'notch','on')
-% title(['Interface force for edges with connectivity: ',num2str(1:max(deg_vals_8))])
-% xlabel('Minimal deg. of connectivity')
-% ylabel('Interface force [nN]')
-% clear M
+boxplot(fc_mag_dbl_sorted_8,num2str(deg_dbl_sorted_8(:,1)),'labels',M_8,'notch','on')
+title(['Interface force for edges with connectivity: ',num2str(1:max(deg_vals_8))])
+xlabel('Minimal deg. of connectivity')
+ylabel('Interface force [nN]')
+%clear M_8
 
 
 maxDeg=(max([deg_dbl_sorted_8(:,1);deg_dbl_sorted_35(:,1)]));
@@ -117,11 +132,14 @@ G8kPa    =repmat({'8kPa'} ,length(deg_dbl_sorted_8(:,1)) ,1);
 G35kPa   =repmat({'35kPa'},length(deg_dbl_sorted_35(:,1)),1);
 
 G1=[deg_dbl_sorted_8(:,1);deg_dbl_sorted_35(:,1)];
-G2=vertcat(G8kPa,G35kPa);
+Gcol=vertcat(G8kPa,G35kPa);
+
+G2=[M_8(deg_dbl_sorted_8(:,1)),M_35(deg_dbl_sorted_35(:,1))]';
 
 figure()
 % plot both together:
-boxplot([fc_mag_dbl_sorted_8;fc_mag_dbl_sorted_35],{G1 G2},'factorgap',[20 0],'colorgroup',G2,'color','bg','notch','on','symbol','+')
+% 'labels',{[M_8 M_35]}
+boxplot([fc_mag_dbl_sorted_8;fc_mag_dbl_sorted_35],{G1 G2},'factorgap',[20 0],'colorgroup',Gcol,'color','bg','notch','on','symbol','+')
 % this should be compared with the following to make sure that the counting is correct:
 % h=boxplot(fc_mag_dbl_sorted,num2str(deg_dbl_sorted(:,1)));
 title(['Interface force for edges with connectivity: ',num2str(1:maxDeg)])
@@ -133,16 +151,15 @@ box on
 
 figure()
 % plot both together:
-boxplot([intfStress_dbl_sorted_8;intfStress_dbl_sorted_35],{G1 G2},'factorgap',[20 0],'colorgroup',G2,'color','bg','notch','on','symbol','+')
+boxplot([intfStress_dbl_sorted_8;intfStress_dbl_sorted_35],{G1 G2},'factorgap',[20 0],'colorgroup',Gcol,'color','bg','notch','on','symbol','+')
 % this should be compared with the following to make sure that the counting is correct:
 % h=boxplot(fc_mag_dbl_sorted,num2str(deg_dbl_sorted(:,1)));
-title(['Interface force for edges with connectivity: ',num2str(1:maxDeg)])
+title(['Interface stress for edges with connectivity: ',num2str(1:maxDeg)])
 xlabel('Minimal deg. of connectivity')
-ylabel('Interface force [nN]')
+ylabel('Interface stress [nN/um]')
 set(gca,'fontsize',20)
 ylim([0,8])
 box on
-clear M
 
 % plot the stress:
 figure()
