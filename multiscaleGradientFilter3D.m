49ba7559b1ce8cbd237372d243e9d6673be3a40b
Hunter Elliott <hunter_elliott@hms.harvard.edu>
2011-09-29 21:09:54 +0000

    AM   multiscaleGradientFilter3D.m - needs doc.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/3dmigration/main@7461 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ multiscaleGradientFilter3D.m
@@ -0,0 +1,28 @@
+function [maxResp,dX,dY,dZ,maxRespScale] = multiscaleSurfaceFilter3D(imageIn)
+
+sMin = 1;
+sMax = 3;
+nSig = 10;
+
+sigmas = linspace(sMin,sMax,nSig);
+
+maxResp = zeros(size(imageIn));
+maxRespScale = zeros(size(imageIn));
+dX = zeros(size(imageIn));
+dY = zeros(size(imageIn));
+dZ = zeros(size(imageIn));
+
+for j = 1:nSig
+            
+    [dXtmp,dYtmp,dZtmp] = gradientFilterGauss3D(imageIn,sigmas(j));            
+    
+    sMag = sqrt(dXtmp.^2 + dYtmp .^2 + dZtmp.^2);
+    
+    isBetter = sMag > maxResp;    
+    maxRespScale(isBetter) = sigmas(j); 
+    maxResp(isBetter) = sMag(isBetter);
+    dX(isBetter) = dXtmp(isBetter);
+    dY(isBetter) = dYtmp(isBetter);
+    dZ(isBetter) = dZtmp(isBetter);
+                
+end
