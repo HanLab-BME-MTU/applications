29e4b2742041e43e6e83f3a0169f8c62626d56fc
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2011-10-17 17:41:29 +0000

    Fixed bug in transformationProcessGUI due to package uniformisation
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/biosensors/main@7612 0bcb941b-74d1-0310-bb26-e6636707f451

--- transformationProcessGUI.m
+++ transformationProcessGUI.m
@@ -22,7 +22,7 @@ function varargout = transformationProcessGUI(varargin)
 
 % Edit the above text to modify the response to help transformationProcessGUI
 
-% Last Modified by GUIDE v2.5 09-Nov-2010 20:26:19
+% Last Modified by GUIDE v2.5 17-Oct-2011 13:39:04
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -46,126 +46,23 @@ end
 
 % --- Executes just before transformationProcessGUI is made visible.
 function transformationProcessGUI_OpeningFcn(hObject, eventdata, handles, varargin)
-% Available tools 
-% UserData data:
-%       userData.mainFig - handle of main figure
-%       userData.handles_main - 'handles' of main figure
-%       userData.procID - The ID of process in the current package
-%       userData.crtProc - handle of current process
-%       userData.crtPackage - handles of current package
-%       userData.procConstr - constructor of current process
-%
-%       userData.questIconData - help icon image information
-%       userData.colormap - color map information
-%
 
-[copyright openHelpFile] = userfcn_softwareConfig(handles);
-set(handles.text_copyright, 'String', copyright)
+processGUI_OpeningFcn(hObject, eventdata, handles, varargin{:},'initChannel',1);
 
 userData = get(handles.figure1, 'UserData');
-% Choose default command line output for transformationProcessGUI
-handles.output = hObject;
 
-% Get main figure handle and process id
-t = find(strcmp(varargin,'mainFig'));
-userData.mainFig = varargin{t+1};
-userData.procID = varargin{t+2};
-userData.handles_main = guidata(userData.mainFig);
-
-% Get current package and process
-userData_main = get(userData.mainFig, 'UserData');
-userData.crtPackage = userData_main.crtPackage;
-userData.crtProc = userData.crtPackage.processes_{userData.procID};
-
-% Get current process constructer
-eval ( [ 'userData.procConstr = @', ...
-    userData.crtPackage.getProcessClassNames{userData.procID},';']);
-
-% If process does not exist, create a default one in user data.
-if isempty(userData.crtProc)
-    userData.crtProc = userData.procConstr(userData_main.MD(userData_main.id), ...
-                                userData.crtPackage.outputDirectory_);
-end
-
-% Get icon infomation
-userData.questIconData = userData_main.questIconData;
-userData.colormap = userData_main.colormap;
-
-% ---------------------- Channel Setup -------------------------
-
+%  Parameter Setup
 funParams = userData.crtProc.funParams_;
 
-% Set up available input channels
-set(handles.listbox_1, 'String', {userData_main.MD(userData_main.id).channels_.channelPath_},...
-        'Userdata', 1: length(userData_main.MD(userData_main.id).channels_));
-    
-% Set up selected input data channels and channel index
-parentI = find( userData.crtPackage.depMatrix_(userData.procID,:) );
-
-if isempty(parentI) || ~isempty( userData.crtPackage.processes_{userData.procID} )
-    
-    % If process has no dependency, or process already exists, display saved channels 
-    set(handles.listbox_2, 'String', ...
-        {userData_main.MD(userData_main.id).channels_(funParams.ChannelIndex).channelPath_}, ...
-        'Userdata',funParams.ChannelIndex);
-    
-elseif isempty( userData.crtPackage.processes_{userData.procID} )
-    % If new process
-        empty = false;
-        for i = parentI
-           if isempty(userData.crtPackage.processes_{i})
-               empty = true;
-               break;
-           end
-        end
-            
-        if ~empty
-
-            % If all dependent processes exist
-            channelIndex = userData.crtPackage.processes_{parentI(1)}.funParams_.ChannelIndex;
-            for i = 2: length(parentI)
-                channelIndex = intersect(channelIndex, ...
-                    userData.crtPackage.processes_{parentI(i)}.funParams_.ChannelIndex);
-            end  
-            
-            if ~isempty(channelIndex)
-                
-                set(handles.listbox_2, 'String', ...
-                    {userData_main.MD(userData_main.id).channels_(channelIndex).channelPath_}, ...
-                    'Userdata',channelIndex);     
-            end
-        end
+if any(~cellfun(@isempty, funParams.TransformFilePaths))
+    set(handles.listbox_transform, 'String', ...
+        funParams.TransformFilePaths(funParams.ChannelIndex))
 end
 
-% ---------------------- Parameter Setup -------------------------
+set(handles.checkbox_mask, 'Value', funParams.TransformMasks)
 
-if any(cellfun(@(x)~isempty(x), funParams.TransformFilePaths))
-    set(handles.listbox_transform, 'String', funParams.TransformFilePaths(funParams.ChannelIndex))
-end
-
-if ~funParams.TransformMasks
-    set(handles.checkbox_mask, 'Value', 0)
-end
-
-% ----------------------Set up help icon------------------------
-
-% Set up help icon
-set(hObject,'colormap',userData.colormap);
-% Set up package help. Package icon is tagged as '0'
-axes(handles.axes_help);
-Img = image(userData.questIconData); 
-set(gca, 'XLim',get(Img,'XData'),'YLim',get(Img,'YData'),...
-    'visible','off','YDir','reverse');
-set(Img,'ButtonDownFcn',@icon_ButtonDownFcn);
-if openHelpFile
-    set(Img, 'UserData', struct('class',class(userData.crtProc)))
-end
-
-% ----------------------------------------------------------------
-
-% Update user data and GUI data
-set(userData.mainFig, 'UserData', userData_main);
-set(hObject, 'UserData', userData);
+% Choose default command line output for transformationProcessGUI
+handles.output = hObject;
 
 uicontrol(handles.pushbutton_done)
 guidata(hObject, handles);
@@ -196,9 +93,9 @@ userData_main = get(userData.mainFig, 'UserData');
 
 % -------- Check user input --------
 
-channelIndex = get (handles.listbox_2, 'Userdata');
+channelIndex = get (handles.listbox_selectedChannels, 'Userdata');
 
-if isempty(get(handles.listbox_2, 'String'))
+if isempty(get(handles.listbox_selectedChannels, 'String'))
     errordlg('Please select at least one channel as input channel.','Setting Error','modal') 
     return;
 end
@@ -285,83 +182,6 @@ end
 userData.userDir = dir;
 
 set(handles.figure1, 'Userdata', userData)
-
-% --- Executes on button press in checkbox_all.
-function checkbox_all_Callback(hObject, eventdata, handles)
-% Hint: get(hObject,'Value') returns toggle state of checkbox_all
-contents1 = get(handles.listbox_1, 'String');
-
-chanIndex1 = get(handles.listbox_1, 'Userdata');
-chanIndex2 = get(handles.listbox_2, 'Userdata');
-
-% Return if listbox1 is empty
-if isempty(contents1)
-    return;
-end
-
-switch get(hObject,'Value')
-    case 1
-        set(handles.listbox_2, 'String', contents1);
-        chanIndex2 = chanIndex1;
-    case 0
-        set(handles.listbox_2, 'String', {}, 'Value',1);
-        chanIndex2 = [ ];
-end
-set(handles.listbox_2, 'UserData', chanIndex2);
-
-
-% --- Executes on button press in pushbutton_select.
-function pushbutton_select_Callback(hObject, eventdata, handles)
-% call back function of 'select' button
-
-contents1 = get(handles.listbox_1, 'String');
-contents2 = get(handles.listbox_2, 'String');
-id = get(handles.listbox_1, 'Value');
-
-% If channel has already been added, return;
-chanIndex1 = get(handles.listbox_1, 'Userdata');
-chanIndex2 = get(handles.listbox_2, 'Userdata');
-
-for i = id
-    if any(strcmp(contents1{i}, contents2) )
-        continue;
-    else
-        contents2{end+1} = contents1{i};
-        
-        chanIndex2 = cat(2, chanIndex2, chanIndex1(i));
-
-    end
-end
-
-set(handles.listbox_2, 'String', contents2, 'Userdata', chanIndex2);
-
-
-% --- Executes on button press in pushbutton_delete.
-function pushbutton_delete_Callback(hObject, eventdata, handles)
-% Call back function of 'delete' button
-contents = get(handles.listbox_2,'String');
-id = get(handles.listbox_2,'Value');
-
-% Return if list is empty
-if isempty(contents) || isempty(id)
-    return;
-end
-
-% Delete selected item
-contents(id) = [ ];
-
-% Delete userdata
-chanIndex2 = get(handles.listbox_2, 'Userdata');
-chanIndex2(id) = [ ];
-set(handles.listbox_2, 'Userdata', chanIndex2);
-
-% Point 'Value' to the second last item in the list once the 
-% last item has been deleted
-if (id >length(contents) && id>1)
-    set(handles.listbox_2,'Value',length(contents));
-end
-% Refresh listbox
-set(handles.listbox_2,'String',contents);
 
 % --- Executes during object deletion, before destroying properties.
 function figure1_DeleteFcn(hObject, eventdata, handles)
