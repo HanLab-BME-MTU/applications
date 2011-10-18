e45e0c27e0eccfcdfc9222ced54929fc65496019
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2011-10-18 21:38:31 +0000

    Fixed bug due to depMatrix_ property replacement by getDependencyMatrix
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/biosensors/main@7631 0bcb941b-74d1-0310-bb26-e6636707f451

--- shadeCorrectionProcessGUI.m
+++ shadeCorrectionProcessGUI.m
@@ -105,7 +105,7 @@ set(handles.listbox_1, 'String', {userData_main.MD(userData_main.id).channels_.c
         'Userdata', 1: length(userData_main.MD(userData_main.id).channels_));
     
 % Set up selected input data channels and channel index
-parentI = find( userData.crtPackage.depMatrix_(userData.procID,:) );
+parentI = find( userData.crtPackage.getDependencyMatrix(userData.procID,:) );
 
 if isempty(parentI) || ~isempty( userData.crtPackage.processes_{userData.procID} )
     
