cd45ce527e824e61dfcba6883d4ce4f7deb8fcbe
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-10-26 23:06:33 +0000

    Updated for multi-segment tracks.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@7686 0bcb941b-74d1-0310-bb26-e6636707f451

--- getTrackStack.m
+++ getTrackStack.m
@@ -2,7 +2,7 @@
 %     Returns the image data around a track in a 4-sigma window as a cell array
 %     containing all channels.
 
-% Francois Aguet, Jan 26 2011
+% Francois Aguet, Jan 26 2011 (last modified 10/26/2011)
 
 function [stack, dx, dy] = getTrackStack(data, track, varargin)
 
@@ -10,17 +10,18 @@ ip = inputParser;
 ip.CaseSensitive = false;
 ip.addRequired('data', @isstruct);
 ip.addRequired('track', @isstruct);
-ip.addParamValue('Reference', 'frame', @(x) strcmpi(x, 'track') | strcmpi(x, 'frame'));
+ip.addParamValue('Reference', 'track', @(x) strcmpi(x, 'track') | strcmpi(x, 'frame'));
 ip.addParamValue('WindowWidth', 4, @isscalar);
 ip.addParamValue('Buffer', 5, @isscalar);
 ip.parse(data, track, varargin{:});
 buffer = ip.Results.Buffer;
 
 nc = length(data.channels);
-masterChannel = find(strcmp(data(1).source, data(1).channels));
-slaveChannels = setdiff(1:nc, masterChannel);
+mCh = strcmp(data(1).source, data(1).channels);
+ny = data.imagesize(1);
+nx = data.imagesize(2);
 
-sigma = getGaussianPSFsigma(data.NA, data.M, data.pixelSize, name2wavelength(data.markers{masterChannel}));
+sigma = getGaussianPSFsigma(data.NA, data.M, data.pixelSize, name2wavelength(data.markers{mCh}));
 w = ceil(ip.Results.WindowWidth*sigma);
 
 % buffer with 5 frames before and after
@@ -31,39 +32,40 @@ nf = length(idx);
 
 stack = cell(nc,nf);
 
-if isfield(track, 'startBuffer') && ~isempty(track.startBuffer.x{1})
-    x = [track.startBuffer.x{1} track.x{1} track.endBuffer.x{1}];
-    y = [track.startBuffer.y{1} track.y{1} track.endBuffer.y{1}];
-else
-    x = track.x{1};
-    y = track.y{1};
-end
-if size(x,1)==1 % expand if master channel detection only
-    x = repmat(x, [nc 1]);
-    y = repmat(y, [nc 1]);
-end
+ns = numel(track.x);
+xv = arrayfun(@(s) [track.startBuffer.x{s} track.x{s} track.endBuffer.x{s}], 1:ns, 'UniformOutput', false);
+yv = arrayfun(@(s) [track.startBuffer.y{s} track.y{s} track.endBuffer.y{s}], 1:ns, 'UniformOutput', false);
 
-if strcmpi(ip.Results.Reference, 'frame')
-    xi = repmat(round(mean(x,2)), [1 nf]);
-    yi = repmat(round(mean(y,2)), [1 nf]);
-else
-    xi = round(x);
-    yi = round(y);
-end
+mu_x = cellfun(@(s) round(mean(s,2)), xv, 'UniformOutput', false);
+mu_x = [mu_x{:}];
+mu_x = mu_x(mCh,:);
+mu_y = cellfun(@(s) round(mean(s,2)), yv, 'UniformOutput', false);
+mu_y = [mu_y{:}];
+mu_y = mu_y(mCh,:);
 
-% adjust w if track is close to image border
-w = min([w min(xi(:))-1 min(yi(:))-1 data.imagesize(2)-max(xi(:)) data.imagesize(1)-max(yi(:))]);
+if ns==1 && strcmpi(ip.Results.Reference, 'track') % align frames to track
+    xi = round(xv{1});
+    yi = round(yv{1});
+    % ensure that window falls within frame bounds
+    x0 = xi - min(min(xi,[],2)-1,w,[],2);
+    x1 = xi + min(nx-max(xi,[],2),w,[],2);
+    y0 = yi - min(min(yi,[],2)-1,w,[],2);
+    y1 = yi + min(ny-max(yi,[],2),w,[],2);
+    dx = xv{1}-xi;
+    dy = yv{1}-yi;
+else
+    x0 = repmat(max(1, min(mu_x)-w), [1 nf]);
+    x1 = repmat(min(data.imagesize(2), max(mu_x)+w), [1 nf]);
+    y0 = repmat(max(1, min(mu_y)-w), [1 nf]);
+    y1 = repmat(min(data.imagesize(1), max(mu_y)+w), [1 nf]);
+    dx = [];
+    dy = [];
+end
 
 % load all visible frames of this track and store
-for c = [masterChannel slaveChannels]
-    tifFiles = dir([data.channels{c} '*.tif*']);
-    tifFiles = tifFiles(idx);
+for c = 1:nc
     for k = 1:nf
-        frame = imread([data.channels{c} tifFiles(k).name]);
-        stack{c,k} = frame(yi(c,k)-w:yi(c,k)+w, xi(c,k)-w:xi(c,k)+w);
+        frame = imread(data.framePaths{c}{k});
+        stack{c,k} = frame(y0(k):y1(k), x0(k):x1(k));
     end
 end
-
-% return deviation
-dx = x-xi;
-dy = y-yi;
