8c4ce67401dee79056ae735d3969a78ad1edd6ea
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-11-01 13:57:02 +0000

    Sorting works again
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@7732 0bcb941b-74d1-0310-bb26-e6636707f451

--- iofun/loadTracks.m
+++ iofun/loadTracks.m
@@ -7,7 +7,7 @@ ip.CaseSensitive = false;
 ip.addRequired('data', @(x) isstruct(x) & numel(x)==1);
 ip.addParamValue('FileName', 'trackAnalysis.mat', @ischar); 
 ip.addParamValue('Cutoff', 4, @isscalar);
-% ip.addParamValue('Sort', 'on', @(x) strcmpi(x, 'on') | strcmpi(x, 'off'));
+ip.addParamValue('Sort', true, @islogical);
 % ip.addParamValue('PostProc', [], @isscalar);
 ip.addParamValue('Type', 'valid', @(x) any(strcmpi(x, {'valid', 'all'})));
 ip.parse(data, varargin{:});
@@ -16,8 +16,16 @@ cutoff = ip.Results.Cutoff * data.framerate;
 
 load([data.source 'Tracking' filesep ip.Results.FileName]);
 
+if ip.Results.Sort
+    [~, sortIdx] = sort([tracks.lifetime_s], 'descend');
+    tracks = tracks(sortIdx);
+    trackInfo.track2segIndex = trackInfo.track2segIndex(sortIdx);
+end
+
+
 if strcmpi(ip.Results.Type, 'valid')
     idx = [tracks.valid]==1 & [tracks.lifetime_s] >= cutoff & [tracks.nSeg]==1;
+    
 %     if ~isempty(ip.Results.PostProc)
 %         kLevel = norminv(1-0.05/2.0, 0, 1); % ~2 std above background
 %         sb = arrayfun(@(t) sum(sum(t.startBuffer.A(1,:) > t.startBuffer.sigma_r(1,:)*kLevel))>1, tracks);
@@ -31,8 +39,12 @@ if strcmpi(ip.Results.Type, 'valid')
 else % if all
     idx = [tracks.lifetime_s] >= cutoff;
 end
-
 tracks = tracks(idx);
+
+
+
+
+
 
 if nargout>1
     
@@ -57,10 +69,3 @@ if nargout>1
     trackInfo.lifetimes_f = [tracks.end]-[tracks.start]+1;
 end
 
-% if strcmpi(ip.Results.Sort, 'on')
-%     rTrackIdx = find([tracks.type]==1);%find(arrayfun(@(t) ~iscell(t.x), tracks));
-%     mTrackIdx = setdiff(1:length(tracks), rTrackIdx);
-%     [~, rSortIdx] = sort([tracks(rTrackIdx).lifetime_s], 'descend');
-%     [~, mSortIdx] = sort([tracks(mTrackIdx).lifetime_s], 'descend');
-%     tracks = tracks([rSortIdx mSortIdx+rTrackIdx(end)]);
-% end
