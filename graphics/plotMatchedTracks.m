3e21fa3ff7ffa5d8f4e46880b7ce47d5bace0453
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-12-01 07:01:25 +0000

    Added options, improved display
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@7877 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/plotMatchedTracks.m
+++ graphics/plotMatchedTracks.m
@@ -11,39 +11,47 @@ fillDarkBuffer = hsv2rgb([hues(2) 0.2 0.85]);
 kLevel = norminv(1-0.05/2.0, 0, 1); % ~2 std above background
 
 
-pos = get(0, 'DefaultFigurePosition');
-ssize = get(0, 'ScreenSize');
-b = min(ssize(4)-pos(4), 400);
-pos(2) = pos(2) - b;
-pos(4) = pos(4) + b;
-figure('Position', pos, 'PaperPositionMode', 'auto');
+% pos = get(0, 'DefaultFigurePosition');
+% ssize = get(0, 'ScreenSize');
+% b = min(ssize(4)-pos(4), 400);
+% pos(2) = pos(2) - b;
+% pos(4) = pos(4) + b;
+% figure('Position', pos, 'PaperPositionMode', 'auto');
 
-ha(1) = axes('Position', [0.15 0.7 0.8 0.27]);
-plotTrack(data, masterTrack, 1, 1, 'Handle', ha(1), 'Legend', 'hide');
+figure('Position', [440 378 560 720], 'PaperPositionMode', 'auto');
 
-ha(2) = axes('Position', [0.15 0.4 0.8 0.27]);
-plotTrack(data, masterTrack, 1, 2, 'Handle', ha(2), 'Legend', 'hide');
+ha(1) = axes('Position', [0.15 0.72 0.8 0.27]);
+hold on;
+plotTrack(data, masterTrack, 1, 'Handle', ha(1), 'DisplayMode', 'print');
+YLim{1} = get(ha(1), 'YLim');
+
+ha(2) = axes('Position', [0.15 0.42 0.8 0.27]);
+hold on;
+plotTrack(data, masterTrack, 2, 'Handle', ha(2), 'DisplayMode', 'print');
+YLim{2} = get(ha(2), 'YLim');
 
 % plot track fragments
-ha(3) = axes('Position', [0.15 0.1 0.8 0.27]);
+ha(3) = axes('Position', [0.15 0.08 0.8 0.27]);
+hold(ha(3), 'on');
 for k = 1:length(slaveTracks)
     
-    % Plot track
-    A = slaveTracks(k).A(1,:);
-    c = slaveTracks(k).c(1,:);
-    sigma_r = slaveTracks(k).sigma_r(1,:);
+    A = slaveTracks(k).A{1}(1,:);
+    c = slaveTracks(k).c{1}(1,:);
+    bgcorr = nanmean(c);
+    c = c-bgcorr;
+    sigma_r = slaveTracks(k).sigma_r{1}(1,:);
     t = (slaveTracks(k).start-1:slaveTracks(k).end-1)*data.framerate;
     
     % alpha = 0.05 level
     fill([t t(end:-1:1)], [c c(end:-1:1)+kLevel*sigma_r(end:-1:1)],...
         fillDark, 'EdgeColor', 'none', 'Parent', ha(3));
-    hold(ha(3), 'on');
+    
 
-    gapIdx = arrayfun(@(x,y) x:y, slaveTracks(k).gapStarts, slaveTracks(k).gapEnds, 'UniformOutput', false);
+    gapIdx = arrayfun(@(x,y) x:y, slaveTracks(k).gapStarts{1}, slaveTracks(k).gapEnds{1}, 'UniformOutput', false);
     gapIdx = [gapIdx{:}];
     
     % plot amplitude std.
-    sigma_a = slaveTracks(k).A_pstd(1,:);
+    sigma_a = slaveTracks(k).A_pstd{1}(1,:);
     
     rev = c+A-sigma_a;
     fill([t t(end:-1:1)], [c+A+sigma_a rev(end:-1:1)],...
@@ -52,24 +60,43 @@ for k = 1:length(slaveTracks)
     % plot track
     ampl = A+c;
     ampl(gapIdx) = NaN;
-    plot(ha(3), t, ampl, '.-', 'Color', trackColor, 'LineWidth', 1);
+    plot(ha(3), t, ampl, '.-', 'Color', trackColor, 'LineWidth', 2, 'MarkerSize', 21);
     
     ampl = A+c;
     ampl(setdiff(gapIdx, 1:length(ampl))) = NaN;
     if ~isempty(gapIdx)
-        lh(3) = plot(ha(3), t, ampl, '--', 'Color', trackColor, 'LineWidth', 1);
-        lh(4) = plot(ha(3), t(gapIdx), A(gapIdx)+c(gapIdx), 'o', 'Color', trackColor, 'MarkerFaceColor', 'w', 'LineWidth', 1);
+        lh(3) = plot(ha(3), t, ampl, '--', 'Color', trackColor, 'LineWidth', 2);
+        lh(4) = plot(ha(3), t(gapIdx), A(gapIdx)+c(gapIdx), 'o', 'Color', trackColor,...
+            'MarkerFaceColor', 'w', 'LineWidth', 2, 'MarkerSize', 7);
     end
     
     % plot background level
-    plot(ha(3), t, c, '-', 'Color', trackColor);
+    plot(ha(3), t, c, '-', 'Color', trackColor, 'LineWidth', 2);
 end
-set(ha(3), 'XLim', get(ha(2), 'XLim'), 'YLim', get(ha(2), 'YLim'), 'Box', 'off');
 
 set(ha(1:2), 'XTickLabel', []);
-set(ha, 'FontName', 'Helvetica', 'FontSize', 16);
+set(ha, 'FontName', 'Helvetica', 'FontSize', 20, 'LineWidth', 2, 'TickDir', 'out');
 xlabel('Time (s)');
-arrayfun(@(x) ylabel(x, 'Fluo. intensity (A.U.)'), ha);
+hy = arrayfun(@(x) ylabel(x, 'Intensity (A.U.)'), ha);
+
+set(ha(1), 'YLim', YLim{1});
+set(ha(2), 'YLim', YLim{2});
+
+set(ha(3), 'XLim', get(ha(2), 'XLim'), 'YLim', get(ha(2), 'YLim'),...
+    'YTick', get(ha(2), 'YTick'), 'Box', 'off');
+
+
+xpos = zeros(1,3);
+for k = 1:3
+    pos = get(hy(k), 'Position');
+    xpos(k) = pos(1);
+end
+xpos = min(xpos);
+for k = 1:3
+    pos = get(hy(k), 'Position');
+    pos(1) = xpos;
+    set(hy(k), 'Position', pos);
+end
 
 
 
--- graphics/plotTrack.m
+++ graphics/plotTrack.m
@@ -24,11 +24,16 @@ ip.addRequired('ch', @isnumeric);
 ip.addParamValue('Visible', 'on', @(x) any(strcmpi(x, {'on', 'off'})));
 ip.addParamValue('FileName', [], @ischar);
 ip.addParamValue('Handle', []);
-ip.addParamValue('Legend', 'show', @(x) any(strcmpi(x, {'show','hide'})));
+ip.addParamValue('Legend', 'hide', @(x) any(strcmpi(x, {'show','hide'})));
 ip.addParamValue('Segment', 1, @isscalar);
 ip.addParamValue('Background', 'on', @(x) any(strcmpi(x, {'on', 'off'})));
 ip.addParamValue('BackgroundValue', 'zero', @(x) any(strcmpi(x, {'zero', 'data'})));
 ip.addParamValue('Hues', []);
+ip.addParamValue('Time', 'Movie', @(x) any(strcmpi(x, {'Movie', 'Track'})));
+ip.addParamValue('XTick', []);
+ip.addParamValue('YTick', []);
+ip.addParamValue('XLim', []);
+ip.addParamValue('DisplayMode', 'Screen', @(x) any(strcmpi(x, {'Print', 'Screen'})));
 ip.parse(data, track, ch, varargin{:});
 
 s = ip.Results.Segment;
@@ -47,6 +52,13 @@ else
     ha = axes('Position', [0.15 0.15 0.8 0.8]);
     standalone = true;
 end
+
+if strcmpi(ip.Results.Time, 'Track')
+    dt = track.t{1}(1); % first segment always contains first time point
+else
+    dt = 0;
+end
+
 
 trackColor = hsv2rgb([hues(ch) 1 0.8]);
 fillLight = hsv2rgb([hues(ch) 0.4 1]);
@@ -76,7 +88,7 @@ for s = 1:track.nSeg
         bgcorr = 0;
     end
     sigma_r = track.sigma_r{s}(ch,:);
-    t = track.t{s};
+    t = track.t{s} - dt;
     
     % alpha = 0.05 level
     if strcmpi(ip.Results.Background, 'on')
@@ -92,7 +104,7 @@ for s = 1:track.nSeg
     sigma_a = track.A_pstd{s}(ch,:);
     
     rev = c+A-sigma_a;
-    fill([t t(end:-1:1)], [c+A+sigma_a rev(end:-1:1)],...
+    lh(2) = fill([t t(end:-1:1)], [c+A+sigma_a rev(end:-1:1)],...
         fillLight, 'EdgeColor', 'none', 'Parent', ha);
     
     % plot track
@@ -100,21 +112,22 @@ for s = 1:track.nSeg
     if ch==mCh
         ampl(gapIdx) = NaN;
     end
-    lh(2) = plot(ha, t, ampl, '.-', 'Color', trackColor, 'LineWidth', 1);
+    lh(3) = plot(ha, t, ampl, '.-', 'Color', trackColor, 'LineWidth', 1);
     
     % plot gaps separately
     if ch==mCh
         ampl = A+c;
-        ampl(setdiff(gapIdx, 1:length(ampl))) = NaN;
         if ~isempty(gapIdx)
-            lh(3) = plot(ha, t, ampl, '--', 'Color', trackColor, 'LineWidth', 1);
-            lh(4) = plot(ha, t(gapIdx), A(gapIdx)+c(gapIdx), 'o', 'Color', trackColor, 'MarkerFaceColor', 'w', 'LineWidth', 1);
+            % dashed line everywhere
+            lh(4) = plot(ha, t, ampl, '--', 'Color', trackColor, 'LineWidth', 1);
+            % plot gaps as white disks
+            lh(5) = plot(ha, t(gapIdx), A(gapIdx)+c(gapIdx), 'o', 'Color', trackColor, 'MarkerFaceColor', 'w', 'LineWidth', 1);
         end
     end
     
     % plot background level
     if strcmpi(ip.Results.Background, 'on')
-        lh(5) = plot(ha, t, c, '-', 'Color', trackColor);
+        lh(6) = plot(ha, t, c, '-', 'Color', trackColor);
     end    
 end
 
@@ -125,10 +138,10 @@ if isfield(track, 'startBuffer') && ~isempty(track.startBuffer.A{s})
     
     sigma_a = [track.startBuffer.A_pstd{s}(ch,:) track.A_pstd{s}(ch,1)];
     sigma_r = [track.startBuffer.sigma_r{s}(ch,:) track.sigma_r{s}(ch,1)];
-    t = [track.startBuffer.t{s} track.t{s}(1)];
+    t = [track.startBuffer.t{s} track.t{s}(1)] - dt;
     
     if strcmpi(ip.Results.Background, 'on')
-        fill([t t(end:-1:1)], [c c(end:-1:1)+kLevel*sigma_r(end:-1:1)],...
+        lh(12) = fill([t t(end:-1:1)], [c c(end:-1:1)+kLevel*sigma_r(end:-1:1)],...
             fillDarkBuffer, 'EdgeColor', 'none', 'Parent', ha);
     end
 
@@ -136,9 +149,9 @@ if isfield(track, 'startBuffer') && ~isempty(track.startBuffer.A{s})
     fill([t t(end:-1:1)], [c+A+sigma_a rev(end:-1:1)],...
         fillLightBuffer, 'EdgeColor', 'none', 'Parent', ha);
     
-    lh(6) = plot(ha, t, A+c, '.--', 'Color', trackColor, 'LineWidth', 1);
+    lh(7) = plot(ha, t, A+c, '.--', 'Color', trackColor, 'LineWidth', 1);
     if strcmpi(ip.Results.Background, 'on')
-        lh(7) = plot(ha, t, c, '--', 'Color', trackColor);
+        lh(8) = plot(ha, t, c, '--', 'Color', trackColor);
     end
 end
 
@@ -149,7 +162,7 @@ if isfield(track, 'endBuffer') && ~isempty(track.endBuffer.A{s})
     
     sigma_a = [track.A_pstd{s}(ch,end) track.endBuffer.A_pstd{s}(ch,:)];
     sigma_r = [track.sigma_r{s}(ch,end) track.endBuffer.sigma_r{s}(ch,:)];
-    t = [track.t{s}(end) track.endBuffer.t{s}];
+    t = [track.t{s}(end) track.endBuffer.t{s}] - dt;
     
     if strcmpi(ip.Results.Background, 'on')
         fill([t t(end:-1:1)], [c c(end:-1:1)+kLevel*sigma_r(end:-1:1)],...
@@ -160,16 +173,18 @@ if isfield(track, 'endBuffer') && ~isempty(track.endBuffer.A{s})
     fill([t t(end:-1:1)], [c+A+sigma_a rev(end:-1:1)],...
         fillLightBuffer, 'EdgeColor', 'none', 'Parent', ha);
     
-    lh(8) = plot(ha, t, A+c, '.--', 'Color', trackColor, 'LineWidth', 1);
+    lh(9) = plot(ha, t, A+c, '.--', 'Color', trackColor, 'LineWidth', 1);
     if strcmpi(ip.Results.Background, 'on')
-        lh(9) = plot(ha, t, c, '--', 'Color', trackColor);
+        lh(10) = plot(ha, t, c, '--', 'Color', trackColor);
     end
 end
 
 
 % legend
 if strcmpi(ip.Results.Legend, 'show')
-    l = legend(lh([2 5 1]), ['Amplitude ch. ' num2str(ch)], ['Background ch. ' num2str(ch)], '\alpha = 0.95 level', 'Location', 'NorthEast');
+    lh(11) = plot(-20:-10, rand(1,11), 'o--', 'MarkerSize', 7, 'LineWidth', 2, 'Color', trackColor, 'MarkerFaceColor', 'w');
+    %l = legend(lh([2 5 1]), ['Amplitude ch. ' num2str(ch)], ['Background ch. ' num2str(ch)], '\alpha = 0.95 level', 'Location', 'NorthEast');
+    l = legend(lh([3 2 11 6 1 12 8]), 'Intensity', 'Intensity uncertainty', 'Gap', 'Background intensity', 'Significance level (\alpha = 0.95)', 'Buffer', 'Buffer intensity', 'Location', 'NorthEast');
     %legend(l, ip.Results.Legend);
 end
 
@@ -186,41 +201,69 @@ else
     bEnd = 0;
 end
 tlength = track.end+bEnd - track.start-bStart + 1;
-set(ha, 'XLim', ([track.start-bStart-0.1*tlength track.end+bEnd+0.1*tlength]-1)*data.framerate, 'Layer', 'top');
+
+
+
+if ~isempty(ip.Results.XTick)
+    XTick = ip.Results.XTick;
+    set(ha, 'XTick', XTick, 'XLim', [XTick(1) XTick(end)]);
+end
+if ~isempty(ip.Results.XLim)
+    set(ha, 'XLim', ip.Results.XLim);
+end
+
+if ~isempty(ip.Results.YTick)
+    YTick = ip.Results.YTick;
+    YLim = [YTick(1) YTick(end)];
+    YTick(YTick<0) = [];
+    set(ha, 'YTick', YTick, 'YLim', YLim);
+else
+    YTick = get(ha, 'YTick');
+    YTick(YTick<0) = [];
+    set(ha, 'YTick', YTick, 'Layer', 'top');
+    % set(ha, 'XLim', ([track.start-bStart-0.1*tlength track.end+bEnd+0.1*tlength]-1)*data.framerate);
+
+end
+
+
 
 box off;
 
 
-
+% Bigger fonts, line widths etc
 if standalone
-    tfont = {'FontName', 'Helvetica', 'FontSize', 14};
-    sfont = {'FontName', 'Helvetica', 'FontSize', 18};
-    lfont = {'FontName', 'Helvetica', 'FontSize', 22};
+    tfont = {'FontName', 'Helvetica', 'FontSize', 20};
+    sfont = {'FontName', 'Helvetica', 'FontSize', 20};
+    lfont = {'FontName', 'Helvetica', 'FontSize', 24};
     
     if strcmpi(ip.Results.Legend, 'show')
         set(l, tfont{:});
     end
     
-    set(gca, 'LineWidth', 1.5, sfont{:});
+    set(gca, 'LineWidth', 2, sfont{:}, 'TickDir', 'out');
     xlabel('Time (s)', lfont{:})
     ylabel('Intensity (A.U.)', lfont{:});
+end
 
-    for k = lh([2 3 5 6:9])
+if strcmpi(ip.Results.DisplayMode, 'Print')
+    for k = lh([3 4 6 7:10])
         if ~isnan(k)
             set(k, 'LineWidth', 2);
         end
     end
     
-    for k = lh([2 6 8])
+    for k = lh([3 7 9])
         if ~isnan(k)
             set(k, 'MarkerSize', 21);
         end
     end
     
-    if ~isnan(lh(4))
-        set(lh(4), 'MarkerSize', 7, 'LineWidth', 2);
+    if ~isnan(lh(5))
+        set(lh(5), 'MarkerSize', 7, 'LineWidth', 2);
     end
 end
+    
+
 
 if ~isempty(ip.Results.FileName)
     fpath = [data.source 'Figures' filesep];
