d9a601d978af2ae26a40abd36467166ef7037873
Jeffrey Werbin <jeffrey_werbin@hms.harvard.edu>
2011-12-20 23:00:53 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/receptors/main@7982 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ ErbBproject/PointP_LocalSignal_Cross.m
@@ -0,0 +1,64 @@
+%Calculates local signal in point point process in a second point process
+%
+%Jeffrey L. Werbin
+%Harvard Medical School
+%
+%Last Update: 12/20/2011
+%
+%
+%
+
+function [out]=PointP_LocalSignal_Cross(posA,posB,rA,rB)
+%PointP_LocalSignal_Cross takes two related point processes and evaulates
+%two measures around each point in posA: the number of posB less than rB
+%from posA(i) and the number of other posA less than rA from posA(i)
+%both processes are assumed to fill the retangular area defined by the
+%maxium and minimum points in the 
+
+
+%edge = 1-(4/(3*pi))*((r/L)+(r/W))+((11/(3*pi))-1)*(r^2/(L*W));
+edge=1;
+
+%The number of total points
+n = size(posA,1);
+m = size(posB,1);
+
+pos = [posA ; posB]; %concatinating the two lists allows for processing with one dist matrix
+
+%find boundaries
+limX = [max(pos(:,1)), min(pos(:,1))];
+limY = [max(pos(:,2)), min(pos(:,2))];
+W = limX(1)-limX(2);
+L = limY(1)-limY(2);
+
+%to analyze only points > max(rA,rB) from the boundries of the rectangle
+%removes edge effect problems
+
+Rmax = max(rA,rB);
+x = posA(:,1)-limX(2);
+y = posA(:,2)-limY(2);
+
+ind = find( (x > Rmax) & (x < W -Rmax) & (y > Rmax) & (y < L-Rmax));
+
+%Make distance array
+dis = squareform(pdist(pos));
+
+%Uncomputed values are returned as NaNs
+out = NaN(n,2);
+
+%c
+for i=ind
+    %selects one point in a
+    tempA = dis(i,1:n);
+    tempB = dis(i,n+1:end);
+    
+    %applies the local conditions
+    tempA = tempA<rA & tempA>0;
+    tempB = tempB<rB & tempB>0;
+    
+    out(i,1)= sum(tempB(i,:))/(edge*pi*rB^2);
+    out(i,2)= sum(tempA(i,:))/(edge*pi*rA^2);
+end
+
+end
+    
