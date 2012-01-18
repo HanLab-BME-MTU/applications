ce46d9ff2789f575cc8e1c30169ea7881fb4fd3c
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2012-01-18 15:41:31 +0000

    Fixed bug in outputDirectory validation when initializing the package
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@8045 0bcb941b-74d1-0310-bb26-e6636707f451

--- QFSMPackage.m
+++ QFSMPackage.m
@@ -13,7 +13,7 @@ classdef QFSMPackage < Package
                 % Check input
                 ip =inputParser;
                 ip.addRequired('owner',@(x) isa(x,'MovieObject'));
-                ip.addOptional('outputDir',owner.outputDirectory_,@(x) isa(x,'MovieObject'));
+                ip.addOptional('outputDir',owner.outputDirectory_,@ischar);
                 ip.parse(owner,varargin{:});
                 outputDir = ip.Results.outputDir;
                 
