5e809088bcd69f57ceeb490d2da627af5785e9f1
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-02-06 19:07:10 +0000

    Adapted to new track structure/categories
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@8145 0bcb941b-74d1-0310-bb26-e6636707f451

--- iofun/loadTracks.m
+++ iofun/loadTracks.m
@@ -1,6 +1,23 @@
-% Francois Aguet, July 18 2011
+%tracks = loadTracks(data, varargin)
+%
+% Inputs:
+%
+%  data      : data structure
+% 
+% Options:
+%
+% 'Category' : 'Ia'  Single tracks with valid gaps
+%              'Ib'  Single tracks with invalid gaps
+%              'Ic'  Single tracks cut at beginning or end
+%              'Id'  Single tracks, persistent
+%              'IIa' Compound tracks with valid gaps
+%              'IIb' Compound tracks with invalid gaps
+%              'IIc' Compound tracks cut at beginning or end
+%              'IId' Compound tracks, persistent
 
-function [tracks trackInfo] = loadTracks(data, varargin)
+% Francois Aguet (last modified: 02/06/12)
+
+function tracks = loadTracks(data, varargin)
 
 ip = inputParser;
 ip.CaseSensitive = false;
@@ -9,22 +26,43 @@ ip.addParamValue('FileName', 'trackAnalysis.mat', @ischar);
 ip.addParamValue('Cutoff', 4, @isscalar);
 ip.addParamValue('Sort', true, @islogical);
 % ip.addParamValue('PostProc', [], @isscalar);
-ip.addParamValue('Type', 'valid', @(x) any(strcmpi(x, {'valid', 'all'})));
+ip.addParamValue('Category', 'all', @(x) any(strcmpi(x, {'all', 'Ia', 'Ib', 'Ic', 'Id', 'IIa', 'IIb', 'IIc', 'IId'})));
 ip.parse(data, varargin{:});
 
-cutoff = ip.Results.Cutoff * data.framerate;
+cutoff_s = ip.Results.Cutoff * data.framerate;
 
 load([data.source 'Tracking' filesep ip.Results.FileName]);
 
 if ip.Results.Sort
-    [~, sortIdx] = sort([tracks.lifetime_s], 'descend');
+    [~, sortIdx] = sort([tracks.lifetime_s], 'descend'); %#ok<NODEF>
     tracks = tracks(sortIdx);
-    trackInfo.track2segIndex = trackInfo.track2segIndex(sortIdx);
 end
 
-
-if strcmpi(ip.Results.Type, 'valid')
-    idx = [tracks.valid]==1 & [tracks.lifetime_s] >= cutoff & [tracks.nSeg]==1;
+singleIdx = [tracks.nSeg]==1;
+validGaps = arrayfun(@(t) max([t.gapStatus 4]), tracks)==4;
+vis = [tracks.visibility];
+switch ip.Results.Type
+    case 'Ia'
+        idx = singleIdx & validGaps & vis==1;
+    case 'Ib'
+        idx = singleIdx & ~validGaps & vis==1;
+    case 'Ic'
+        idx = singleIdx & vis==2;
+    case 'Id'
+        idx = singleIdx & vis==3;
+    case 'IIa'
+        idx = ~singleIdx & validGaps & vis==1;
+    case 'IIb'
+        idx = ~singleIdx & ~validGaps & vis==1;
+    case 'IIc'
+        idx = ~singleIdx & vis==2;
+    case 'IId'
+        idx = ~singleIdx & vis==3;
+    case 'all'
+        idx = 1:numel(tracks);
+end
+idx = idx & [tracks.lifetime_s] >= cutoff_s;
+tracks = tracks(idx);
     
 %     if ~isempty(ip.Results.PostProc)
 %         kLevel = norminv(1-0.05/2.0, 0, 1); % ~2 std above background
@@ -34,38 +72,4 @@ if strcmpi(ip.Results.Type, 'valid')
 %         % threshold max intensity
 %         maxRatio = arrayfun(@(t) max(t.A(1,:) ./ (t.sigma_r(1,:)*kLevel)), tracks);
 %         tracks(maxRatio < ip.Results.PostProc) = [];
-%         
 %     end
-else % if all
-    idx = [tracks.lifetime_s] >= cutoff;
-end
-tracks = tracks(idx);
-
-
-
-
-
-
-if nargout>1
-    
-    % segments to retain
-    sidx = trackInfo.track2segIndex(idx);
-    sidx = [sidx{:}];
-    
-    [gapMap, segStarts, segEnds, seg2trackIndex, track2segIndex] = catTrackFields(tracks, data.movieLength, 'gapVect', 1);
-   
-    trackInfo.x = trackInfo.x(sidx,:);
-    trackInfo.y = trackInfo.y(sidx,:);
-    trackInfo.gapMap = gapMap;
-    trackInfo.segStarts = segStarts;
-    trackInfo.segEnds = segEnds;
-    trackInfo.seg2trackIndex = seg2trackIndex;
-    trackInfo.track2segIndex = track2segIndex;
-    
-    trackInfo.nSeg = [tracks.nSeg];
-    trackInfo.status = [tracks.status];
-    trackInfo.valid = [tracks.valid];
-    
-    trackInfo.lifetimes_f = [tracks.end]-[tracks.start]+1;
-end
-
