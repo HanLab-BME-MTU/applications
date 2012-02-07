87e58048ac8487de6110fe60476bcd36b8b5d9de
Pascal Berard <pascal_berard@hms.harvard.edu>
2012-02-07 21:14:20 +0000

    Added the Data class with all its methods
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@8158 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ storm/@Data/Data.m
@@ -0,0 +1,117 @@
+classdef Data < handle & matlab.mixin.Copyable
+    
+    % ---------------------
+    % This class contains the input data of the algorithm, intermediate 
+    % and final outputs. 
+    % ---------------------
+    
+    properties (GetAccess = 'public',SetAccess = 'public')
+        
+        % #####################
+        % Data
+        % #####################
+        
+        % ROI
+        roiPosition = []; % Absolute position of the region of interest
+        roiSize = []; % The size of the region of interest
+
+        % Points
+        rawPoints; % The points before preprocessing
+        intensity; % Intensity of the point
+        frame; % Frame in which the point has been localized
+        error; % Localization precision (standard deviation) for x,y and z
+        points; % The points after preprocessing
+        
+        % #####################
+        % Outputs
+        % #####################
+        
+        % Points
+        orientation; % Normalized vector representing the orientation of a point
+        magnitude; % Magnitude of the orientation detection
+        neighbors; % Neighbor points indices
+        
+        % Clusters
+        clusters; % Cell array containing point indices
+        nullCluster; % Indices of the points belonging to the noise cluster
+        clusterColor; % The color of the cluster (Visualization)
+        
+        % Cluster models
+        modelIsOutOfDate; % Boolean: True if the cluster members changed and the model has not yet been updated
+        modelType; % The degree of the Bezier curve
+        modelLength;
+        modelRes; % Residuals of the fit ( n x 3 )
+        modelProj; % Projections of the point onto the model (t)
+        modelBezCP; % Bezier curve model control points
+        modelVar; % The mean square of the normalized distances 
+        
+        % Edges
+        initialEdges; % All allowed edges
+        edges; % Current edges
+        weights; % Corresponding weights
+        
+        % Running time
+        runTime; % Total algorithm runtime
+        
+        % #####################
+        % Visualization
+        % #####################
+        
+        edgesHistory;
+        clustersHistory;
+        modelsHistory;
+        
+        % #####################
+        % Simulation
+        % #####################
+        
+        simModelBezCP; % Control points of the model that generated the points
+        
+    end
+    
+    properties (GetAccess = 'public',SetAccess = 'private',Dependent = true)
+        nPoints;
+        nClusters;
+        parents; % Parent cluster
+    end
+    
+    methods(Static = true)
+        obj = load(fullPath);
+        obj = read(fullPath);
+    end
+    
+    methods
+        % Constructor
+        function obj = Data()
+        end
+        
+        display(obj);
+        save(obj,fullPath);
+
+        function value = get.nPoints(obj)
+            value = size(obj.points,1);
+        end
+        
+        function value = get.nClusters(obj)
+            value = size(obj.clusters,1);
+        end
+        
+        function parents = get.parents(obj)
+            % Update the cluster parents
+            nPoints = cellfun(@numel,obj.clusters);
+            lastPoint = cumsum(nPoints);
+            clusterIdxs = (1:size(lastPoint,1))';
+            clustersVector = horzcat(obj.clusters{:})';
+            
+            clusterIdxs = arrayfun(@(a,b) ones(b,1)*a,clusterIdxs,nPoints,'UniformOutput',false);
+            clusterIdxs = vertcat(clusterIdxs{:});
+
+            parents = zeros(obj.nPoints,1);
+            parents(clustersVector,1) = clusterIdxs;
+        end
+        
+    end % methods
+    
+end % class
+
+
