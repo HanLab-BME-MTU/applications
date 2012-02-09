53c5f2d8dbedc563f86fb0e918637b5e60d17d2b
Pascal Berard <pascal_berard@hms.harvard.edu>
2012-02-09 22:40:44 +0000

    STORM: stormPath.m should not be in the repository
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@8209 0bcb941b-74d1-0310-bb26-e6636707f451

--- storm/main/stormPath.m
+++
@@ -1,5 +0,0 @@
-function [localPath,orchestraPath] = stormPath()
-% Don't forget the filesep at the end of the path
-localPath = 'Y:\fsm\harvard\data\Zhuang\';
-orchestraPath = '/research.files/LCCB/fsm/harvard/data/Zhuang/';
-end
