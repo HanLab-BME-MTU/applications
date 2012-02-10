a66b37be960c752c489763f936ca5bbd4acef557
Pascal Berard <pascal_berard@hms.harvard.edu>
2012-02-10 19:24:29 +0000

    STORM: Added linux workstation path
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/qfsm/main@8232 0bcb941b-74d1-0310-bb26-e6636707f451

--- storm/main/stormPathExample.m
+++ storm/main/stormPathExample.m
@@ -1,8 +1,10 @@
+
 function path = stormPath()
 % The storm path is the directory location where the _data and the _queue
 % directories are stored. Don't forget the filesep at the end of the path.
 
 path = 'Y:\fsm\harvard\data\Zhuang\';
 % path = '/research.files/LCCB/fsm/harvard/data/Zhuang/';
+% path = '~/files/LCCB/fsm/harvard/data/Zhuang/';
 
 end
