ebdfa7347f3fba050dd653279148935fd0ebc2fe
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-03-07 13:49:15 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@8365 0bcb941b-74d1-0310-bb26-e6636707f451

--- getMaxIntensityDistributions.m
+++ getMaxIntensityDistributions.m
@@ -16,9 +16,9 @@ for i = 1:nd
         res(i).maxA{k} = arrayfun(@(t) max(t.A(1,:)), itracks);
         res(i).maxA4{k} = arrayfun(@(t) max(t.A(1,1:w)), itracks);
         res(i).sum4{k} = arrayfun(@(t) sum(t.A(1,1:w)), itracks);
-        res(i).int4{k} = arrayfun(@(t) cumsum(t.A(1,1:w)), itracks);
+        res(i).int4{k} = arrayfun(@(t) cumsum(t.A(1,1:w)), itracks, 'UniformOutput', false);
         if ub(k)<=10
-            res(i).int{k} = arrayfun(@(t) cumsum(t.A(1,:)), itracks);
+            res(i).int{k} = arrayfun(@(t) cumsum(t.A(1,:)), itracks, 'UniformOutput', false);
             res(i).sum{k} = arrayfun(@(t) sum(t.A(1,:)), itracks);
         end
     end
