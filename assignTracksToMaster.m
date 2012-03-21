29e7e58763a3b5782cb7e8346a51b62add1d0839
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-03-21 02:34:33 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@8465 0bcb941b-74d1-0310-bb26-e6636707f451

--- assignTracksToMaster.m
+++ assignTracksToMaster.m
@@ -1,31 +1,32 @@
 
-% Note: it is assumed that the first channel was used for tracking in each track set
-% Both structures must contain single-segment tracks only
 
+% Note: it is assumed that the first channel was used for tracking in each track set
+
+% Francois Aguet (last modified 03/20/2012)
 
 function [idxMap unassignedSlave unassignedMaster] = assignTracksToMaster(slaveTracks, masterTracks, varargin)
 
 ip = inputParser;
 ip.CaseSensitive = false;
-ip.addRequired('slaveTracks', @(t) isstruct(t) && all([t.nSeg]==1));
-ip.addRequired('masterTracks', @(t) isstruct(t) && all([t.nSeg]==1));
+% ip.addRequired('slaveTracks', @(t) isstruct(t) && all([t.nSeg]==1));
+% ip.addRequired('masterTracks', @(t) isstruct(t) && all([t.nSeg]==1));
 ip.addParamValue('MinOverlap', 1, @isscalar);
 ip.addParamValue('MaxDistance', 10, @isscalar);
-ip.parse(slaveTracks, masterTracks, varargin{:});
+ip.parse(varargin{:});
 minOverlap = ip.Results.MinOverlap;
 R = ip.Results.MaxDistance;
 
 % mean positions
-mMeans = arrayfun(@(t) [mean(t.x(1,:)) mean(t.y(1,:))], masterTracks, 'UniformOutput', false);
+mMeans = arrayfun(@(t) [nanmean(t.x(1,:)) nanmean(t.y(1,:))], masterTracks, 'UniformOutput', false);
 mMeans = vertcat(mMeans{:});
-sMeans = arrayfun(@(t) [mean(t.x(1,:)) mean(t.y(1,:))], slaveTracks, 'UniformOutput', false);
+sMeans = arrayfun(@(t) [nanmean(t.x(1,:)) nanmean(t.y(1,:))], slaveTracks, 'UniformOutput', false);
 sMeans = vertcat(sMeans{:});
 
 % slave tracks in vicinity to master tracks
 idxMap = KDTreeBallQuery(sMeans, mMeans, R);
 
-masterLengths = arrayfun(@(i) numel(i.t), masterTracks);
-slaveLengths = arrayfun(@(i) numel(i.t), slaveTracks);
+% masterLengths = arrayfun(@(i) numel(i.t), masterTracks);
+% slaveLengths = arrayfun(@(i) numel(i.t), slaveTracks);
 
 % parse each set of assignments and check for overlap
 nm = numel(masterTracks);
@@ -38,13 +39,17 @@ for k = 1:nm
         sel = find(overlap>=minOverlap);
         idxMap{k} = idxMap{k}(sel);
         if ~isempty(sel)
-            %
-            %overlapStart = overlapStart(sel);
-            %overlapEnd = overlapEnd(sel);
+            
             dist = zeros(1,numel(sel));
             for i = 1:numel(sel)
-                masterIdx = masterTracks(k).f>=overlapStart(sel(i)) & masterTracks(k).f<=overlapEnd(sel(i));
-                slaveIdx = slaveTracks(idxMap{k}(i)).f>=overlapStart(sel(i)) & slaveTracks(idxMap{k}(i)).f<=overlapEnd(sel(i));
+                mFrames = unique(masterTracks(k).f);
+                mFrames(isnan(mFrames)) = [];
+                masterIdx = mFrames>=overlapStart(sel(i)) & mFrames<=overlapEnd(sel(i));
+                
+                sFrames = unique(slaveTracks(idxMap{k}(i)).f);
+                sFrames(isnan(sFrames)) = [];
+                slaveIdx = sFrames>=overlapStart(sel(i)) & sFrames<=overlapEnd(sel(i));
+                
                 dist(i) = min(sqrt( (masterTracks(k).x(1,masterIdx)-slaveTracks(idxMap{k}(i)).x(1,slaveIdx)).^2 +...
                                 (masterTracks(k).y(1,masterIdx)-slaveTracks(idxMap{k}(i)).y(1,slaveIdx)).^2));
             end
