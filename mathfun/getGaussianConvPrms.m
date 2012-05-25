895fb77bbb50ec5a2d6b0457e83cb9fe91b221fd
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-05-25 14:37:46 +0000

    Corrected normalization
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@8661 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/getGaussianConvPrms.m
+++ mathfun/getGaussianConvPrms.m
@@ -14,7 +14,7 @@
 
 % Francois Aguet, 03/06/2012
 
-function p = getGaussianConvPrms(x, pdf1, pdf2, varargin)
+function [p, y] = getGaussianConvPrms(x, pdf1, pdf2, varargin)
 
 ip = inputParser;
 ip.CaseSensitive = false;
@@ -29,17 +29,19 @@ opts = optimset('Jacobian', 'off', ...
     'Tolfun', 1e-8);
 
 dx = x(2)-x(1);
-pdf1 = pdf1/sum(pdf1)/dx;
-pdf2 = pdf2/sum(pdf2)/dx;
+pdf1 = pdf1/sum(pdf1);
+pdf2 = pdf2/sum(pdf2);
 
 % estimate shift
-mu0 = sum((pdf2-pdf1).*x)*dx;
+mu0 = sum((pdf2-pdf1).*x);
 
 lb = [0 0.5]; % lower limits on mu, sigma
 ub = [Inf Inf];
 [p,resnorm,~,~,~,~,J] = lsqnonlin(@costX, [mu0 2], lb, ub, opts, x, pdf1, pdf2);
 
-y = filterX(p(1), p(2), dx, pdf1);
+y = filterX(p(1), p(2), pdf1);
+p = p*dx;
+y = y/sum(y)/dx;
 
 if ip.Results.Display
     
@@ -53,25 +55,24 @@ end
 
 
 function v = costX(p, x, pdf1, pdf2)
-dx = x(2)-x(1);
-y = filterX(p(1), p(2), dx, pdf1);
+y = filterX(p(1), p(2), pdf1);
 v = y - pdf2;
 
 
-function y = filterX(mu, sigma, dx, f)
+function y = filterX(mu, sigma, f)
 mu0 = floor(mu);
 
 % first shift
-f = zeroshift(f, mu0/dx);
+f = zeroshift(f, mu0);
 
 % then convolve with quasi-centered PSF
 dm = mu-mu0;
 w = ceil(4*sigma);
-x = -w:dx:w;
+x = -w:w;
 g = exp(-(x-dm).^2/(2*sigma^2));
-g = g/sum(g)/dx;
+g = g/sum(g);
 
-y = dx * conv(padarrayXT(f, [0 (numel(x)-1)/2]), g, 'valid');
+y = conv(padarrayXT(f, [0 (numel(x)-1)/2]), g, 'valid');
 
 
 % s: integer
