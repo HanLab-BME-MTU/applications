ae619b3a670a7fc60bcb5c33c87301d87e02ccf9
Daniel Nunez <daniel_nunez@hms.harvard.edu>
2012-06-08 19:25:20 +0000

    allowing for the specification of segments;not finished making changes
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@8732 0bcb941b-74d1-0310-bb26-e6636707f451

--- calculateNumberAndBrightness.m
+++ calculateNumberAndBrightness.m
@@ -1,4 +1,4 @@
-function [number,brightness] = calculateNumberAndBrightness(imageStack,background,backgroundVariance)
+function [number,brightness] = calculateNumberAndBrightness(imageStack,background,backgroundVariance,varargin)
 
 %calculateNumberAndBrightness takes an image stack and calculates the
 %number and brightness of flourescent particles in each pixel
@@ -10,8 +10,25 @@ function [number,brightness] = calculateNumberAndBrightness(imageStack,backgroun
 %differnt intensities (see most of Digman and Gratton's work)
 %determine average and variance for each pixel
 
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addRequired('imageStack', @isnumeric);
+ip.addRequired('background', @isnumeric);
+ip.addRequired('backgroundVariance', @isnumeric);
+ip.addParamValue('segmentLength', [], @isscalar);
+ip.addParamValue('segmentSpacing', [], @isscalar);
+ip.parse(imageStack,background,backgroundVariance,varargin);
 
+segmentLength = ip.Results.segmentLength;
+if isempty(segmentLength)
+    segmentLength = size(imageStack,3);
+end
+
+number = nan(size(imageStack,1),size(imageStack,2),floor(size(imageStack,3)/segmentLength));
+brightness = number;
+for isegment = 1:floor(size(imageStack,3)/segmentLength)
 %calculate number
-number = (nanmean(imageStack,3) - background).^2./(nanstd(imageStack,0,3).^2-backgroundVariance.^2);
+number(:,:,isegment) = (nanmean(imageStack,3) - background).^2./(nanstd(imageStack,0,3).^2-backgroundVariance.^2);
 %calculate brightness
-brightness = (nanstd(imageStack,0,3).^2-backgroundVariance.^2)./(nanmean(imageStack,3) - background);
+brightness(:,:,isegment)  = (nanstd(imageStack,0,3).^2-backgroundVariance.^2)./(nanmean(imageStack,3) - background);
+end
