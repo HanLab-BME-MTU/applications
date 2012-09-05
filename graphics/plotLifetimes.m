10fdd6739a1f92b5763251ef24027af922c6e7c1
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-09-05 23:34:55 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@9127 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/plotLifetimes.m
+++ graphics/plotLifetimes.m
@@ -9,7 +9,9 @@ ip.addParamValue('CargoName', 'cargo');
 ip.addParamValue('YTick', 0:0.01:0.05);
 ip.parse(varargin{:});
 ya = ip.Results.YTick;
-yal = ['0' arrayfun(@(x) num2str(x, '%.2f'), ya(2:end), 'UniformOutput', false)];
+
+fmt = ['%.' num2str(ceil(abs(log10(ya(2))))) 'f'];
+yal = ['0' arrayfun(@(x) num2str(x, fmt), ya(2:end), 'UniformOutput', false)];
 
 fset = loadFigureSettings(ip.Results.DisplayMode);
 
@@ -70,28 +72,34 @@ if isstruct(lftRes)
                 
         hp = zeros(1,7);
         % total distr
-        hp(1) = plot(lftRes.t, mean(vertcat(lftRes.lftHist_Ia), 1), 'Color', 0.6*[1 1 1], 'LineWidth', lw);
+        mu = mean(vertcat(lftRes.lftHist_Ia), 1);
+        %bAll = 1.96*getSE(lftRes, 'lftHist_Ia');
+        %fill([lftRes.t lftRes.t(end:-1:1)], [mu+bAll mu(end:-1:1)-bAll(end:-1:1)], 'r', 'EdgeColor', 'none');
+        hp(1) = plot(lftRes.t, mu, 'Color', 0.6*[1 1 1], 'LineWidth', lw);
         
         % Cargo-negative distributions
         hue = 0.6;
-        hp(5) = plot(lftRes.t, mean(vertcat(lftRes.lftHist_neg), 1), 'Color', hsv2rgb([hue 0.7 1]), 'LineWidth', lw);
-        hp(7) = plot(lftRes.t, pBNS/(pANS+pBNS)*mean(lftRes.lftHist_Bneg,1), '-', 'Color', hsv2rgb([hue-0.1 0.7 0.9]), 'LineWidth', lw);
-        hp(6) = plot(lftRes.t, pANS/(pANS+pBNS)*mean(lftRes.lftHist_Aneg,1), '-', 'Color', hsv2rgb([hue 1 0.9]), 'LineWidth', lw+1);
+        hp(5) = plot(lftRes.t, (pANS+pBNS)*mean(vertcat(lftRes.lftHist_neg), 1), 'Color', hsv2rgb([hue 0.7 1]), 'LineWidth', lw);
+        hp(7) = plot(lftRes.t, pBNS*mean(lftRes.lftHist_Bneg,1), '-', 'Color', hsv2rgb([hue-0.1 0.7 0.9]), 'LineWidth', lw);
+        mu = mean(lftRes.lftHist_Aneg,1);
+        %bAll = 1.96*getSE(lftRes, 'lftHist_Aneg');
+        %fill([lftRes.t lftRes.t(end:-1:1)], pANS*[mu+bAll mu(end:-1:1)-bAll(end:-1:1)], 'r', 'EdgeColor', 'none');
+        hp(6) = plot(lftRes.t, pANS*mu, '-', 'Color', hsv2rgb([hue 1 0.9]), 'LineWidth', lw+1);
         
         % Cargo-positive distributions
         hue = 0.33;
-        hp(2) = plot(lftRes.t, mean(vertcat(lftRes.lftHist_pos), 1), 'Color', hsv2rgb([hue 0.7 1]), 'LineWidth', lw);
-        hp(4) = plot(lftRes.t, pBS/(pAS+pBS)*mean(lftRes.lftHist_Bpos,1), '-',  'Color', hsv2rgb([hue-0.1 0.7 0.9]), 'LineWidth', lw);
-        hp(3) = plot(lftRes.t, pBS/(pAS+pBS)*mean(lftRes.lftHist_Apos,1), '-', 'Color', hsv2rgb([hue 1 0.9]), 'LineWidth', lw+1);
+        hp(2) = plot(lftRes.t, (pAS+pBS)*mean(vertcat(lftRes.lftHist_pos), 1), 'Color', hsv2rgb([hue 0.7 1]), 'LineWidth', lw);
+        hp(4) = plot(lftRes.t, pBS*mean(lftRes.lftHist_Bpos,1), '-',  'Color', hsv2rgb([hue-0.1 0.7 0.9]), 'LineWidth', lw);
+        hp(3) = plot(lftRes.t, pAS*mean(lftRes.lftHist_Apos,1), '-', 'Color', hsv2rgb([hue 1 0.9]), 'LineWidth', lw+1);
         
         % All, above/below threshold
         %hp(2) = plot(lftRes.t, mean(lftRes.pctBelow)*lftRes.meanLftHist_B, '-', 'Color', hsv2rgb([2/3 0.3 0.9]), 'LineWidth', 2);
         %hp(1) = plot(lftRes.t, mean(lftRes.pctAbove)*lftRes.meanLftHist_A, '-', 'Color', hsv2rgb([2/3 1 0.9]), 'LineWidth', 2);
         cargo = ip.Results.CargoName;
         fmt = '%.1f';
-        legendText = {'All', ['+ ' cargo ' (' num2str((pAS+pBS)*100, fmt) '%)'], ['+ ' cargo ', max(I) > T (' num2str(pAS*100, fmt) '%)'],...
-            ['+ ' cargo ', max(I) < T (' num2str(pBS*100, fmt) '%)'], ['- ' cargo ' (' num2str((pANS+pBNS)*100, fmt) '%)'],...
-            ['- ' cargo ', max(I) > T (' num2str(pANS*100, fmt) '%)'], ['- ' cargo, ', max(I) < T (' num2str(pBNS*100, fmt) '%)']};
+        legendText = {'All', ['+ ' cargo ' (' num2str((pAS+pBS)*100, fmt) '%)'], ['+ ' cargo ', max. int. > T (' num2str(pAS*100, fmt) '%)'],...
+            ['+ ' cargo ', max. int. < T (' num2str(pBS*100, fmt) '%)'], ['- ' cargo ' (' num2str((pANS+pBNS)*100, fmt) '%)'],...
+            ['- ' cargo ', max. int. > T (' num2str(pANS*100, fmt) '%)'], ['- ' cargo, ', max. int. < T (' num2str(pBNS*100, fmt) '%)']};
         
         axis([0 min(120, lftRes.t(end)) 0 ya(end)]);
         set(gca, 'XTick', 0:20:200, 'YTick', ya, 'YTickLabel', yal);
@@ -134,3 +142,13 @@ elseif iscell(lftRes)
 else
     error('Incompatible input');
 end
+
+
+function SE = getSE(lftData, fieldName)
+M = lftData.(fieldName);
+nd = size(M,1);
+meanM = zeros(size(M));
+for i = 1:nd
+    meanM(i,:) = mean(M(setdiff(1:nd,i),:),1);
+end
+SE = std(meanM,[],1) / sqrt(nd);
