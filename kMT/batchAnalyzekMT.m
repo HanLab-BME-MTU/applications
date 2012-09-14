5d7fae4dda567a51ac9bb54615489d65cea7e1d3
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2012-09-14 13:34:07 +0000

    Fix MovieData creation and add comments to batch analysis function
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/maki/main@9149 0bcb941b-74d1-0310-bb26-e6636707f451

--- kMT/batchAnalyzekMT.m
+++ kMT/batchAnalyzekMT.m
@@ -1,4 +1,4 @@
-% List input files
+% List all TIFF files in the main folder
 mainFolder = '/Users/sebastien/Documents/Julie/kMTProject';
 tiffFiles = dir(fullfile(mainFolder,'*.tif'));
 nMovies = numel(tiffFiles);
@@ -6,12 +6,10 @@ nMovies = numel(tiffFiles);
 % Create array of movies
 MD(nMovies,1) = MovieData();
 for i=1:nMovies
-   
-   outputDir = fullfile(mainFolder,tiffFiles(i).name(1:end-4));
-   if ~isdir(outputDir),mkdir(outputDir); end
-   MD(i) = MovieData.load(fullfile(mainFolder,tiffFiles(i).name),false,...
-       'outputDirectory',outputDir,'movieDataPath_',outputDir,...
-       'movieDataFileName_',[tiffFiles(i).name(1:end-4) '.mat']);
+    % Create MovieData object using Bioformats
+    % For a moviexxxx.tif file, a moviexx folder will be created where the
+    % moviexxx.mat file and all analysis will be stored
+    MD(i) = MovieData.load(fullfile(mainFolder,tiffFiles(i).name));
 end
 
 % Create list of movies
@@ -20,43 +18,58 @@ if ~isdir(listFolder),  mkdir(listFolder); end
 ML=MovieList(MD,listFolder);
 ML.setPath(listFolder);
 ML.setFilename('kMTList.mat');
-ML.save
+ML.save;
 
 
-%% Create processes
-arrayfun(@reset,MD);
-for i=1:nMovies
-    MD(i).addPackage(UTrackPackage(MD(i)));
-    MD(i).packages_{1}.createDefaultProcess(1);
-    MD(i).packages_{1}.createDefaultProcess(2);
-    MD(i).addProcess(SisterGroupingProcess(MD(i)));
-    MD(i).addProcess(KMTDetectionProcess(MD(i)));
+%% Recreate analysis processes
+
+% Flag to reset all analysis
+resetAnalysis = true; 
+
+if resetAnalysis
+    arrayfun(@reset,MD); % Wipe out all existing analysis
+    for i=1:nMovies
+        % Create UTrackPackage and its first 2 processes (detection &
+        % tracking)
+        MD(i).addPackage(UTrackPackage(MD(i)));
+        MD(i).packages_{1}.createDefaultProcess(1);
+        MD(i).packages_{1}.createDefaultProcess(2);
+        
+        % Create sister grouping and kMT detection processes
+        MD(i).addProcess(SisterGroupingProcess(MD(i)));
+        MD(i).addProcess(KMTDetectionProcess(MD(i)));
+    end
 end
 
-%% Set parameters
+%% Set analysis parameters
 for i=1:nMovies
+    
+    % Set parameters for the Gaussian mixture-model fitting
     iProc=MD(i).getProcessIndex('SubResolutionProcess',1,false);
-    funParams = MD(i).processes_{1}.funParams_;
-    funParams.ChannelIndex=2;
-    funParams.detectionParam.psfSigma=1.5;
+    funParams = MD(i).processes_{iProc}.funParams_;
+    funParams.ChannelIndex=2; % Detect mCherry-CENPA objects
+    funParams.detectionParam.psfSigma=1.5; % Set the psfSigma
     parseProcessParams(MD(i).processes_{1},funParams);
     
+    % Set tracking parameters
     funParams = MD(i).processes_{2}.funParams_;
-    funParams.ChannelIndex=2;
-    funParams.costMatrices(1).parameters.diagnostics=[];
+    funParams.ChannelIndex=2; % Track mCherry-CENPA objects
+    funParams.costMatrices(1).parameters.diagnostics=[]; % Do not output diagnostics
     parseProcessParams(MD(i).processes_{2},funParams);
     
+    % Set sister pairing parameters
+    funParams = MD(i).processes_{3}.funParams_;
+    funParams.ChannelIndex=2; % Group mCherry-CENPA tracks 
+    parseProcessParams(MD(i).processes_{3}, funParams);
     
-    parseProcessParams(MD(i).processes_{3},struct('ChannelIndex',2));
-    
-    parseProcessParams(MD(i).processes_{4},struct('ChannelIndex',2));
+    % Set kMT detection parameters
+    funParams = MD(i).processes_{4}.funParams_;
+    funParams.ChannelIndex=1; % Detect gfp-EB3 comets
+    parseProcessParams(MD(i).processes_{4}, funParams);
 
 end
 
-%%
+%% Run analysis processes
 for i=1:nMovies
     cellfun(@run,MD(i).processes_);
-    close all
 end
-
-
