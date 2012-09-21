d50dd09dc2203dec27e700a74fbbb710a001b554
Khuloud Jaqaman <khuloud_jaqaman@hms.harvard.edu>
2012-09-21 18:08:06 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/receptors/main@9175 0bcb941b-74d1-0310-bb26-e6636707f451

--- integrins/scripts/scriptAnalysisSptVsWindows.m
+++ integrins/scripts/scriptAnalysisSptVsWindows.m
@@ -1,42 +1,54 @@
 
-
-
-% sliceRange = [];
-sliceRange = (40:170)';
-frameRange = [];
 lengthMinMax = [5 99];
 
-load ../tracksDiffusionLength5InMask.mat
-
-try
-    load windowsActivityTracks.mat
-catch
-    load windowsActivityTracks1.mat
-    load windowsActivityTracks2.mat
-    load windowsActivityTracks3.mat
-    load windowsActivityTracks4.mat
-    windowTrackAssignExt = cat(4,windowTrackAssignExt1,windowTrackAssignExt2);
-    clear windowTrackAssignExt1 windowTrackAssignExt2
+for i = 5 : length(movieStructAlphaV717Trunc)
+    
+    disp(num2str(i))
+    
+    activityLevel = movieStructAlphaV717Trunc(i).activityLevel;
+    
+    if activityLevel > 1
+        
+        tmp = movieStructAlphaV717Trunc(i).fileName{1};
+        tmp = tmp(11:end);
+        cd([tmp '/analysisAlphaV717Trunc/furtherAnalysis/adaptiveWindows'])
+        
+        sliceRange = movieStructAlphaV717Trunc(i).sliceRange;
+        frameRange = movieStructAlphaV717Trunc(i).frameRange;
+        
+        load ../tracksDiffusionLength5InMask.mat
+        load ../diffusionModeClassification.mat
+        
+        seqOfEvents = vertcat(tracksFinal(end-10:end).seqOfEvents);
+        maxFrame = max(seqOfEvents(:,1));
+        
+        try
+            load windowsActivityTracks.mat
+        catch
+            load windowsActivityTracks1.mat
+            load windowsActivityTracks2.mat
+            load windowsActivityTracks3.mat
+            load windowsActivityTracks4.mat
+            windowTrackAssignExt = cat(4,windowTrackAssignExt1,windowTrackAssignExt2);
+            clear windowTrackAssignExt1 windowTrackAssignExt2
+        end
+        
+        load directTrackChar.mat
+        
+        load windowNumbersAssignExt.mat
+        
+        maskDir = [tmp '/analysisCellEdgeModSmall/refined_masks/refined_masks_for_channel_1/'];
+        tmp = dir([maskDir '*.tif']);
+        firstMaskFile = [maskDir tmp(1).name];
+        
+        [sptPropInWindow,windowDistFromEdge,analysisParam] = sptRelToActivityOnsetAdaptiveWindows(...
+            tracksFinal,diffAnalysisRes,diffModeAnalysisRes,trackChar,windowsAll,...
+            protSamples,windowTrackAssignExt,windowNumbersAssignExt,...
+            lengthMinMax,sliceRange,frameRange,firstMaskFile);
+        
+        save('particleBehaviorAdaptiveWindows120907','sptPropInWindow',...
+            'windowDistFromEdge','analysisParam');
+        
+    end
+    
 end
-
-load ../diffusionModeClassification.mat
-load directTrackChar.mat
-
-load windowNumbersAssignExt.mat
-
-firstMaskFile = '/home/kj35/files/LCCB/receptors/Galbraiths/data/alphaVandCellEdge/110114/Cs1_CHO02/Cs1_CHO02A/analysisCellEdgeSmall2/refined_masks/refined_masks_for_channel_1/refined_mask_110114_Cs1_CHO02A_new_0001.tif';
-
-seqOfEvents = vertcat(tracksFinal(end-10:end).seqOfEvents);
-maxFrame = max(seqOfEvents(:,1));
-
-protWinParam = struct('numTypeProt',9,'numPixInBand',2,...
-    'numSmallBandsInBigBand',3,'numBigBands',10,'maxNegInc',3,'maxPosInc',18);
-
-[sptPropInWindow,windowDistFromEdge,analysisParam] = sptRelToActivityOnsetAdaptiveWindows(...
-    tracksFinal,diffAnalysisRes,diffModeAnalysisRes,trackChar,windowsAll,...
-    protSamples,windowTrackAssignExt,windowNumbersAssignExt,...
-    lengthMinMax,sliceRange,frameRange,firstMaskFile,protWinParam);
-
-save('particleBehaviorAdaptiveWindows120917','sptPropInWindow',...
-    'windowDistFromEdge','analysisParam');
-
--- integrins/scripts/scriptAssignNumbersWindows.m
+++ integrins/scripts/scriptAssignNumbersWindows.m
@@ -1,43 +1,56 @@
-% clear all
-% close all
 
 lengthMinMax = [5 99];
 
-load ../tracksDiffusionLength5InMask.mat
-load ../diffusionModeClassification.mat
-
-seqOfEvents = vertcat(tracksFinal(end-10:end).seqOfEvents);
-maxFrame = max(seqOfEvents(:,1));
-
-try
-    load windowsActivityTracks.mat
-catch
-    load windowsActivityTracks1.mat
-    load windowsActivityTracks2.mat
-    load windowsActivityTracks3.mat
-    load windowsActivityTracks4.mat
-    windowTrackAssignExt = cat(4,windowTrackAssignExt1,windowTrackAssignExt2);
-    clear windowTrackAssignExt1 windowTrackAssignExt2
+for i = 1 : length(analysisLifeactInd);
+    
+    disp(num2str(i))
+    
+    activityLevel = analysisLifeactInd(i).activityLevel;
+    
+    if activityLevel > 1
+        
+        tmp = analysisLifeactInd(i).fileName{1};
+        tmp = tmp(11:end);
+        cd([tmp '/analysisLifeact/furtherAnalysis/adaptiveWindows'])
+        
+        load ../tracksDiffusionLength5InMask.mat
+        load ../diffusionModeClassification.mat
+        
+        seqOfEvents = vertcat(tracksFinal(end-10:end).seqOfEvents);
+        maxFrame = max(seqOfEvents(:,1));
+        
+        try
+            load windowsActivityTracks.mat
+        catch
+            load windowsActivityTracks1.mat
+            load windowsActivityTracks2.mat
+            load windowsActivityTracks3.mat
+            load windowsActivityTracks4.mat
+            windowTrackAssignExt = cat(4,windowTrackAssignExt1,windowTrackAssignExt2);
+            clear windowTrackAssignExt1 windowTrackAssignExt2
+        end
+        
+        load directTrackChar.mat
+        
+        try
+            
+            load windowNumbersAssignExt.mat
+            windowNumbersAssignExtIn = windowNumbersAssignExt;
+            
+            windowNumbersAssignExt = assignNumbers2Windows(tracksFinal,diffAnalysisRes,...
+                diffModeAnalysisRes,trackChar,windowsAll,1:400:maxFrame+1,...
+                windowTrackAssignExt,lengthMinMax,[2 3],windowNumbersAssignExtIn);
+            
+        catch
+            
+            windowNumbersAssignExt = assignNumbers2Windows(tracksFinal,diffAnalysisRes,...
+                diffModeAnalysisRes,trackChar,windowsAll,1:400:maxFrame+1,...
+                windowTrackAssignExt,lengthMinMax);
+            
+        end
+        
+        save('windowNumbersAssignExt','windowNumbersAssignExt','-v7.3')
+        
+    end
+    
 end
-
-load directTrackChar.mat
-
-try
-    
-    load windowNumbersAssignExt.mat
-    windowNumbersAssignExtIn = windowNumbersAssignExt;
-    
-    windowNumbersAssignExt = assignNumbers2Windows(tracksFinal,diffAnalysisRes,...
-        diffModeAnalysisRes,trackChar,windowsAll,1:400:maxFrame+1,...
-        windowTrackAssignExt,lengthMinMax,[2 3],windowNumbersAssignExtIn);
-    
-catch
-    
-    windowNumbersAssignExt = assignNumbers2Windows(tracksFinal,diffAnalysisRes,...
-        diffModeAnalysisRes,trackChar,windowsAll,1:400:maxFrame+1,...
-        windowTrackAssignExt,lengthMinMax);
-
-end
-
-save('windowNumbersAssignExt','windowNumbersAssignExt','-v7.3')
-
