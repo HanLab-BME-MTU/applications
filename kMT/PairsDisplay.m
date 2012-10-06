ef870df0ca39f1f450eb42406697207f321dea50
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2012-10-06 16:49:24 +0000

    Add pair number display to PairsDisplay class
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/maki/main@9237 0bcb941b-74d1-0310-bb26-e6636707f451

--- kMT/PairsDisplay.m
+++ kMT/PairsDisplay.m
@@ -3,7 +3,7 @@ classdef PairsDisplay < MovieDataDisplay
     properties
         Linestyle='-';
         GapLinestyle='--';
-        Color='r';  
+        Color='r';
         showLabel=false;
     end
     methods
@@ -17,71 +17,46 @@ classdef PairsDisplay < MovieDataDisplay
         end
         function h=initDraw(obj,data,tag,varargin)
             nPairs = numel(data);
-            h=-ones(nPairs,3);
+            h=-ones(nPairs,4);
+            % List valid pairs
+            validPairs = find(~arrayfun(@(x) all(isnan(x.coords1)),data));
+            delta = -5;
             
-            for i=1:nPairs
+            for i = validPairs'
+                % Concatenate coordinates from positions 1 and 2
                 coords = vertcat(data(i).coords1(1:3),data(i).coords2(1:3));
+
+                % Plot pairs
                 h(i,1)=plot3(coords(:,1),coords(:,2),coords(:,3),'-',...
                     'Color',obj.Color,'Linestyle',obj.Linestyle);
+                
+                % Draw kinetochore intensities as circles
                 if isfield(data(i),'kMTcoords1') && ~isnan(data(i).kMTcoords1(1))
-                   h(i,2) =plot3(data(i).kMTcoords1(1,1),data(i).kMTcoords1(1,2),0,'o',...
-                    'Color','r','MarkerSize',ceil(data(i).kMTamp1(1)*10^4));
+                    h(i,2) =plot3(data(i).kMTcoords1(1,1),data(i).kMTcoords1(1,2),0,'o',...
+                        'Color','r','MarkerSize',ceil(data(i).kMTamp1(1)*10^4));
                 end
                 if isfield(data(i),'kMTcoords2') && ~isnan(data(i).kMTcoords2(1))
-                   h(i,3) =plot3(data(i).kMTcoords2(1,1),data(i).kMTcoords2(1,2),0,'o',...
-                    'Color','r','MarkerSize',ceil(data(i).kMTamp2(1)*10^4));
+                    h(i,3) =plot3(data(i).kMTcoords2(1,1),data(i).kMTcoords2(1,2),0,'o',...
+                        'Color','r','MarkerSize',ceil(data(i).kMTamp2(1)*10^4));
+                end
+                
+                % Display pair numbers if option is selected
+                if obj.showLabel
+                    h(i,3) = text(mean(coords(:,1)) + delta, mean(coords(:,2)) + delta, ...
+                        mean(coords(:,3)) + delta, num2str(i), 'Color', obj.Color);
                 end
             end
-            set(h(ishandle(h)),'Tag',tag);            
+            
+            set(h(ishandle(h)),'Tag',tag);
         end
-
+        
         function updateDraw(obj,allh,data)
             tag=get(allh(1),'Tag');
             delete(allh);
             obj.initDraw(data,tag);
             return;
-            nTracks = numel(data);
-
-            h=findobj(allh,'Type','line');
-            delete(h(2*nTracks+1:end));
-            h(2*nTracks+1:end)=[];
-            hlinks=findobj(h,'LineStyle',obj.Linestyle);
-            hgaps=findobj(h,'LineStyle',obj.GapLinestyle);
-            
-            % Update existing windows
-            for i=1:min(numel(hlinks),nTracks) 
-                xData= data.x{i}(max(1,end-obj.dragtailLength):end);
-                yData= data.y{i}(max(1,end-obj.dragtailLength):end);
-                set(hgaps(i),'Xdata',xData(~isnan(xData)),'YData',yData(~isnan(yData)));
-                set(hlinks(i),'Xdata',xData,'YData',yData);
-            end
-            for i=min(numel(hlinks),nTracks)+1:nTracks
-                xData= data.x{i}(max(1,end-obj.dragtailLength):end);
-                yData= data.y{i}(max(1,end-obj.dragtailLength):end);
-                hgaps(i)=plot(xData(~isnan(xData)),yData(~isnan(yData)),...
-                    'Linestyle',obj.GapLinestyle','Color',obj.Color);
-                hlinks(i)=plot(xData,yData,...
-                    'Linestyle',obj.Linestyle,'Color',obj.Color);
-            end
-            set([hlinks hgaps],'Tag',tag);
-
-            
-            if isfield(data,'label') && obj.showLabel
-                ht=findobj(allh,'Type','text');
-                delete(ht(nTracks+1:end));
-                ht(nTracks+1:end)=[];
-                for i=1:min(numel(ht),nTracks)
-                    set(ht(i),'Position',[data.x{i}(end),data.y{i}(end)],...
-                        'String',data.label(i));
-                end
-                for i=min(numel(ht),nTracks)+1:nTracks
-                    ht(i) = text(data.x{i}(end),data.y{i}(end),num2str(data.label(i)));
-                end
-                set(ht,'Tag',tag); 
-            end
-           
         end
-    end    
+    end
     
     methods (Static)
         function params=getParamValidators()
@@ -94,9 +69,9 @@ classdef PairsDisplay < MovieDataDisplay
             params(4).name='showLabel';
             params(4).validator=@isscalar;
         end
-
-        function f=getDataValidator() 
+        
+        function f=getDataValidator()
             f=@isstruct;
         end
-    end    
+    end
 end
