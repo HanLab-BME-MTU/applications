79c8afd6bc9cc4dcff8b202e4752f932071bf16c
Ulrich Schmidt <ulrich_schmidt@hms.harvard.edu>
2012-11-15 22:38:18 +0000

    generate artoficial STORM experiments
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/receptors/main@9429 0bcb941b-74d1-0310-bb26-e6636707f451

--- ErbBproject/counting/generateSTORMmovie.m
+++ ErbBproject/counting/generateSTORMmovie.m
@@ -1,4 +1,4 @@
-function stack=generateSTORMmovie(pxSize,lambda,kon,koff,kb,varargin)
+function [stack,emState,settings]=generateSTORMmovie(lambda,kon,koff,kb,varargin)
 %GENERATESTORMMOVIE creates an artificial STORM experiment
 %   
 %   required input arguments:
@@ -16,10 +16,13 @@ function stack=generateSTORMmovie(pxSize,lambda,kon,koff,kb,varargin)
 %          tExp -> exposure time, default 0.1 sec
 %       nCycles -> number of activation cycles, default 250
 %          nRep -> number of frames in activation cycle, default 10
-%      nPhotons -> number of emitted photons per blink, default 1000
+%        ngamma -> average number of photons per activation, default 6000
+%          gain -> gain of the camera, default 4
+%            bg -> background noise, default 100
 %
 %   output:
-%       stack -> imSize-by-imSize-by-nCycles*nRep array
+%         stack -> imSize-by-imSize-by-nCycles*nRep image array
+%       emState -> matrix monitoring emitter state over time
 %
 %   US 2012/11/14
 %
@@ -44,7 +47,9 @@ ip.addOptional('tAct',0.01,@isscalar);
 ip.addOptional('tExp',0.1,@isscalar);
 ip.addOptional('nCycles',250,@isscalar);
 ip.addOptional('nRep',10,@isscalar);
-ip.addOptional('nPhotons',1000,@isscalar);
+ip.addOptional('ngamma',6000,@isscalar);
+ip.addOptional('gain',4,@isscalar);
+ip.addOptional('bg',100,@isscalar);
 
 % parse arguments
 ip.parse(lambda,kon,koff,kb,varargin{:});
@@ -64,7 +69,9 @@ tAct=ip.Results.tAct;
 tExp=ip.Results.tExp;
 nCycles=ip.Results.nCycles;
 nRep=ip.Results.nRep;
-nPhotons=ip.Results.nPhotons;
+ngamma=ip.Results.ngamma;
+gain=ip.Results.gain;
+bg=ip.Results.bg;
 
 if kon*tAct >= 1.0 || koff*tExp >= 1.0 || tExp*kb >= 1.0
     emsg='probability from rate constants greater than 1';
@@ -76,8 +83,6 @@ if nEmi == 0
     if isempty(pos)
         nEmi=floor(imSize^2*(pxSize/1000)^2);
         pos=rand(nEmi,2)*imSize;
-    else
-        nEmi=numel(pos(:,1));
     end
 else
     if isempty(pos)
@@ -85,7 +90,6 @@ else
     end
 end
 
-nEmi=numel(pos(:,1));
 
 % thermal activation rate is much smaller than the UV induced rate
 kth=kon/1000;
@@ -93,25 +97,37 @@ kth=kon/1000;
 % 3D array for movie output
 stack=zeros(imSize,imSize,nCycles*nRep);
 
-% vector for monitoring state of emitters (ON=1, OFF=0, BLEACHED=2)
+% vector for monitoring state of emitters (OFF=0, ON=1, BLEACHED=2)
 emState=zeros(nEmi,1);
 
-% average spread of localizations around true mean
-sigmaLoc=(lambda/2)/sqrt(nPhotons)
+% sigma of PSF: sigma=FWHM*2*sqrt[2*ln(2)]
+sigmaPSF=(lambda/2)/2.35482;
 
-% spread of point spread function: sigma=FWHM*2*sqrt[2*ln(2)]
-sigmaPSF=(672/2)/2.35482
+aveAmp=gain*ngamma/(2*pi*(sigmaPSF/pxSize)^2);
 
-% avergae amplitude
-aveAmp=nPhotons/sqrt(2*pi*(sigmaLoc/pxSize)^2);
+% save settings as return variable
+settings=ip.Results;
+settings.pos=pos;
+settings.nEmi=nEmi;
+settings.kth=kth;
+settings.sigmaPSF=sigmaPSF;
+settings.aveAmp=aveAmp;
 
 % randomize initial state of emitters (this can of course be altered)
 for ne=1:nEmi
-    xi=rand();
-    if xi < 0.01
+    eta=rand();
+    if eta < 0.01
         emState(ne)=1;
     end
 end
+
+% patch size of Gaussian spot
+w=4*floor(sigmaPSF/pxSize);
+wg=-w:w;
+[xg,yg]=meshgrid(wg,wg);
+
+% matrix monitoring state of emitters over time
+emStateM=NaN(nEmi,nCycles*nRep);
 
 % begin imaging
 nFrame=1;
@@ -122,8 +138,8 @@ for nc=1:nCycles
         switch( emState(ne) )
             % emitter is in OFF state
             case 0
-                xi=rand();
-                if xi < kon*tAct
+                eta=rand();
+                if eta < kon*tAct
                     emState(ne)=1;
                 end
         end
@@ -131,18 +147,33 @@ for nc=1:nCycles
     
     % step through imaging cycle
     for nr=1:nRep
+        
+        emStateM(:,nFrame)=emState;
+        
+        frame=zeros(imSize,imSize);
         for ne=1:nEmi
             switch( emState(ne) )
                 % emitter is in ON state
                 case 1
                     % create Gaussian spot at emitter position
-                    x=pos(ne,1);
-                    y=pos(ne,2);
+                    xi=round(pos(ne,1));
+                    yi=round(pos(ne,2));
+                    x=pos(ne,1)-xi;
+                    y=pos(ne,2)-yi;
+                    
+                    xa=max(1,xi-w):min(imSize,xi+w);
+                    ya=max(1,yi-w):min(imSize,yi+w);
+                    
+                    xaa=xa-xa(1)+1;
+                    yaa=ya-ya(1)+1;
+                    
                     amp=aveAmp+sqrt(aveAmp)*randn();
-                    signal=simGaussianSpot(
+                    g=amp*exp(-((xg-x).^2+(yg-y).^2)/(2*(sigmaPSF/pxSize)^2));
+                    frame(xa,ya)=frame(xa,ya)+g(xaa,yaa);
+                    
                     % does emitter turn off or bleach?
-                    xi=rand()
-                    if xi < tExp*(koff+kb)
+                    eta=rand();
+                    if eta < tExp*(koff+kb)
                         zeta=rand();
                         if zeta < koff/(koff+kb)
                             emState(ne)=0;
@@ -152,14 +183,21 @@ for nc=1:nCycles
                     end
                 % thermal activation from OFF state???
                 case 0
-                    xi=rand();
-                    if xi < tExp*kth
+                    eta=rand();
+                    if eta < tExp*kth
                         emState(ne)=1;
                     end
             end
         end
+        frame=frame+bg;
+        frame=poissrnd(frame);
+        stack(:,:,nFrame)=frame;
+        nFrame=nFrame+1;
     end        
 end
+
+emState=emStateM;
+stack=uint16(stack);
 
 end
 
