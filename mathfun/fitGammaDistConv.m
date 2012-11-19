bf2bf87b51c09b9b50b06428af9561aa0f8e136a
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-11-19 01:30:57 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@9449 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/fitGammaDistConv.m
+++ mathfun/fitGammaDistConv.m
@@ -3,17 +3,18 @@
 
 % Francois Aguet, 11/17/2012
 
-function [k, n, a, BIC] = fitGammaDistConv(x, f1, f2, varargin)
+function [k, n, RSS, BIC] = fitGammaDistConv(x, f1, f2, varargin)
 
 ip = inputParser;
 ip.CaseSensitive = false;
 ip.addRequired('x', @isvector);
 ip.addRequired('f1', @isvector);
 ip.addRequired('f2', @isvector);
-ip.addParamValue('Mode', 'kna', @ischar);
+ip.addParamValue('Mode', 'kn', @ischar);
 ip.addParamValue('k', NaN, @isscalar);
 ip.addParamValue('n', NaN, @isscalar);
-ip.addParamValue('a', NaN, @isscalar);
+% ip.addParamValue('a', NaN, @isscalar);
+ip.addParamValue('cut', [], @isscalar);
 ip.parse(x, f1, f2, varargin{:});
 
 opts = optimset('Jacobian', 'off', ...
@@ -22,6 +23,15 @@ opts = optimset('Jacobian', 'off', ...
     'Display', 'off', ...
     'TolX', 1e-8, ...
     'Tolfun', 1e-8);
+
+cut = ip.Results.cut;
+if isempty(cut)
+    cut = x(1);
+end
+
+dx = x(2)-x(1);
+% f1 = f1/sum(f1)/dx;
+% f2 = f2/sum(f2)/dx;
 
 p0 = [ip.Results.k ip.Results.n];% ip.Results.a];
 
@@ -33,42 +43,43 @@ prmVect = [1/mu 2];
 idx = ~isnan(p0);
 prmVect(idx) = p0(idx);
 
-F1 = fft(f1);
+F1 = fft(f1(x>=cut));
 N = sum(estIdx);
-[p,RSS] = lsqnonlin(@cost, prmVect(estIdx), zeros(N,1), inf(N,1), opts, x, F1, f2, prmVect, estIdx);
+
+lb = [0 0];
+ub = [Inf Inf];
+[p,RSS] = lsqnonlin(@cost, prmVect(estIdx), lb(estIdx), ub(estIdx), opts, x, F1, f2, prmVect, estIdx, cut);
 prmVect(estIdx) = p;
 k = prmVect(1);
 n = prmVect(2);
-% a = prmVect(3);
 
 d = numel(x);
 BIC = d*log(RSS/d) + N*log(d); 
 
-fprintf('k = %.3f, n = %.3f, BIC = %.3f\n', k, n, BIC);
+fprintf('k = %.3f, n = %.3f, RSS = %.3e, BIC = %.3f\n', k, n, RSS, BIC);
 
-dx = x(2)-x(1);
+h = exp(-k*x)*k^n.*x.^(n-1)/gamma(n);
+h = h/sum(h)/dx;
+H = fft(h);
+fx = ifft(H.*F1)*dx;
 
-g = exp(-k*x)*k^n.*x.^(n-1)/gamma(n);
-g = g/sum(g)/dx;
-G = fft(g);
-fx = ifft(G.*F1)*dx;
-
-figure;
-hold on;
-plot(x, f1, 'k');
-plot(x, f2, 'b');
-plot(x, fx, 'r--');
+% figure;
+% hold on;
+% plot(x, f1, 'k');
+% plot(x, f2, 'b');
+% plot(x, fx, 'r--');
 
 
-function v = cost(p, x, F1, f2, prmVect, estIdx)
+function v = cost(p, x, F1, f2, prmVect, estIdx, cut)
 dx = x(2)-x(1);
 prmVect(estIdx) = p;
 k = prmVect(1);
 n = prmVect(2);
 
-g = exp(-k*x)*k^n.*x.^(n-1)/gamma(n);
-g = g/sum(g)/dx;
+h = exp(-k*x)*k^n.*x.^(n-1)/gamma(n);
+h = h/sum(h)/dx;
 
-G = fft(g);
+H = fft(h);
 
-v = ifft(G.*F1)*dx - f2;
+v = ifft(H.*F1)*dx - f2;
+% plot(ifft(H.*F1)*dx, 'k');
