75ce34cf8fef0c2be0746181c57b206bc8e2abf3
Marco Vilela <marco_vilela@hms.harvard.edu>
2012-12-11 03:24:10 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/morphodynamics/main@9539 0bcb941b-74d1-0310-bb26-e6636707f451

--- edgeVelocityAnalysis/excludeWindowsFromAnalysis.m
+++ edgeVelocityAnalysis/excludeWindowsFromAnalysis.m
@@ -16,16 +16,18 @@ exclude  = cell(nCell,1);
 cellData = loadingMovieResultsPerCell(ML);
 
 
-%exclude{1} = [1:35 95:112];
+exclude{2} = [1:35 95:112];
 
 for iCell = 1:nCell
     
     nWin   = size( cellData(iCell).data.rawEdgeMotion,1 );
     border = [1:3 nWin-2:nWin];
     %Excluding border
+    cellData(iCell).data.procEdgeMotion(border) = [];
     cellData(iCell).data.excludeWin = unique([cellData(iCell).data.excludeWin border]);
     
     %Excluding pre-selected windows
+    cellData(iCell).data.procEdgeMotion(exclude{iCell}) = [];
     cellData(iCell).data.excludeWin = unique([cellData(iCell).data.excludeWin exclude{iCell}]);
     
 end
