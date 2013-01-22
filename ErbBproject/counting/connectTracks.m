e181bc86230186da2826acf7cba6e30344e0b1c7
Ulrich Schmidt <ulrich_schmidt@hms.harvard.edu>
2013-01-22 21:33:44 +0000

    auxiliary functions
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/receptors/main@9648 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ ErbBproject/counting/connectTracks.m
@@ -0,0 +1,100 @@
+function tracksCombined=connectTracks(tracks,varargin)
+%CONNECTTRACKS links single tracks from single molecules in space and time
+%   
+%
+% US, 2013/21/01
+%
+
+% check input
+ip=inputParser;
+
+ip.addRequired('tracks',@isstruct);
+ip.addOptional('ton',2.5,@isnumeric);
+ip.addOptional('toff',10.0,@isnumeric);
+
+ip.parse(tracks,varargin{:});
+
+tracks=ip.Results.tracks;
+ton=ip.Results.ton;
+toff=ip.Results.toff;
+
+% output struct array
+tracksCombined=struct();
+
+startEnd=vertcat(tracks.info);
+idxFirst=startEnd(:,1) == 1;
+
+% init first entries of tracksCombined with tracks starting in frame 1
+k=1;
+nCombTracks=0;
+while( idxFirst(k) )
+    path=tracks(k).trajectory;
+    tracksCombined(k).path=path;
+    tracksCombined(k).info=tracks(k).info;
+    
+    % calculate weigthed mean of trajectory
+    idx=~isnan(path(:,1));
+    [wm,ws]=weigthedStats(path(idx,1:2),path(idx,5:6));
+    
+    tracksCombined(k).center=[wm,ws,sqrt(sum(ws.^2))];
+    nCombTracks=nCombTracks+1;
+    
+    k=k+1;
+end
+
+% distribute remaining tracks
+while( true )
+    
+    % path holds current trajectory
+    path=tracks(k).trajectory;
+    idx=~isnan(path(:,1));
+    [wm,ws]=weigthedStats(path(idx,1:2),path(idx,5:6));
+    delta=sqrt(sum(ws.^2));
+    % centers obtained so far
+    centers=vertcat(tracksCombined.center);
+    delta=delta+centers(:,end);
+    centers=centers(:,1:2);
+    % squared distance between current centers and present mean
+    distM=distance(centers',wm');
+    idxDist=distM < delta*delta;
+    
+    
+    % temporal overlap of trajectories
+    startEnd=path(:,end);
+    allStartEnd=vertcat(tracksCombined.info);
+    idxFrame=tempOverlap(startEnd,allStartEnd);
+    
+    idx=idxDist & idxFrame;
+    
+    if( sum(idx) == 0 )
+        tracksCombined(k).path=path;
+        tracksCombined(k).center=[wm,ws,sqrt(sum(ws.^2))];
+        nCombTracks=nCombTracks+1;
+        k=k+1;
+        continue;
+    elseif( sum(idx) == 1 )
+        l=find(idx == 1);
+        tmp=tracksCombined(l);
+        tmp=vertcat(tmp,path);
+        tracksCombined(l).path=tmp;
+        [wm,ws]=weightedStats(tmp(:,1:2),tmp(:,5:6));
+        tracksCombined(l).center=[wm,ws,sqrt(ws.^2)];
+    end
+    
+    
+end
+
+
+end
+
+function idxFrame=tempOverlap(fseq,allSeq)
+
+idxFrame=false(size(allSeq,1),1);
+
+for n=1:size(allSeq,1)
+    first=allSeq(n,1);
+    last=allSeq(n,2);
+    idxFrame(n)=isempty(intersect(fseq,first:last));
+end
+
+end
---
+++ ErbBproject/counting/statsuTrack.m
@@ -0,0 +1,183 @@
+function results = statsuTrack(tracksFinal,varargin)
+%STATSUTRACK performs analysis of STORM data using 'tracksFinal' of uTrack
+%
+% US, 01/10/2013
+%
+
+ip=inputParser;
+ip.addRequired('tracksFinal',@isstruct);
+ip.addOptional('rep',10,@isscalar);
+ip.addOptional('tstep',100,@isscalar);
+ip.addOptional('nFrames',3000,@isscalar);
+ip.addOptional('tact',0.05,@isscalar);
+ip.addOptional('texp',0.1,@isscalar);
+
+ip.parse(tracksFinal,varargin{:});
+
+tracksFinal=ip.Results.tracksFinal;
+rep=ip.Results.rep;
+tstep=ip.Results.tstep;
+nFrames=ip.Results.nFrames;
+tact=ip.Results.tact;
+texp=ip.Results.texp;
+
+nTracks=numel(tracksFinal);
+
+tracks=repmat(...
+    struct('trajectory',[],'driftCorrected',[],'info',NaN(1,3),'fromUV',false,'gapClosed',false),nTracks,1);
+trackLengths=zeros(nTracks,2);
+
+for i=1:nTracks
+       
+    % determine start, end and length of trajectory
+    seqOfEvents=tracksFinal(i).seqOfEvents;
+    startFrame=min(seqOfEvents(:,1));
+    endFrame=max(seqOfEvents(:,1));
+    
+    tracks(i).info(1)=startFrame;
+    tracks(i).info(2)=endFrame;
+    tracks(i).info(3)=endFrame-startFrame+1;
+    
+    trackLengths(i,:)=[endFrame-startFrame+1,startFrame];
+    
+    if( mod(startFrame,rep) == 1 )
+        tracks(i).fromUV=true;
+    end
+    
+    % positions, amplitudes, and frames of tracked particles
+    p=tracksFinal(i).tracksCoordAmpCG;
+    [nrows,ncols]=size(p);
+    tmp=reshape(p,8,ncols/8)';
+    tmp(:,end+1)=startFrame:endFrame;
+    tracks(i).trajectory=tmp;
+    
+    % mark tracks with gap closing
+    if( any(isnan(tmp(:))) )
+        tracks(i).gapClosed=true;
+    end
+    
+end
+
+% analyse entire tracks
+[histTotLen,xTotLen]=hist(trackLengths(:,1),1:max(trackLengths(:,1)));
+
+% analyse tracks starting right after UV pulse
+idx=vertcat(tracks.fromUV);
+lengthUV=trackLengths(idx,:);
+[histLenUV,xUV]=hist(lengthUV(:,1),1:max(lengthUV(:,1)));
+% and those starting in the middle of an activation cycle
+lengthNonUV=trackLengths(~idx,:);
+[histLenNonUV,xNonUV]=hist(lengthNonUV(:,1),1:max(lengthNonUV(:,1)));
+
+% save histograms as MATLAB figure
+
+% calculate average track length dependent on starting frame
+aveLengthUV=zeros(nFrames/tstep,3);
+i=1;
+k=1;
+t=k*tstep;
+tmp=[];
+while( 1 )
+    tmp=horzcat(tmp,lengthUV(i));
+    
+    if( lengthUV(i,2) > t )
+        aveLengthUV(k,1)=t;
+        aveLengthUV(k,2)=mean(tmp);
+        aveLengthUV(k,3)=std(tmp);
+        tmp=[];
+        k=k+1;
+        t=k*tstep;
+    end
+    
+    i=i+1;
+    if( i > size(lengthUV,1) )
+        aveLengthUV(k,1)=t;
+        aveLengthUV(k,2)=mean(tmp);
+        aveLengthUV(k,3)=std(tmp);
+        break;
+    end
+    
+    if( t > nFrames )
+        break;
+    end
+end
+
+aveLengthNonUV=zeros(nFrames/tstep,3);
+i=1;
+k=1;
+t=k*tstep;
+tmp=[];
+while( 1 )
+    tmp=horzcat(tmp,lengthNonUV(i));
+    
+    if( lengthNonUV(i,2) > t )
+        aveLengthNonUV(k,1)=t;
+        aveLengthNonUV(k,2)=mean(tmp);
+        aveLengthNonUV(k,3)=std(tmp);
+        tmp=[];
+        k=k+1;
+        t=k*tstep;
+    end
+    
+    i=i+1;
+    if( i > size(lengthNonUV,1) )
+        aveLengthNonUV(k,1)=t;
+        aveLengthNonUV(k,2)=mean(tmp);
+        aveLengthNonUV(k,3)=std(tmp);
+        break;
+    end
+    
+    if( t > nFrames )
+        break;
+    end
+end
+
+% estimated activation rates: UV-induced and spontaneous
+konUV=size(lengthUV,1)/(tact*nFrames/rep);
+konTH=size(lengthNonUV,1)/(texp*(nFrames-nFrames/rep));
+% konTH=size(lengthNonUV,1)/(texp*nFrames);
+
+% fit track length distributions to single exponential
+fitHistTot=fit(xTotLen(1:rep)',log(histTotLen(1:rep))','poly1');
+fitHistUV=fit(xUV(1:rep)',log(histLenUV(1:rep))','poly1');
+fitHistNonUV=fit(xNonUV(1:rep)',log(histLenNonUV(1:rep))','poly1');
+
+% calculate frame-to-frame drift
+shiftMatrix=NaN(nTracks,nFrames,2);
+for k=1:nTracks
+    shift=tracks(k).trajectory(:,1:2);
+    startFrame=tracks(k).info(1);
+    endFrame=tracks(k).info(2);
+    tmp=[shift(1,1:2);shift(1:end-1,:)];
+    shiftMatrix(k,startFrame:endFrame,:)=shift-tmp;
+end
+
+shift=squeeze(nanmean(shiftMatrix,1));
+idx=isnan(shift(:,1));
+shift(idx,:)=0.0;
+
+% correct for drift
+drift=cumsum(shift);
+for k=1:nTracks
+    startFrame=tracks(k).info(1);
+    endFrame=tracks(k).info(2);
+    tracks(k).info(2);
+    t=tracks(k).trajectory;
+    t(:,1:2)=t(:,1:2)-drift(startFrame:endFrame,:);
+    tracks(k).driftCorrected=t;
+end
+
+% store results in struct 'results'
+results.tracks=tracks;
+results.shift=shift;
+results.trackLengths=trackLengths;
+results.histLenTot=[xTotLen',histTotLen'];
+results.histLenUV=[xUV',histLenUV'];
+results.histLenNonUV=[xNonUV',histLenNonUV'];
+results.aveLengthUV=aveLengthUV;
+results.aveLengthNonUV=aveLengthNonUV;
+results.konUV=konUV;
+results.konTH=konTH;
+results.fitHistTot=fitHistTot;
+results.fitHistUV=fitHistUV;
+results.fitHistNonUV=fitHistNonUV;
