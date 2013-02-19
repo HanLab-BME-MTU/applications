ff77c8e694f9fe4d8016b5809519150672bfa4bd
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-02-19 06:09:09 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@9730 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ graphics/densityplot.m
@@ -0,0 +1,33 @@
+% Francois Aguet, 02/19/2013
+
+function densityplot(x, y, varargin)
+
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addOptional('xv', 1:numel(x));
+ip.addOptional('yv', 1:numel(y));
+ip.addParamValue('Handle', gca, @ishandle);
+ip.addParamValue('AdjustBorder', false, @islogical);
+ip.parse(varargin{:});
+xv = ip.Results.xv(:)';
+yv = ip.Results.yv(:)';
+ha = ip.Results.Handle;
+
+XLim = xv([1 end]);
+YLim = yv([1 end]);
+dx = xv(2)-xv(1);
+dy = yv(2)-yv(1);
+if ip.Results.AdjustBorder % bug in eps files? png works fine
+    XLim = [xv(1)-dx/2 xv(end)+dx/2];
+    YLim = [yv(1)-dx/2 yv(end)+dy/2];
+end
+xv = [xv(1)-dx xv xv(end)+dx];
+yv = [yv(1)-dy yv yv(end)+dy];
+M = hist3([y(:) x(:)], {yv, xv});
+
+imagesc(xv, yv, M);
+axis([XLim YLim]);
+
+% re-plot axes on top to create box w/o ticks
+ha2 = copyobj(ha, get(ha, 'Parent'));
+set(ha2, 'XTick', [], 'YTick', [], 'Color', 'none', 'Box', 'on');
