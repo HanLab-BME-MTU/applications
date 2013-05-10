6b5346e4e80e5bad7cc5356b1a77f3a5c615129a
Daniel Nunez <daniel_nunez@hms.harvard.edu>
2013-05-10 18:25:17 +0000

    updating for use with new tracks structure
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10104 0bcb941b-74d1-0310-bb26-e6636707f451

--- calculateWindowInformation.m
+++ calculateWindowInformation.m
@@ -1,4 +1,4 @@
-function [windows,windowPolygons]  = calculateWindowInformation(data,mask);
+function [windows,windowPolygons]  = calculateWindowInformation(data,varargin);
 % calculateWindowInformation divides a cell mask into windows and
 % calculates pairCorrelation, nucleationDensity, and pit lifetimes for
 % each window
@@ -13,50 +13,36 @@ function [windows,windowPolygons]  = calculateWindowInformation(data,mask);
 %       windows: structure with the pairCorrelation, nucleationDensity,
 %       list of pit nucleation sites, and lifetimes for a given window
 
-
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addRequired('data', @(var) isstruct(var)&numel(var) == 1);
+ip.addParamValue('mask', [], @isnumeric);
+ip.addParamValue('chooseImageToSegment', false, @islogical);
+ip.parse(data, varargin{:});
 %MAKE CELL MASK
 
 %BY HAND
-if nargin < 2 || isempty(mask)
-    [filename, pathname] = uigetfile('.tif','choose image to segment by hand');
-    image = imread([pathname filesep filename]);
+if isempty(ip.Results.mask)
+    if ip.Results.chooseImageToSegment
+        [filename, pathname] = uigetfile('.tif','choose image to segment by hand');
+        image = imread([pathname filesep filename]);
+    else
+        image = double(imread(data.framePaths{1}{1}));
+    end
     handle = figure;
     imshow(image,[]);
     %make mask
     maskHandCut = roipoly;
     close(handle)
     
-    %REFINE USING DETECTIONS
-    %area mask parameters
-    closureRadius = 50;
-    dilationRadius = 5;
-    doFill = 1;
-    imsize  = data.imagesize;
-    %Load Lifetime Information
-    try load([data.source filesep 'Tracking' filesep 'trackAnalysis.mat'])
-        
-        %positions used to calculate mask
-        maskPositionsX = arrayfun(@(t) t.x(1),tracks)';
-        maskPositionsY = arrayfun(@(t) t.y(1),tracks)';
-        maskPositions = [maskPositionsX, maskPositionsY];
-        
-    catch ME
-        
-        lftInfo = load([data.source filesep 'LifetimeInfo' filesep 'lftInfo']);
-        lftInfo = lftInfo.lftInfo;
-        matX = lftInfo.Mat_xcoord;
-        % y-coordinate matrix
-        matY = lftInfo.Mat_ycoord;
-        %positions used to calculate mask
-        maskPositions = [matX(~isnan(matX)),matY(~isnan(matY))];
-        
-    end
+    %refine using intensity
     
-    %MAKE MASK
-    imsizS = [imsize(2) imsize(1)];
-    maskDetections = makeCellMaskDetections(maskPositions,closureRadius,dilationRadius,doFill,imsize,0,[]);
-    mask = zeros(size(maskDetections));
-    mask = maskDetections & maskHandCut;
+    areamask = getCellMask(data);
+    mask = zeros(size(areamask));
+    mask = areamask & maskHandCut;
+else
+    image = double(imread(data.framePaths{1}{1}));
+    mask = ip.Results.mask;
 end
 
 %MAKE WINDOWS
@@ -67,8 +53,11 @@ while ~goodWindows
     %make polygons
     windowPolygons = getMaskWindows(mask,perpSize,paraSize);
     %visualize polygons
-    handle = figure;
-    plotWindows(windowPolygons)
+    handle  = imshow(image,[]);
+    hold on
+        plotWindows(windowPolygons)
+  
+    
     goodWindows = input('Is the window size good (1 for yes 0 for no)?');
     if ~goodWindows
         perpSize = input('Enter window dimension perpendicular to membrane');
