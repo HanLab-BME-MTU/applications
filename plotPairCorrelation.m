b7a77ce8cc91404d4ddcc591840a5a049d302608
Daniel Nunez <daniel_nunez@hms.harvard.edu>
2013-05-10 18:27:12 +0000

    updating for use with new tracks structure
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10106 0bcb941b-74d1-0310-bb26-e6636707f451

--- plotPairCorrelation.m
+++ plotPairCorrelation.m
@@ -1,4 +1,4 @@
-function [experiment] = plotPairCorrelation(experiment,dist,rest,plotMask,statusValue)
+function [experiment] = plotPairCorrelation(experiment,dist,plotMask)
 
 % plotPairCorrelation calculates the density of pits defined by rest
 % and that fall within an inputMask
@@ -19,13 +19,6 @@ function [experiment] = plotPairCorrelation(experiment,dist,rest,plotMask,status
 %                       population1 have a status value 1 (this is useful
 %                       in calculating pair correlations for the different
 %                       populations)
-%
-%           rest    =   [optional] restriction vector can have variable length;
-%                       minimum length is five, where the entries are
-%                       [stat da minfr minlft maxlft]
-%                       (default is [1 1 4 1 300])
-%                       optionally, the length can be extended to nine,
-%                       where the additional entries are
 %                       [... minint maxint minmot maxmot]
 %           dist =      [optional] distance vector for binning (default is
 %                       1:20)
@@ -44,42 +37,7 @@ function [experiment] = plotPairCorrelation(experiment,dist,rest,plotMask,status
 %       calculatePitDenFromLR
 %       makeCorrFactorMatrix
 %
-% Daniel Nunez, updated May 05, 2009
-
-%% EXPLANATION of restriction values:
-% rest = [stat da minfr minlft maxlft minint maxint minmot maxmot]
-%
-% stat  =   object status, possible values are 1, 2, 3
-%           1: object appears and disappears in the movie
-%           2: object is present for entire length of the movie
-%           3: object is present in either first or last frame of the movie
-% da    =   appearance/disappearance status, possible values are 1,0,-1
-%           1: marks first frame
-%           0: marks middle frame
-%           -1: marks last frame
-% minfr =   minimum lifetime/length in FRAMES - e.g. 4, to exclude tracking
-%           artifacts of false detection positives
-% minlft =  minimum lifetime in SECONDS - e.g. 60 to select for productive
-%           clathrin-coated pits
-% maxlft =  maximum lifetime in SECONDS - e.g. 25 to select for abortive
-%           clathrin-coated pits
-%
-% OPTIONAL:
-%
-% minint =  minimum normalized intensity (ranging from 0 to 1)
-% maxint =  maximum normalized intensity (ranging from 0 to 1)
-% minmot =  minimum normalized motility (ranging from 0 to 1)
-% maxmot =  maximum normalized motility (ranging from 0 to 1)
-%
-% These latter criteria allow you to select e.g. the brightest 10% of the
-% population, or the faster 50%.
-%
-% EXAMPLE:  to select the positions where productive pits appear
-%           rest1 = [1 1 4 60 300]
-%           to select the positions where abortive pits are located in each
-%           frame
-%           rest2 = [1 0 4 8 25]
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Daniel Nunez, updated May 01, 2013
 
 %%
 %AREA MASK PARAMETERS
@@ -92,14 +50,8 @@ doFill = 0;
 if nargin < 2 || isempty(dist)
     dist = 1:20;
 end
-if nargin < 3 || isempty(rest)
-    rest = [1 1 4 1 300];
-end
-if nargin < 4 || isempty(plotMask)
+if nargin < 3 || isempty(plotMask)
     plotMask = 0;
-end
-if nargin < 5 || isempty(statusValue)
-    statusValue = 1;
 end
 
 
@@ -110,66 +62,16 @@ for iexp = 1:length(experiment)
     % image size
     imsize  = experiment(iexp).imagesize;
     
-    %Load Lifetime Information
-    try load([experiment(iexp).source filesep 'Tracking' filesep 'trackAnalysis.mat'])
-        
-        %positions used to calculate mask
-        maskPositionsX = arrayfun(@(t) t.x(1),tracks)';
-        maskPositionsY = arrayfun(@(t) t.y(1),tracks)';
-        maskPositions = [maskPositionsX, maskPositionsY];
-        
-        %track.status == 1 means track is complete
-        tracks = tracks([tracks.status] == 1 & arrayfun(@(x)all(x.gapStatus~=5),tracks) == 1 &...
-            [tracks.lifetime_s] > rest(1,3)*framerate & ...
-            [tracks.lifetime_s] > rest(1,4) & [tracks.lifetime_s] < rest(1,5));
-        %THIS DOES NOT FILTER FOR
-        %NUCLEATIONS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        
-        %positions used to calculate pair correlation
-        pairPositionsX = arrayfun(@(t) t.x(1),tracks)';
-        pairPositionsY = arrayfun(@(t) t.y(1),tracks)';
-        mpm1 = [pairPositionsX pairPositionsY];
-        
-    catch ME
-        
-        lftInfo = load([experiment(iexp).source filesep 'LifetimeInfo' filesep 'lftInfo']);
-        lftInfo = lftInfo.lftInfo;
-        % status matrix
-        statMat = lftInfo.Mat_status;
-        % lifetime matrix
-        lftMat = lftInfo.Mat_lifetime;
-        % x-coordinate matrix
-        matX = lftInfo.Mat_xcoord;
-        % y-coordinate matrix
-        matY = lftInfo.Mat_ycoord;
-        % disapp status matrix
-        daMat = lftInfo.Mat_disapp;
-        
-        %pit status
-        if isfield(experiment,'status')
-            if isrow(experiment(iexp).status)
-                status = experiment(iexp).status';
-            else
-                status = experiment(iexp).status;
-            end
-        else
-            status = ones(1,size(daMat,1));
-        end
-        
-        
-        %find all pits in movie that meet requirements specified by restriction
-        %vector
-        findPos = ((statMat==rest(1,1)) & (daMat==rest(1,2)) &...
-            (lftMat>rest(1,3)) & (lftMat>round(rest(1,4)/framerate)) & (lftMat<round(rest(1,5)/framerate)) &...
-            repmat(status,1,size(statMat,2)) == statusValue);
-        
-        %positions used to calculate pair correlation 
-        mpm1 = [full(matX(findPos)) full(matY(findPos))];
-        
-        %positions used to calculate mask
-        maskPositions = [matX(~isnan(matX)),matY(~isnan(matY))];
-        
-    end
+    tracks = loadTracks(experiment(iexp));
+    % all positions
+    maskPositions = [];
+            maskPositions(:,1) = cell2mat(arrayfun(@(var)var.x(1,:),tracks,'UniformOutput',false))';
+            maskPositions(:,2) = cell2mat(arrayfun(@(var)var.y(1,:),tracks,'UniformOutput',false))';
+    %all track nucleations
+    mpm = nan(length(tracks),2);
+            mpm(:,1) = cell2mat(arrayfun(@(var)var.x(1,1),tracks,'UniformOutput',false))';
+            mpm(:,2) = cell2mat(arrayfun(@(var)var.y(1,1),tracks,'UniformOutput',false))';
+    
     %MAKE MASK
     imsizS = [imsize(2) imsize(1)];
     [areamask] = makeCellMaskDetections(maskPositions,closureRadius,...
@@ -195,19 +97,17 @@ for iexp = 1:length(experiment)
     %     else
     %
     
-    [kr,lr]=RipleysKfunction(mpm1,mpm1,imsizS,dist,corrFacMat,normArea);
-    [currDen] = calculatePitDenFromLR(kr,dist);
-    pair = currDen;
+    [kr,lr,pair]=RipleysKfunction(mpm,mpm,imsizS,dist,corrFacMat,normArea);
     
     %store pair in each experiment structure
     experiment(iexp).pairCorrelation = pair;
     experiment(iexp).clustering = sum(pair(1:2));
+    experiment(iexp).nucleationDensity = length(tracks)/normArea; 
     
     %SCRAMBLE MPM
-    [mpm1] = makeRandomMPM(mpm1, areamask',1);
-    [kr,lr]=RipleysKfunction(mpm1,mpm1,imsizS,dist,corrFacMat,normArea);
-    [currDen] = calculatePitDenFromLR(kr,dist);
-    experiment(iexp).pairCorrelationRandom = currDen;
+    [mpm] = makeRandomMPM(mpm, areamask',1);
+    [kr,lr,pair]=RipleysKfunction(mpm,mpm,imsizS,dist,corrFacMat,normArea);
+    experiment(iexp).pairCorrelationRandom = pair;
     
     
 end
