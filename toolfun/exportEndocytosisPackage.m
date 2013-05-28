f8265c0140b8d875e7e18777e6c5a91883950464
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-05-28 23:55:38 +0000

    Preliminary version of export function, only exports PointSourceDetection
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10164 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ toolfun/exportEndocytosisPackage.m
@@ -0,0 +1,94 @@
+
+% Adapted from buildPackage.m
+
+% Francois Aguet, 05/28/2013
+
+function exportEndocytosisPackage(exportDir)
+
+if nargin<1 || isempty(exportDir)
+    exportDir = '.';
+end
+
+% list of variable names erroneously detected as functions
+ignoreList = {'consist'; 'knn'};
+
+% packages to include from extern
+externRoot = [getParentDir(mfilename('fullpath'),3) 'extern' filesep];
+externList = {'kdtree_andrea'};
+externName = {'kdtree'};
+
+%-----------------------------------------------------
+% 1) Export pointSourceDetection.m separately
+%-----------------------------------------------------
+[fctList, toolboxList] = getFunDependencies('pointSourceDetection.m');
+[fpaths, fnames, fexts] = cellfun(@fileparts, fctList, 'unif', 0);
+% remove unneeded functions
+rmIdx = ismember(fnames, ignoreList);
+tmp = cellfun(@(i) regexpi(fpaths, i, 'once'), externList, 'unif', 0);
+tmp = horzcat(tmp{:});
+rmIdx = rmIdx | any(~cellfun(@(i) isempty(i), tmp),2);
+fpaths(rmIdx) = [];
+fnames(rmIdx) = [];
+fexts(rmIdx) = [];
+
+% Update ignore list for main package
+ignoreList = [ignoreList; fnames];
+
+mexExts = {'.mexmaci64'; '.mexa64'; '.mexw64'};
+mexIdx = find(ismember(fexts, mexExts));
+% Expand MEX list to all platforms
+mexNames = fnames(mexIdx);
+mexNames = arrayfun(@(i) cellfun(@(e) [mexNames{i} e], mexExts, 'unif', 0), 1:numel(mexNames), 'unif', 0);
+mexNames = vertcat(mexNames{:});
+mexPaths = fpaths(mexIdx);
+mexPaths = reshape(repmat(mexPaths', [numel(mexExts) 1]), [numel(mexExts)*numel(mexPaths) 1]);
+fpaths(mexIdx) = [];
+fnames(mexIdx) = [];
+fexts(mexIdx) = [];
+fnames = arrayfun(@(i) [fnames{i} fexts{i}], 1:numel(fnames), 'unif', 0)';
+
+
+
+% copy core functions
+dest = [exportDir filesep 'PointSourceDetection' filesep];
+[~,~] = mkdir(dest);
+for i = 1:numel(fnames)
+    copyfile([fpaths{i} filesep fnames{i}], [dest fnames{i}]);
+end
+
+% copy MEX functions
+mdest = [dest filesep 'mex' filesep];
+[~,~] = mkdir(mdest);
+for i = 1:numel(mexNames)
+    copyfile([mexPaths{i} filesep mexNames{i}], [mdest mexNames{i}]);
+end
+
+% copy external packages
+for i = 1:numel(externList)
+    edest = [dest externName{i}];
+    source = [externRoot externList{i}];
+    %cmd = ['svn export ' source ' ' edest];
+    %system(cmd);
+    % -or-
+    [~,~] = mkdir(edest);
+    copyfile(source, edest);
+end
+
+% set permissions
+cmd = ['chmod -R 755 ' dest];
+system(cmd);
+
+%-----------------------------------------------------
+% 2) Export Endocytosis project
+%-----------------------------------------------------
+masterList = {'runDetection.m', 'runTracking.m'};
+for i = 1:numel(masterList);
+    [fctList, toolboxList] = getFunDependencies(masterList{i});
+    fctList
+end
+% [fpaths, fnames, fexts] = cellfun(@fileparts, fctList, 'unif', 0);
+
+
+
+% disp('The package uses the following toolboxes:')
+% disp(toolboxes)
