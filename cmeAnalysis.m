f149b379c41efd95aa7b837ad6cf8c95f156126f
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-05-30 18:41:44 +0000

    Bug fixes
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10185 0bcb941b-74d1-0310-bb26-e6636707f451

--- cmeAnalysis.m
+++ cmeAnalysis.m
@@ -10,10 +10,19 @@ ip.addOptional('data', [], @isstruct);
 ip.addParamValue('Overwrite', false, @islogical);
 ip.addParamValue('TrackingRadius', [3 6], @(x) numel(x)==2);
 ip.addParamValue('TrackingGapLength', 2, @(x) numel(x)==1);
+ip.addParamValue('Parameters', [], @(x) numel(x)==3);
 ip.parse(varargin{:});
 data = ip.Results.data;
+
 if isempty(data)
-    data = loadConditionData();
+    parameters = ip.Results.Parameters;
+    if isempty(parameters)
+        parameters = zeros(1,3);
+        parameters(1) = input('Enter the N.A. of the objective: ');
+        parameters(2) = input('Enter the magnification of the objective: ');
+        parameters(3) = 1e-6*input('Enter the camera pixel size, in [um]: ');
+    end
+    data = loadConditionData('Parameters', parameters);
 end
 
 opts = {'Overwrite', ip.Results.Overwrite};
