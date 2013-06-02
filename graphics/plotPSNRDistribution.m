bf4baea162c1c7d9e0df5352f5e0ae887b07d33c
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-06-02 19:34:58 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10205 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ graphics/plotPSNRDistribution.m
@@ -0,0 +1,31 @@
+function ha = plotPSNRDistribution(data, varargin)
+
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addRequired('data', @isstruct);
+ip.addOptional('ha', [], @isnumeric);
+ip.addParamValue('Color', hsv2rgb([0.33 1 0.9]));
+ip.addParamValue('XLim', [1 300]);
+ip.parse(data, varargin{:});
+ha = ip.Results.ha;
+
+psnr = arrayfun(@(i) getPSNRDistribution(i, 'Mode', 'all'), data, 'unif', 0);
+psnr = [psnr{:}];
+
+% PSNR values, in [dB]
+dx = 0.2;
+xi = 10.^((0:dx:30)/10); 
+fi = hist(psnr, xi)/numel(psnr);
+
+fset = loadFigureSettings('print');
+if isempty(ha)
+    figure(fset.fOpts{:});
+    ha = axes(fset.axOpts{:}, 'xscale', 'log');
+    hold on;
+end
+plot(xi, fi, '-', 'Color', ip.Results.Color, 'LineWidth', 1);
+set(ha, 'XLim', ip.Results.XLim, 'XTick', 10.^(0:2), 'XTickLabel', {'1', '10', '100'},...
+    fset.sfont{:});
+xlabel('PSNR', fset.sfont{:});
+ylabel('Frequency', fset.sfont{:});
+formatTickLabels();
