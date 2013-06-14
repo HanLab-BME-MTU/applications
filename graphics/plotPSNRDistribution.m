6c8a5b6ba7a49c0cfb9cb8e68fc5f384c5a14ebe
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-06-14 23:10:40 +0000

    Added options for overlaying distributions from multiple cells
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10278 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/plotPSNRDistribution.m
+++ graphics/plotPSNRDistribution.m
@@ -10,24 +10,43 @@ ip.addRequired('data', @isstruct);
 ip.addOptional('ha', [], @isnumeric);
 ip.addParamValue('Color', hsv2rgb([0.33 1 0.9]));
 ip.addParamValue('XLim', [1 300]);
+ip.addParamValue('Pool', true, @islogical);
+ip.addParamValue('DisplayMode', 'screen', @(x) any(strcmpi(x, {'screen', 'print'})));
 ip.parse(data, varargin{:});
 ha = ip.Results.ha;
 
-psnr = arrayfun(@(i) getPSNRDistribution(i, 'Mode', 'all'), data, 'unif', 0);
-psnr = [psnr{:}];
+nd = numel(data);
+psnr = cell(1,nd);
+parfor i = 1:nd
+    psnr{i} = getPSNRDistribution(data(i), 'Mode', 'all');
+end
 
-% PSNR values, in [dB]
-dx = 0.2;
-xi = 10.^((0:dx:30)/10); 
-fi = hist(psnr, xi)/numel(psnr);
-
-fset = loadFigureSettings('print');
+fset = loadFigureSettings(ip.Results.DisplayMode);
 if isempty(ha)
     figure(fset.fOpts{:});
     ha = axes(fset.axOpts{:}, 'xscale', 'log');
     hold on;
 end
-plot(xi, fi, '-', 'Color', ip.Results.Color, 'LineWidth', 1);
+% PSNR values, in [dB]
+dx = 0.2;
+xi = 10.^((0:dx:30)/10);
+if ~ip.Results.Pool
+    cmap = jet(nd);
+    [~,idx] = sort(cellfun(@median, psnr));
+    hp = zeros(1,nd);
+    for i = 1:numel(data)
+        fi = hist(psnr{i}, xi)/numel(psnr{i});
+        hp(i) = plot(xi, fi, '-', 'Color', cmap(i,:), 'LineWidth', 1);
+    end
+    ltext = arrayfun(@(x) getCellDir(x), data, 'unif', 0);
+    hl = legend(hp(idx), ltext(idx));
+    set(hl, 'Interpreter', 'none', fset.tfont{:}, 'Box', 'off')
+else
+    psnr = [psnr{:}];
+    fi = hist(psnr, xi)/numel(psnr);
+    plot(xi, fi, '-', 'Color', ip.Results.Color, 'LineWidth', 1);
+end
+
 set(ha, 'XLim', ip.Results.XLim, 'XTick', 10.^(0:2), 'XTickLabel', {'1', '10', '100'},...
     fset.sfont{:});
 xlabel('PSNR', fset.sfont{:});
