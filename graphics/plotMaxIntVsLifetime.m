38781a38d0eef00c82aae762569d40ba1a35c5df
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-06-20 17:52:07 +0000

    Added options
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10310 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/plotMaxIntVsLifetime.m
+++ graphics/plotMaxIntVsLifetime.m
@@ -1,14 +1,24 @@
+%plotMaxIntVsLifetime(data, varargin) plots maximum CCS intensity vs. lifetime
+%
+% Inputs:
+%          data : structure returned by loadConditionData()
+%
+% Options ('specifier', value):
+%    
+%      'ExcludeVisitors' : true|{false}
+%             'Cutoff_f' : Minimum track length, in frames
+%         'FirstNFrames' : calculates max. intensity for the first N frames
+
+% Francois Aguet, 2011 (last modified 06/20/2013)
+
 function plotMaxIntVsLifetime(data, varargin)
 
 ip = inputParser;
 ip.CaseSensitive = false;
 ip.addRequired('data', @(x) isstruct(x) && numel(unique([data.framerate]))==1);
-ip.addOptional('lb', [1  11 16 21 41 61]);
-ip.addOptional('ub', [10 15 20 40 60 120]);
-ip.addParamValue('Display', 'on', @(x) any(strcmpi(x, {'on', 'off', 'all'})));
 ip.addParamValue('ExcludeVisitors', false, @islogical);
 ip.addParamValue('Cutoff_f', 5, @isscalar);
-ip.addParamValue('FirstNFrames', [], @isposint);
+ip.addParamValue('FirstNFrames', [], @isvector);
 ip.parse(data, varargin{:});
 
 opts = {'Scale', true, 'ReturnValidOnly', true, 'Cutoff_f', ip.Results.Cutoff_f,...
@@ -20,16 +30,27 @@ avec = 0:2:300;
 
 fset = loadFigureSettings('print');
 A = vertcat(lftData.A);
-if ~isempty(ip.Results.FirstNFrames)
-    A = A(:,1:ip.Results.FirstNFrames);
-end
-maxA = max(A,[],2);
 lft = vertcat(lftData.lifetime_s);
 
-figure(fset.fOpts{:}, 'Position', [10 10 6.5 6.5]);
-axes(fset.axOpts{:}, 'Position', [1.5 1.5 4.5 4.5], 'TickLength', fset.TickLength/4.5*6);
-hold on;
-densityplot(lft, maxA, lvec, avec, 'DisplayFunction', @log);
-ylabel('Max. fluo. intensity (A.U.)', fset.lfont{:});
-xlabel('Lifetime (s)', fset.lfont{:});
-set(gca, 'XTick', 0:20:120);
+if isempty(ip.Results.FirstNFrames)
+    maxA = max(A,[],2);
+    
+    figure(fset.fOpts{:}, 'Position', [10 10 6.5 6.5]);
+    axes(fset.axOpts{:}, 'Position', [1.5 1.5 4.5 4.5], 'TickLength', fset.TickLength/4.5*6);
+    hold on;
+    densityplot(lft, maxA, lvec, avec, 'DisplayFunction', @log);
+    ylabel('Max. fluo. intensity (A.U.)', fset.lfont{:});
+    xlabel('Lifetime (s)', fset.lfont{:});
+    set(gca, 'XTick', 0:20:120);
+else
+    for i = 1:numel(ip.Results.FirstNFrames)
+        maxA = max(A(:,1:ip.Results.FirstNFrames(i)),[],2);
+        figure(fset.fOpts{:}, 'Position', [10 10 6.5 6.5]);
+        axes(fset.axOpts{:}, 'Position', [1.5 1.5 4.5 4.5], 'TickLength', fset.TickLength/4.5*6); %#ok<LAXES>
+        hold on;
+        densityplot(lft, maxA, lvec, avec, 'DisplayFunction', @log);
+        ylabel('Max. fluo. intensity (A.U.)', fset.lfont{:});
+        xlabel('Lifetime (s)', fset.lfont{:});
+        set(gca, 'XTick', 0:20:120);
+    end
+end
