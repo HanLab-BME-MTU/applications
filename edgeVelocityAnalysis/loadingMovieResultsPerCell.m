e1906ce2abb12bacc30e72d99bbddc7fa663162a
Marco Vilela <marco_vilela@hms.harvard.edu>
2013-06-25 01:07:16 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/morphodynamics/main@10331 0bcb941b-74d1-0310-bb26-e6636707f451

--- edgeVelocityAnalysis/loadingMovieResultsPerCell.m
+++ edgeVelocityAnalysis/loadingMovieResultsPerCell.m
@@ -1,4 +1,4 @@
-function  out = loadingMovieResultsPerCell(ML)
+function  out = loadingMovieResultsPerCell(ML,analysisDir,fileName)
 %Loading edge velocity analysis for each cell in the MovieList
 
 nCell = numel(ML.movies_);
@@ -8,9 +8,9 @@ for iCell = 1:nCell
 
     currMD = ML.movies_{iCell};
      %Saving results for each cell
-    edgePath = [currMD.outputDirectory_ filesep 'EdgeVelocityAnalysis'];
+    edgePath = [currMD.outputDirectory_ filesep analysisDir];
     if isdir(edgePath)
-        filePath = [edgePath filesep 'edgeVelocity.mat'];
+        filePath = [edgePath filesep fileName '.mat'];
         aux      = load(filePath);
         out(cF)  = aux.analysisResults;
         cF       = cF + 1;
