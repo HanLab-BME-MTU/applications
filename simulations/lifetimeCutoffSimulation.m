c635aecbfbbc98ab2b53c2397277d184c2d6cc87
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-07-15 20:10:38 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10376 0bcb941b-74d1-0310-bb26-e6636707f451

--- simulations/lifetimeCutoffSimulation.m
+++ simulations/lifetimeCutoffSimulation.m
@@ -1,10 +1,13 @@
 function lifetimeCutoffSimulation()
 
-% this simulation is not complete, since it does not include cut-off tracks
-% at beginning/end
+N = 1e5; % new objects per frame
+% N = 5;
 
+% lifetime distribution is modeled as a gamma function
 lambda = 80;
 k = 2;
+lftPDF = @(lambda, k, t) k./lambda.*(t/lambda).^(k-1).*exp(-(t/lambda).^k);
+lftCDF = @(lambda, k, t) 1 - exp(-(t/lambda).^k);
 
 % for PDF/CDF calc.
 % tmax = lambda*nthroot(-log(1-0.9999), k)
@@ -12,35 +15,28 @@ k = 2;
 tmin = lambda*nthroot(-log(0.01), k);
 fprintf('Time interval to record 99th percentile: %f\n', tmin);
 
-tmax = 250;
+tmax = 160;
 framerate = 4;
 dt = framerate;
 t = dt:dt:tmax;
 
-
 nFrames = length(t);
 
 % generate theoretical histogram by stepwise integration of the PDF
-F = 20; % subsampling factor
-dtf = dt/F;
-ti = t(1):dtf:(tmax+dt-dtf);
-PDF = k./lambda.*(ti/lambda).^(k-1).*exp(-(ti/lambda).^k);
-histTH = arrayfun(@(k) sum(PDF(1+(k-1)*F:k*F)), 1:length(t));
+histTH = diff(lftCDF(lambda, k, dt:dt:t(end)+dt));
 
 % generate PDF for plotting
-t_fine = 0:dtf:tmax;
-PDF = k./lambda.*(t_fine/lambda).^(k-1).*exp(-(t_fine/lambda).^k);
-%CDF = 1 - exp(-(t/lambda).^k);
+t_fine = 0:dt/20:tmax;
+PDF = lftPDF(lambda, k, t_fine);
 
 
-N = 1e4; % new objects per frame
 samples = cell(1,nFrames);
 for n = 1:nFrames
 
     % generate lifetimes for events starting in current frame
     csamples = lambda*nthroot(-log(1-rand(1,N)), k);
     % cut off at lower frame bound
-    csamples = csamples - mod(csamples, dtf);
+    %csamples = csamples - mod(csamples, dtf);
     % throw out short (< 1 frame) tracks
     csamples(csamples==0) = []; 
 
@@ -71,20 +67,11 @@ set(h, 'BarWidth', 1, 'FaceColor', [0 0.8 0], 'EdgeColor', [0 0.4 0]);
 hold on;
 plot(t_bc, histTH, 'r.');
 plot(t_fine, PDF, 'r', 'LineWidth', 1);
-xlim([0 tmax]);
-%title('Measured lifetimes', 'FontName', 'Helvetica', 'FontSize', 16);
-%xlabel('Time (s)', 'FontName', 'Helvetica', 'FontSize', 16);
+xlim([0 tmax+10]);
 ylabel('Relative frequency', 'FontName', 'Helvetica', 'FontSize', 16);
-set(gca, 'FontName', 'Helvetica', 'FontSize', 14, 'LineWidth', 1.5);
+set(gca, 'FontName', 'Helvetica', 'FontSize', 14, 'LineWidth', 1.5, 'Layer', 'top');
 legend('Measured lifetimes', 'PDF');
 
-m = length(ni);
-w = m./(m:-1:1);
-wx = w;
-% ni = ni.*w;
-nix = ni.*w;
-nix = nix / sum(nix*dt);
-% SE = sum((nix-histTH).^2)
 
 % weights as a function of frame #: w = nFrames ./ (nFrames-(1:nFrames)-1)
 % lengths of 'framerate' and 'framerate-1' cannot be measured -> set to zero
@@ -94,20 +81,19 @@ ni = ni.*w;
 % normalize
 ni = ni/sum(ni*dt);
 
-% SE = sum((ni-histTH).^2)
-
 
 subplot(2,1,2);
 h = bar(t_bc, ni);
 set(h, 'BarWidth', 1, 'FaceColor', [0 0.8 0], 'EdgeColor', [0 0.4 0]);
 hold on;
-plot(t_bc, ni, '.', 'Color', [0 0.4 0]);
 plot(t_bc, histTH, 'ro');
 plot(t_fine, PDF, 'r-', 'LineWidth', 1);
-xlim([0 tmax]);
-%title('Corrected lifetimes', 'FontName', 'Helvetica', 'FontSize', 16);
+xlim([0 tmax+10]);
 xlabel('Time (s)', 'FontName', 'Helvetica', 'FontSize', 16);
 ylabel('Relative frequency', 'FontName', 'Helvetica', 'FontSize', 16);
-set(gca, 'FontName', 'Helvetica', 'FontSize', 14, 'LineWidth', 1.5);
-legend('Corrected lifetimes', 'PDF');
+set(gca, 'FontName', 'Helvetica', 'FontSize', 14, 'LineWidth', 1.5, 'Layer', 'top');
+legend('Corrected lifetimes', 'disc. PDF', 'cont. PDF');
 %print('-depsc2', '-r300', 'lifetimeCutoffCorrection.eps');
+
+figure; plot(t_bc, ni-histTH);
+
