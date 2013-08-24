845fcbf59b03a94d68d31b09da24f1af47fc3467
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-08-24 20:44:11 +0000

    Function for adding timestamps and cropping movie frames. Uses ImageMagick.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10549 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ toolfun/imxTimestamp.m
@@ -0,0 +1,49 @@
+%imxTimestamp() uses ImageMagick to add a time stamp to movie frames
+
+% Francois Aguet, 08/2013
+
+function imxTimestamp(paths, t, varargin)
+
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addRequired('paths');
+ip.addRequired('t');
+ip.addParamValue('FontSize', 20);
+ip.addParamValue('Location', 'SouthWest');
+ip.addParamValue('Offsets', [100 20]);
+ip.addParamValue('Crop', []);
+ip.parse(paths, t, varargin{:});
+crop = ip.Results.Crop;
+
+if ischar(paths)
+    paths = {paths};
+end
+
+info = imfinfo(paths{1});
+nx = info.Width;
+ny = info.Height;
+
+dx = ip.Results.Offsets(1);
+dy = ip.Results.Offsets(2);
+wx = 11;
+
+for i = 1:numel(paths)
+    
+    str = num2str(t(i));
+    cmd = ['export DYLD_LIBRARY_PATH=""; convert ' paths{i} ...
+        ' -fill black -gravity SouthEast -font Helvetica -pointsize '  num2str(ip.Results.FontSize)];
+
+    if ~isempty(crop)
+                cmd = [cmd ' -crop ' num2str(round(nx*crop(3))) 'x' num2str(round(ny*crop(4))) ...
+            '+' num2str(round(nx*(1-crop(1)-crop(3)))) '+' num2str(round(ny*crop(2))) ' +repage '];
+        nx = round(nx*crop(3));
+        ny = round(ny*crop(4));
+    end
+
+    cmd = [cmd ' -annotate +' num2str(nx-dx) '+' num2str(dy) ' "s" ']; %#ok<*AGROW>
+    for k = numel(str):-1:1
+        cmd = [cmd ' -annotate +' num2str(nx-dx+wx*(k)) '+' num2str(dy) ' "' str(numel(str)-k+1) '" '];
+    end
+    cmd = [cmd paths{i}];
+    system(cmd);
+end
