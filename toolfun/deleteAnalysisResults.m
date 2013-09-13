4ce1188ca12a16d63794ef7671781c4e0aa7400b
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-09-13 20:31:03 +0000

    Added option for deleting multiple directories
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10673 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/deleteAnalysisResults.m
+++ toolfun/deleteAnalysisResults.m
@@ -1,15 +1,20 @@
-function deleteAnalysisResults(data)
+function deleteAnalysisResults(data, dirList)
+if ~iscell(dirList);
+    dirList = {dirList};
+end
 
 for i = 1:numel(data)
-    delpath = [data(i).source 'Detection'];
-    if exist(delpath, 'dir')==7
-        str = [];
-        while ~any(strcmpi(str, {'y','n'}))
-            str = input(['Delete results for ' getShortPath(data(i)) ' ? [y/n]: '],'s');
-        end
-        if strcmpi(str, 'y')
-            rmdir(delpath, 's');
-            fprintf('Deleted %s\n', delpath);
+    for d = 1:numel(dirList)
+        delpath = [data(i).source dirList{d}];
+        if exist(delpath, 'dir')==7
+            str = [];
+            while ~any(strcmpi(str, {'y','n'}))
+                str = input(['Delete ' getShortPath(data(i)) dirList{d} ' ? [y/n]: '],'s');
+            end
+            if strcmpi(str, 'y')
+                rmdir(delpath, 's');
+                fprintf('Deleted %s\n', delpath);
+            end
         end
     end
 end
