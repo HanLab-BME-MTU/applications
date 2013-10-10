795619d6c4f260fd250046be9f116736d38f1cf0
Jeffrey Werbin <jeffrey_werbin@hms.harvard.edu>
2013-10-10 13:37:16 +0000

    Code for visualizing exchange paint images
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/receptors/main@10816 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ ErbBproject/analysis/exchangePaintVisualizer.m
@@ -0,0 +1,35 @@
+function [handle]=exchangePaintVisualizer(PL, varargin)
+% This function generates a merged exchange paint graph 
+
+%default labels
+labels = {'EGFR','ErbB2','ErbB3','IGF-1R','cMet'};
+
+cmap = {'r','g','b','c','m','y','k'};
+
+
+ip=inputParser;
+ip.CaseSensitive=false;
+ip.StructExpand=true;
+
+ip.addRequired('PL',@iscell);
+
+ip.addOptional('labels',labels, @iscell);
+ip.addOptional('cmap',cmap, @iscell);
+
+ip.parse(PL,varargin{:});
+
+labels = ip.Results.labels;
+cmap = ip.Results.cmap;
+
+handle = figure;
+hold;
+
+
+for i=1:numel(PL)
+    temp = PL{i}.com;
+    scatter(temp(:,1),temp(:,2),[cmap{mod(i-1,7)+1},'.']);   
+end
+
+legend(labels);
+
+end
