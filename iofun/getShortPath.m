4cc88ef694097cf035248afc0d6357a429f02ba0
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-11-15 00:34:26 +0000

    level option added
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10924 0bcb941b-74d1-0310-bb26-e6636707f451

--- iofun/getShortPath.m
+++ iofun/getShortPath.m
@@ -2,16 +2,19 @@
 
 % Francois Aguet, 05/13/2011
 
-function spath = getShortPath(data)
+function spath = getShortPath(data, level)
+if nargin<2
+    level = 3;
+end
 
 if numel(data.channels)>1
     mCh = strcmp(data.channels, data.source);
     sCh = setdiff(1:length(data.channels),mCh);
     spath = data.channels{sCh(1)};
     idx = regexp(spath, filesep);
-    spath = spath(idx(end-4)+1:idx(end-1));
+    spath = spath(idx(end-level-1)+1:idx(end-1));
 else
     spath = data.channels{1};
     idx = regexp(spath, filesep);
-    spath = spath(idx(max(1,end-3))+1:idx(end)-1);
+    spath = spath(idx(max(1,end-level))+1:idx(end)-1);
 end
