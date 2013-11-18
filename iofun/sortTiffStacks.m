6f96fb00731af589747bb8068b4f8f50a625a8f2
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-11-18 23:13:47 +0000

    Added multi-channel functionality
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@10930 0bcb941b-74d1-0310-bb26-e6636707f451

--- iofun/sortTiffStacks.m
+++ iofun/sortTiffStacks.m
@@ -7,7 +7,8 @@ function sortTiffStacks(varargin)
 ip = inputParser;
 ip.CaseSensitive = false;
 ip.addOptional('path', [], @(x) ischar(x) || isempty(x) || iscell(x));
-ip.addParamValue('ChannelSpecifier', []);
+ip.addOptional('ChannelSpecifier', [], @iscell);
+ip.addParamValue('MovieSelector', 'cell', @ischar);
 ip.parse(varargin{:});
 stkpath = ip.Results.path;
 chSpec = ip.Results.ChannelSpecifier;
@@ -21,7 +22,7 @@ end
 
 % Recursive call if input is cell array
 if iscell(stkpath)
-    cellfun(@(x)sortTiffStacks(x, 'ChannelSpecifier', chSpec), stkpath);
+    cellfun(@(x)sortTiffStacks(x, chSpec), stkpath);
     return
 end
 
@@ -36,9 +37,34 @@ if N==0
     fprintf('No TIFF files found in input directory.\n');
 end
 
-for k = 1:N
-    [~,dirName] = fileparts(stkList{k});
+% stack index/identifier following selector (i.e., 12 for 'Cell12_2s')
+movieID = str2double(regexpi(stkList, ['(?<=' ip.Results.MovieSelector ')\d+'], 'match', 'once'));
+
+% frame rate (if in file name)
+framerate = str2double(regexpi(stkList, '(?<=_)\d+(?=s)', 'match', 'once'));
+[movieID,idx] = unique(movieID);
+framerate = framerate(idx);
+
+nCh = numel(chSpec);
+
+N = numel(movieID);
+for k = N:-1:1
+    if ~isnan(framerate(k))
+        dirName = [ip.Results.MovieSelector num2str(movieID(k)) '_' num2str(framerate(k)) 's'];
+    else
+        dirName = [ip.Results.MovieSelector num2str(movieID(k))];
+    end
     [~,~] = mkdir([stkpath dirName]);
     
-    movefile([stkpath stkList{k}], [stkpath dirName]);
+    if nCh>0
+        for c = 1:nCh
+            % create sub-directory for each channel
+            [~,~] = mkdir([stkpath dirName filesep chSpec{c}]);
+            % move matching files to sub-directory
+            movefile([stkpath ip.Results.MovieSelector num2str(movieID(k)) '*' chSpec{c} '*.*'],...
+                [stkpath dirName filesep chSpec{c}]);
+        end
+    else
+        movefile([stkpath ip.Results.MovieSelector num2str(movieID(k)) '*.*'], [stkpath dirName]);
+    end
 end
