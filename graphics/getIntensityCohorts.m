2d6c1acd006092078033d3a4f4cb6b48c7a6929a
Francois Aguet <francois_aguet@hms.harvard.edu>
2014-03-28 04:22:22 +0000

    Bug fix; new format
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@11205 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/getIntensityCohorts.m
+++ graphics/getIntensityCohorts.m
@@ -1,7 +1,7 @@
 
 % Francois Aguet, 2013
 
-function [res, cT] = getIntensityCohorts(data, varargin)
+function [res, ctime] = getIntensityCohorts(data, varargin)
 
 ip = inputParser;
 ip.CaseSensitive = false;
@@ -9,18 +9,12 @@ ip.addRequired('data', @isstruct);
 ip.addOptional('lftData', [], @isstruct);
 ip.addParamValue('Overwrite', false, @islogical);
 ip.addParamValue('CohortBounds_s', [10 20 40 60 80 100 120]);
-% ip.addParamValue('ShowVariation', true, @islogical);
-% ip.addParamValue('FillMode', 'SEM', @(x) any(strcmpi(x, {'SEM', 'pct'})));
-% ip.addParamValue('FrontLayer', false, @islogical);
-% ip.addParamValue('ShowBackground', false, @islogical);
 ip.addParamValue('Rescale', true, @islogical);
 ip.addParamValue('ExcludeVisitors', true, @islogical);
-% ip.addParamValue('TrackIndex', []);
 ip.addParamValue('Cutoff_f', 5);
 ip.addParamValue('Alpha', 0.05);
+ip.addParamValue('DetectionMode', [], @(x) isempty(x) || any(strcmpi(x, {'m', 's'})));
 ip.addParamValue('SelIndex', [], @iscell); % selection index for each lftData
-% ip.addParamValue('RemoveOutliers', false, @islogical);
-% ip.addParamValue('AvgFun', @nanmean, @(x) isa(x, 'function_handle'));
 ip.addParamValue('LftDataName', 'lifetimeData.mat');
 ip.parse(data, varargin{:});
 cohortBounds = ip.Results.CohortBounds_s;
@@ -35,9 +29,6 @@ end
 
 framerate = data(1).framerate;
 
-% expand bounds to include longest lifetime
-% cohortBounds = [cohortBounds max(vertcat(lftData.lifetime_s))+framerate];
-
 nCh = numel(data(1).channels);
 nd = numel(data);
 nc = numel(cohortBounds)-1;
@@ -49,12 +40,12 @@ kLevel = norminv(1-ip.Results.Alpha/2.0, 0, 1);
 %     selIndex = arrayfun(@(i) size(i.A,1), lftData, 'unif', 0);
 % end
 
-% # data points in cohort (including buffer frames)
+% # data points in cohort (including buffers)
 b = size(lftData(1).sbA,2);
 iLength = arrayfun(@(c) floor(mean(cohortBounds([c c+1]))/framerate) + 2*b, 1:nc);
 
 % time vectors for cohorts
-cT = arrayfun(@(i) (-b:i-b-1)*framerate, iLength, 'unif', 0);
+ctime = arrayfun(@(i) (-b:i-b-1)*framerate, iLength, 'unif', 0);
 
 res(1:nd) = struct('aInterp', [], 'rInterp', []);
 cohortBounds(end) = cohortBounds(end)+framerate;
@@ -65,16 +56,21 @@ for i = 1:nd
     
     nt = numel(lftData(i).lifetime_s);
     
-    % cohort index (index of tracks outside of selected cohorts is 0)
+    % cohort index
+    % index of tracks < first cohort is 0; > last cohort is nc+1
     cidx = zeros(nt,1);
     for c = 1:nc
         cidx(cohortBounds(c)<=lftData(i).lifetime_s & lftData(i).lifetime_s<cohortBounds(c+1)) = c;
     end
-
+    cidx(cohortBounds(c+1)<lftData(i).lifetime_s) = nc+1;
+    
+    % valid cohort index
+    vidx = cidx>0 & cidx<=nc;
+    
     for ch = 1:nCh % channels
         % loop through tracks and interpolate to cohort time vector
         for t = 1:nt
-            if cidx(t)~=0%~isnan(cidx(t))
+            if vidx(t)
                 tidx = 1:lftData(i).trackLengths(t);
                 A = [lftData(i).sbA(t,:,ch) lftData(i).A(t,tidx,ch) lftData(i).ebA(t,:,ch)];
                 sigma_r = [lftData(i).sbSigma_r(t,:,ch) lftData(i).sigma_r(t,tidx,ch) lftData(i).ebSigma_r(t,:,ch)];
@@ -87,19 +83,22 @@ for i = 1:nd
             end
         end
     end
-    res(i).aInterp = aInterp;
-    res(i).rInterp = kLevel*rInterp;
-    res(i).cidx = cidx;
+    
+    if nCh>1 && ~isempty(ip.Results.DetectionMode) % apply further selection based on slave channel
+        if strcmpi(ip.Results.DetectionMode, 'm')
+            idx = lftData(i).significantMaster(:,2)==1;
+        elseif strcmpi(ip.Results.DetectionMode, 's')
+            idx = lftData(i).significantSlave(:,2)==1;
+        end
+        vidx = vidx & idx;
+    end
+    res(i).aInterp = aInterp(vidx,:,:);
+    res(i).rInterp = kLevel*rInterp(vidx,:,:);
+    res(i).cidx = cidx(vidx,:,:);
+    res(i).lifetime = lftData(i).lifetime_s(vidx);
+    res(i).cidxAll = cidx;
 end
     
-%     for ch = 1:nCh % channels
-%         % interpolate tracks to mean cohort length
-%         for c = 1:nc % cohorts
-%             % tracks in current cohort (above threshold)
-%             %cidx = find(cohortBounds(c)<=lftData(i).lifetime_s & lftData(i).lifetime_s<cohortBounds(c+1));
-%             nt = numel(cidx);
-%             if nt>0
-%                 
 %                 % split as a function of slave channel signal
 %                 if isfield(lftData(i), 'significantMaster')
 %                     sigIdx = lftData(i).significantMaster(:,ch)==1;
