117768b0036724b069480651e7363ecde197fee6
Jeffrey Werbin <jeffrey_werbin@hms.harvard.edu>
2014-04-04 19:38:51 +0000

    For mean shift analysis on manually aligned movies
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/receptors/main@11228 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ ErbBproject/DNApaint/wyssFileMeanShiftManualAligned.m
@@ -0,0 +1,31 @@
+function success = wyssFileMeanShiftManualAligned(PointList, name)
+% takes in a directory that contains .mat files with data from the wyss
+% converts the data into a PointList structure, drift corrects and aligns
+% the data and then runs meanshift clustering on the data.
+%
+% PointList is the algined point list of each channel
+% name is the name for saving the results
+%
+% Results are then saved in the parent directory of dir as "dir".mat
+%
+
+%approx parameters for wyss microscope
+Imsize = [256,256];
+difLim = 2.1075;
+
+success = 1;
+
+for i=1:numel(list);
+   TotalPnts = vertcat(TotalPnts,[PointList{i}.pnts,i*ones(size(PointList{i}.pnts(:,1)))]); 
+end
+
+
+
+% since pixelSize of wyss system is ~107 nm this is 32 nm bandwitdh
+[clusterInfo,clusterMap]=MeanShiftClustering(TotalPnts(:,1:2),0.3,'kernel','flat','flagDebug',true);
+
+clusterInfo = addToClusterInfo(clusterInfo,TotalPnts,'pixelSize',107);
+
+save([dir,'_MeanShiftCluster.mat'],'clusterInfo','clusterMap','TotalPnts');
+
+end
