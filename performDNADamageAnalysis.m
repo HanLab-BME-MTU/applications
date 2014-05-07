a2c9883f608bc5556cfa2413d6003fabac31f462
Deepak Chittajallu <deepak_chittajallu@hms.harvard.edu>
2014-05-07 22:29:49 +0000

    Deepak -- intravital -- stable -- DNADamageAnalysis code
    Changes to old code:
    
    --------------------------------
    
    -- Added a GUI feature to CellSegmentationQualityAnnotator.m, CellPatternAnnotator.m, CellStateAnalyzer.m
    
    Now u can click on a cell with the mouse and that cell will become the active cell and also the three ortho slice image panels (XY, XZ, YZ) will be updated accordingly.
    
    -- ImarisDataVisualizer.m 
    
    Fixed the spot visualization part
    
    --------------------------------
    
    New additions:
    
    -- DNADamageAnalyzer.m, segmentFociInsideNuclei.m
    
    Developed a tool for detecting foci/puncta in the channel tagging 53BP1 DNA repair protein in HT-1080 cells. The tool segments the nuclei and then find and detects foci inside the nuclei. The goal is to study the potency of platinum compound drugs (ex: cisplatin) at inducing 53BP1 puncta.
    
    -- batchDNADamageAnalysis.m, performDNADamageAnalysis.m
    
    These are scripts for running DNADamageAnalysis on the cluster (orchestra).
    
    
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/intravital/main@11378 0bcb941b-74d1-0310-bb26-e6636707f451

--- performDNADamageAnalysis.m
+++ performDNADamageAnalysis.m
@@ -290,6 +290,7 @@ function [flagSuccess] = performDNADamageAnalysis(imageDataFilePath, channelId53
         ylabel('Proportion of cells');
         ylim([0, 1]);
         grid on;
+		Maximize(hFociDist);
         SaveFigure(hFociDist , fullfile(resultsDir, 'fociCountDistributionPlot.png') , 'png');
         SaveFigure(hFociDist , fullfile(resultsDir, 'fociCountDistributionPlot.fig') , 'fig');
         close(hFociDist);
