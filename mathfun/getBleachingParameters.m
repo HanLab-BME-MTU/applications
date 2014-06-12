927675947da5ca3f7864adec6931dbea41eec7ee
Francois Aguet <francois_aguet@hms.harvard.edu>
2014-06-12 03:25:53 +0000

    Cleaned up
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/endocytosis@11457 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/getBleachingParameters.m
+++ mathfun/getBleachingParameters.m
@@ -1,124 +1,80 @@
 % Francois Aguet, 08/31/2011
 
-function res = getBleachingParameters(data, varargin)
+function res = analyzeBleaching(data)
 
 ip = inputParser;
 ip.CaseSensitive = false;
 ip.addRequired('data', @isstruct);
-ip.addParamValue('Overwrite', false, @islogical);
-ip.addParamValue('Display', 'off', @(x) strcmpi(x, 'on') | strcmpi(x, 'off'));
-ip.parse(data, varargin{:});
+ip.parse(data);
 
 nd = length(data);
 res(1:nd) = struct('x', [], 'intensity', [], 'model', [], 'k', [], 'a', [], 'd', []);
 for i = 1:nd
-    if ~(exist([data(i).source 'Detection' filesep 'bleaching.mat'], 'file') == 2) || ip.Results.Overwrite
-        res(i) = main(data(i), ip.Results.Display);
-    else
-        tmp = load([data(i).source 'Detection' filesep 'bleaching.mat']);
-        res(i) = tmp.res;
+
+    % load data (much faster than loading frames individually)
+    frames = readtiff(data.framePaths{1});
+    
+    % generate/load cell mask
+    cellMask = getCellMask(data)~=0;
+    
+    % load detection mask, if it exists
+    ccpMasks = [];
+    if exist(data.maskPaths, 'file')==2
+        ccpMasks = readtiff(data.maskPaths)~=0;
     end
-end
 
 
-function res = main(data, display)
+    t = (0:data.movieLength-1)*data.framerate;
+    rawInt = squeeze(mean(mean(frames)));
 
-load([data.source 'Detection' filesep 'detection_v2.mat']);
+    
+    cellMaskInt = zeros(1,data.movieLength);
+    cytosolInt = zeros(1,data.movieLength);
+    for f = 1:data.movieLength
+        iframe = double(frames(:,:,f));
+        cellMaskInt(f) = mean(iframe(cellMask));
+        if ~isempty(ccpMasks)
+            imask = cellMask & ~ccpMasks(:,:,f);
+            cytosolInt(f) = mean(iframe(imask));
+        end
+    end
 
-mpath = [data.source 'Detection' filesep 'cellmask.tif'];
-if ~(exist(mpath, 'file')==2)
-    getCellMask(data);
-end
-
-mask = double(imread(mpath));
-mask = mask~=0;
-
-nf = data.movieLength;
-
-int_mask = NaN(1,nf);
-%int_c = NaN(1,nf);
-int_A = NaN(1,nf);
-fprintf('Bleaching estimation (%s):     ', getDirFromPath(data.source));
-for f = 1:nf
-    frame = double(imread(data.framePaths{1}{f}));
-    int_mask(f) = mean(frame(mask));
-    %int_c(f) = mean(frameInfo(f).c);
-    int_A(f) = mean(frameInfo(f).A + frameInfo(f).c);
-    fprintf('\b\b\b\b%3d%%', round(100*f/(nf)));
-end
-fprintf('\n');
-
-x = 1:data.movieLength;
-
-% on mean intensity
-% [k a d] = fitDoubleExp(x, int_mask);
-% bleachingModel = a(1)*exp(-k(1)*x) + a(2)*exp(-k(2)*x) + d;
-% on CCP background
-% [k a d] = fitDoubleExp(x, int_c);
-% model_c = a(1)*exp(-k(1)*x) + a(2)*exp(-k(2)*x) + d;
-
-[k a d] = fitDoubleExp(x, int_A);
-model_A = a(1)*exp(-k(1)*x) + a(2)*exp(-k(2)*x) + d;
-
-%A0 = bleachingModel(1);
-A0 = model_A(1);
-
-
-res.x = x;
-res.intensity = int_A;
-res.model = model_A;
-res.k = k;
-res.a = a;
-res.d = d;
-
-save([data.source 'Detection' filesep 'bleaching.mat'], 'res');
-
-
-if strcmpi(display, 'on')
-    [~,~] = mkdir([data.source 'Figures']);
+    if ~isempty(ccpMasks)
+        [k, a, d] = fitDoubleExp(t, cytosolInt);
+    else
+        [k, a, d] = fitDoubleExp(t, cellMaskInt);
+    end
+        
     figure;
     hold on;
-    %plot(x, int_mask, 'k');
-    %plot(x, bleachingModel, 'r');
-    %plot(x, int_A, 'k');
-    %plot(x, model_A, 'g');
+    plot(t, rawInt, 'Color', 0.6*[1 1 1]);
+    plot(t, cellMaskInt, 'g');
+    if ~isempty(ccpMasks)
+        plot(t, cytosolInt, 'r');
+    end
+    plot(t, expModel(k,a,d,t), 'k');
+    % YLim = get(gca, 'YLim');
+    % set(gca, 'YLim', [0, YLim(2)]);
+    xlabel('Time (s)');
+    ylabel('Mean intensity');
     
-    plot(x, int_A/A0, 'k', 'LineWidth', 2);
-    plot(x, model_A/A0, 'r', 'LineWidth', 2);
-    set(gca, 'FontName', 'Helvetica', 'FontSize', 18, 'LineWidth', 1.5);
+    if ~isempty(ccpMasks)
+        hl = legend(' Mean frame intensity', ' Mean cell intensity',...
+            ' Mean cytosol intensity', ' Exp. fit');
+    else
+        hl = legend('Mean frame intensity', ' Mean cell intensity', ' Exp. fit');
+    end
+    set(hl, 'EdgeColor', 'w');
     
-    xlabel('Time (frames)');
-    ylabel('Mean CCP intensity (relative)');
-    axis([1 data.movieLength 0.7 1.02]);
-    print('-depsc2', [data.source 'Figures' filesep 'bleaching.eps']);
-    
-    dRange = frameInfo(1).dRange{1};
-    dRange(2) = dRange(2)*0.8;
-    px = data.pixelSize/data.M;
-    
-    h(1) = figure('Visible', 'off', 'Color', [1 1 1]);
-    imagesc(double(imread(data.framePaths{1}{1})));
-    caxis(dRange);
-    plotScaleBar(5e-6/px);
-    axis image off;
-    colormap(gray(256)); colorbar;
-    print('-depsc', [data.source 'Figures' filesep 'frame1_bleaching.eps']);
-    
-    h(2) = figure('Visible', 'off', 'Color', [1 1 1]);
-    imagesc(double(imread(data.framePaths{1}{end})));
-    caxis(dRange);
-    plotScaleBar(5e-6/px);
-    axis image off;
-    colormap(gray(256)); colorbar;
-    print('-depsc', [data.source 'Figures' filesep 'frame' num2str(data.movieLength) '_bleaching.eps']);
-    
-    close(h);
+    res.t = t;
+    res.model = expModel(k,a,d,t);
+    res.k = k;
+    res.a = a;
+    res.d = d;
 end
+    
 
-
-
-
-function [k a d] = fitDoubleExp(x, fx)
+function [k, a, d] = fitDoubleExp(t, f)
 
 opts = optimset('Jacobian', 'off', ...
     'MaxFunEvals', 1e4, ...
@@ -127,23 +83,21 @@ opts = optimset('Jacobian', 'off', ...
     'TolX', 1e-6, ...
     'Tolfun', 1e-6);
 
-dy = fx(end);%mean(fx(end-20:end));
-mu = sum(x.*(fx-dy))/sum(fx-dy);
-init = [1/mu fx(1)-dy 1/mu fx(1)-dy dy];
+dy = f(end);
+mu = sum(t.*(f-dy))/sum(f-dy);
+init = [1/mu f(1)-dy 1/mu f(1)-dy dy];
 
 lb = zeros(1,5);
 ub = Inf(1,5);
 
-[p,resnorm,~,~,~,~,J] = lsqnonlin(@cost, init, lb, ub, opts, x, fx);
+% parameters: k1 a1 k2 a2 dy
+p = lsqnonlin(@(p) expModel(p([1 3]), p([2 4]), p(5), t) - f,...
+    init, lb, ub, opts);
 k = p([1 3]);
 a = p([2 4]);
 d = p(5);
 
-C = resnorm*full(inv(J'*J));
-prmStd = sqrt(diag(C)/(numel(fx)-numel(p) - 1));
 
-
-% parameters: k1 a1 k2 a2 dy
-function v = cost(p, x, fx)
-v = p(2)*exp(-p(1)*x) + p(4)*exp(-p(3)*x) + p(5) - fx;
-
+% double exponential model (with constant offset for background);
+function m = expModel(k, a, d, t)
+m = a(1)*exp(-k(1)*t) + a(2)*exp(-k(2)*t) + d;
