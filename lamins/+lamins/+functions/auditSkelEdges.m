ee32a5658f0c7c5302c16adc3ff7547a0c84054f
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2014-10-24 13:31:10 -0500

    Integrated changes made during trip to MBL for Cian Created aliases for parameter sets for image subsets Removed use of an obsolete function Enabled use of image caching

--- lamins/+lamins/+functions/auditSkelEdges.m
+++ lamins/+lamins/+functions/auditSkelEdges.m
@@ -1,6 +1,8 @@
 function [ A ] = auditSkelEdges( skel, I )
 %auditSkelEdges Check to see if skeleton edges are reasonable
 
+import connectedComponents.*;
+
 endpts = bwmorph(skel,'endpoints');
 branchpts = bwmorph(skel,'branchpoints');
 branchpt.idx = find(branchpts);
@@ -17,7 +19,7 @@ A.width = ([A.rp.MaxIntensity]-[A.rp.MinIntensity])./[A.rp.MeanIntensity];
 
 thresh = thresholdRosin(A.width);
 filter = (A.width < thresh | [A.rp.MeanIntensity] > 0.5) & [A.rp.MinIntensity] > 0.2;
-A.fcc = filtercc(A.cc,filter);
+A.fcc = ccFilter(A.cc,filter);
 A.frp = regionprops(A.fcc,I,'MaxIntensity','MeanIntensity','MinIntensity','Area');
 
 vert_neigh = imdilate(vertices,strel('square',3)) & skel;
