3eeba53fbf2e36069e0da059e5f271faadc872aa
dinglyosu <Liya.Ding@UTSouthwestern.edu>
2014-11-08 16:00:04 -0600

    Update in network analysis 1. Separate network analysis and analysis related to the intensity or steerable response of the filament. 2. Add user input control of whether to plot, whether to save plots, which feature to calculate. 3. Separate the distribution plotting from analysis functions.

--- cytoskeleton/filamentNetworkAnalysis/load_MD_network_for_analysis.m
+++ cytoskeleton/filamentNetworkAnalysis/load_MD_network_for_analysis.m
@@ -1,15 +1,68 @@
-function network_feature = load_MD_network_for_analysis(MD,ROI,radius)
+function network_feature = load_MD_network_for_analysis(MD,ROI,radius,figure_flag, save_everything_flag,feature_flag)
 % function to do network analysis with input MD
 
 % input:    MD:    the loaded movieData object.
 %           ROI:   the user input ROI, if not defined [], will use the whole
 %                  image area
+%           radius: the definition of neighborhood
+%           figure_flag: 1 to plot histgrams, 0 not to
+%           save_everything_flag: 1 to save the plot, 0 not to
+%           feature_flag: the flag for controlling which feature to
+%                         calculate, a vector of 16 bits, 1 for need to
+%                         calculate, 0 for skip it to save time.
+
+%           %% here is a list of feature_flag corrspondence:
+% 
+%            1:   output_feature.straightness_per_filament_pool
+%            2:   output_feature.length_per_filament_pool
+%            3:   output_feature.pixel_number_per_filament_pool
+%            4:   output_feature.density_filament
+%            5:   output_feature.scrabled_density_filament
+% 
+%            6:   output_feature.orientation_pixel_pool_display
+%            7:   output_feature.orientation_pixel_pool_display_center
+
+%            8:   output_feature.intensity_per_filament_pool
+%            9:   output_feature.mean_intensity_per_filament_pool
+%           10:   output_feature.intensity_per_fat_filament_pool
+%           11:   output_feature.mean_intensity_per_fat_filament_pool
+%           12:   output_feature.scale_per_filament_pool
+% 
+%           13:   output_feature.st_per_filament_pool 
+%           14:   output_feature.mean_st_per_filament_pool 
+%           15:   output_feature.st_per_fat_filament_pool
+%           16:   output_feature.mean_st_per_fat_filament_pool 
+
 % output:   network_feature, a cell structure for each channel, each frame.
-%           Each struct with field of:
-%           'straightness_per_filament_pool','orientation_pixel_pool_display',...
-%           'length_per_filament_pool'. 
+%           Each struct with field of the 16 features as above
             
 % Liya Ding 2013
+
+% if no input, ROI is full image
+if(nargin<2)
+    ROI = [];
+end
+
+% if no input for radius, set it as default 20
+if(nargin<3)
+    radius = 20;
+end
+
+% if no input to plot or not, plot
+if(nargin<4)
+    figure_flag = 1;
+end
+
+% if no input to save plot or not, save
+if(nargin<5)
+    save_everything_flag = 1;
+end
+
+% if no input as which feature to calculate, do for all features
+if(nargin<6)
+    feature_flag = ones(1,16);
+end
+%% Get movie data ready
 
 movie_Dir = MD.outputDirectory_;
 
@@ -98,32 +151,44 @@ for iChannel = 1 :  length(MD.channels_)
         if(numel(min_length)>1)
         min_length = min_length(iChannel);
         end
+        
         %% % do analysis
         im_name = MD.channels_(iChannel).getImageFileNames(iFrame);
         current_img =  MD.channels_(iChannel).loadImage(iFrame);
                 
-        output_feature = network_analysis(VIF_current_model,VIF_orientation, ...
-            VIF_current_seg,outdir,iChannel,iFrame,ROI,min_length,...
-            im_name,radius);
+        % get network feature that is only related to the network
+        [output_network_features, VIF_ROI_model, VIF_ROI_orientation_model] ...
+            = network_analysis(VIF_current_model,...
+                VIF_current_seg, ROI, radius,feature_flag);
         
-        output_feature_Filaments = perfilament_analysis(VIF_current_model,VIF_orientation, ...
-            VIF_current_seg,outdir,iChannel,iFrame,ROI,min_length,...
-            im_name,radius,scaleMap,current_img,MAX_st_res);
+        % get network feature that is related to intensity or st image
+         output_image_features = perfilament_analysis(VIF_ROI_model, VIF_ROI_orientation_model,...
+                 VIF_current_seg, scale_map,current_img, MAX_st_res, feature_flag);
         
-        output_feature=  cat_struct(output_feature,output_feature_Filaments);
+        % putting the network features together
+        output_feature =  cat_struct(output_network_features,output_image_features);
+        
+        % plot the network features in hists
+        network_features_plotting(output_feature, figure_flag, save_everything_flag);
                     
 %         close all;
 
+        % add one last component, the cell_mask
         Cell_Mask = ROI.*((MD.processes_{indexCellRefineProcess}.loadChannelOutput(iChannel,iFrame))>0);
           
         output_feature.Cell_Mask = Cell_Mask;
-            save([outdir,filesep,'network_orientationrose_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.mat'],...
+        
+        % save output feature for single image(single channel, single frame)
+        save([outdir,filesep,'network_analysis_feature_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.mat'],...
             'output_feature');
      
-        network_feature{iChannel,iFrame} =output_feature;
+        % put output feature to cell for all channels, all frames
+        network_feature{iChannel,iFrame} = output_feature;
         
     end
-    save([outdir,filesep,'network_orientationrose_ch_',num2str(iChannel),'_allframe.mat'],...
+    
+    % save output feature for all channels, all frames)       
+    save([outdir,filesep,'network_analysis_feature_ch_',num2str(iChannel),'_allframe.mat'],...
             'network_feature');
 end
 
---
+++ cytoskeleton/filamentNetworkAnalysis/network_features_plotting.m
@@ -0,0 +1,145 @@
+function network_features_plotting(output_feature, figure_flag, save_everything_flag,...
+    im_name, outdir,iChannel,iFrame)
+% function to plot network features
+% Input: output_feature:   network feautre
+%        figure_flag:      to plot to not
+%        save_everything_flag: to save or not
+
+
+% Plot only if the user requested
+
+if(figure_flag>0)
+    
+    if(iscell(im_name))
+        im_name = im_name{1};
+    end
+    
+    % current_img = imread(im_name);
+    
+    im_name(im_name=='_')='-';
+    im_name_title{1} = im_name;
+
+    if(feature_flag(3)>0)
+        % length distribution, by how many pixels
+        h3 =  figure(3);
+        
+        [h,bin] = hist(output_feature.pixel_number_per_filament_pool,0:20:1000);
+        h = h/length(output_feature.pixel_number_per_filament_pool);
+        bar(bin,h);
+        axis([-10 310 0 0.3]);
+        
+        title([im_name_title,' Pixels Number Distribution']);
+        
+        if(save_everything_flag>0)
+            saveas(h3, [outdir,filesep,'network_pixels_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.fig']);
+            saveas(h3, [outdir,filesep,'network_pixels_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.tif']);
+        end
+    end
+    
+    if(feature_flag(2)>0)
+        
+        h2 =  figure(2);
+        [h,bin] = hist(length_per_filament_pool,0:20:1000);
+        h = h/length(length_per_filament_pool);
+        
+        bar(bin,h);
+        axis([-10 310 0 0.3]);
+        
+        title([im_name_title,' Length Distribution']);
+        if(save_everything_flag>0)
+            saveas(h2, [outdir,filesep,'network_length_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.fig']);
+            saveas(h2, [outdir,filesep,'network_length_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.tif']);
+        end
+    end
+    
+    
+    if(feature_flag(1)>0)
+        
+        % the straightness distribution
+        h1 =  figure(1);
+        [h,bin] = hist(output_feature.straightness_per_filament_pool,0:0.02:1);
+        h = h/length(output_feature.straightness_per_filament_pool);
+        bar(bin,h);
+        axis([0.69 0.97 0 0.2]);
+        
+        title([im_name_title,' Straightness']);
+        
+        if(save_everything_flag>0)
+            saveas(h1, [outdir,filesep,'network_straight_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.fig']);
+            saveas(h1, [outdir,filesep,'network_straight_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.tif']);
+        end
+        
+        h5 =  figure(5);
+        h = boxplot(output_feature.straightness_per_filament_pool);
+        set(h(7,:),'Visible','off');
+        axis([0 2 0.6 1.01]);
+        
+        title([im_name_title,' Straightness']);
+        if(save_everything_flag>0)
+            
+            saveas(h5, [outdir,filesep,'network_box_straight_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.fig']);
+            saveas(h5, [outdir,filesep,'network_box_straight_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.tif']);
+        end
+    end
+    
+       
+    
+    
+    if(feature_flag(6)>0)
+        
+        
+        h6 =  figure(6);
+        rose(orientation_pixel_pool_display);
+        title([im_name_title,' Orientation of Filaments']);
+        if(save_everything_flag>0)
+            saveas(h6, [outdir,filesep,'network_orientationrose_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.fig']);
+            saveas(h6, [outdir,filesep,'network_orientationrose_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.tif']);
+        end
+        
+        % the orientation in histogram
+        h16 =  figure(16);
+        
+        [h,b] = hist(orientation_pixel_pool_display,-pi/2:pi/18:pi/2);
+        h=h(1:end);
+        h = h/length(orientation_pixel_pool_display);
+        bin = (-pi/2:pi/18:pi/2) +pi/36;
+        bar(bin,h);
+        axis([-pi/2-pi/36 pi/2+pi/36 0 0.3]);
+        
+        title([im_name_title,' Orientation of Filaments']);
+        if(save_everything_flag>0)
+            saveas(h16, [outdir,filesep,'network_orientationhist_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.fig']);
+            saveas(h16, [outdir,filesep,'network_orientationhist_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.tif']);
+        end
+    end
+    
+    if(feature_flag(7)>0)
+        
+        
+        h7 =  figure(7);
+        rose(orientation_pixel_pool_display_center);
+        title([im_name_title,' Orientation of Filaments']);
+        
+        if(save_everything_flag>0)
+            saveas(h7, [outdir,filesep,'network_orientationrose_centered_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.fig']);
+            saveas(h7, [outdir,filesep,'network_orientationrose_centered_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.jpg']);
+        end
+        
+        % the orientation in histogram
+        h17 =  figure(17);
+        
+        [h,b] = hist(orientation_pixel_pool_display_center,-pi/2+mode_bin:pi/18:pi/2+mode_bin);
+        h=h(1:end);
+        h = h/length(orientation_pixel_pool_display_center);
+        bin = (-pi/2:pi/18:pi/2)+mode_bin+pi/36;
+        bar(bin,h);
+        axis([-pi/2-pi/36+mode_bin pi/2+pi/36+mode_bin 0 0.3]);
+        
+        title([im_name_title,' Orientation of Filaments']);
+        if(save_everything_flag>0)
+            saveas(h17, [outdir,filesep,'network_orientationhist_centered_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.fig']);
+            saveas(h17, [outdir,filesep,'network_orientationhist_centered_ch_',num2str(iChannel),'_frame_',num2str(iFrame),'.jpg']);
+        end
+               
+    end
+end
