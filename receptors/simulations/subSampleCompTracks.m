719396df46082168d512118302767efb1baa6c6f
Khuloud Jaqaman <khuloud.jaqaman@utsouthwestern.edu>
2014-12-16 12:42:04 -0600

    updated subSampleCompTracks.m

--- receptors/simulations/subSampleCompTracks.m
+++ receptors/simulations/subSampleCompTracks.m
@@ -50,53 +50,53 @@ for iTrack = 1 : numTracks
     %convert the current track's information to matrix format
     [trackedFeatureInfo,trackedFeatureIndx] = convStruct2MatIgnoreMS(tracks0(iTrack));
     
-    %get each track segment's start, end and life time
-    segSEL = getTrackSEL(trackedFeatureInfo);
-    numSeg = size(segSEL,1);
-    
-    %find segments that last for less than the sub-sampling factor
-    indxShort = find(segSEL(:,3)<subSampFact);
-    
-    %go over these segments, and for those that do not overlap with any of
-    %the surviving time points, shift them in time to the closest surviving time point
-    for iSeg = indxShort'
-        tpSeg = segSEL(iSeg,1) : segSEL(iSeg,2);
-        if isempty(intersect(tpSeg,tpKeep))
-            startTP0 = tpSeg(1);
-            endTP0 = tpSeg(end);
-            startDiffAbs = abs(startTP0 - tpKeep);
-            endDiffAbs = abs(endTP0 - tpKeep);
-            if min(startDiffAbs) <= min(endDiffAbs)
-                subTP = tpKeep(startDiffAbs==min(startDiffAbs));
-                startCol0 = (startTP0-1)*8 + 1;
-                subCol = (subTP-1)*8 + 1;
-                trackedFeatureIndx(iSeg,subTP) = trackedFeatureIndx(iSeg,startTP0);
-                trackedFeatureInfo(iSeg,subCol:subCol+7) = trackedFeatureInfo(iSeg,startCol0:startCol0+7);
-            else
-                subTP = tpKeep(endDiffAbs==min(endDiffAbs));
-                endCol0 = (endTP0-1)*8 + 1;
-                subCol = (subTP-1)*8 + 1;
-                trackedFeatureIndx(iSeg,subTP) = trackedFeatureIndx(iSeg,endTP0);
-                trackedFeatureInfo(iSeg,subCol:subCol+7) = trackedFeatureInfo(iSeg,endCol0:endCol0+7);
-            end
-        end
-    end
-    
-    %keep only the time points aof interest
+    %keep only the time points of interest
     trackedFeatureIndx = trackedFeatureIndx(:,tpKeep);
     trackedFeatureInfo = trackedFeatureInfo(:,colKeep);
     
-    %get the new start and end time of each track segment
+    %get each track segment's new start, end and life time
     segSEL = getTrackSEL(trackedFeatureInfo);
+    numSeg = size(segSEL,1);
     
-    %modify the track's sequence of events
+    %find segments that survive the subsampling and those that do not
+    indxStay = find(~isnan(segSEL(:,3)));
+    indxGone = setdiff((1:numSeg)',indxStay);
+    
+    %get the track's sequence of events
     seqOfEvents = tracks0(iTrack).seqOfEvents;
+    
+    %assign new start and end times - merges will need an addition of one,
+    %done two steps down
     for iSeg = 1 : numSeg
-        seqOfEvents(seqOfEvents(:,2)==1&seqOfEvents(:,3)==iSeg,1) = segSEL(iSeg,1);
-        seqOfEvents(seqOfEvents(:,2)==2&seqOfEvents(:,3)==iSeg,1) = segSEL(iSeg,2);
+        rowsSeg = find(seqOfEvents(:,3)==iSeg);
+        seqOfEvents(rowsSeg(1),1) = segSEL(iSeg,1);
+        seqOfEvents(rowsSeg(2),1) = segSEL(iSeg,2);
     end
-    rowIndx = find(seqOfEvents(:,2)==2&~isnan(seqOfEvents(:,4)));
-    seqOfEvents(rowIndx,1) = seqOfEvents(rowIndx,1) + 1;
+    
+    %replace segments that did not survive the subsampling with NaN in both
+    %the 3rd and 4th column - this means that some merges/splits might not
+    %survive the subsampling
+    for iSeg = indxGone'
+        seqOfEvents(seqOfEvents(:,3)==iSeg,3) = NaN;
+        seqOfEvents(seqOfEvents(:,4)==iSeg,4) = NaN;
+    end
+    
+    %for surviving merges, add one to end time to follow convention
+    rowsSeg = find(seqOfEvents(:,2)==2 & ~isnan(seqOfEvents(:,4)));
+    seqOfEvents(rowsSeg,1) = seqOfEvents(rowsSeg,1) + 1;
+    
+    %keep only rows that belong to surviving segments
+    seqOfEvents = seqOfEvents(~isnan(seqOfEvents(:,3)),:);
+    trackedFeatureInfo = trackedFeatureInfo(indxStay,:);
+    trackedFeatureIndx = trackedFeatureIndx(indxStay,:);
+    
+    %renumber segments to reflect new trackedFeatureInfo and
+    %trackedFeatureIndx
+    for iStay = 1 : length(indxStay)
+        iSeg = indxStay(iStay);
+        seqOfEvents(seqOfEvents(:,3)==iSeg,3) = iStay;
+        seqOfEvents(seqOfEvents(:,4)==iSeg,4) = iStay;
+    end
     
     %store the sub-sampled compound track
     tracksSub(iTrack).tracksFeatIndxCG = trackedFeatureIndx;
@@ -105,4 +105,5 @@ for iTrack = 1 : numTracks
     
 end
 
+%% ~~~ the end ~~~
 
