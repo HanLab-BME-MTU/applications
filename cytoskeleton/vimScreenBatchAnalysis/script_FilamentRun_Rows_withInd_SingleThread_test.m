e40291a677df9bdfebf9ba05166bcd46720782ff
dinglyosu <Liya.Ding@UTSouthwestern.edu>
2015-01-20 13:56:34 -0800

    Vim screen, new script with single thread.

---
+++ cytoskeleton/vimScreenBatchAnalysis/script_FilamentRun_Rows_withInd_SingleThread_test.m
@@ -0,0 +1,36 @@
+function script_FilamentRun_Rows_withInd_SingleThread_test(rowInd,rootDataFolder)
+% run the filament analysis package for vim screen plates
+
+% take care of Matlab path
+matlabDir = '/home2/lding/Ding/GitMatlab'; 
+addpath(genpath(matlabDir),'-end'); 
+
+% Set the root folder, as input
+if(isempty(rootDataFolder))
+ rootDataFolder ='/project/cellbiology/gdanuser/vimentin/tonyzhang/live cell nikon data/well plate screening/screen_20141204/raw data_analysis3/';
+end
+% set the data folder according to the row index
+% 1 for row A, 2 for B, and so on
+rowName = char('A' + rowInd - 1);
+rowFolderName = [ rowName,'_Row_Analysis'];
+
+% load the MD file
+MD = MovieData.load([rootDataFolder,filesep,rowFolderName,'movieData.mat']);
+
+% run the filament analysis package
+vimscreen_load_MD_run_filament_analysis_package(MD,[],'run_with_new_param',1,'save_old_data_tag','time'); 
+
+% Mark the Completion of the network analysis
+
+disp('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
+disp(['Finished filament segmentation for row ',rowName]);
+disp('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
+
+% run network analysis
+load_MD_network_for_analysis(MD,[],30, 1,1, ones(16,1),1);
+
+% Mark the Completion of the network analysis
+
+disp('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+disp(['Finished network analysis for row ',rowName]);
+disp('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
--- cytoskeleton/vimScreenBatchAnalysis/vimscreen_All16Rows_Batch_01202015.m
+++ cytoskeleton/vimScreenBatchAnalysis/vimscreen_All16Rows_Batch_01202015.m
@@ -1,14 +1,13 @@
 #!/bin/bash
  
-#SBATCH --job-name=Tony_test_RowWithInd
-#SBATCH --partition=64GB
+#SBATCH --job-name=Liya_test_RowWithInd
+#SBATCH --partition=384GB
 #SBATCH --nodes=1
-#SBATCH --mem=16384 
-#SBATCH --time=7-0:0:0
+#SBATCH --time=0
 #SBATCH --mail-type=ALL
 #SBATCH --output=myjob.%j.%N.out
-#SBATCH --error=myjob.%j.%N.err
- 
+#SBATCH --error=myjob.%j.%N.err 
+
 # Load matlab 2013a using module
 module add matlab/2013a
 
@@ -16,22 +15,22 @@ module add matlab/2013a
 # run commands specified with -r option
 # direct output info specified -logfile
 # run as a background process (&)
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(1,[]), exit" -logfile "matlab_script_01142015_1_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(2,[]), exit" -logfile "matlab_script_01142015_2_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(3,[]), exit" -logfile "matlab_script_01142015_3_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(4,[]), exit" -logfile "matlab_script_01142015_4_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(5,[]), exit" -logfile "matlab_script_01142015_5_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(6,[]), exit" -logfile "matlab_script_01142015_6_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(7,[]), exit" -logfile "matlab_script_01142015_7_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(8,[]), exit" -logfile "matlab_script_01142015_8_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(9,[]), exit" -logfile "matlab_script_01142015_9_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(10,[]), exit" -logfile "matlab_script_01142015_10_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(11,[]), exit" -logfile "matlab_script_01142015_11_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(12,[]), exit" -logfile "matlab_script_01142015_12_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(13,[]), exit" -logfile "matlab_script_01142015_13_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(14,[]), exit" -logfile "matlab_script_01142015_14_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(15,[]), exit" -logfile "matlab_script_01142015_15_output.txt" &
-matlab -nodisplay -r "script_FilamentRun_Rows_withInd(16,[]), exit" -logfile "matlab_script_01142015_16_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(1,[]), exit" -logfile "matlab_script_01202015_1_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(2,[]), exit" -logfile "matlab_script_01202015_2_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(3,[]), exit" -logfile "matlab_script_01202015_3_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(4,[]), exit" -logfile "matlab_script_01202015_4_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(5,[]), exit" -logfile "matlab_script_01202015_5_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(6,[]), exit" -logfile "matlab_script_01202015_6_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(7,[]), exit" -logfile "matlab_script_01202015_7_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(8,[]), exit" -logfile "matlab_script_01202015_8_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(9,[]), exit" -logfile "matlab_script_01202015_9_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(10,[]), exit" -logfile "matlab_script_01202015_10_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(11,[]), exit" -logfile "matlab_script_01202015_11_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(12,[]), exit" -logfile "matlab_script_01202015_12_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(13,[]), exit" -logfile "matlab_script_01202015_13_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(14,[]), exit" -logfile "matlab_script_01202015_14_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(15,[]), exit" -logfile "matlab_script_01202015_15_output.txt" &
+matlab -singleCompThread -nodisplay -r "script_FilamentRun_Rows_withInd_SingleThread_test(16,[]), exit" -logfile "matlab_script_01202015_16_output.txt" &
 
 # wait for all background processes to finish
 wait
