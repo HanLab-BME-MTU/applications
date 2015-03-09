e103e6d05a1827fa3ea2180d5d64744d28f00cab
dinglyosu <Liya.Ding@UTSouthwestern.edu>
2015-03-09 10:48:06 -0500

    vim screen changes in whole stat movieList cal

--- cytoskeleton/FilamentAnalysis/whole_movie_movieList_gather_cal.m
+++ cytoskeleton/FilamentAnalysis/whole_movie_movieList_gather_cal.m
@@ -1,4 +1,4 @@
-% load whole movie stat pools and gather them, calculate the stat for 
+% load whole movie stat pools and gather them, calculate the stat for
 % the whole movieList
 
 % ML.sanityCheck();
@@ -10,8 +10,10 @@ Whole_movie_stat_cell = cell(1,nChannel);
 
 
 %% for length first
+
 Group_length_pool = [];
-for iChannel = 1 : nChannel    
+
+for iChannel = 1 : nChannel
     
     %% Set everything to nan first
     Whole_movie_stat.mean_ST = nan;
@@ -30,7 +32,7 @@ for iChannel = 1 : nChannel
     Whole_movie_stat.otsu_mode_NMS = nan;
     Whole_movie_stat.rosin_NMS = nan;
     Whole_movie_stat.rosin_mode_NMS = nan;
-   
+    
     
     
     Whole_movie_stat.mean_INT = nan;
@@ -54,7 +56,7 @@ for iChannel = 1 : nChannel
         MD =  MovieData.load(ML.movieDataFile_{iM});
         [Length_pool,~,~ ,~] = load_whole_movie_stat_MD(MD,iChannel);
         if(~isempty(Length_pool))
-            Group_length_pool = [Group_length_pool Length_pool];            
+            Group_length_pool = [Group_length_pool Length_pool];
         end
     end
     
@@ -63,28 +65,28 @@ for iChannel = 1 : nChannel
         continue;
     end
     
- Group_length_pool = Group_length_pool(Group_length_pool>1);
+    Group_length_pool = Group_length_pool(Group_length_pool>1);
     [hist_n,bin] = hist(Group_length_pool,50);
     ind_mode = find(hist_n==max(hist_n));
-    mode_Group_Length = bin(ind_mode(1));    
-
+    mode_Group_Length = bin(ind_mode(1));
+    
     Whole_movie_stat.mean_Length = mean(Group_length_pool);
     Whole_movie_stat.std_Length = std(Group_length_pool);
     Whole_movie_stat.mode_Length = mode_Group_Length;
     Whole_movie_stat.rosin_Length = thresholdRosin(Group_length_pool);
     Whole_movie_stat.rosin_mode_Length = thresholdRosin(Group_length_pool(find(Group_length_pool>mode_Group_Length)));
-
+    
     
     Group_length_pool=[];
     
     %% Load movie by movie, just for STNMS
-     Group_NMS_pool=[];
+    Group_NMS_pool=[];
     
     for iM = 1 : numel(ML.movieDataFile_);
         MD =  MovieData.load(ML.movieDataFile_{iM});
         [~,NMS_pool,~ ,~] = load_whole_movie_stat_MD(MD,iChannel);
         if(~isempty(NMS_pool))
-            Group_NMS_pool = [Group_NMS_pool NMS_pool];            
+            Group_NMS_pool = [Group_NMS_pool; NMS_pool];
         end
     end
     
@@ -104,35 +106,35 @@ for iChannel = 1 : nChannel
     Whole_movie_stat.otsu_mode_NMS = thresholdOtsu(Group_NMS_pool(find(Group_NMS_pool>mode_Group_NMS)));
     
     try
-      Whole_movie_stat.rosin_NMS = thresholdRosin(Group_NMS_pool);
+        Whole_movie_stat.rosin_NMS = thresholdRosin(Group_NMS_pool);
     catch
         Whole_movie_stat.rosin_NMS = Whole_movie_stat.otsu_NMS;
     end
     
     try
-    Whole_movie_stat.rosin_mode_NMS = thresholdRosin(Group_NMS_pool(find(Group_NMS_pool>mode_Group_NMS)));
+        Whole_movie_stat.rosin_mode_NMS = thresholdRosin(Group_NMS_pool(find(Group_NMS_pool>mode_Group_NMS)));
     catch
         Whole_movie_stat.rosin_mode_NMS = Whole_movie_stat.otsu_mode_NMS;
     end
     
     Group_NMS_pool=[];
     
-    %% ST
-    %% Load movie by movie, just for ST
+    % ST
+    % Load movie by movie, just for ST
     Group_ST_pool=[];
     
     for iM = 1 : numel(ML.movieDataFile_);
         MD =  MovieData.load(ML.movieDataFile_{iM});
         [~,~,ST_pool ,~] = load_whole_movie_stat_MD(MD,iChannel);
         if(~isempty(ST_pool))
-            Group_ST_pool = [Group_ST_pool ST_pool];            
+            Group_ST_pool = [Group_ST_pool; ST_pool];
         end
     end
     
     % if there is no information for this channel, leave it as empty
     if(isempty(Group_ST_pool))
         continue;
-    end    
+    end
     
     [hist_n,bin] = hist(Group_ST_pool,50);
     ind_mode = find(hist_n==max(hist_n));
@@ -161,13 +163,13 @@ for iChannel = 1 : nChannel
     %% INT
     
     
-     Group_INT_pool=[];
+    Group_INT_pool=[];
     
     for iM = 1 : numel(ML.movieDataFile_);
         MD =  MovieData.load(ML.movieDataFile_{iM});
         [~,~,~,INT_pool] = load_whole_movie_stat_MD(MD,iChannel);
         if(~isempty(INT_pool))
-            Group_INT_pool = [Group_INT_pool INT_pool];            
+            Group_INT_pool = [Group_INT_pool INT_pool];
         end
     end
     
@@ -176,7 +178,7 @@ for iChannel = 1 : nChannel
         continue;
     end
     
-     Group_INT_pool =  double(Group_INT_pool);
+    Group_INT_pool =  double(Group_INT_pool(:));
     
     % find the mode of the intensity of the curves/lines
     [hist_n,bin] = hist((Group_INT_pool),50);
@@ -190,7 +192,7 @@ for iChannel = 1 : nChannel
     Whole_movie_stat.otsu_mode_INT = thresholdOtsu(Group_INT_pool(find(Group_INT_pool>mode_Group_INT)));
     Group_INT_pool=[];
     %%
- Whole_movie_stat_cell{iChannel} = Whole_movie_stat;
+    Whole_movie_stat_cell{iChannel} = Whole_movie_stat;
 end
 
-
+save([ML.outputDirectory_,filesep,'whole_movie_list_stat.mat'],'Whole_movie_stat_cell');
