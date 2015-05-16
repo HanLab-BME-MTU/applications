9125c07b286934a97788fccbef9c021d859460ea
maria bagonis <mbagonis5@gmail.com>
2015-05-16 09:58:24 -0500

    start new test for outlier distribution function: will be part of the analysis toolkit
    Former-commit-id: 99d525b71ccccdd225e1307acec6e811a1e42e4d

--- GrowthConeAnalyzer/ANALYSIS/ReportFunctions/gcaAnalysisMakeTestIndividualMovieDistributionPlot.m
+++ GrowthConeAnalyzer/ANALYSIS/ReportFunctions/gcaAnalysisMakeTestIndividualMovieDistributionPlot.m
@@ -1,4 +1,4 @@
-function [ output_args ] = gcaAnalysisMakeTestIndividualMovieDistributionPlot(dataMat,names ,axisFont,labelFont,titleFont,orderForBoxplot)
+function [ output_args ] = gcaAnalysisMakeTestIndividualMovieDistributionPlot(dataMat,varargin)
 % gcaAnalysisMakeTestIndividualMovieDistributionPlot 
 % small helper function in the GCAAnalysis Group Report to help visualize
 % the distributions for a given group of 
@@ -10,37 +10,48 @@ function [ output_args ] = gcaAnalysisMakeTestIndividualMovieDistributionPlot(da
 % names: the names of the movies for the boxplot
 % orderForBoxplot: nx1 double array specifying the order of the boxplot- 
 %                   as we sometimes want sorted by neurite outgrowth or some other specified parameter. 
+%% Input check 
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addRequired('dataMat')
+ip.addParameter('sampleNames',[],@(x) iscell(x));
+ip.addParameter('axisFont',12,@(x) ischar(x)); 
+ip.addParameter('measurementName','Measurement X',@(x) ischar(x)); 
+ip.addParameter('orderForBoxplot',[],@(x) isnumeric(x)); 
+ip.parse(dataMat,varargin{:});
+p = ip.Results;
 %%
-
-% plot first the boxplot (easier to see the names of the 
- subplot(2,2,1:2); 
-% 
- samplesCell = arrayfun(@(x) dataMat(:,x),1:size(dataMat,2),'uniformoutput',0); % each column is a sample from a specific cell. 
- samplesCell = cellfun(@(x) x(~isnan(x)),samplesCell,'uniformoutput',0); 
- 
- outlierIdx = detectEDFOutliers(samplesCell); 
-
-
-  % note fix this input later!! 
-  if nargin<6 
-      orderForBoxplot = []; 
-  end 
-  
- if ~isempty(orderForBoxplot); 
+names = ip.Results.sampleNames;
+orderForBoxplot = ip.Results.orderForBoxplot; 
+if ~isempty(orderForBoxplot); 
  % sort the data by array 
  dataMat = dataMat(:,orderForBoxPlot) ; 
  names = names(orderForBoxPlot); 
  end 
+%% Set Figure
+%fsFigure(0.75); 
+
+%% Detect Outlier Distributions 
+subplot(2,2,1:2); 
+% Put data into cell format
+ samplesCell = arrayfun(@(x) dataMat(:,x),1:size(dataMat,2),'uniformoutput',0); % each column is a sample from a specific cell. 
+% Take out NaNs
+ samplesCell = cellfun(@(x) x(~isnan(x)),samplesCell,'uniformoutput',0); 
  
-  subplot(2,2,3:4); 
-     h1=  boxplot(dataMat,'color','k','notch','on','outlierSize',1,'labelorientation','inline');
- set(h1(:),'Linewidth',1);
+% Outlier Detect
+ outlierIdx = detectEDFOutliers(samplesCell); 
+ xlabel(p.measurementName);
 
-
- set(gca,'FontName','Arial','FontSize',axisFont); 
- %ylabel('Filopodia Length (um)','FontName','Arial','FontSize',labelFont); 
- %title(['Group: ' toPlotGroup.info.names{1} ],'FontName','Arial','FontSize',titleFont);
- %axis([0.5,size(dataMat,2)+0.5,0,15]); 
+%% Use Anderson Darling test to screen for prototypical distributions  
+subplot(2,2,3:4); 
+% First Make Boxplot of Sample Distributions
+     h1=  boxplot(dataMat,'color','k','notch','on','outlierSize',1,'labelorientation','inline','labels',names);
+     
+     set(h1(:),'Linewidth',1);
+     set(gca,'FontName','Arial','FontSize',ip.Results.axisFont); 
+     %set(gca,'XTick',[1:numel(names)]); 
+     %set(gca,'XTickLabels',names, 'FontSize',14); 
+ 
 
  testDists{1} = 'norm'; 
  testDists{2} = 'exp'; 
@@ -64,30 +75,27 @@ function [ output_args ] = gcaAnalysisMakeTestIndividualMovieDistributionPlot(da
  %arrayfun(@(i) testMatrix(:,i),
  % put a text box with the distribution over each hit boxplot
  
-     % for each distribution
- for iDist = 1:4
+% for each distribution
+for iDist = 1:4
      if ~isempty(idxOfDistHitsAll{iDist})
      idxOfDistHists = idxOfDistHitsAll{iDist};
  
      arrayfun(@(x) text(idxOfDistHists(x),nanmedian(dataMat(:,x)),testDists{iDist},'FontSize',10),1:length(idxOfDistHists));
      end
- 
- 
- end
- idxN = 1:size(dataMat,2);
- if ~isempty(outlierIdx)
-    
-     
- idxLOut = arrayfun(@(x) idxN==outlierIdx(x),1:length(outlierIdx),'uniformoutput',0);
- idxLOutFinal = sum(vertcat(idxLOut{:}),1)'; 
- idxIn = setdiff(idxN,outlierIdx);  
- dataMat(1:size(dataMat,1),idxIn)= NaN;
-  hold on
-  h22 = boxplot(dataMat,'color','r','colorGroup',idxLOutFinal,'notch','on','outlierSize',1,'labelorientation','inline','labels',names);
-%    set(gca,'XTick',[1:numel(names)]); 
-%    set(gca,'XTickLabels',names, 'FontSize',14); 
+end
 
- set(h22(:),'Linewidth',5);
+%% HighLight any outlier distributions from 
+ %idxN = 1:size(dataMat,2);
+ if ~isempty(outlierIdx)   
+ %idxLOut = arrayfun(@(x) idxN==outlierIdx(x),1:length(outlierIdx),'uniformoutput',0);
+ %idxLOutFinal = sum(vertcat(idxLOut{:}),1)'; 
+ %idxIn = setdiff(idxN,outlierIdx);  
+ %dataMat(1:size(dataMat,1),idxIn)= NaN;
+ % hold on
+ % h22 = boxplot(dataMat,'color','r','colorGroup',idxLOutFinal,'notch','on','outlierSize',1,'labelorientation','inline','labels',names);
+
+ set(h1(:,outlierIdx),'color','r');
+ set(h1(:,outlierIdx),'Linewidth',5);
  end 
 end
 
