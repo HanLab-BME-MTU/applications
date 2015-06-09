83249985c524113b8112629ce1e7f39f073b647c
Khuloud Jaqaman <khuloud.jaqaman@utsouthwestern.edu>
2015-06-09 16:20:16 -0500

    continued work on compareClusterRatesAndDensityPair.m
    Former-commit-id: c3460e95704d79b678d5c3ff39686eb1533c37ba

--- receptors/simulations/postprocessing/compareClusterRatesAndDensityPair.m
+++ receptors/simulations/postprocessing/compareClusterRatesAndDensityPair.m
@@ -25,43 +25,83 @@ function [distMaha,pValue] = compareClusterRatesAndDensityPair(ratesDensity1,rat
 MIN_NUM = 5;
 
 %get indices of good measurements
-indxOn1 = find(ratesDensity1.rateOnPerClust(:,3)>=MIN_NUM);
-indxOff1 = find(ratesDensity1.rateOffPerClust(:,3)>=MIN_NUM);
-indxDen1 = find(ratesDensity1.densityPerClust(:,3)>=MIN_NUM);
-indxOn2 = find(ratesDensity2.rateOnPerClust(:,3)>=MIN_NUM);
-indxOff2 = find(ratesDensity2.rateOffPerClust(:,3)>=MIN_NUM);
-indxDen2 = find(ratesDensity2.densityPerClust(:,3)>=MIN_NUM);
+indxGoodOn1 = find(ratesDensity1.rateOnPerClust(:,3)>=MIN_NUM);
+indxGoodOff1 = find(ratesDensity1.rateOffPerClust(:,3)>=MIN_NUM);
+indxGoodDen1 = find(ratesDensity1.densityPerClust(:,3)>=MIN_NUM);
+indxGoodOn2 = find(ratesDensity2.rateOnPerClust(:,3)>=MIN_NUM);
+indxGoodOff2 = find(ratesDensity2.rateOffPerClust(:,3)>=MIN_NUM);
+indxGoodDen2 = find(ratesDensity2.densityPerClust(:,3)>=MIN_NUM);
 
-%thus define maximum cluster size for each measurement
-numOn = max([indxOn1; indxOn2]);
-numOff = max([indxOff1; indxOff2]);
-numDen = max([indxDen1; indxDen2]);
+% %get indices of bad measurements
+% indxBadOn1 = find(ratesDensity1.rateOnPerClust(:,3)<MIN_NUM);
+% indxBadOff1 = find(ratesDensity1.rateOffPerClust(:,3)<MIN_NUM);
+% indxBadDen1 = find(ratesDensity1.densityPerClust(:,3)<MIN_NUM);
+% indxBadOn2 = find(ratesDensity2.rateOnPerClust(:,3)<MIN_NUM);
+% indxBadOff2 = find(ratesDensity2.rateOffPerClust(:,3)<MIN_NUM);
+% indxBadDen2 = find(ratesDensity2.densityPerClust(:,3)<MIN_NUM);
+
+%define maximum cluster size for each measurement
+numOn1 = size(ratesDensity1.rateOnPerClust,1);
+numOn2 = size(ratesDensity2.rateOnPerClust,1);
+numOn = max([numOn1 numOn2]);
+numOff1 = size(ratesDensity1.rateOffPerClust,1);
+numOff2 = size(ratesDensity2.rateOffPerClust,1);
+numOff = max([numOff1 numOff2]);
+numDen1 = size(ratesDensity1.densityPerClust,1);
+numDen2 = size(ratesDensity2.densityPerClust,1);
+numDen = max([numDen1 numDen2]);
+numTot1 = numOn1 + numOff1 + numDen1;
+numTot2 = numOn2 + numOff2 + numDen2;
+numTot = numOn + numOff + numDen;
 
 %initialize parameter vectors and variance-covariance matrices
 [paramVec1,paramVec2] = deal(NaN(numOn+numOff+numDen,1));
-[paramCovMat1,paramCovMat2] = deal(NaN(numOn+numOff+numDen));
 
 %fill the vectors
-paramVec1(indxOn1) = ratesDensity1.rateOnPerClust(indxOn1,1);
-paramVec1(numOn+indxOff1) = ratesDensity1.rateOffPerClust(indxOff1,1);
-paramVec1(numOn+numOff+indxDen1) = ratesDensity1.densityPerClust(indxDen1,1);
-paramVec2(indxOn2) = ratesDensity2.rateOnPerClust(indxOn2,1);
-paramVec2(numOn+indxOff2) = ratesDensity2.rateOffPerClust(indxOff2,1);
-paramVec2(numOn+numOff+indxDen2) = ratesDensity2.densityPerClust(indxDen2,1);
+paramVec1(indxGoodOn1) = ratesDensity1.rateOnPerClust(indxGoodOn1,1);
+paramVec1(numOn+indxGoodOff1) = ratesDensity1.rateOffPerClust(indxGoodOff1,1);
+paramVec1(numOn+numOff+indxGoodDen1) = ratesDensity1.densityPerClust(indxGoodDen1,1);
+paramVec2(indxGoodOn2) = ratesDensity2.rateOnPerClust(indxGoodOn2,1);
+paramVec2(numOn+indxGoodOff2) = ratesDensity2.rateOffPerClust(indxGoodOff2,1);
+paramVec2(numOn+numOff+indxGoodDen2) = ratesDensity2.densityPerClust(indxGoodDen2,1);
+
+%find NaN entries to make the same in variance-covariance matrix
+indxNaN1 = find(isnan(paramVec1));
+indxNaN2 = find(isnan(paramVec2));
 
 %fill the matrices
-paramCovMat1(indxOn1,indxOn1)
+paramCovMat1 = ratesDensity1.paramVarCovMat;
+paramCovMat1 = [paramCovMat1(:,1:numOn1) NaN(numTot1,numOn-numOn1) ...
+    paramCovMat1(:,numOn1+1:numOn1+numOff1) NaN(numTot1,numOff-numOff1) ...
+    paramCovMat1(:,numOn1+numOff1+1:numTot1) NaN(numTot1,numDen-numDen1)];
+paramCovMat1 = [paramCovMat1(1:numOn1,:); NaN(numOn-numOn1,numTot); ...
+    paramCovMat1(numOn1+1:numOn1+numOff1,:); NaN(numOff-numOff1,numTot); ...
+    paramCovMat1(numOn1+numOff1+1:numTot1,:); NaN(numDen-numDen1,numTot)];
+paramCovMat1(indxNaN1,:) = NaN;
+paramCovMat1(:,indxNaN1) = NaN;
+paramCovMat2 = ratesDensity2.paramVarCovMat;
+paramCovMat2 = [paramCovMat2(:,1:numOn2) NaN(numTot2,numOn-numOn2) ...
+    paramCovMat2(:,numOn2+1:numOn2+numOff2) NaN(numTot2,numOff-numOff2) ...
+    paramCovMat2(:,numOn2+numOff2+1:numTot2) NaN(numTot2,numDen-numDen2)];
+paramCovMat2 = [paramCovMat2(1:numOn2,:); NaN(numOn-numOn2,numTot); ...
+    paramCovMat2(numOn2+1:numOn2+numOff2,:); NaN(numOff-numOff2,numTot); ...
+    paramCovMat2(numOn2+numOff2+1:numTot2,:); NaN(numDen-numDen2,numTot)];
+paramCovMat2(indxNaN2,:) = NaN;
+paramCovMat2(:,indxNaN2) = NaN;
 
-%identify entries that are not NaN in at least one of the two vectors
-indxNoNaN = find(~isnan(paramVec1) | ~isnan(paramVec2));
+%identify entries that are not NaN in both vectors
+indxNoNaN = find(~isnan(paramVec1) & ~isnan(paramVec2));
 
-%remove these entries from parameter vectors
+%remove these entries from parameter vectors and matrices
 paramVec1 = paramVec1(indxNoNaN);
 paramVec2 = paramVec2(indxNoNaN);
+paramCovMat1 = paramCovMat1(:,indxNoNaN);
+paramCovMat1 = paramCovMat1(indxNoNaN,:);
+paramCovMat2 = paramCovMat2(:,indxNoNaN);
+paramCovMat2 = paramCovMat2(indxNoNaN,:);
 
-%expand parameter variance-covariance matrices to accommodate additional
-%parameter rows if necessary
-
+%calculate Mahalanobis distance between parameter vectors
+distMaha = ((paramVec2-paramVec1)'/(paramCovMat1+paramCovMat2))*(paramVec2-paramVec1);
 
 
 %% ~~~ the end ~~~
