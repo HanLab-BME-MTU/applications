0facfe28c730081706ad61df8bf8460c4ec1e212
mbagonis <maria.bagonis@utsouthwestern.edu>
2015-06-16 08:08:13 -0600

    Former-commit-id: 36ba04ec1511e45277103325c5cb4b4995657252

--- GrowthConeAnalyzer/x_GenericDependencies/gcaImdilateWithScale.m
+++ GrowthConeAnalyzer/x_GenericDependencies/gcaImdilateWithScale.m
@@ -4,23 +4,24 @@ function bw_out = gcaImdilateWithScale(bw_in,scaleMap,levels)
 bw_out = bw_in;
 bw_in = logical(bw_in); 
 
+%% SMALL quick and dirty to not trust the largest scale size
 % get the highest level nearest neighbor and set it to that value  
-pix_high = find((scaleMap == levels(end) | scaleMap == levels(end-1))& bw_in>0) ; 
-bw_inNoHi  = bw_in; 
-bw_inNoHi(pix_high) = 0; 
-scaleMed = median(scaleMap(bw_inNoHi)); 
-if ~isinteger(scaleMed/0.5); 
-    scaleMed = round(scaleMed); 
-end 
-if isnan(scaleMed); 
-    % guess size 
-    scaleMed = 3; 
-end 
-scaleMap(pix_high) = scaleMed; 
+% pix_high = find((scaleMap == levels(end) | scaleMap == levels(end-1))& bw_in>0) ; 
+% bw_inNoHi  = bw_in; 
+% bw_inNoHi(pix_high) = 0; 
+% scaleMed = median(scaleMap(bw_inNoHi)); 
+% if ~isinteger(scaleMed/0.5); 
+%     scaleMed = round(scaleMed); 
+% end 
+% if isnan(scaleMed); 
+%     % guess size 
+%     scaleMed = 3; 
+% end 
+% scaleMap(pix_high) = scaleMed; 
 
 % ideally want the pixels right before the pixel turns to noise 
 
-
+%%
 
 for i = 1 : length(levels)-2 % don't trust max level
     if levels(i)>0
