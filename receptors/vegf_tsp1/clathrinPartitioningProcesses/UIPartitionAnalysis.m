a52b4a9ef05cf3d1b4ab71c1a69a97a8cd97b6e0
Tae Kim <Tae.Kim@UTSouthWestern.edu>
2015-08-03 17:16:35 -0500

    can control how many randomized controls are done
    Former-commit-id: aae76dd28632424f622d431fcee5dbbb32f381d8

--- receptors/vegf_tsp1/clathrinPartitioningProcesses/UIPartitionAnalysis.m
+++ receptors/vegf_tsp1/clathrinPartitioningProcesses/UIPartitionAnalysis.m
@@ -13,6 +13,8 @@ function [] = UIPartitionAnalysis(varargin)
 %                                 pasfSigma. Part of maskDetectedStructure.
 %       'scrambleTracks'        : scrambles mean track position for control
 %                                 data set generation <Unused>
+%       'nControl'              : number of times the randomized control
+%                                 (scrambling track location) is repeated.
 %
 %Tae H Kim, July 2015
 
@@ -23,6 +25,7 @@ ip.CaseSensitive = false;
 ip.KeepUnmatched = true;
 ip.addParameter('psfSigmaMult', 3, @isnumeric);
 ip.addParameter('scrambleTracks', false, @(x) islogical(x) || isnumeric(x));
+ip.addParameter('nControl', 10, @isnumeric);
 ip.parse(varargin{:});
 psfSigmaMult = ip.Results.psfSigmaMult;
 scrambleTracks = ip.Results.scrambleTracks;
@@ -64,7 +67,7 @@ end
 %}
 
 %% Partition Analysis
-progressTextMultiple('Analyzing MD', nMD);
+progressTextMultiple('Analyzing ML', nMD);
 for iMD = 1:nMD
     %SubcellMaskProcess-------------------------------------------------------
     %get default para
@@ -74,7 +77,8 @@ for iMD = 1:nMD
     maskDetectedStructure(ML_Mask.movies_{iMD}, maskPara);
     %track partitioning process--------------------------------------------
     %get default para
-    trackPara = PartitionAnalysisProcess.getDefaultParams(ML_Mask.movies_{iMD}.outputDirectory_);
+    trackPara = PartitionAnalysisProcess.getDefaultParams(ML_Track.movies_{iMD}.outputDirectory_);
+    trackPara.nControl = ip.Results.nControl;
     %trackPara.scrambleTracks = scrambleTracks;
     %call analysis function
     trackPartitioning(ML_Track.movies_{iMD}, ML_Mask.movies_{iMD}, trackPara);
