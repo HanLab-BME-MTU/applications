92249968b19111dd6e9f4649a019e1ea6d2c42f7
mbagonis <maria.bagonis@utsouthwestern.edu>
2015-09-07 14:26:11 -0600

    Fixed Input
    Former-commit-id: b4eeed215c8c7cc108aee02cbabf14226b5e4f95

--- GrowthConeAnalyzer/ANALYSIS/Filopodia/GCAAddFilopodiaCurvatureMetricMovie.m
+++ GrowthConeAnalyzer/ANALYSIS/Filopodia/GCAAddFilopodiaCurvatureMetricMovie.m
@@ -1,25 +1,46 @@
-function [ output_args ] = GCAAddFilopodiaCurvatureMetricMovie(movieData)
+function [ output_args ] = GCAAddFilopodiaCurvatureMetricMovie(movieData,varargin)
 %GCAAddActinContentMetricMovie(movieData) 
+% 
 
-load([movieData.outputDirectory_ filesep 'filopodia_fits' filesep 'Filopodia_Fits_Channel_1' ...
-    filesep 'analInfoTestSave.mat']); 
+%% Input check
+% for now check movieData separately.
+if nargin < 1 || ~isa(movieData,'MovieData')
+    error('The first input must be a valid MovieData object!')
+end
 
-for iFrame = 1:length(analInfo)-1
+ip = inputParser;
+
+ip.CaseSensitive = false;
+
+% PARAMETERS
+defaultInDir = [movieData.outputDirectory_ filesep 'SegmentationPackage' ... 
+    filesep 'StepsToReconstruct' filesep... 
+    'VII_filopodiaBranch_fits' filesep 'Channel_1']; 
+ip.addParameter('InputDirectory',defaultInDir,@(x) ischar(x)); 
+
+ip.parse(varargin{:});
+
+%% Initiate
+load([ip.Results.InputDirectory filesep 'filoBranch.mat']); 
+
+%% Wrap
+
+for iFrame = 1:length(filoBranch)-1
 % extract the veil
 %veilMask = analInfo(iFrame).masks.neuriteEdge;
 % extract the img to feed into the function
 %img = double(imread([movieData.getChannelPaths{1} filesep movieData.getImageFileNames{1}{iFrame}])); 
 % extract the filo info to read into the function 
-filoInfo = analInfo(iFrame).filoInfo;
+filoInfo = filoBranch(iFrame).filoInfo;
 % add the metric to the filo info - NOTE in the future might want to just
 % calculate automaticaly at the time of fitting to be more efficient. 
 filoInfo = GCAAddFilopodiaCurvature(filoInfo); 
-analInfo(iFrame).filoInfo = filoInfo; 
+filoBranch(iFrame).filoInfo = filoInfo; 
 
 end
-% resave the values 
-save([movieData.outputDirectory_ filesep 'filopodia_fits' filesep 'Filopodia_Fits_Channel_1'...
-    filesep 'analInfoTestSave.mat'],'analInfo','-v7.3'); 
+
+%% Save all the information per a given movie in the filoBranch Structure  
+save([ip.Results.InputDirectory filesep 'filoBranch.mat'],'filoBranch','-v7.3'); 
 
 
 
