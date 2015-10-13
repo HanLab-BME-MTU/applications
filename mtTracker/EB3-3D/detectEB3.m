e1a6fd47f163e190bb0ddb0213a273447aae488e
Philippe roudot <proudot@biohpcws033.biohpc.swmed.edu>
2015-10-13 11:31:08 -0500

    detectEB3: handle 3D masking

--- mtTracker/EB3-3D/detectEB3.m
+++ mtTracker/EB3-3D/detectEB3.m
@@ -1,4 +1,4 @@
-function [movieInfo,labels] = detectEB3(MD,varargin)
+function [movieInfo,labels,scales] = detectEB3(MD,varargin)
 % SUMMARY: Wrap multiple detection function with standardized input and output. Why not using Virtual class ? B/c:
 %  - basic and readable switching between multiple detector type in an anylisis process
 %  - help systematic comparison
@@ -17,93 +17,98 @@ ip.addParamValue('lowFreq',3, @isnumeric);
 ip.addParamValue('highFreq',1, @isnumeric);
 ip.addParamValue('scales',[], @isnumeric);
 ip.addParamValue('Alpha',0.05, @isnumeric);
+ip.addParamValue('mask',[], @isnumeric);
 ip.addParamValue('showAll', false, @islogical);
 ip.addParamValue('printAll', false, @islogical);
+ip.addParamValue('subDirectory','EB3', @ischar);
 ip.addParamValue('type', 'watershedApplegate',  @ischar);
 ip.parse(MD, varargin{:});
 
+p=ip.Results;
+
 processFrames=[];
-if isempty(ip.Results.processFrames)
-    processFrames=1:numel(MD.getChannel(ip.Results.channel).getImageFileNames);
+if isempty(p.processFrames)
+    processFrames=1:numel(MD.getChannel(p.channel).getImageFileNames);
 else
-    processFrames=ip.Results.processFrames;
+    processFrames=p.processFrames;
 end
 
-if (strcmp(ip.Results.type,'pointSourceAutoSigma') ...
-    ||strcmp(ip.Results.type,'pointSourceAutoSigmaFit') ...
-    ||strcmp(ip.Results.type,'pSAutoSigmaMarkedWatershed') ...
-    ||strcmp(ip.Results.type,'pointSourceAutoSigmaMixture') ... 
-    ||strcmp(ip.Results.type,'pointSourceAutoSigmaLM') ...     
-    ||strcmp(ip.Results.type,'pointSourceAutoSigmaFitSig') ... 
-    ||strcmp(ip.Results.type,'pSAutoSigmaWatershed')) ... 
-    &&(isempty(ip.Results.scales))
+if (strcmp(p.type,'pointSourceAutoSigma') ...
+    ||strcmp(p.type,'pointSourceAutoSigmaFit') ...
+    ||strcmp(p.type,'pSAutoSigmaMarkedWatershed') ...
+    ||strcmp(p.type,'pointSourceAutoSigmaMixture') ... 
+    ||strcmp(p.type,'pointSourceAutoSigmaLM') ...     
+    ||strcmp(p.type,'pointSourceAutoSigmaFitSig') ... 
+    ||strcmp(p.type,'pSAutoSigmaWatershed')) ... 
+    &&(isempty(p.scales))
     volList=[];
     for i=1:5
-        volList=[volList double(MD.getChannel(ip.Results.channel).loadStack(i))];
+        volList=[volList double(MD.getChannel(p.channel).loadStack(i))];
     end
-    scales=getGaussianPSFsigmaFrom3DData(volList,'Display',ip.Results.showAll);
+    scales=getGaussianPSFsigmaFrom3DData(volList,'Display',p.showAll);
     disp(['Estimed scales: ' num2str(scales)]);
 else
-    scales=ip.Results.scales;
+    scales=p.scales;
 end 
 
 labels=cell(1,numel(processFrames));
 movieInfo(numel(processFrames),1) = struct('xCoord', [], 'yCoord',[],'zCoord', [], 'amp', [], 'int',[]);
 
-outputDirDetect=[MD.outputDirectory_ filesep 'EB3' filesep ip.Results.type];
+outputDirDetect=[MD.outputDirectory_ filesep p.subDirectory filesep p.type];
 mkdir(outputDirDetect);
 mkdir([outputDirDetect filesep 'mask']);
 parfor frameIdx=1:numel(processFrames)
     timePoint=processFrames(frameIdx);
     disp(['Processing time point ' num2str(timePoint,'%04.f')])
-    vol=double(MD.getChannel(ip.Results.channel).loadStack(timePoint));
-    switch ip.Results.type
+    vol=double(MD.getChannel(p.channel).loadStack(timePoint));
+    switch p.type
       case 'watershedApplegate'
-            filterVol=filterGauss3D(vol,ip.Results.highFreq)-filterGauss3D(vol,ip.Results.lowFreq);
-            [movieInfo(frameIdx),labels{frameIdx}]=detectComets3D(filterVol,ip.Results.waterStep,ip.Results.waterThresh,[1 1 1]);
+          filterVol=filterGauss3D(vol,p.highFreq)-filterGauss3D(vol,p.lowFreq);
+            [movieInfo(frameIdx),labels{frameIdx}]=detectComets3D(filterVol,p.waterStep,p.waterThresh,[1 1 1]);
       case 'watershedApplegateAuto'
-            filterVol=filterGauss3D(vol,ip.Results.highFreq)-filterGauss3D(vol,ip.Results.lowFreq);
-            thresh=QDApplegateThesh(filterVol,ip.Results.showAll);
-            [movieInfo(frameIdx),labels{frameIdx}]=detectComets3D(filterVol,ip.Results.waterStep,thresh,[1 1 1]);
+            filterVol=filterGauss3D(vol,p.highFreq)-filterGauss3D(vol,p.lowFreq);
+            thresh=QDApplegateThesh(filterVol,p.showAll);
+            [movieInfo(frameIdx),labels{frameIdx}]=detectComets3D(filterVol,p.waterStep,thresh,[1 1 1]);
       case 'bandPassWatershed'
-        filterVol=filterGauss3D(vol,ip.Results.highFreq)-filterGauss3D(vol,ip.Results.lowFreq);
-        label=watershed(-filterVol); label(filterVol<ip.Results.waterThresh)=0;labels{frameIdx}=label;
+        filterVol=filterGauss3D(vol,p.highFreq)-filterGauss3D(vol,p.lowFreq);
+        label=watershed(-filterVol); label(filterVol<p.waterThresh)=0;labels{frameIdx}=label;
         movieInfo(frameIdx)=labelToMovieInfo(label,filterVol);
       case 'watershedMatlab'
-        label=watershed(-vol); label(vol<ip.Results.waterThresh)=0;[dummy,nFeats]=bwlabeln(label);
+        label=watershed(-vol); label(vol<p.waterThresh)=0;[dummy,nFeats]=bwlabeln(label);
         labels{frameIdx}=label;
         movieInfo(frameIdx)=labelToMovieInfo(label,vol);
       case 'markedWatershed'
-        [labels{frameIdx}]=markedWatershed(vol,scales,ip.Results.waterThresh);
+        [labels{frameIdx}]=markedWatershed(vol,scales,p.waterThresh);
         movieInfo(frameIdx)=labelToMovieInfo(labels{frameIdx},vol);
       case {'pointSource','pointSourceAutoSigma'}
-        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,varargin{:});
-        labels{frameIdx}=double(mask); % adjust label
+        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,'Mask',p.mask,varargin{:});
+        labels{frameIdx}=double(mask); % adjust labellabel
         movieInfo(frameIdx)=labelToMovieInfo(double(mask),vol);
       case {'pointSourceLM','pointSourceAutoSigmaLM'}
-        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,varargin{:});
+        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,'Mask',p.mask,varargin{:});
         labels{frameIdx}=double(mask); % adjust label
         movieInfo(frameIdx)=pointCloudToMovieInfo(imgLM,vol);  
       case 'pSAutoSigmaMarkedWatershed'
-        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,varargin{:});
+        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,'Mask',p.mask,varargin{:});
         wat=markedWatershed(vol,scales,0);wat(mask==0)=0;       
         labels{frameIdx}=double(wat); % adjust label
         movieInfo(frameIdx)=labelToMovieInfo(double(wat),vol);
       case 'pSAutoSigmaWatershed'
-        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,varargin{:});
+        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,'Mask',p.mask,varargin{:});
         wat=watershed(-vol.*mask);wat(mask==0)=0;
         labels{frameIdx}=double(wat); % adjust label
         movieInfo(frameIdx)=labelToMovieInfo(double(wat),vol);
       case {'pointSourceFit','pointSourceAutoSigmaFit'}
-        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,varargin{:});
+        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,'Mask',p.mask,varargin{:});
         movieInfo(frameIdx)= pstructToMovieInfo(pstruct);
+          labels{frameIdx}=double(mask);
         %labels{frameIdx}=double(mask); % adjust label
       case {'pointSourceAutoSigmaMixture'}
-        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,'FitMixtures', true, varargin{:});
+        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,'Mask',p.mask,'FitMixtures', true, varargin{:});
         movieInfo(frameIdx)= pstructToMovieInfo(pstruct);
         labels{frameIdx}=double(mask); % adjust label
       case {'pointSourceAutoSigmaFitSig'}
-        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,'Mode','xyzAcsr',varargin{:});
+        [pstruct,mask,imgLM,imgLoG]=pointSourceDetection3D(vol,scales,'Mask',p.mask,'Mode','xyzAcsr',varargin{:});
         movieInfo(frameIdx)= pstructToMovieInfo(pstruct);
         labels{frameIdx}=double(mask); % adjust label
       otherwise 
@@ -112,18 +117,19 @@ parfor frameIdx=1:numel(processFrames)
         disp('\twatershed');
     end
     
-    if ip.Results.showAll
+    if p.showAll
         figure()
         imseriesmaskshow(vol,logical(labels{frameIdx}));
 %         figure()
 %         stackShow(vol,'overlay',labels{frameIdx});
     end
-    if(ip.Results.printAll)
-    stackWrite(labels{frameIdx},[outputDirDetect filesep 'mask' filesep 'detect_T_' num2str(frameIdx,'%05d') '.tif']);
+    if(p.printAll)
+        stackWrite(labels{frameIdx},[outputDirDetect filesep 'mask' filesep 'detect_T_' num2str(frameIdx,'%05d') '.tif']);
     end
     
 end 
-if(ip.Results.printAll)
+
+if(p.printAll)
  mkdir([outputDirDetect filesep 'amiraVertex']);
  amiraWriteMovieInfo([outputDirDetect filesep 'amiraVertex' filesep 'detect.am'],movieInfo,'scales',[MD.pixelSize_ MD.pixelSize_ MD.pixelSizeZ_]);
 end
