7e2a56b845a65a4301e8bb82dff1712f21f63a3d
Philippe roudot <proudot@biohpcws057.biohpc.swmed.edu>
2015-11-10 10:27:01 -0600

    pTT3d: display now support diffusion coefficient estimation and speed stat

--- mtTracker/EB3-3D/amiraWriteTracks.m
+++ mtTracker/EB3-3D/amiraWriteTracks.m
@@ -1,8 +1,10 @@
 function amiraWriteTracks(filename,tracks,varargin)
 % Write an Amira Mesh file with name [<filename>_%04d.am] representing tracks. 
 % Options
-%    - <scales>: [x y z] defines relative pixel size (must be synced to
-%    amira stack opening) 
+%    - <scales>: [x y z t] defines relative pixel size (must be synced to
+%    amira stack opening)
+%    - <movieData>: alternative to scale input (WARNING: the scale option
+%    has priority)
 %    - <vertexProp>: {{'name',{NVertex x 1, ...}},...} is the vertex-associated 
 %    properties  each frame must be described
 %    in a cell. 
@@ -12,7 +14,8 @@ function amiraWriteTracks(filename,tracks,varargin)
 ip=inputParser();
 ip.CaseSensitive = false;
 ip.KeepUnmatched = true;
-ip.addParamValue('scales', [1 1 1], @isnumeric);
+ip.addParamValue('scales', [1 1 1 1], @isnumeric);
+ip.addParamValue('MD',[],@(MD) isa(MD,'MovieData'));
 ip.addParamValue('vertexProp',{}, @iscell);
 ip.addParamValue('edgeProp',{}, @iscell);
 ip.parse( varargin{:});
@@ -21,7 +24,13 @@ p=ip.Results;
 [pathstr,name,ext] = fileparts(filename); 
 basename=[pathstr filesep name];
 
+
 s=ip.Results.scales;
+if(length(s)==3) s=[s 1]; end; %BW comp check. 
+
+if ((all(s==[1 1 1 1]))&&(~isempty(p.MD)))
+    s=[p.MD.pixelSize_ p.MD.pixelSize_ p.MD.pixelSizeZ_ p.MD.timeInterval_];
+end
 
 if(~exist(fileparts(filename))) mkdir(fileparts(filename)); end;
 
@@ -39,20 +48,27 @@ for tIdx=1:length(tracks)
     end
 end
 
-parfor fIdx=1:tracks.numTimePoints
+% Frame 0 is the cumulative track distribution. Ugly ? I know, stfu.
+parfor fIdx=0:tracks.numTimePoints
     
     %% Indx of tracks on the current frame
-    tracksOn=([tracks.endFrame]>=fIdx)&(fIdx>[tracks.startFrame]);
+    if(fIdx>0)
+        tracksOn=([tracks.endFrame]>=fIdx)&(fIdx>[tracks.startFrame]);
+    else
+        tracksOn=true(1,length(tracks));
+    end
     nbTracsOn=sum(tracksOn);
     
     %% tracks extremity
     tracksEnds=size(nbTracsOn*2,3);
     % relative Idx of the end of each tracks (e.g. for use in tracks(i).x)
     endRelIdx=fIdx-[tracks.startFrame]+1; 
+    if(fIdx==0)
+        endRelIdx=[tracks.lifetime];
+    end;
     count=1;
     for tIdx=find(tracksOn)
         tr=tracks(tIdx);
-        % Handling gap
         tracksEnds(count+1,1)=(tr.x(endRelIdx(tIdx))-1)*s(1);
         tracksEnds(count+1,2)=(tr.y(endRelIdx(tIdx))-1)*s(2);
         tracksEnds(count+1,3)=(tr.z(endRelIdx(tIdx))-1)*s(3);
@@ -72,9 +88,9 @@ parfor fIdx=1:tracks.numTimePoints
     
     %% tracks point    
     % numPointsPerEdge=max(2,endRelIdx(tracksOn)'); %if one single time point Amira still needs two time points...
-    numPointsPerEdge=endRelIdx(tracksOn)';
+    numPointsPerEdge=endRelIdx((tracksOn))';
     numPoints=sum(numPointsPerEdge);
-    tracksPoints=zeros(size(numPoints,3));
+    tracksPoints=zeros([numPoints,3]);
     count=1;
     for tIdx=find(tracksOn)
         tr=tracks(tIdx);
@@ -84,17 +100,29 @@ parfor fIdx=1:tracks.numTimePoints
         tracksPoints(count:(count+endIdx-1),3)=(tr.z(1:endIdx)-1)*s(3);        
         count=count+endIdx;
     end
-
     
     %% Track id (edge property)
     tracksId=find(tracksOn)';
     
     %% Track lifetime (edge property)
     tracksLft=[tracks(tracksOn).lifetime]';
-      
+    
+    %% Track Median Speed (edge property)
+    tracksMedSpeed= arrayfun(@(t) nanmedian(sum((   [s(1)*t.x(1:end-1);s(2)*t.y(1:end-1);s(3)*t.z(1:end-1)]- ... 
+                                                    [s(1)*t.x(2:end);  s(2)*t.y(2:end); s(3)*t.z(2:end)  ]).^2).^0.5/s(4)) ,tracks(tracksOn));
+    %% Track Max Speed (edge property)
+    tracksMaxSpeed= arrayfun(@(t) nanmax(sum((   [s(1)*t.x(1:end-1);s(2)*t.y(1:end-1);s(3)*t.z(1:end-1)]- ... 
+                                                    [s(1)*t.x(2:end);  s(2)*t.y(2:end); s(3)*t.z(2:end)  ]).^2).^0.5/s(4)) ,tracks(tracksOn));
+                                                
+    %% Track diffCoeff (edge property)
+    tracksDiffCoeff=arrayfun(@(t) nanmean(sum([s(1)*t.x(1)-s(1)*t.x(2:end); s(2)*t.y(1)-s(2)*t.y(2:end); s(3)*t.z(1)-s(3)*t.z(2:end)].^2))/(6*t.lifetime*s(4)) ,tracks(tracksOn));
+    
     % Write 
     paramCount=0;
     frameFilename=[basename '_t_' num2str(fIdx,'%04.0f'),'.am'];
+    if(fIdx==0)
+        frameFilename=[basename '_t_' num2str(fIdx,'%04.0f'), '_cumulative_display.am'];
+    end
     fid = fopen(frameFilename, 'w');
     fprintf(fid,['# AmiraMesh 3D ASCII 2.0\n\n']);
     fprintf(fid,['define VERTEX ' num2str(nbTracsOn*2) '\n']);
@@ -109,6 +137,9 @@ parfor fIdx=1:tracks.numTimePoints
     fprintf(fid,'EDGE { int trackId } @5\n');
     fprintf(fid,'VERTEX {int startEnd } @6\n');
     fprintf(fid,'EDGE { int lifetime } @7\n');    
+    fprintf(fid,'EDGE { float medianSpeed} @8\n');  
+    fprintf(fid,'EDGE { float maxSpeed} @9\n');  
+    fprintf(fid,'EDGE { float diffCoeff} @10\n');  
     fclose(fid);
     if(nbTracsOn)
         paramCount=paramCount+1;
@@ -152,6 +183,24 @@ parfor fIdx=1:tracks.numTimePoints
         fprintf(fid,'\n@7\n');
         fclose(fid);
         dlmwrite(frameFilename, tracksLft, '-append', 'delimiter',' ','precision', 16);
+        
+        paramCount=paramCount+1;
+        fid = fopen(frameFilename, 'a');
+        fprintf(fid,'\n@8\n');
+        fclose(fid);
+        dlmwrite(frameFilename, tracksMedSpeed, '-append', 'delimiter',' ','precision', 16);
+
+        paramCount=paramCount+1;
+        fid = fopen(frameFilename, 'a');
+        fprintf(fid,'\n@9\n');
+        fclose(fid);
+        dlmwrite(frameFilename, tracksMaxSpeed, '-append', 'delimiter',' ','precision', 16);
+        
+        paramCount=paramCount+1;
+        fid = fopen(frameFilename, 'a');
+        fprintf(fid,'\n@10\n');
+        fclose(fid);
+        dlmwrite(frameFilename, tracksDiffCoeff, '-append', 'delimiter',' ','precision', 16);
         
         for propIdx=1:length(p.vertexProp)
             fid = fopen(frameFilename, 'a');
