030457a85d6f6cf2efd529dfa0d91cf7df65aaf7
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2015-11-28 07:23:43 -0600

    lamins: Initial commit of LaminsAnalysisProcess
    Former-commit-id: 63bce7c003e919ca80ed2ff9189e63f019957844

---
+++ lamins/+lamins/+classes/@LaminsAnalysisProcess/LaminsAnalysisProcess.m
@@ -0,0 +1,50 @@
+classdef LaminsAnalysisProcess < ImageAnalysisProcess
+    %LaminsAnalysisProcess Analyzes lamin curvilinear structures
+    
+    properties
+    end
+    
+    methods
+        function obj = LaminsAnalysisProcess(owner)
+%             obj = obj@ImageAnalysisProcess(owner,'LaminsAnalysisProcess',@lamins.functions.analyzeLaminsForProcess);
+              obj = obj@ImageAnalysisProcess(owner, ...
+                  'LaminsAnalysisProcess', ...
+                  @lamins.functions.analyzeLaminsForProcess, ...
+                  struct());
+            
+        end
+        function run(obj,varargin)
+            basePath = [obj.owner_.outputDirectory_ filesep 'LaminsAnalysisProcess'];
+            
+            changed = false;
+            for c=1:length(obj.owner_.channels_)
+                outPath = [basePath filesep 'channel_' num2str(c)];
+                if(isempty(obj.outFilePaths_{c}))
+                    obj.setOutFilePaths(outPath,c);
+                    changed = true;
+                end
+            end
+            if(changed)
+                obj.owner_.save();
+            end
+            
+            params = obj.getParameters();
+            if(~isfield(params,'outFilePaths'))
+                params.outFilePaths = obj.outFilePaths_;
+            end
+            
+            % Call the superclass runner
+            obj.run@ImageAnalysisProcess(params,varargin{:});
+        end
+    end
+    methods (Static)
+        function name = getName(varargin)
+            name = 'Lamins Analysis Process';
+        end
+        function params = getDefaultParams(varargin)
+            params = struct();
+        end
+    end
+    
+end
+
