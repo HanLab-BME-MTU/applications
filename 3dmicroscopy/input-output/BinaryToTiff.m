d89eabafc2e482bc18c1b81447ef3d4848516cf3
Gerard Garrelts <g.garrelts@colemantech.com>
2015-12-10 16:59:53 -0600

    binary to tiff conversion
    Former-commit-id: b369091fe41857508b93b382935a8f0b79ebe39c

--- 3dmicroscopy/input-output/BinaryToTiff.m
+++ 3dmicroscopy/input-output/BinaryToTiff.m
@@ -1,7 +1,8 @@
 %%
-cd C:\Users\ALSM_Master\Desktop\Data\Reto\TestSpeed\0_75ms\151208\Cell9
+clear all
+cd C:\Users\ALSM_Master\Desktop\Data\Reto\RPE_1ms\tractinGFP\151208\Cell10
 
-fileID = fopen('1_CAM01_000001.bin','r+','l');
+fileID = fopen('1_CAM02_000002.bin','r+','l');
 
 test=fread(fileID,'uint16');
 
@@ -9,15 +10,17 @@ fclose(fileID);
 
 numRows=512;
 
-numCols=128;
+numCols=64;
 
-numZ=168;
+numZ=68;
 
 
 test2=zeros(numRows,numCols,numZ);
+
+
 test2=reshape(test,[numRows,numCols,numZ]);
 
-figure;imshow(squeeze(sum(test2,2)),[])
+%figure;imshow(squeeze(sum(test2,2)),[])
 figure;imshow(squeeze(max(permute(test2,[2,1,3]))),[])
 
 bl=squeeze(max(permute(test2,[2,1,3])));
