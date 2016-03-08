48dea269699bfd5339c7a08af9776f14229b3577
mbagonis <maria.bagonis@utsouthwestern.edu>
2016-03-08 13:37:45 -0600

    more generic input
    Former-commit-id: e010a9cdc8e14a23b5b50d154579518a3428c838

--- GrowthConeAnalyzer/VALIDATION/FiloBranchEmbed_ManualValidationTool/GCAValidationRandomImageSelection.m
+++ GrowthConeAnalyzer/VALIDATION/FiloBranchEmbed_ManualValidationTool/GCAValidationRandomImageSelection.m
@@ -12,7 +12,7 @@ projectsAll = repmat(projList,120,1);
 frames = arrayfun(@(i) repmat(i,length(projList),1),1:120,'uniformoutput',0); 
 frames = vertcat(frames{:}); 
 
-projectSamp = projectsAll(randsample(1:length(frames),nSamples)); 
+projectSamp = projectsAll(randsample(1:length(frames),nSamples),:); 
 framesSamp = frames(randsample(1:length(frames),nSamples)); 
 
 selectedFiles = [projectSamp  num2cell(framesSamp)];  
