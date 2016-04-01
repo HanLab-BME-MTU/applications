b314c67393adad2ce95afeedc24547e78af390d1
Ning <ning1.zhang@utsouthwestern.edu>
2016-03-31 20:45:50 -0500

    Optimize otsu thresholding post processing
    Former-commit-id: 159d1ecb5420944b8a595035c2f4f165800966f3

--- FISHprobe/segmentNucleiLocalOtsu.m
+++ FISHprobe/segmentNucleiLocalOtsu.m
@@ -18,7 +18,7 @@ localWindowPace = round(localWindowRadius * parameters.localWindowPaceFraction);
 
 
 % local otsu thresholding in each slice
-mask = zeros(size(imInput));
+otsuMask = zeros(size(imInput));
 
 % Calculate the threshold for whole 3D stack
 globalStackThresh = thresholdOtsu( imInput );
@@ -32,54 +32,75 @@ for sliceId = 1:size(imInput, 3)
     if sliceGlobalThresh < parameters.minSliceToStackThresholdRatio * globalStackThresh
         imMask = imMask > globalStackThresh;
     end
-    mask(:,:,sliceId) = imMask;
+    otsuMask(:,:,sliceId) = imMask;
 end
 
-% post-processing
+% post-processing from /home2/nzhang/matlab/applications/FISHprobe
+% /InvivoCytometer_2.0_source_code/code_package/segmentCellForegroundUsingLocalOtsu.m
 diskRad = ones(1,ndims(imInput));
 diskRad(1:2) = 3;
-mask = imopen(mask, streldisknd(diskRad));
-% mask = bwlabeln( mask > 0 );
+otsuMask = imopen(otsuMask, streldisknd(diskRad));
 
-
-% Possible post-processing from /home2/nzhang/matlab/applications/FISHprobe
+% post-processing from /home2/nzhang/matlab/applications/FISHprobe
 % /InvivoCytometer_2.0_source_code/code_package/segmentCellsInIntravitalData.m
-% 
-mask = imfill( double(mask) ); 
-mask = imclose(mask, streldisknd(2*ones(1,ndims(imInput))) );
+otsuMask = imfill( double(otsuMask) ); 
+otsuMask = imclose(otsuMask, streldisknd(2*ones(1,ndims(imInput))) );
 
-% remove regions with small and invalid/unusual sizes
-threshL = bwlabeln( mask );
-thRegStats = regionprops( threshL, {'Area', 'BoundingBox'} );
+% Remove unqualified regions
+threshL = bwlabeln(otsuMask);
+regionStats = regionprops( threshL, {'Area', 'BoundingBox'} );
 
-flagIsBBoxSizeValid = false(1, numel(thRegStats));
-imsize = size(imInput);
+% Remove regions with unreasonable small/large size
+minCellDiameter = input('Enter the minimum cell diameter in um for selection > ');
+maxCellDiameter = input('Enter the maximum cell diameter in um for selection > ');
+minCellVolume = (4/3) * pi * (0.5 * minCellDiameter)^3;
+maxCellVolume = (4/3) * pi * (0.5 * maxCellDiameter)^3;
+spacing = [dataProperties.PIXELSIZE_XY, dataProperties.PIXELSIZE_XY, dataProperties.PIXELSIZE_Z];
 
-for i = 1:numel( thRegStats )
+regArea = [regionStats.Area] * prod(spacing);
+smallRegionIndex = find(regArea < minCellVolume | regArea > maxCellVolume);
+threshL(ismember(threshL, smallRegionIndex)) = 0;
 
-    bboxSideLength = thRegStats(i).BoundingBox((ndims(imInput)+1):end);
-    flagIsCurBBoxBigEnough = all( bboxSideLength >= minCellBBox );
-    if ~flagIsCurBBoxBigEnough
-        continue;
-    end
-
-    % check if this is the region containing the grid using a heuristic
-    bboxSideLengthXY = bboxSideLength(1:2);
-    flagIsGridRegion = any(bboxSideLengthXY >= 0.5 * imsize([2,1])) && min(bboxSideLengthXY) / max(bboxSideLengthXY) < 0.25;
-    flagIsBBoxSizeValid(i) = ~flagIsGridRegion;
-
+% Remove regions that touch the boundary
+flagCrossBoundary = false(1, numel(regionStats));
+if dataProperties.imSize(1) ~= dataProperties.imSize(2)
+    error('SizeX and SizeY are not identical.')
 end
+for i = 1:numel(regionStats)
+    if any(regionStats(i).BoundingBox(1:(ndims(imInput))) <1)
+        flagCrossBoundary(i) = 1;
+    else if any((regionStats(i).BoundingBox(1:2) + regionStats(i).BoundingBox(4:5))...
+                > dataProperties.imSize(1)) || ((regionStats(i).BoundingBox(3) + ...
+                regionStats(i).BoundingBox(6)) > dataProperties.nDepth)
+            flagCrossBoundary(i) = 1;
+        end
+    end
+end
+CrossBoundaryIndex = find(flagCrossBoundary);
+threshL(ismember(threshL, CrossBoundaryIndex)) = 0;
 
-regArea = [thRegStats.Area] * prod(spacing);
+% Remove regions with unclosed holes in 3D that cannot be filled with
+% morphological processing
+xyMaxProj = max(threshL,[],3);
+xyMaxProjFilled = imfill(xyMaxProj);
+xyDiff = xyMaxProjFilled - xyMaxProj;
+threshL(ismember(threshL, xyDiff)) = 0;
 
-smallRegInd = find( ~flagIsBBoxSizeValid | regArea < minCellVolume );
-threshL( ismember(threshL, smallRegInd) ) = 0;
-mask = double(threshL > 0);
+yzMaxProj = max(threshL,[],1);
+yzMaxProjFilled = imfill(yzMaxProj);
+yzDiff = yzMaxProjFilled - yzMaxProj;
+threshL(ismember(threshL, yzDiff)) = 0;
 
-cleanDiskRad = max( [round(0.25 * min(cellDiameterRange) ./ spacing); [2,2,1]] );
-mask = imopen(mask, streldisknd(cleanDiskRad) ); % removes thin vessel like structures
+xzMaxProj = max(threshL,[],2);
+xzMaxProjFilled = imfill(xzMaxProj);
+xzDiff = xzMaxProjFilled - xzMaxProj;
+threshL(ismember(threshL, xzDiff)) = 0;
 
-imAdjusted( ~mask ) = min( imAdjusted(:) );
 
-    
+
+mask = bwlabeln(double(threshL > 0));
+
+% cleanDiskRad = max( [round(0.25 * min(cellDiameterRange) ./ spacing); [2,2,1]] );
+% mask = imopen(mask, streldisknd(cleanDiskRad) ); % removes thin vessel like structures
+
 end
