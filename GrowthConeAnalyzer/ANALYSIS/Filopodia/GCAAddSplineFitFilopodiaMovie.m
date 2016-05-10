d12058a86b20c9951cfb0a337cb54b0bc84364f2
mbagonis <maria.bagonis@utsouthwestern.edu>
2016-05-10 08:43:34 -0500

    added overlays
    Former-commit-id: 384d9e6ad9cd980dc3784ba714248452ad363c4a

--- GrowthConeAnalyzer/ANALYSIS/Filopodia/GCAAddSplineFitFilopodiaMovie.m
+++ GrowthConeAnalyzer/ANALYSIS/Filopodia/GCAAddSplineFitFilopodiaMovie.m
@@ -12,11 +12,17 @@ defaultInDir = [movieData.outputDirectory_ filesep 'SegmentationPackage' ...
     'VII_filopodiaBranch_fits' filesep 'Channel_1']; 
 ip.addParameter('InputDirectory',defaultInDir,@(x) ischar(x)); 
 
-
+ip.addParameter('TSOverlays',false); 
 ip.parse(varargin{:});
 %%
 
-load([ip.Results.InputDirectory filesep 'filoBranch.mat']); 
+load([ip.Results.InputDirectory filesep 'filoBranch.mat']);
+if ip.Results.TSOverlays
+    overlayDir = [ip.Results.InputDirectory filesep 'SplineFitOverlays'];
+    if ~isdir(overlayDir)
+        mkdir(overlayDir);
+    end
+end
 
 for iFrame = 1:length(filoBranch)-1
 %for iFrame = 1:35
@@ -26,9 +32,25 @@ for iFrame = 1:length(filoBranch)-1
 %img = double(imread([movieData.getChannelPaths{1} filesep movieData.getImageFileNames{1}{iFrame}])); 
 % extract the filo info to read into the function 
 filoInfo = filoBranch(iFrame).filoInfo;
+
+if ip.Results.TSOverlays 
+    img = double(imread([movieData.getChannelPaths{1} filesep movieData.getImageFileNames{1}{iFrame}]));
+    [ny,nx] = size(img); 
+    setFigure(nx,ny,'on'); 
+   
+    imshow(-img,[]) ; 
+    hold on 
+    
+end 
 % add the metric to the filo info - NOTE in the future might want to just
 % calculate automaticaly at the time of fitting to be more efficient. 
-filoInfo = GCASplineFitFilopodia(filoInfo); 
+filoInfo = GCASplineFitFilopodia(filoInfo,'TSOverlays',ip.Results.TSOverlays);
+
+if ip.Results.TSOverlays
+    saveas(gcf,[overlayDir filesep num2str(iFrame,'%03d') '.fig']); 
+    close gcf 
+end 
+
 filoBranch(iFrame).filoInfo = filoInfo; 
 %display(['Finished Frame ' num2str(iFrame)]); 
 
