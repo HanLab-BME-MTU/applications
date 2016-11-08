47e02361600df3564a998ddbf9e0678a55880766
Anthony Vega <anthony.vega@utsouthwestern.edu>
2016-11-08 13:42:36 -0600

    Added more comments to coloc script
    Former-commit-id: be696f9b4e996b70bb987ad8bf0ec1867b352c38

--- receptors/colocalization/scriptGeneralColocalization.m
+++ receptors/colocalization/scriptGeneralColocalization.m
@@ -1,7 +1,10 @@
 function [] = scriptGeneralColocalization()
 
-     %% Create Movie Data objec
-        MD = MovieData('dataSample.tif'); %Indicate image file to be analyzed; all channels should be in single tiff file
+     %% Create Movie Data object
+     % Make sure image file properties are properly set (correct number of
+     % slices, channels and frames if applicable). Otherwise running
+     % processes below may result in error
+        MD = MovieData('test_0001.tif'); %Indicate image file to be analyzed; all channels should be in single tiff file
      %% Initialize and add all processes
         process = SubResolutionProcess(MD); %Detection
         MD.addProcess(process);
@@ -14,7 +17,7 @@ function [] = scriptGeneralColocalization()
     %% Detection Process
     % The core function being used here is detectSubResFeatures2D_StandAlone
         p = MD.getProcess(1).getParameters();
-        p.ChannelIndex = 1; %Channel in image to undergo detection process 
+        p.ChannelIndex = 2; %Channel in image to undergo detection process 
         p.detectionParam.psfSigma = 1; %Point spread function sigma of image (in pixels)
         p.detectionParam.testAlpha = struct('alphaR',0.1,'alphaA',0.1,'alphaD',0.1,'alphaF',0);%alpha-values for detection statistical tests
         p.detectionParam.alphaLocMax = 0.05;%alpha-value for initial detection of local maxima
@@ -26,7 +29,7 @@ function [] = scriptGeneralColocalization()
     %% Masking Process
     % The core function used for masking is calcCellBoundaryImage
         p = MD.getProcess(2).getParameters();
-        p.ChannelIndex = 1;%Channel in image to undergo masking process, usually continuum channel
+        p.ChannelIndex = 3;%Channel in image to undergo masking process, usually continuum channel
         p.GaussFilterSigma = 2;% Sigma of gaussian filter used to smooth image
         p.MaxJump = 1; %If function fails to find a threshold in a stack of images, any value > 1 indicates to use the previous threshold 
         MD.getProcess(2).setParameters(p);
@@ -37,8 +40,8 @@ function [] = scriptGeneralColocalization()
     % colocalMeasurePt2Cnt
         p = MD.getProcess(3).getParameters();
         p.ChannelRef = 2; %Punctate channel which underwent detection process
-        p.ChannelObs = 1; %Continuum channel
-        p.ChannelMask = 1; %Channel that was masked
+        p.ChannelObs = 3; %Continuum channel
+        p.ChannelMask = 2; %Channel that was masked
         p.SearchRadius = 2; %Radius around detection used for analysis
         p.RandomRuns = 1;% Number of times randomized data is analyzed
         MD.getProcess(3).setParameters(p);
