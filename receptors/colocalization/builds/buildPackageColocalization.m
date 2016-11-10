15d2593d7588a98517f8baa4db22a519846d1260
Andrew R. Jamieson <andrew.jamieson@UTSouthwestern.edu>
2016-11-10 14:21:28 -0600

    Added input variable, test_img, to pass test image into function and remove hard coded file path.
    Former-commit-id: 5e8f353b278b1146906c75a5cb0b99d9e91c1df4

--- receptors/colocalization/builds/buildPackageColocalization.m
+++ receptors/colocalization/builds/buildPackageColocalization.m
@@ -1,4 +1,38 @@
-% Script for building application packages
+% Simple script for building application packages
 
-buildPackage('scriptGeneralColocalization.m','/home2/s170480/matlab/builds/colocalize/')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Set output path for package build files
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Build on BioHPC - Linux
+% out_dir = '/home2/s170480/matlab/builds/colocalize/'
+% Build on Andrew's Windows 10 machine
+out_dir = 'C:\Users\Andrew\GIT\matlab\builds\colocalization';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Build the package
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+buildPackage('scriptGeneralColocalization.m',out_dir);
+cd(out_dir); % check results
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Gather Test image
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% move test image to build package dir.
+test_img = 'C:\Users\Andrew\Data\raw\Tony\Colocalization\test_0001.tif';
+copyfile(test_img, out_dir); 
+[a, b, c] = fileparts(test_img);
+test_img = [b c];
+
+% ---> Basically Zip up this directory now for the package.
+zip('../colocalization.zip', '../colocalization/');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Test the package build works!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+restoredefaultpath;  % Clears out repo paths
+addpath(genpath(out_dir)); % add the build package path
+scriptGeneralColocalization(test_img);
+
+
+
 
