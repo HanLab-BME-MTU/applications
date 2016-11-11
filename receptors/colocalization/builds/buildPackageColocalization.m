63235629704157a85112fb562ce128169481025c
Andrew R. Jamieson <andrew.jamieson@UTSouthwestern.edu>
2016-11-11 14:54:57 -0600

    Updated build script.  Added in license/copyright info to .m files.
    Former-commit-id: 9617222e57973028b51b7e3763e40e50b3d95ad4

--- receptors/colocalization/builds/buildPackageColocalization.m
+++ receptors/colocalization/builds/buildPackageColocalization.m
@@ -1,18 +1,42 @@
 % Simple script for building application packages
 
+% Build on BioHPC - Linux
+% matlab_repo_root = '/home2/s170480/matlab/'
+% Build on Andrew's Windows 10 machine
+matlab_repo_root = 'C:\Users\Andrew\GIT\matlab\';
+package_name = 'colocalization';
+institution_name = 'UTSouthwestern';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Set output path for package build files
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Build on BioHPC - Linux
-% out_dir = '/home2/s170480/matlab/builds/colocalize/'
-% Build on Andrew's Windows 10 machine
-out_dir = 'C:\Users\Andrew\GIT\matlab\builds\colocalization';
+
+out_dir = fullfile(matlab_repo_root, ['builds' filesep 'colocalization'])
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Build the package
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-buildPackage('scriptGeneralColocalization.m',out_dir);
+restoredefaultpath;  % Clears out repo paths
+% Add necesary repos for building 
+repo_dirs = fullfile(matlab_repo_root, {'extern'; 'applications'; 'common'});
+for i = 1:length(repo_dirs)
+    cur_dir = repo_dirs{i};
+    disp(['Adding ' cur_dir]);
+    addir(cur_dir);
+end
+
+buildPackage('scriptGeneralColocalization.m', out_dir);
 cd(out_dir); % check results
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Add license 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Be sure to modify the statement as needed.
+addCopyingStatement = fullfile(matlab_repo_root,['documentation' filesep 'license' filesep 'addCopyingStatement']);
+CopyingStatement = fullfile(matlab_repo_root,['documentation' filesep 'license' filesep 'CopyingStatement']);
+GPL_license = fullfile(matlab_repo_root,['documentation' filesep 'license' filesep 'GPL-License.txt']);
+copyfile(addCopyingStatement, out_dir);
+copyfile(CopyingStatement, out_dir);
+system(['bash addCopyingStatement ' package_name ' ' institution_name]);
+copyfile(GPL_license, out_dir);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Gather Test image
