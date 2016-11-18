955216f9a1fb6796c255b74bceccfceced312827
Andrew R. Jamieson <andrew.jamieson@UTSouthwestern.edu>
2016-11-18 15:32:35 -0600

    Backup scripts/builds for Andrew's Win10 machine.
    Former-commit-id: 8e180ad0c5fca6537ac5419f3feb3f8b08a8ea5a

---
+++ receptors/colocalization/.scriptMultiChannelColocalization_AJ.m
@@ -0,0 +1,33 @@
+function [] = scriptMultiChannelColocalization
+
+
+%% Colocalization
+% The core function used for colocalization analysis is colocalMeasurePt2Cnt
+test_img = 'C:\Users\Andrew\Data\raw\Tony\Colocalization\test_0001.tif';
+% MD = MovieData.load('/home2/avega/Documents/test_0001/test_0001.mat');%Indicate location of MD object file from scriptGeneralColocalization analysis
+MD = MovieData.load(test_img); %Indicate location of MD object file from scriptGeneralColocalization analysis
+p = MD.getProcess(3).getParameters();
+orgChannel = 3; %Original continuum channel used in scriptGeneralColocalization
+p.ChannelObs = 1; %New context channel
+p.SearchRadius = 2; %Radius around detection to use to read out intensity 
+p.RandomRuns = 1;% Number of times randomized data is analyzed
+MD.getProcess(3).setParameters(p);
+MD.getProcess(3).run;
+
+
+%% Comparison
+% The core function used for colocalization analysis is multiChannelColocalization
+% Load original colocalization results
+load([p.OutputDirectory 'ColocalizationPt2Cnt/colocalInfo' num2str(p.ChannelRef) num2str(orgChannel) '.mat'],'enrichInd')
+colocEnrich = enrichInd;
+% Load new colocalization results
+load([p.OutputDirectory 'ColocalizationPt2Cnt/colocalInfo' num2str(p.ChannelRef) num2str(p.ChannelObs) '.mat'],'enrichInd','randEnrichInd')
+bgEnrich = enrichInd;
+randBgEnrich = randEnrichInd;
+% Run multi-channel colocalization analysis
+visual = 1; %Shows boxplot visual of continuum enrichment at punctate detections separated by high and low context enrichment. Data taken from high/lowColoc outputs
+[occupHigh,lowColoc,highColoc] = multiChannelColocalization(bgEnrich,colocEnrich,randBgEnrich,visual); 
+% Save results
+mkdir([p.OutputDirectory 'MultiChannelColocalization' ])
+save([p.OutputDirectory 'MultiChannelColocalization/mccResults' ],'occupHigh','lowColoc','highColoc')
+end
