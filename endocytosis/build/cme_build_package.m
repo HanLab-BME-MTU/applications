65facbb50fb22211fe3876015c1c5eae60bace88
Andrew R. Jamieson <andrew.jamieson@UTSouthwestern.edu>
2016-11-28 18:22:15 -0600

    Scripts for testing and building the CME package.
    Former-commit-id: 693d069771b9fe7539706b6e613d0a6e6136c300

---
+++ endocytosis/build/cme_build_package.m
@@ -0,0 +1,135 @@
+
+if strcmp(computer('arch'),'win64')
+	data_root_win10 = 'C:\Users\Andrew\Data\CME\xinxin\';
+else
+	data_root = '/work/bioinformatics/s170480/Data/CME/xinxin/';
+end	
+
+cmeData_dir = fullfile(data_root, ['single_channel' filesep 'Zuzana_ARPE_CLC_egfp_07032014_control']);
+epiTIRFData_dir = fullfile(data_root, ['epi_tirf' filesep 'Zuzana_arpe_clc_egfp_14082015_control']);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Set output path for package build files
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Build on BioHPC - Linux
+
+if strcmp(computer('arch'),'win64')
+	% matlab_repo_root = '/home2/s170480/matlab/'
+	matlab_repo_root = 'C:\Users\Andrew\GIT\matlab\';
+	% Build on Andrew's Windows 10 machine
+else
+	matlab_repo_root = [getenv('HOME') filesep 'matlab'];
+end
+package_name = 'CME';
+institution_name = 'Danuser & Schmid Labs - UTSouthwestern';
+
+build_dir = fullfile(matlab_repo_root, 'builds');
+out_dir = fullfile(matlab_repo_root, ['builds' filesep package_name]);
+zip_file = fullfile(matlab_repo_root, ['builds' filesep package_name '.zip']);
+start_paths = path;
+start_dir = pwd;
+
+if exist(out_dir, 'dir') == 7
+    choice = questdlg(['Remove old build dir?'],'Question..','Yes','No','Yes');
+    if strcmp(choice, 'Yes')
+    	rmdir(out_dir, 's');
+    	pause(.25);
+    	rehash(); % weird issue with matlab rmdir?
+    end
+end
+rehash();
+mkdir(out_dir);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Build the package
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+restoredefaultpath;  % Clears out repo paths
+% Add necesary repos for building 
+repo_dirs = fullfile(matlab_repo_root, {'extern'; 'applications'; 'common'});
+for i = 1:length(repo_dirs)
+    cur_dir = repo_dirs{i};
+    disp(['Adding ' cur_dir]);
+    addir(cur_dir);
+end
+cellScript{1} = 'scriptCMEautotest.m';
+buildPackage(cellScript, out_dir);
+cd(out_dir); % check results
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % Add license 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % Be sure to modify the statement as needed.
+% addCopyingStatement = fullfile(matlab_repo_root,['documentation' filesep 'license' filesep 'addCopyingStatement']);
+% CopyingStatement = fullfile(matlab_repo_root,['documentation' filesep 'license' filesep 'CopyingStatement']);
+% GPL_license = fullfile(matlab_repo_root,['documentation' filesep 'license' filesep 'GPL-License.txt']);
+% copyfile(addCopyingStatement, out_dir);
+% copyfile(CopyingStatement, out_dir);
+% copyfile(GPL_license, out_dir);
+
+% if strcmp(computer('arch'),'win64')
+%     disp('Win64');
+%     disp(['Run this code in the pop up: "bash addCopyingStatement ' package_name ' ' institution_name '"']);
+%     winopen(out_dir);
+%     uiwait(msgbox(['Run this code in the pop up: "bash addCopyingStatement ' package_name ' ' institution_name '"']));
+%     system('C:\Program Files\Git\git-bash.exe');
+% else
+% 	disp(['Run this code in the pop up: "bash addCopyingStatement ' package_name '  "' institution_name '"']);
+% 	disp(['in this directory          : ' out_dir])
+% 	uiwait(msgbox(['Run this code in the pop up: "bash addCopyingStatement ' package_name ' "' institution_name '"']));
+% 	% system(['bash addCopyingStatement ' package_name ' ' institution_name]);
+% end
+
+% %{
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % Gather Test image
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % move test image to build package dir.
+% test_img = '/home2/avega/Documents/test_0001.tif';
+% copyfile(test_img, out_dir); 
+% [a, b, c] = fileparts(test_img);
+% test_img_name = [b c];
+% %}
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % Zip up package
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % ---> Basically Zip up this directory now for the package.
+% cd(build_dir);
+% if exist(zip_file, 'file')
+% 	zip_file = [zip_file(1:end-4) '1_.zip'];
+% end
+% zip(zip_file, out_dir);
+% msgbox(['Package zipped here ' zip_file])
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % Test the package build works in tmp dir!
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% choice = questdlg(['Run test on zip package?'],'Question..','Yes','No','Yes');
+
+% if strcmp(choice, 'Yes')
+% 	java_tmpdir = char(java.lang.System.getProperty('java.io.tmpdir'));
+% 	uuid = java.util.UUID.randomUUID();
+% 	uuid = char(uuid.toString());
+% 	tmpdir = fullfile(java_tmpdir, uuid);
+% 	mkdir(tmpdir);
+% 	% Unzip test imagesa
+% 	unzip(zip_file, tmpdir);
+% 	% Run
+% 	restoredefaultpath;  % Clears out repo paths
+% 	addpath(genpath(tmpdir)); % add the build package path
+% 	cd([tmpdir filesep package_name]);
+% 	scriptGeneralColocalization
+%     scriptMultiChannelColocalization
+%     cd(start_dir);
+% 	restoredefaultpath;
+% 	try
+%         rmdir(tmpdir, 's');
+%     	pause(.25);
+%     	rehash(); % weird issue with matlab rmdir?
+%     catch ee
+%          disp(['unable to delete tmp dir' tmpdir]);
+%     end
+% end
+
+% addpath(start_paths);
+% cd(start_dir);
---
+++ endocytosis/build/scriptCMEautotest.m
@@ -0,0 +1,34 @@
+
+if strcmp(computer('arch'),'win64')
+	data_root = 'C:\Users\Andrew\Data\raw\CME\xinxin\auto_test\';
+else
+	data_root = '/work/bioinformatics/s170480/Data/CME/xinxin/auto_test/';
+end	
+
+cmeData_dir = fullfile(data_root, ['single_channel' filesep 'Zuzana_ARPE_CLC_egfp_07032014_control']);
+epiTIRFData_dir = fullfile(data_root, ['epi_tirf' filesep 'Zuzana_arpe_clc_egfp_14082015_control']);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+data = loadConditionData(cmeData_dir, {''}, {'EGFP'});
+cmeAnalysis(data);
+ccpSorter(data);
+cmeDataViewer(data);
+analyzeBleaching(data);
+lftRes = runLifetimeAnalysis(data);
+track = loadTracks(data(1));
+
+plotLifetimes(lftRes);
+plotTrack(data(1), track(1));
+plotIntensityCohorts(data);
+plotInitiationDensity(lftRes);
+plotCellArea(lftRes);
+plotInitIntensityVsLifetime(data);
+plotIntensityDistributions(data);
+plotLifetimeComparison(lftRes);
+plotLifetimes(lftRes);
+plotMaxIntensityDistribution(data);
+
+sortTiffStacks; % ???
+data = loadConditionData(epiTIRFData_dir, {'TIRF', 'EPI'}, {'EGFP', 'EGFP'});
+epiTIRFAnalysis(data);
