e480798792797985b6b7f9a91d2a6daa84eb84a5
proudot <philippe.roudot@gmail.com>
2016-12-07 18:35:30 -0600

    +TIP3D: MT capture and bundle detection code (in dev) Isotropised kinetochore tracking amiraWriteTracks: allowing cumulative display only Misc. stuff
    Former-commit-id: 95e2b7da395999dac6ff06ebb6c1b1b643e462d5

--- mtTracker/EB3-3D/packagePlusTipTracker3D/scriptBased/user-script/scriptKinetochoreTracking.m
+++ mtTracker/EB3-3D/packagePlusTipTracker3D/scriptBased/user-script/scriptKinetochoreTracking.m
@@ -4,8 +4,8 @@
 %% Loading MovieList file
 % MovieList FileName (the combination of condition you want to compare). 
 movieListFileNames={'prometaphaseCells.mat'};
-MLPath='/project/bioinformatics/Danuser_lab/externBetzig/packaging/alpha/plusTipTracker3D-alpha-1/data/A1_HeLa_Cells_EB1/prometaphase/analysis/';
-movieListFileNames={'prometaphaseCells.mat'};
+MLPath='/project/bioinformatics/Danuser_lab/externBetzig/analysis/proudot/anaProject/sphericalProjection/smallData/prometaphase/analysis/';
+movieListFileNames={'Cell2.mat'};
 % Build the array of MovieList (automatic)
 aMovieListArray=cellfun(@(x) MovieList.loadMatFile([MLPath filesep x]), movieListFileNames,'unif',0);
 aMovieListArray=aMovieListArray{:};
@@ -16,11 +16,11 @@ aMovieListArray=aMovieListArray{:};
 runDetection=1;     
 runSpindleRef=1;
 runTracking=1; 
-printAmiraFile=0;      
+     
  
 %% Optional output
-printAmiraFile=0;      
-printDetectionMask=0;
+printAmiraFile=1;      
+printDetectionMask=1;
 
 %% Parameter
 detectionMethod='pointSourceAutoSigmaFit';
@@ -35,13 +35,17 @@ for k=1:length(aMovieListArray)
     for i=1:length(ML.movieDataFile_)
         MD=MovieData.loadMatFile(ML.movieDataFile_{i});
         disp(['Processing movie: ' MD.getFullPath]);
-        dataAnisotropy=[MD.pixelSize_ MD.pixelSize_ MD.pixelSizeZ_];
+        dataAnisotropy=[MD.pixelSize_ MD.pixelSize_ MD.pixelSize_];
         
         %% detect
         outputDirDetect=[MD.outputDirectory_ filesep 'Kin'  filesep 'detection' filesep];mkdir(outputDirDetect);
         if(runDetection)
             [detectionsLabRef,lab]=detectEB3(MD,'type',detectionMethod,'showAll',false,'channel',2)
+            for fIdx=1:length(detectionsLabRef)
+                detectionsLabRef(fIdx).zCoord(:,1)=detectionsLabRef(fIdx).zCoord(:,1)*MD.pixelSizeZ_/MD.pixelSize_;
+            end
             save([outputDirDetect filesep 'detectionLabRef.mat'],'detectionsLabRef');
+
             if(printAmiraFile)
                 amiraWriteMovieInfo([outputDirDetect filesep 'Amira' filesep 'amiraVertexLabRef' filesep  'detectionsLabRef.am'], detectionsLabRef,'scales',dataAnisotropy);
             end
@@ -86,7 +90,7 @@ for k=1:length(aMovieListArray)
         
         
         
-        %% Pole detection
+        %% Set detection in the spindle referential
         outputDirPoleDetect=[MD.outputDirectory_ filesep 'EB3' filesep 'poles' filesep EB3poleDetectionMethod filesep];mkdir(outputDirPoleDetect);
         if(runSpindleRef)
             poleData=load([outputDirPoleDetect filesep 'poleDetection.mat']);
@@ -130,11 +134,14 @@ for k=1:length(aMovieListArray)
             
             %% Convert tracks final in a user-friendlier format
             %  (using the registered Frame of Ref and the lab FoR)
-            tracksLabRef=TracksHandle(tracksFinal,detectionsLabRef);
+            tracksFinalStripped=rmfield(tracksFinal,'tracksCoordAmpCG');
+            tracksLabRef=TracksHandle(tracksFinalStripped,detectionsLabRef);
             save([outputDirTrack filesep  'tracksLabRef.mat'],'tracksLabRef')
-            tracksStageRef=TracksHandle(tracksFinal,detectionsStageRef);
+            tracksFinalStripped=rmfield(tracksFinal,'tracksCoordAmpCG');
+            tracksStageRef=TracksHandle(tracksFinalStripped,detectionsStageRef);
             save([outputDirTrack filesep 'tracksStageRef.mat'],'tracksStageRef')
-            tracksSpindleRef=TracksHandle(tracksFinal,detectionsSpindleRef);
+            tracksFinalStripped=rmfield(tracksFinal,'tracksCoordAmpCG');
+            tracksSpindleRef=TracksHandle(tracksFinalStripped,detectionsSpindleRef);
             save([outputDirTrack filesep  'tracksSpindleRef.mat'],'tracksSpindleRef')
         else
             if (exist([outputDirTrack  filesep 'tracksLabRef.mat'], 'file') == 2)
@@ -149,16 +156,16 @@ for k=1:length(aMovieListArray)
         if(printAmiraFile)
             
             %% Tracks in the lab FoR.
-            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep 'AmiraTrackLabRef' filesep 'tracksLabRef.am'],tracksLabRef,'MD',MD);
-            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep  'AmiraTrackLabRef20plus' filesep 'trackLabRef20plus.am'],tracksLabRef([tracksLabRef.lifetime]>20),'MD',MD);
+            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep 'AmiraTrackLabRef' filesep 'tracksLabRef.am'],tracksLabRef,'MD',MD,'scales',dataAnisotropy);
+            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep  'AmiraTrackLabRef20plus' filesep 'trackLabRef20plus.am'],tracksLabRef([tracksLabRef.lifetime]>20),'MD',MD,'scales',dataAnisotropy);
             
             %% Tracks in the Stage FoR.
-            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep  'AmiraTracksStageRef' filesep 'tracksLabRef.am'],tracksStageRef,'MD',MD);
-            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep  'AmiraTrackStageRef20plus' filesep 'trackLabRef20plus.am'],tracksStageRef([tracksStageRef.lifetime]>20),'MD',MD);
+            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep  'AmiraTracksStageRef' filesep 'tracksLabRef.am'],tracksStageRef,'MD',MD,'scales',dataAnisotropy);
+            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep  'AmiraTrackStageRef20plus' filesep 'trackLabRef20plus.am'],tracksStageRef([tracksStageRef.lifetime]>20),'MD',MD,'scales',dataAnisotropy);
             
             %% Tracks in the spindle FoR.
-            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep  'AmiraTrackSpindleRef' filesep 'trackSpindleRef.am'],tracksSpindleRef,'MD',MD);
-            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep  'AmiraTrackSpindleRef20plus' filesep 'trackSpindleRef20plus.am'],tracksSpindleRef([tracksSpindleRef.lifetime]>20),'MD',MD);
+            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep  'AmiraTrackSpindleRef' filesep 'trackSpindleRef.am'],tracksSpindleRef,'MD',MD,'scales',dataAnisotropy);
+            amiraWriteTracks([outputDirTrack filesep 'Amira' filesep  'AmiraTrackSpindleRef20plus' filesep 'trackSpindleRef20plus.am'],tracksSpindleRef([tracksSpindleRef.lifetime]>20),'MD',MD,'scales',dataAnisotropy);
             
         end
         
