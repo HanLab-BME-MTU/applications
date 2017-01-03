9d08232ad5ae6654224b060ac11b6f6c40b63d9b
Andrew R. Jamieson <andrew.jamieson@UTSouthwestern.edu>
2017-01-03 16:25:11 -0600

    junk update
    Former-commit-id: c4ec1511c32e85515d8ad06791ffebdc73b4b042

--- 2dActionRecognition/explore/cellXplore.m
+++ 2dActionRecognition/explore/cellXplore.m
@@ -22,7 +22,7 @@ function varargout = cellXplore(varargin)
 
 % Edit the above text to modify the response to help cellXplore
 
-% Last Modified by GUIDE v2.5 21-Dec-2016 18:17:51
+% Last Modified by GUIDE v2.5 22-Dec-2016 13:17:53
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -140,6 +140,59 @@ function popupmenu2_Callback(hObject, eventdata, handles)
 % --- Executes during object creation, after setting all properties.
 function popupmenu2_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to popupmenu2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on selection change in celltypePop.
+function celltypePop_Callback(hObject, eventdata, handles)
+% hObject    handle to celltypePop (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns celltypePop contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from celltypePop
+
+
+% --- Executes during object creation, after setting all properties.
+function celltypePop_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to celltypePop (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in popupmenu4.
+function popupmenu4_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+pop
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu4 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu4
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu4 (see GCBO)pop
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
