7b17955c800be9ae3d366b778482c72f8566fbbe
proudot <philippe.roudot@gmail.com>
2017-02-03 16:21:31 -0600

    +TipTracker: loading data and some more optimization
    Former-commit-id: 1768a3213870049f138f3950bb704ae33ecc5a6e

--- mtTracker/EB3-3D/bundles/loadAngleSwipingData.m
+++ mtTracker/EB3-3D/bundles/loadAngleSwipingData.m
@@ -9,19 +9,23 @@ ip.addParameter('process',[]);
 ip.parse(MD,varargin{:});
 p=ip.Results;
 
-fiberCell={};
-randFiberCell={};
+angleRange=0.02:0.01:0.1;
 
-for angleDist=0.02:0.01:0.1
-       
+fiberCell=cell(1,length(angleRange));
+randFiberCell=cell(1,length(angleRange));
+
+parfor angleDistIdx=1:length(angleRange)
+
+angleDist=angleRange(angleDistIdx);
+
 %% Estimate bundle in KinPole axis
 outputDirBundle=[MD.outputDirectory_ filesep 'Kin' filesep 'bundles' filesep ['inlier_angle_' num2str(angleDist)]];
 tmp=load([outputDirBundle filesep 'kin-MT-bundle.mat']);
-fiberCell=[fiberCell {tmp.kinTracks}];
+fiberCell{angleDistIdx}=tmp.kinTracks;
 
 outputDirBundle=[MD.outputDirectory_ filesep 'Kin' filesep 'bundles' filesep ['randInlier_angle_' num2str(angleDist)]];
 tmp=load([outputDirBundle filesep 'kin-MT-bundle.mat']);
-randFiberCell=[randFiberCell {randomFiber}];
+randFiberCell{angleDistIdx}=tmp.kinTracks;
 end
 
 displayAngleSwipeResults(fiberCell,randFiberCell)
