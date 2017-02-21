d6877987ba18fa6f149f15ac0959e2729f6199bc
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2017-02-20 23:41:41 -0600

    lamins.gui.exploreOrientationScaleWidth: Polish with K selection, color selection
    Former-commit-id: 6556fa5657f42a3bc4f51e7470cc1b79db3b970f

--- lamins/+lamins/+gui/exploreOrientationScaleWidth.m
+++ lamins/+lamins/+gui/exploreOrientationScaleWidth.m
@@ -5,8 +5,8 @@ function [ h_img_fig, h_scale_fig, h_width_fig ] = exploreOrientationScaleWidth(
 % I - image to display upon which to select a pixel
 %     (Could be the actual image analyzed or any image the same size as the
 %     original including the Response, NMS, NLMS)
-% Rscale - OrientationSpaceResponse at different scales
-% Rwidth - OrientationScaleSpaceFilter response at different widths
+% Rscale - (optional) OrientationSpaceResponse at different scales
+% Rwidth - (optional) OrientationScaleSpaceFilter response at different widths
 % theta  - (optional) orientation to automatically steer to when moving the
 %           pointer
 %
@@ -14,21 +14,42 @@ function [ h_img_fig, h_scale_fig, h_width_fig ] = exploreOrientationScaleWidth(
 % h_img_fig   - handle to the image figure
 % h_scale_fig - handle to the orientation/scale figure
 % h_width_fig - handle to the orientation/width figure
+% 
+% Basic Usage
+% lamins.gui.exploreOrientationScaleWidth(I);
 %
 % Example Usage
-% F = OrientationSpaceFilter.constructByRadialOrder(1/2/pi./chebpts(5,[1 8]),1,8);
-% FF = OrientationScaleSpaceFilter.constructByRadialOrder(1/2/pi./chebpts(5,[1 8]),1,8);
+% F = OrientationSpaceFilter.constructByRadialOrder(1/2/pi./chebpts(5,[1 4]),1,8);
+% FF = OrientationScaleSpaceFilter.constructByRadialOrder(1/2/pi./chebpts(5,[1 4]),1,8);
 % R = F*I;
 % RR = FF*I;
-% lamins.gui.exploreOrientationScaleWidth(I,R,RR)
+% maxima = R(2).getRidgeOrientationLocalMaxima;
+% lamins.gui.exploreOrientationScaleWidth(I,R,RR,maxima)
 %
 % Mark Kittisopikul, Feburary 2017 (earlier actually, but major revisions)
 % Jaqaman Lab
 % UT Southwestern
 
-if(nargin < 4)
+if(nargin < 2)
+    % Do the filtering for the user, DEMO mode
+    gcp;
+    F = OrientationSpaceFilter.constructByRadialOrder(1/2/pi./chebpts(5,[1 4]),1,8);
+    FF = OrientationScaleSpaceFilter.constructByRadialOrder(1/2/pi./chebpts(5,[1 4]),1,8);
+    Rscale = F*I;
+    Rwidth = FF*I;
+    theta = Rscale(2).getRidgeOrientationLocalMaxima;
+elseif(nargin < 3)
+    % Just do the width filter
+    F = [Rscale.filter];
+    FF = OrientationScaleSpaceFilter.constructByRadialOrder([F.f_c],(F(1).f_c/F(1).b_f).^2,F(1).K);
+    Rwidth = FF*I;
+elseif(nargin < 4)
+    % If theta is empty, do not snap to the orientation
     theta = [];
 end
+
+Rscale_orig = Rscale;
+Rwidth_orig = Rwidth;
 
 % Setup filter image overlay
 % fparams.Kf = 1;
@@ -38,12 +59,13 @@ fparams.K = Rscale(1).filter.K;
 fparams.sz = 101;
 % Half filter image size
 fparams.hsz = floor(fparams.sz/2);
-% Filter color
-fparams.color = [1 0 1];
+% Filter color, if empty, use impoint color
+% fparams.color = [1 0 1];
+fparams.color = [];
 % Threshold for complete transparency
 fparams.thresh = 0.1;
 % Max alpha value. 1.0 = Opaque, 0.0 = Transparent
-fparams.alpha = 0.5;
+fparams.alpha = 0.7;
 
 % filtim = real(fftshift(fft2(real(orientationSpace.kernel(1/2/pi/2,1/2/pi/2,fparams.K,0,fparams.sz)))));
 % filtmask = mat2gray(abs(filtim));
@@ -60,11 +82,23 @@ h_filt = imshow(zeros(fparams.sz));
 hold off;
 % h_img_pt = impoint;
 
+menu = uicontextmenu(h_img_fig);
+m_d = uimenu(menu,'Label',['K = ' num2str(Rscale(1).filter.K)],'Checked','on','Callback',@(~,~) selectK(Rscale(1).filter.K));
+m_5 = uimenu(menu,'Label','K = 5','Checked','off','Callback',@(~,~) selectK(5));
+m_3  = uimenu(menu,'Label','K = 3','Checked','off','Callback',@(~,~) selectK(3));
+% m_0 = uimenu(menu,'Label','K = 0','Checked','off','Callback',@(~,~) selectK(0));
+% m_neg = uimenu(menu,'Label','K = -0.5','Checked','off','Callback',@(~,~) selectK(-0.5));
+m_custom = uimenu(menu,'Label','K = ?','Checked','off','Callback',@promptK);
+% m_set = [m_d m_5 m_3 m_0 m_neg m_custom];
+m_set = [m_d m_5 m_3 m_custom];
+h_img_fig.UIContextMenu = menu;
+h_img.UIContextMenu = menu;
+
 h_scale_fig = figure;
 
 h_width_fig = figure;
 
-h_quiver = [];
+% h_quiver = [];
 
 
    
@@ -74,6 +108,7 @@ assertEqual(scaleWidthRange,1/2/pi./[Rwidth(1).filter.f_c Rwidth(end).filter.f_c
 
 figure(h_img_fig);
 h_img_pt = impoint(gca,size(I,1)/2,size(I,2)/2);
+h_img_pt.setColor('m');
 
 
     % Setup axes for orientation/scale and orientation/width filters
@@ -122,7 +157,7 @@ h_img_pt = impoint(gca,size(I,1)/2,size(I,2)/2);
         % Setup Scale Figure
         figure(h_scale_fig);
         h_scale = imagesc(B_scale);
-        h_scale_title = title(sprintf('Scale Response at x = %d, y = %d',h_img_pt_pos(1),h_img_pt_pos(2)));
+        h_scale_title = title(sprintf('Scale Response at x = %d, y = %d, K = %d',h_img_pt_pos(1),h_img_pt_pos(2),fparams.K));
         hold on;
         h_scale_scale_roots = line( ...
             'XData',repmat(scaleWidthRangePoints(:),size(B_scale_roots,2),1), ...
@@ -141,7 +176,7 @@ h_img_pt = impoint(gca,size(I,1)/2,size(I,2)/2);
         % Setup Width Figure
         figure(h_width_fig);
         h_width = imagesc(B_width);
-        h_width_title = title(sprintf('Width Response at x = %d, y = %d',h_img_pt_pos(1),h_img_pt_pos(2)));
+        h_width_title = title(sprintf('Width Response at x = %d, y = %d, K = %d',h_img_pt_pos(1),h_img_pt_pos(2),fparams.K));
         hold on;
         xlabel('Width');
         h_width_scale_roots = line( ...
@@ -176,7 +211,7 @@ h_img_pt = impoint(gca,size(I,1)/2,size(I,2)/2);
         B_width_roots = roots(diff(B_width{degreePoints,:})).';
         
         % Update Orientation/Scale at pixel of interest
-        h_scale_title.String = sprintf('Scale Response at x = %d, y = %d',h_img_pt_pos(1),h_img_pt_pos(2));
+        h_scale_title.String = sprintf('Scale Response at x = %d, y = %d, K = %d',h_img_pt_pos(1),h_img_pt_pos(2),fparams.K);
         h_scale.CData = double(B_scale.sample(360,101));
         h_scale_scale_roots.XData = repmat(scaleWidthRangePoints(:),size(B_scale_roots,2),1);
         h_scale_scale_roots.YData = B_scale_roots(:);
@@ -184,7 +219,7 @@ h_img_pt = impoint(gca,size(I,1)/2,size(I,2)/2);
         h_scale_width_roots.YData = repmat(degreePoints(:),size(B_width_roots,2),1);
         
         % Update Orientation/Width at pixel of interest
-        h_width_title.String = sprintf('Width Response at x = %d, y = %d',h_img_pt_pos(1),h_img_pt_pos(2));
+        h_width_title.String = sprintf('Width Response at x = %d, y = %d, K = %d',h_img_pt_pos(1),h_img_pt_pos(2),fparams.K);
         h_width.CData = double(B_width.sample(360,101));
         h_width_scale_roots.XData = repmat(scaleWidthRangePoints(:),size(B_scale_roots,2),1);
         h_width_scale_roots.YData = B_scale_roots(:);
@@ -220,7 +255,11 @@ h_img_pt = impoint(gca,size(I,1)/2,size(I,2)/2);
         filtim = real(fftshift(fft2(real(orientationSpace.kernel(1/2/pi/h_scale_width_pts_pos(1),1/2/pi/h_scale_width_pts_pos(1),fparams.K,h_scale_width_pts_pos(2)/180*pi,fparams.sz)))));
         filtmask = mat2gray(abs(filtim));
         filtim = mat2gray(filtim);
-        filtim = bsxfun(@times,filtim,shiftdim(fparams.color,-1));
+        if(isempty(fparams.color))
+            filtim = bsxfun(@times,filtim,shiftdim(getColor(h_img_pt),-1));
+        else
+            filtim = bsxfun(@times,filtim,shiftdim(fparams.color,-1));
+        end
 %         filtim(:,:,3) = 0;
         h_filt.CData = filtim;
         h_filt.AlphaData = double(filtmask > fparams.thresh)*fparams.alpha;
@@ -236,6 +275,44 @@ h_img_pt = impoint(gca,size(I,1)/2,size(I,2)/2);
 %         h_quiver = quiver(x-u/2*scale,y-v/2*scale,u,v,scale);
 %         set(h_quiver,'LineWidth',h_scale_width_pts_pos(1));
 %         hold off;
+    end
+
+    function selectK(K)
+        assert(K <= Rscale_orig(1).filter.K,'K must be less than or equal to the original angular order');
+        fparams.K = K;
+        set(m_set,'Checked','off');
+        switch(K)
+            case Rscale_orig(1).filter.K
+                m_d.Checked = 'on';
+            case 5
+                m_5.Checked = 'on';
+            case 3
+                m_3.Checked = 'on';
+%             case 0
+%                 m_0.Checked = 'on';
+%             case -0.5
+%                 m_neg.Checked = 'on';
+            otherwise
+                m_custom.Checked = 'on';
+        end
+                
+        if(K == Rscale_orig(1).filter.K)
+            Rscale = Rscale_orig;
+            Rwidth = Rwidth_orig;
+        else
+            Rscale = Rscale_orig.getResponseAtOrderFT(K);
+            Rwidth = Rwidth_orig.getResponseAtOrderFT(K);
+        end
+        drawScaleWidth();
+    end
+    function promptK(~,~)
+        answer = inputdlg('Enter new K value:');
+        answer = str2double(answer);
+        if(~isnan(answer))
+            selectK(answer);
+        else
+            warning('Could not parse K');
+        end
     end
 
 end
