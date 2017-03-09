f1ba0a83f155643654bdfd536d461f84cab4754f
proudot <philippe.roudot@utsouthwestern.edu>
2017-03-08 23:30:53 -0600

    normalize cumul plot
    Former-commit-id: 63c6f68e029421801fa1780bcac5d9c2288a943d

--- mtTracker/EB3-3D/bundles/printMTTipsPoleRef.m
+++ mtTracker/EB3-3D/bundles/printMTTipsPoleRef.m
@@ -4,6 +4,7 @@ ip.CaseSensitive = false;
 ip.KeepUnmatched = true;
 ip.addParameter('EB3ColorAfter',ones(1,length(EB3Tracks)));
 ip.addParameter('valueRange',[]);
+ip.addParameter('normAxis',false);
 ip.addParameter('handle',[]);
 
 ip.addParameter('colormap',[], @islogical);
@@ -36,15 +37,22 @@ if(isempty(p.handle))
 [handles,~,fhandle]=setupFigure(1,1,'AxesWidth',8,'AxesHeight',4,'DisplayMode', 'print');
 end
 hold(handles(1),'on');
-
+if(~p.normAxis)
 scatter(handles,kinTrack.z,kinTrack.x,'r');
+else
+    scatter(handles,1,kinTrack.x,'r');
+end
 scatter(handles,0,0,'g');
 
 X=nan(1,length(EB3Tracks));
 Y=nan(1,length(EB3Tracks));
 for mIdx=1:length(EB3Tracks)
     mt=EB3Tracks(mIdx);
-    X(mIdx)=mt.z(MTTipsIdx(mIdx));
+    if(~p.normAxis)
+        X(mIdx)=mt.z(MTTipsIdx(mIdx));
+    else
+        X(mIdx)=mt.z(MTTipsIdx(mIdx))/kinTrack.z(mt.f(MTTipsIdx(mIdx))==kinTrack.f);
+    end
     Y(mIdx)=mt.x(MTTipsIdx(mIdx));
     %Project on the plan defined by the poleKin axis and the interpolar
     %axis.
