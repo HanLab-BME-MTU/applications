ae86dd015ef3afa8ebd98c7fe14f87fc4e5a1618
proudot <philippe.roudot@utsouthwestern.edu>
2017-03-21 11:49:42 -0500

    External Process on legacy bundling function and start projection
    Former-commit-id: a49cdd86dbb940a7869f4f0126028c9885c7f7bd

--- mtTracker/EB3-3D/bundles/MTBundling.m
+++ mtTracker/EB3-3D/bundles/MTBundling.m
@@ -6,7 +6,9 @@ ip.addRequired('MD',@(MD) isa(MD,'MovieData'));
 ip.addOptional('kinTracksWithSpindle',[]);
 ip.addOptional('EB3TracksWithSpindle',[]);
 ip.addOptional('randKinTracksWithSpindle',[]);
+ip.addOptional('processSpindleRef',[]);
 ip.addParameter('printAll',false, @islogical);
+ip.addOptional('processRandKin',[]);
 ip.addParameter('kinRange',[],@isnumeric);
 %ip.addParameter('kinRange',[19 46 156],@isnumeric);
 ip.addParameter('cutoffs',250, @isnumeric);
@@ -21,14 +23,12 @@ printAll=p.printAll;
 
 %%
 %% Loading kin and EB3
-poleRefProcessIdx=(cellfun(@(p) strcmp(p.name_,'poleRef'),MD.processes_));
-if(any(poleRefProcessIdx)&&isempty(p.kinTracksWithSpindle))
+if(~isempty(p.processSpindleRef)&&isempty(p.kinTracksWithSpindle))
     disp('reading process');tic;
-    poleRefProcess=MD.getProcess(find(poleRefProcessIdx,1,'last'));
+    poleRefProcess=p.processSpindleRef;% MD.getProcess(find(poleRefProcessIdx,1,'last'));
     EB3Tracks=load(poleRefProcess.outFilePaths_{1}); EB3Tracks=EB3Tracks.EB3Tracks;
-    EB3TracksInliers=load(poleRefProcess.outFilePaths_{3}); EB3TracksInliers=EB3TracksInliers.EB3TracksInliers;
-    %%
     kinTracks=load(poleRefProcess.outFilePaths_{2}); kinTracks=kinTracks.kinTracks;
+    EB3TracksInliers=load(poleRefProcess.outFilePaths_{3}); EB3TracksInliers=EB3TracksInliers.EB3TracksInliers;
     kinTracksInliers=load(poleRefProcess.outFilePaths_{4}); kinTracksInliers=kinTracksInliers.kinTracksInliers;
     toc
 else
@@ -40,26 +40,34 @@ else
     kinTracksInliers=kinTracks(inliersKin);
 end
 
-%% Loading rand kin 
+%% Loading rand kin
 if(isempty(kinRange))
     kinRange=1:length(kinTracksInliers);
 end
 
+
 %% Functionalize and processize
-randKinProcIdx=cellfun(@(p) strcmp(p.name_,'randKin'),MD.processes_);
-if(any(randKinProcIdx)&&isempty(p.randKinTracksWithSpindle))
-    randKinProc=MD.getProcess(find(randKinProcIdx,1,'last'));
-    tmp=load(randKinProc.outFilePaths_{1});
+if(~isempty(p.processRandKin)&&isempty(p.randKinTracksWithSpindle))
+    tmp=load(p.processRandKin.outFilePaths_{1});
     randKinTracks=tmp.randKinTracks;
-    inliersKin=logical(arrayfun(@(k) k.inliers(1),kinTracks));
-    randKinTracksInliers=randKinTracks(inliersKin);
 else
     randKinTracks=p.randKinTracksWithSpindle;
-    inliersKin=logical(arrayfun(@(k) k.inliers(1),kinTracks));
-    randKinTracksInliers=randKinTracks(inliersKin);
 end
-
 %%
+
+%% Functionalize and processize
+% randKinProcIdx=cellfun(@(p) strcmp(p.name_,'randKin'),MD.processes_);
+% if(any(randKinProcIdx)&&isempty(p.randKinTracksWithSpindle))
+%     randKinProc=MD.getProcess(find(randKinProcIdx,1,'last'));
+%     tmp=load(randKinProc.outFilePaths_{1});
+%     randKinTracks=tmp.randKinTracks;
+%     inliersKin=logical(arrayfun(@(k) k.inliers(1),kinTracks));
+%     randKinTracksInliers=randKinTracks(inliersKin);
+% else
+%     randKinTracks=p.randKinTracksWithSpindle;
+%     inliersKin=logical(arrayfun(@(k) k.inliers(1),kinTracks));
+%     randKinTracksInliers=randKinTracks(inliersKin);
+% edn
 
 %%  Plot randomize
 if(p.printAll)
@@ -87,20 +95,19 @@ parfor kIdx=1:length(Inlier)
 end
 
 %%
-captureDetection(MD,'kinTracks',randKinTracksInliers(kinRange),'EB3tracks',EB3TracksInliers,'name','inliersRandom');
-%RandomInlier=detectMTAlignment(MD,'kinCapture',randKinTracksInliers(kinRange),'testKinIdx',[],'name','inliersRandom','printAll',false);
-RandomInlier=detectMTAlignment(MD,'kinCapture',randKinTracksInliers(kinRange),'testKinIdx',[],'bundlierPole',true,'name','inliersRandomBundlierPole','printAll',false);
+if(~isempty(randKinTracks))
+  captureDetection(MD,'kinTracks',randKinTracksInliers(kinRange),'EB3tracks',EB3TracksInliers,'name','inliersRandom');
+  %RandomInlier=detectMTAlignment(MD,'kinCapture',randKinTracksInliers(kinRange),'testKinIdx',[],'name','inliersRandom','printAll',false);
+  RandomInlier=detectMTAlignment(MD,'kinCapture',randKinTracksInliers(kinRange),'testKinIdx',[],'bundlierPole',true,'name','inliersRandomBundlierPole','printAll',false);
 
-%% For each kinetochore, plot an Amira file with attached mt
-outputDirAmira=[outputDirBundle filesep 'Amira_random' filesep];
-parfor kIdx=1:length(RandomInlier)
+  %% For each kinetochore, plot an Amira file with attached mt
+  outputDirAmira=[outputDirBundle filesep 'Amira_random' filesep];
+  parfor kIdx=1:length(RandomInlier)
     kinTrack=RandomInlier(kIdx);
     trackSet=[kinTrack; kinTrack.catchingMT];
     trackType=[1; zeros(length(kinTrack.catchingMT),1)];
     bundleInfo=[0 kinTrack.fiber+1];
     amiraWriteTracks([outputDirAmira filesep 'ki/n_' num2str(kIdx) '.am'],trackSet,'cumulativeOnly',true,'edgeProp',{{'kinEB3',trackType},{'bundle',bundleInfo}})
+  end
+  bundleStatistics(MD,'kinBundle',{Inlier,RandomInlier},'kinBundleName',{'Inlier','RandomInlier'});
 end
-bundleStatistics(MD,'kinBundle',{Inlier,RandomInlier},'kinBundleName',{'Inlier','RandomInlier'});
-
-
-
