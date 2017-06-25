362d8b96d142eaf1a9e3e87d6b1dda9ac7b66739
proudot <philippe.roudot@utsouthwestern.edu>
2017-06-25 15:20:55 -0500

    Process mgt and Plotting bundle score histogram
    Former-commit-id: 7a8b0acc89dbff877aa34ab22378f4fbd3ba24f4

---
+++ mtTracker/EB3-3D/bundles/conditionBundleStatistics.m
@@ -0,0 +1,32 @@
+function conditionBundleStatistics(scoringProcessCell)
+% load scoring data associated to cells. A Cell of process list describes the conditions
+
+if(~iscell(scoringProcessCell))
+    scoringProcessCell={scoringProcessCell};
+end
+
+% load score set in cells of cells
+scoreCell=cell(1,length(scoringProcessCell));
+for cIdx=1:length(scoringProcessCell)
+	condScoreCell=cell(1,length(scoringProcessCell{cIdx}));
+	for pIdx=1:length(scoringProcessCell{cIdx})
+		tmp=load(scoringProcessCell{cIdx}(pIdx).outFilePaths_{1});
+		condScoreCell{pIdx}=tmp.zScores;
+	end
+	scoreCell{cIdx}=condScoreCell;
+end
+
+c={'r','b','g','y','k'};
+
+% Plot zScores distribution
+scoresBin=-10:0.2:10;
+histCell=cell(1,length(scoringProcessCell));
+hold on;
+for cIdx=1:length(scoringProcessCell)
+	for pIdx=1:length(scoringProcessCell{cIdx})
+		counts=histcounts(scoreCell{cIdx}{pIdx},scoresBin);
+		histCell{cIdx}=[histCell{cIdx}; counts];
+	end
+	plot(scoresBin(1:end-1),histCell{cIdx},[c{cIdx} '-']);
+end
+hold off;
--- mtTracker/EB3-3D/bundles/scriptStudy/batchFourProm.m
+++ mtTracker/EB3-3D/bundles/scriptStudy/batchFourProm.m
@@ -18,3 +18,16 @@ for i=1:ML4min.getSize()
     MD.setPackage(333,pack)
     MD.save();
 end
+
+%% Collect scoring process (process 5) and compare
+processCell=cell(1,2);
+MLs=[ML1min ML4min];
+for mIdx=1:lengths(MLs)
+    for i=1:MLs(mIdx).getSize()
+        MD=MLs(mIdx).getMovie(i);
+        processCell{mIdx}=[processCell{mIdx} MD.getPackage(333).getProcess(5)];
+    end
+end
+
+conditionBundleStatistics(processCell)
+
