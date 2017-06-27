5b2ad73327ab4d654984bf8cd1ca47e5e3505b38
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2017-06-27 18:38:23 -0500

    orientationSpace.paper: Improve figure functions for display
    Former-commit-id: 0d9b10a31d1092eb1bed6bb4339cdf69a3053ece

--- lamins/+orientationSpace/+paper/detectionFigure.m
+++ lamins/+orientationSpace/+paper/detectionFigure.m
@@ -5,22 +5,30 @@ angles = [
 angles2 = [angles(1:2:end-1) angles(1:2:end-1)+pi angles(end) angles(end)+pi];
 
 nAngles = 12;
+% nAngles = 360;
 
 inAngles = zeros(length(angles2),nAngles);
-outAngles = zeros(16,nAngles);
-outAngleValues = zeros(16,nAngles);
 
-for i = 1:12;
+F = OrientationSpaceFilter.constructByRadialOrder(1/2/pi./2,1,16,'none');
+
+
+outAngles = zeros(F.n-1,nAngles);
+outAngleValues = zeros(F.n-1,nAngles);
+
+for i = 1:nAngles;
     angles2(end-1:end) = angles2(end-1:end)+pi/nAngles;
     angles2 = wraparoundN(angles2,0,2*pi);
     inAngles(:,i) = angles2.';
-    [outAngles(:,i),~,outAngleValues(:,i)] = interpft_extrema(squeeze(real(R.a(51,51,:))));
     I = intersections.drawRadialLines(angles2);
     Ig = imgaussfilt(I,2,'FilterSize',25);
     R = F*Ig;
-    figure;
-    intersections.plotPolarOnIntersection(Ig,R.a(51,51,:));
-%     pause;
+    [outAngles(:,i),~,outAngleValues(:,i)] = interpft_extrema(squeeze(real(R.a(51,51,:))));
+%     hfig = figure;
+%     intersections.plotPolarOnIntersection(Ig,R.a(51,51,:));
+%     set(hfig,'Position',[1 2 1280 946]);
+%     savefig(hfig,['04_fig4_K' num2str(F.K) '_sparse' sprintf('%02d',i) '.fig']);
+%     drawnow;
+%     pause(0.001);
 end
 
 figure;
@@ -28,5 +36,5 @@ inAnglesPlot = wraparoundN(inAngles([1 2 5],:)+pi/2,0,pi);
 plot(inAnglesPlot.','k.');
 hold on;
 outAnglesPlot = outAngles;
-outAnglesPlot(outAngleValues < 0.08) = NaN;
+outAnglesPlot(outAngleValues < 0.001) = NaN;
 plot(outAnglesPlot.'/2,'ro');
