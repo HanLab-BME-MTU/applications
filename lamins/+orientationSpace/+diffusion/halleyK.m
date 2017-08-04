26f4a2270665697cb058ff8d4ea2b4e2bf451c47
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2017-08-04 10:07:50 -0500

    orientationSpace.diffusion: Update newtonK, add halleyK
    Former-commit-id: 19cb193bec58e84a90d9a88fc925955b08fdaec9

--- lamins/+orientationSpace/+diffusion/halleyK.m
+++ lamins/+orientationSpace/+diffusion/halleyK.m
@@ -1,4 +1,4 @@
-function [ K_out ] = newtonK( x, K, R, r, c)
+function [ K_out ] = halleyK( x, K, R, r, c)
 %NEWTONK Use Newton's method to find K where root exists
 %
 % INPUT
@@ -18,12 +18,19 @@ K = repmat(K,1,length(x));
 K_out = K;
 for i=1:10
     rhorho = R.getResponseAtOrderFTatPoint(r,c,K_out);
-    v = cell(1,3);
-    for j=[1 3];
+    vt = cell(1,5);
+    vk = cell(1,5);
+    for j=[1 3 5];
         rhorhod_hat = bsxfun(@times,fft(rhorho),(ifftshift(-8:8)*1i).'.^j);
-        v{j} = interpft1([0 2*pi],rhorhod_hat,x,'horner_freq');
+        n = (j-1)/2;
+        vt{j} = interpft1([0 2*pi],rhorhod_hat,x,'horner_freq').*D.^n;
     end;
-    K_out = K_out + v{1}./(v{3}.*D.*4./(2*K+1).^3);
+    dtdK = -4./(2*K+1).^3;
+    d2tdK2 = 24./(2*K+1).^4;
+    vk{1} = vt{1};
+    vk{3} = -vt{3}.*dtdK;
+    vk{5} =  vt{5}.*(dtdK).^2+vt{3}.*d2tdK2;
+    K_out = K_out + 2*vk{1}.*vk{3}./(2*vk{3}.^2-vk{1}.*vk{5});
 %     hold on; plot(K_out,x,'.')
 end
 
