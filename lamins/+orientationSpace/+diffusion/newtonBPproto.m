1dfb0216f9c943cb65a95e2b26f11ef7d66c04ff
Mark Kittisopikul <mark.kittisopikul@northwestern.edu>
2017-08-11 17:32:03 -0500

    newtonBPproto: Add some robustness, focus on second partial derivative
    Former-commit-id: 39e3ba5ff2bddd0fa14672d108213f91fad31e35

--- lamins/+orientationSpace/+diffusion/newtonBPproto.m
+++ lamins/+orientationSpace/+diffusion/newtonBPproto.m
@@ -16,15 +16,30 @@ dnK_dmn(:,:,1) = 1;
 
 last = Inf;
 
+D = 2*pi.^2;
+
 while(abs(dnK_dmn(:,:,1)) > 1e-6)
 
 xold = xg; Kold = Kg;
-[~,dtn_dnm,dnK_dmn] = orientationMaximaTimeDerivatives(R.getResponseAtOrderFTatPoint(coords.r(n),coords.c(n),Kg),Kg,3,xg,2*pi,false);
-if(abs(dnK_dmn(:,:,1)./dnK_dmn(:,:,2)) < 0.02 || abs(dnK_dmn(:,:,1)./dnK_dmn(:,:,2)) < 0.2 && abs(dnK_dmn(:,:,1)) < 10)
-    xg = xg-dnK_dmn(:,:,1)./dnK_dmn(:,:,2)
+% [~,dtn_dnm,dnK_dmn] = orientationMaximaTimeDerivatives(R.getResponseAtOrderFTatPoint(coords.r(n),coords.c(n),Kg),Kg,3,xg,2*pi,false);
+%  || abs(dnK_dmn(:,:,1)./dnK_dmn(:,:,2)) < 0.2 && abs(dnK_dmn(:,:,1)) < 10
+% if(abs(dnK_dmn(:,:,1)./dnK_dmn(:,:,2)) < 0.1)
+xgd = interpft1_derivatives(R.getResponseAtOrderFTatPoint(coords.r(n),coords.c(n),Kg),xg,2:4);
+d_p2rho_pm2_K = -D.*xgd(:,:,3-1).^2./xgd(:,:,2-1) + D.*xgd(:,:,4-1);
+d_p2rho_pm2_K = d_p2rho_pm2_K.*-4./(2*Kg+1).^3;
+if( xgd(:,:,2-1) ./ d_p2rho_pm2_K < 0.5 )
+    [~,dtn_dnm,dnK_dmn] = orientationMaximaTimeDerivatives(R.getResponseAtOrderFTatPoint(coords.r(n),coords.c(n),Kg),Kg,3,xg,2*pi,false);
+    xg = xg-dnK_dmn(:,:,1)./dnK_dmn(:,:,2);
+%     xg = xg-2*dnK_dmn(:,:,1).*dnK_dmn(:,:,2)./(2*dnK_dmn(:,:,2).^2-dnK_dmn(:,:,1).*dnK_dmn(:,:,3));
+%      2*v.*vd./(2*vd.^2-v.*vdd);
     test = linspace(xold,xg,100);
     plot(Kg+(test-xold)*dnK_dmn(:,:,1)+(test-xold).^2*dnK_dmn(:,:,2)/2+(test-xold).^3*dnK_dmn(:,:,3)/6,test,'--');
-    Kg = halleyK(xg,Kg+(xg-xold)*dnK_dmn(:,:,1)+(xg-xold).^2*dnK_dmn(:,:,2)/2+(xg-xold).^3*dnK_dmn(:,:,3)/6,R,coords.r(n),coords.c(n));
+    Kgpd = (xg-xold)*dnK_dmn(:,:,1)+(xg-xold).^2*dnK_dmn(:,:,2)/2+(xg-xold).^3*dnK_dmn(:,:,3)/6;
+    if(Kgpd < 0 && abs(Kgpd) < 1)
+        Kg = halleyK(xg,Kg+Kgpd,R,coords.r(n),coords.c(n));
+    else
+        Kg = halleyK(xg,Kg,R,coords.r(n),coords.c(n));
+    end
     plot(Kg,xg,'.');
     if(abs(last) < abs(dnK_dmn(:,:,1)))
         dnK_dmn(:,:,1)
@@ -34,11 +49,13 @@ if(abs(dnK_dmn(:,:,1)./dnK_dmn(:,:,2)) < 0.02 || abs(dnK_dmn(:,:,1)./dnK_dmn(:,:
         last = dnK_dmn(:,:,1);
     end
 else
-    dnm_dKn(:,:,1) = 1./dnK_dmn(:,:,1);
-    dnm_dKn(:,:,2) = -dnK_dmn(:,:,2).*dnm_dKn(:,:,1).^3;
-    dnm_dKn(:,:,3) = (-3*dnK_dmn(:,:,2).*dnm_dKn(:,:,2)-dnK_dmn(:,:,3).*dnK_dmn(:,:,1).^(-2)).*dnK_dmn(:,:,1).^(-2);
-    Kg = Kg - 0.5;
-    xg = halleyft( R.getResponseAtOrderFTatPoint(coords.r(n),coords.c(n),Kg), xg - 0.5./dnK_dmn(:,:,1),false,1);
+%     dnm_dKn(:,:,1) = 1./dnK_dmn(:,:,1);
+%     dnm_dKn(:,:,2) = -dnK_dmn(:,:,2).*dnm_dKn(:,:,1).^3;
+%     dnm_dKn(:,:,3) = (-3*dnK_dmn(:,:,2).*dnm_dKn(:,:,2)-dnK_dmn(:,:,3).*dnK_dmn(:,:,1).^(-2)).*dnK_dmn(:,:,1).^(-2);
+%     Kg = Kg - 0.5;
+%     xg = halleyft( R.getResponseAtOrderFTatPoint(coords.r(n),coords.c(n),Kg), xg - 0.5./dnK_dmn(:,:,1),false,1);
+    Kg = Kg - min(xgd(:,:,2-1) ./ d_p2rho_pm2_K,0.5);
+    xg = halleyft( R.getResponseAtOrderFTatPoint(coords.r(n),coords.c(n),Kg), xg,false,1);
     plot(Kg,xg,'.');
     last = Inf;
 end
