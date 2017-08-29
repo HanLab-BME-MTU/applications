ee57242ab52d69bfae415621055b52fd0cbad408
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2017-08-29 02:21:33 -0500

    orientationSpace.diffusion: Add termsToCode to generate derivative calculation code
    Former-commit-id: 9c1b52b63d00cb95f2ae2dd85d9cfe6ae75befb2

--- lamins/+orientationSpace/+diffusion/termToLatex.m
+++ lamins/+orientationSpace/+diffusion/termToLatex.m
@@ -6,6 +6,9 @@ import orientationSpace.diffusion.*;
 
 if(~isscalar(s))
     latex = arrayfun(@termToLatex,s(:),'UniformOutput',false);
+    if(nargout == 0)
+        disp(['  ' strjoin(latex,'\n+ ')]);
+    end
     return;
 end
 
@@ -13,12 +16,16 @@ latex = '';
 
 if(s.coeff ~= 1)
     latex = [latex num2str(s.coeff) ' '];
+else
+    latex = [latex '  '];
 end
 
 if(s.D == 1)
-    latex = [latex 'D '];
+    latex = [latex 'D   '];
 elseif(s.D > 1)
     latex = [latex 'D^' num2str(s.D) ' '];
+else
+    latex = [latex '    '];
 end
 
 for i=1:length(s.rhod)
@@ -37,6 +44,10 @@ for i=1:length(s.rhod)
     end
 end
 
+if(~isfield(s,'timed'))
+    s.timed = [];
+end
+
 for i=1:length(s.timed)
     if(i == 1)
         if(s.timed(i) == 1)
@@ -52,6 +63,30 @@ for i=1:length(s.timed)
         end
     end
 end
+
+if(~isfield(s,'thetad'))
+    s.thetad = [];
+end
+
+for i=1:length(s.thetad)
+    if(i == 1)
+        if(s.thetad(i) == 1)
+            latex = [latex sprintf('\\dv{\\theta}{t} ')];
+        elseif(s.thetad(i) > 1)
+            latex = [latex sprintf('\\left(\\dv{\\theta}{t}\\right)^%d ',s.thetad(i))];
+        end
+    else
+        if(s.thetad(i) == 1)
+            latex = [latex sprintf('\\dv[%d]{\\theta}{t} ',i)];
+        elseif(s.thetad(i) > 1)
+            latex = [latex sprintf('\\left(\\dv[%d]{\\theta}{t}\\right)^%d ',i,s.thetad(i))];
+        end
+    end
+end
+
+    if(nargout == 0)
+        disp(latex);
+    end
 
 end
 
