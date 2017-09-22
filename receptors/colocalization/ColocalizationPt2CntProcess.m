860236e75a3c79367caf1b8cb90d3ebc3d69d306
Anthony Vega <anthony.vega@utsouthwestern.edu>
2017-09-22 14:25:58 -0500

    New processes and appropriate wrappers for different colocalization functions added (pt2pt still needed)
    Former-commit-id: ada3b0adee4fdee3e4a4575a1333148e223db6aa

---
+++ receptors/colocalization/ColocalizationPt2CntProcess.m
@@ -0,0 +1,70 @@
+classdef ColocalizationPt2CntProcess < ImageAnalysisProcess
+   % A concreate class for measuring colocalization between two images
+   % Anthony Vega 09/2014
+        methods (Access = public)
+        function obj = ColocalizationPt2CntProcess(owner,varargin)
+            
+            if nargin == 0
+                super_args = {};
+            else
+                % Input check
+                ip = inputParser;
+                ip.addRequired('owner',@(x) isa(x,'MovieData'));
+                ip.addOptional('outputDir',owner.outputDirectory_,@ischar);
+                ip.addOptional('funParams',[],@isstruct);
+                ip.parse(owner,varargin{:});
+                outputDir = ip.Results.outputDir;
+                funParams = ip.Results.funParams;
+                
+                % Define arguments for superclass constructor
+                super_args{1} = owner;
+                super_args{2} = ColocalizationPt2CntProcess.getName;
+                super_args{3} = @colocalizationPt2CntWrapper;
+                if isempty(funParams)
+                    funParams = ColocalizationPt2CntProcess.getDefaultParams(owner,outputDir);
+                end
+                super_args{4} = funParams;
+                if(nargin > 4)
+                    super_args{5:nargin} = varargin{5:nargin};
+                end
+            end
+            
+            obj = obj@ImageAnalysisProcess(super_args{:});
+        end
+        
+    end
+    methods (Static)
+        function name = getName()
+            name = 'ColocalizationPt2Cnt';
+        end
+
+        function methods = getMethods(varargin)
+            colocalizationMethods(1).name = 'Point2Continuum';
+            colocalizationMethods(1).func = @colocalMeasurePt2Cnt;            
+            
+            ip=inputParser;
+            ip.addOptional('index',1:length(colocalizationMethods),@isvector);
+            ip.parse(varargin{:});
+            index = ip.Results.index;
+            methods=colocalizationMethods(index);
+        end
+        
+        function funParams = getDefaultParams(owner,varargin)
+            % Input check
+            ip=inputParser;
+            ip.addRequired('owner',@(x) isa(x,'MovieData'));
+            ip.addOptional('outputDir',owner.outputDirectory_,@ischar);
+            ip.parse(owner, varargin{:})
+            outputDir=ip.Results.outputDir;
+            
+            % Set default parameters
+            funParams.ChannelRef = 1;
+            funParams.ChannelObs = 2;
+            funParams.ChannelMask = 2;
+            funParams.SearchRadius = 3;
+            funParams.RandomRuns =1;
+            funParams.OutputDirectory = [outputDir  filesep];
+            funParams.ProcessIndex = [];
+        end
+    end
+end
