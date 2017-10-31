537e333a495ea6ec91350f79a717ba74d880dbab
Andrew R. Jamieson <andrew.jamieson@UTSouthwestern.edu>
2017-10-31 08:51:49 -0500

    LCH playing with deep learning
    Former-commit-id: 0ff3bbd3a647fab6ac1f2d79bceff467a033c7ee

--- 2dActionRecognition/explore/torch/MATLAB_DL_script_128x128_valid.m
+++ 2dActionRecognition/explore/torch/MATLAB_DL_script_128x128_valid.m
@@ -58,85 +58,63 @@ end
 
 
 %% Gather image data
-
-imgs =imageDatastore(dataRootDir, 'IncludeSubfolders',true,'FileExtensions','.png','LabelSource','foldernames');
+imdsTrain = imageDatastore(dataRootDirTR, 'IncludeSubfolders',true,'FileExtensions','.png','LabelSource','foldernames');
+imdsValid = imageDatastore(dataRootDirVal, 'IncludeSubfolders',true,'FileExtensions','.png','LabelSource','foldernames');
 % get rid of unMet...
 % [imds1] = splitEachLabel(imgs,1500,'Exclude','unMet');
-[trainData, valData] = splitEachLabel(imgs, 1500, 'randomize');
+% [trainData, valData] = splitEachLabel(imgs, 1500, 'randomize');
 
 
 
 %% define network
 layers = [ ...
-    imageInputLayer([217 217 1])
-    convolution2dLayer(7,96,'Name','conv1')
+    imageInputLayer([128 128 1])
+    convolution2dLayer(7, 6, 'Stride', 1, 'Name','conv1')
+%     batchNormalizationLayer
     reluLayer
     maxPooling2dLayer(3,'Stride',2,'Name','maxPool1')
-    convolution2dLayer(5,128,'Name','conv2')
+    convolution2dLayer(5, 16,'Stride',2,'Name','conv2')
+%     batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(3,'Stride',2,'Name','maxPool2')    
+    convolution2dLayer(3,32,'Name','conv3')
     reluLayer
     maxPooling2dLayer(3,'Stride',2,'Name','maxPool2')
-    convolution2dLayer(3,256,'Name','conv3')
-    reluLayer
-    convolution2dLayer(3,256,'Name','conv4')
-    reluLayer
-    convolution2dLayer(3,128,'Name','conv5')
-    reluLayer
-    maxPooling2dLayer(3,'Stride',3,'Name','maxPool3')
-    fullyConnectedLayer(512,'Name','fc1')
     reluLayer
     dropoutLayer
-    fullyConnectedLayer(512,'Name','fc2')
+%     convolution2dLayer(3,25,'Name','conv4')
+%     reluLayer
+%     convolution2dLayer(3,25,'Name','conv5','Padding','same')
+%     reluLayer
+%     maxPooling2dLayer(3,'Stride', 2,'Name','maxPool3','Padding','same')
+    fullyConnectedLayer(256,'Name','fc1')
+    reluLayer
+    dropoutLayer
+    fullyConnectedLayer(256,'Name','fc2')
     reluLayer
     dropoutLayer
     fullyConnectedLayer(3,'Name','fc3')
     softmaxLayer
     classificationLayer];
 
-%   20x1 Layer array with layers:
-% 
-%      1   ''   Image Input             217x217x1 images with 'zerocenter' normalization
-%      2   ''   Convolution             50 5x5 convolutions with stride [1  1] and padding [0  0]
-%      3   ''   ReLU                    ReLU
-%      4   ''   Max Pooling             3x3 max pooling with stride [2  2] and padding [0  0]
-%      5   ''   Convolution             100 5x5 convolutions with stride [1  1] and padding [0  0]
-%      6   ''   ReLU                    ReLU
-%      7   ''   Max Pooling             3x3 max pooling with stride [2  2] and padding [0  0]
-%      8   ''   Convolution             96 3x3 convolutions with stride [1  1] and padding [0  0]
-%      9   ''   ReLU                    ReLU
-%     10   ''   Convolution             96 3x3 convolutions with stride [1  1] and padding [0  0]
-%     11   ''   ReLU                    ReLU
-%     12   ''   Convolution             128 3x3 convolutions with stride [1  1] and padding [0  0]
-%     13   ''   ReLU                    ReLU
-%     14   ''   Max Pooling             3x3 max pooling with stride [3  3] and padding [0  0]
-%     15   ''   Fully Connected         384 fully connected layer
-%     16   ''   ReLU                    ReLU
-%     17   ''   Dropout                 50% dropout
-%     18   ''   Fully Connected         3 fully connected layer
-%     19   ''   Softmax                 softmax
-%     20   ''   Classification Output   crossentropyex
-
 % 2017b
 % options = trainingOptions('sgdm',...
-%     'MaxEpochs',3, ...
+%     'MaxEpochs',1000, ...
 %     'ValidationFrequency',30,...
+%     'MiniBatchSize',500,...
 %     'Verbose',false,...
-%     'Plots','training-progress');
-% %     'ValidationData',valData,...
-
+%     'Plots','training-progress',...
+%     'ValidationData',imdsValid,'CheckpointPath',fullfile(dataRootDir,'TEMP_val'));
 
 %% Training options
 %2017a
-options = trainingOptions('sgdm',...
-      'LearnRateSchedule','piecewise',...
-      'LearnRateDropFactor',0.2,... 
-      'LearnRateDropPeriod',5,... 
-      'MaxEpochs',1000,... 
-      'MiniBatchSize',100,...
-      'CheckpointPath',fullfile(dataRootDir,'TEMP'));    
+options = trainingOptions('sgdm','LearnRateSchedule','piecewise',...
+      'LearnRateDropFactor',0.2,'LearnRateDropPeriod',5,... 
+      'MaxEpochs',1000,'MiniBatchSize',100,'CheckpointPath',fullfile(dataRootDir,'TEMP_val'));    
 
-trainedNet = trainNetwork(trainData,layers,options);
+trainedNet = trainNetwork(imdsTrain,layers,options);
 
-s
+
 
 %% Looks at results
   
@@ -157,7 +135,7 @@ figure(4);imshow(imgs.readimage(imageIdx),[]);
 %% Evaluation & Visualization
 
 % layer 2
-w1 = net.Layers(5).Weights;
+w1 = net.Layers(2).Weights;
 w1=mat2gray(w1);
 rw1=imresize(w1,2);
 figure; montage(rw1);
