cbe5104a868c94186e26f0d084015fe81d971516
Mark Kittisopikul <mark.kittisopikul@northwestern.edu>
2017-11-14 17:38:26 -0600

    intersections.analyzeOrientationTest: Short for loop to analyze input/output angles intersections.drawGaussianLine: Calculate Gaussians of line directly rather than using a pixel raster
    Former-commit-id: dc90b0283e5a569376d97ef84e68d03a2831dcc4

---
+++ lamins/+intersections/drawGaussianLine.m
@@ -0,0 +1,47 @@
+function [ I ] = drawGaussianLine( angle, sigma, start, radius )
+%drawGaussianLine Draw Gaussian Line
+
+sz = [101 101];
+
+[X,Y] = meshgrid(1:sz(1),1:sz(2));
+
+center = ceil(sz/2);
+
+if(nargin < 3)
+    start = center;
+end
+
+X = X - start(1);
+Y = Y - start(2);
+
+% ax + by + c = 0
+% y = mx + d    => -mx + y - d = 0
+% m = tan(theta)
+% d = y - mx
+
+m = shiftdim(tan(angle),-1);
+dist = bsxfun(@times,-m,X);
+dist = bsxfun(@plus,dist,Y);
+% hypot(m,1) == sqrt(m.^2+1);
+dist = bsxfun(@rdivide,dist,hypot(m,1));
+dist = abs(dist);
+
+if(nargin > 3)
+%     keyboard;
+    distToStart = hypot(X,Y);
+    nearestPointToEndpoint = sqrt(max(distToStart.^2 - dist.^2,0)) - radius;
+    s = nearestPointToEndpoint > 0;
+    dist(s) = hypot(dist(s),nearestPointToEndpoint(s));
+end
+
+I = exp(-dist.^2/2/sigma.^2);
+% I = erf((dist+0.5)/sigma/sqrt(2)) - erf((dist-0.5)/sigma/sqrt(2));
+
+I = sum(I,3);
+I = mat2gray(I);
+
+% keyboard;
+
+
+end
+
