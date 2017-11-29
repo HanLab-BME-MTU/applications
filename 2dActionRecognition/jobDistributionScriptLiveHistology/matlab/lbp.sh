51615d9fbec2372d4f23f4c5ffe06e906f243043
Assaf <assaf.zaritsky@utsouthwestern.edu>
2017-11-29 16:59:45 -0600

    Updated with Assaf Nov 2017
    Former-commit-id: 70450fb11033cd18d098546a58e73821dd525897

--- 2dActionRecognition/jobDistributionScriptLiveHistology/matlab/lbp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=super
-#SBATCH --nodes=1
-#SBATCH --output=./lbp.out
-#SBATCH --error=./lbp.err
-
-/home2/azaritsky/code/applications/2dActionRecognition/jobDistributionScriptLiveHistology/matlab/job_wrapper_lbp.sh /home2/azaritsky/code/applications/2dActionRecognition/metaAnalysis/ pcAccumulateLBPNew pcMetaLBPNew 1>./lbp.std.out 2>./lbp.std.err
