a715df94147a4130de00d582bfb47ba82f351db3
Sangyoon Han <sjhan@mtu.edu>
2018-05-06 23:28:16 -0500

    updated

--- TFM/TFM-Sangyoon/Processes/calculateInitialRiseTimeLagFromTracks.m
+++ TFM/TFM-Sangyoon/Processes/calculateInitialRiseTimeLagFromTracks.m
@@ -503,32 +503,32 @@ end
     %% drawing group1
     fileStore = [epsPath filesep 'ampForcePlotG1.eps'];
     %     plotIntensityForce(tracksNA(idGroup1f),fileStore,false,false)
-    plotIntensityForce(tracksNA(idGroup1),fileStore,false,false); close
+    [~,h]=plotIntensityForce(tracksNA(idGroup1),fileStore,false,false); if ~isempty(h); close(h); end
     %% group 2
     fileStoreG2 = [epsPath filesep 'ampForcePlotG2.eps'];
-    plotIntensityForce(tracksNA(idGroup2f),fileStoreG2,false,true); close
+    [~,h]=plotIntensityForce(tracksNA(idGroup2f),fileStoreG2,false,true); if ~isempty(h); close(h); end
 
     %% group 3 plotting
     fileStoreG3 = [epsPath filesep 'ampForcePlotG3.eps'];
-    plotIntensityForce(tracksNA(idGroup3),fileStoreG3,false,false); close
+    [~,h]=plotIntensityForce(tracksNA(idGroup3),fileStoreG3,false,false); if ~isempty(h); close(h); end
     %% group4 plotting
     fileStoreG4 = [epsPath filesep 'ampForcePlotG4.eps'];
-    plotIntensityForce(tracksNA(idGroup4),fileStoreG4,false,false); close
+    [~,h]=plotIntensityForce(tracksNA(idGroup4),fileStoreG4,false,false); if ~isempty(h); close(h); end
     %% group5 plotting
     fileStoreG5 = [epsPath filesep 'ampForcePlotG5.eps'];
-    plotIntensityForce(tracksNA(idGroup5),fileStoreG5,false,false); close
+    [~,h]=plotIntensityForce(tracksNA(idGroup5),fileStoreG5,false,false); if ~isempty(h); close(h); end
     %% group6 plotting
     fileStoreG6 = [epsPath filesep 'ampForcePlotG6.eps'];
-    plotIntensityForce(tracksNA(idGroup6),fileStoreG6,false,false); close
+    [~,h]=plotIntensityForce(tracksNA(idGroup6),fileStoreG6,false,false); if ~isempty(h); close(h); end
     %% group7 plotting
     fileStoreG7 = [epsPath filesep 'ampForcePlotG7.eps'];
-    plotIntensityForce(tracksNA(idGroup7),fileStoreG7,false,false); close
+    [~,h]=plotIntensityForce(tracksNA(idGroup7),fileStoreG7,false,false); if ~isempty(h); close(h); end
     %% group8 plotting
     fileStoreG8 = [epsPath filesep 'ampForcePlotG8.eps'];
-    plotIntensityForce(tracksNA(idGroup8),fileStoreG8,false,false); close
+    [~,h]=plotIntensityForce(tracksNA(idGroup8),fileStoreG8,false,false); if ~isempty(h); close(h); end
     %% group9 plotting
     fileStoreG9 = [epsPath filesep 'ampForcePlotG9.eps'];
-    plotIntensityForce(tracksNA(idGroup9),fileStoreG9,false,false); close
+    [~,h]=plotIntensityForce(tracksNA(idGroup9),fileStoreG9,false,false); if ~isempty(h); close(h); end
 
 %% G3 vs. G7 comparison
     %% export tracksG1, G2, G3 and G7 separately
