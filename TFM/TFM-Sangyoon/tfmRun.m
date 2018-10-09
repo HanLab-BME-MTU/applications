ac67147b6c53c6e3190de81df1fd61aad125ced3
Sangyoon J Han <sjhan@mtu.edu>
2018-10-09 16:41:25 -0400

    Added 'askUser' to load function of MovieData

--- TFM/TFM-Sangyoon/tfmRun.m
+++ TFM/TFM-Sangyoon/tfmRun.m
@@ -1,42 +1,20 @@
 function [] = tfmRun(MDPath)
 %% set up
 % analysisFolder = '/project/bioinformatics/Danuser_lab/P01adhesion/analysis/Sangyoon/NA_RecruitmentProject/Kevin/2017-06-29//ChoK1_shRNA_WT_Rescue_FACS_5kPa_006';
-MD=MovieData.load(MDPath); %,'askUser',false,'askUserChannel',false);
+if isa(MDPath,'MovieData')
+    MD=MovieData.load(MDPath.getFullPath); %,'askUser',false,'askUserChannel',false);
+else
+    MD=MovieData.load(MDPath,'askUser',false); %,'askUserChannel',false);
+end
 iPack=  MD.getPackageIndex('TFMPackage');
-TFM = MD.getPackage(iPack);
-%% SDC
-prevProcChanged = false;
-SDCprocess = TFM.getProcess(1);
-if ~SDCprocess.success_ || SDCprocess.procChanged_
-    SDCprocess.run
-    MD.save
-    prevProcChanged = true;
-end
-
-%% Displacement
-displProcess = TFM.getProcess(2);
-if ~displProcess.success_  || displProcess.procChanged_ || prevProcChanged
-    displProcess.run
-    MD.save
-    prevProcChanged = true;
-end
-%% Displacement correction
-displCoProcess = TFM.getProcess(3);
-if ~displCoProcess.success_  || displCoProcess.procChanged_ || prevProcChanged
-    displCoProcess.run
-    MD.save
-    prevProcChanged = true;
-end
+TFMPack = MD.getPackage(iPack);
+status = TFMPack.sanityCheck;
 %% Traction reconstruction
-forceProcess = TFM.getProcess(4);
-if ~forceProcess.success_ || forceProcess.procChanged_ || prevProcChanged
-    forceProcess.run
-    MD.save
-    prevProcChanged = true;
-end
-%% Strain Energy quantification
-seProcess = TFM.getProcess(5);
-if ~seProcess.success_ || seProcess.procChanged_ || prevProcChanged
-    seProcess.run
-    MD.save
+curProcess=cell(1,5);
+for ii=find(~status)
+    curProcess{ii} = TFMPack.getProcess(ii);
+    if ~isempty(curProcess{ii})
+        curProcess{ii}.run
+        MD.save
+    end
 end
