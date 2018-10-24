d81caa99e2c5e3f7f6a536133e02ebb6f2a4ea5a
Sangyoon J Han <sjhan@mtu.edu>
2018-10-24 19:14:24 -0400

    changed to parfor again

--- TFM/TFM-Sangyoon/readIntensityFromTracks.m
+++ TFM/TFM-Sangyoon/readIntensityFromTracks.m
@@ -96,8 +96,8 @@ else
 %     end
 end
 
-% parfor (k=1:numTracks, parforArg)
-for k=1:numTracks
+parfor (k=1:numTracks, parforArg)
+% for k=1:numTracks
 %     startFrame = max(1, min(arrayfun(@(x) x.startingFrame,tracksNA))-extraLength);
 %     endFrame = min(numFrames, max(arrayfun(@(x) x.endingFrame,tracksNA))+extraLength);
 %     startFrame = max(1, tracksNA(k).startingFrame-extraLength);
