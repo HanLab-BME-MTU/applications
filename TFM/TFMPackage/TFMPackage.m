4112c26332a3e25833d4ce3ddb632df9429b6bf3
Mehdi Abtahi <mehdiabtahi@sjhan.sabu>
2019-03-20 15:48:19 -0400

    correction of the dependency matrix

--- TFM/TFMPackage/TFMPackage.m
+++ TFM/TFMPackage/TFMPackage.m
@@ -56,8 +56,8 @@ classdef TFMPackage < Package
                  2 0 0 0 0 0;   %2 DisplacementFieldCalculationProcess
                  0 1 0 0 0 0;   %3 DisplacementFieldCorrectionProcess [optional]
                  0 1 2 0 0 0;   %4 ForceFieldCalculationProcess
-                 0 1 2 1 2 0;   %5 StrainEnergyCalculationProcess [optional]
-                 0 1 2 1 0 2];  %6 OutputTFMProcess [optional]
+                 0 1 2 1 0 0;   %5 StrainEnergyCalculationProcess [optional]
+                 0 1 2 1 0 0];  %6 OutputTFMProcess [optional]
             if nargin<2, j=1:size(m,2); end
             if nargin<1, i=1:size(m,1); end
             m=m(i,j);
