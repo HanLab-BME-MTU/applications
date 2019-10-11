737a59cb8de39ce29d95e097fcbc49e0d93a75a4
Sangyoon J Han <sjhan@mtu.edu>
2019-10-11 10:43:36 -0400

    intermediate change for Elizabeth. Explore SpeckleTrackingProcess

--- qfsm/analyzeSpecklePolulation.m
+++ qfsm/analyzeSpecklePolulation.m
@@ -9,12 +9,20 @@ function [] = analyzeSpecklePolulation(MD)
 % October 8 2019
 
 %% Load QFSM Package
-
+iQFSM = MD.getPackageIndex('QFSMPackage');
+qFSM = MD.getPackage(iQFSM);
 %% Get all trajectories
+iSpeckleTrackingProcess = 6;
+speckleTrackingProcess = qFSM.getProcess(iSpeckleTrackingProcess);
 
+tracks = speckleTrackingProcess.loadChannelOutput(1,'output','MPM');
 %% Get cell segmentation
 
 %% Separate cell segmentation with LA and LP (via distance from the edge)
+
+%%
+
+%%
 
 end
 
