3753b1f7c34cef28a1763f557b514687980cac1e
Sangyoon-adm <sangyoon-adm@MacBook-Pro-6.local>
2020-03-20 15:23:52 -0400

    debug

--- TFM/TFM-Sangyoon/strainEnergyBatch.m
+++ TFM/TFM-Sangyoon/strainEnergyBatch.m
@@ -231,7 +231,7 @@ for ii=1:numConditions
 end
 disp('Done')
 %% setting up group name
-if exist(analysisFolderSelectionDone,'var')
+if exist('analysisFolderSelectionDone','var')
     if ~analysisFolderSelectionDone
         groupNames2=groupNames;
         for ii=1:numConditions
