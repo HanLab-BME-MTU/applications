2ad91cc84a3d9dec1b41dbcc98694a7a920a289b
Sam Haarman <sehaarma@mtu.edu>
2020-08-03 16:40:19 -0400

    updating bead image creation function

--- TFM/TFM-Sam/simHydrogelDeformation.m
+++ TFM/TFM-Sam/simHydrogelDeformation.m
@@ -20,7 +20,7 @@ function [bead_x,bead_y,bead_ux,bead_uy,Av]=simHydrogelDeformation(fx,fy,d,nPoin
 %       Av =        vector containing gaussian bead amplitudes
 %
 %   Example:
-%       [bead_x,bead_y,bead_ux,bead_uy,Av]=simHydrogelDeformation(0,1000,40,5000,1000,0.49,'C:\Users\Sam Haarman\Documents\MATLAB\Han Lab\Bead Image Creation')
+%       [bead_x,bead_y,bead_ux,bead_uy,Av]=simHydrogelDeformation(0,1000,40,2000,1000,0.49,'C:\Users\Sam Haarman\Documents\MATLAB\Han Lab\Bead Image Creation')
 
 % //Data path configuration ***********************************************
 imgPath=[dataPath filesep 'Beads'];
@@ -35,36 +35,39 @@ end
 chrRef='img1ref_';
 chrBead='img3bead_';
 %Change these character vectors to rename files for next run
-chr1='1kPa5kbeads'; chr13D='1kPa5kbeads3D';
+chr1='newBeads(0.5kpa,4k,1kE)_1024x'; %chr13D='newBeads3D(1kpa,0.5k,1kE)';
 refstring=[chrRef chr1 '.tif'];
 imgstring=[chrBead chr1 '.tif'];
 
 % //Generate reference bead image *****************************************
-meshPtsFwdSol=2^8;
-xmax=meshPtsFwdSol; xmin=1;
-ymax=meshPtsFwdSol; ymin=1;
-sigma = 1.68;
+beadDiameter = 5; %20nm/bead
+pixelSize = 5; %20 nm/pix
+thickness = 15; %thickness of hydrogel for FEM purposes
+meshPtsFwdSol=2^9; %number of pix/pts in mesh
+numPix_x=meshPtsFwdSol; xmin=1; %number of pixels in X
+numPix_y=meshPtsFwdSol; ymin=1; %number of pixels in Y
+sigma = 1.68; %stdev of gaussian function
 %2D
-[refimg,bead_x, bead_y, ~, Av] = simGaussianBeads(xmax,ymax, sigma, ...
-        'npoints', nPoints, 'Border', 'periodic','A',0.3+rand(1,nPoints));
+[refimg,bead_x, bead_y, ~, Av] = simGaussianBeads(numPix_x,numPix_y, sigma, ...
+        'npoints', nPoints, 'Border', 'periodic','A',0.4+rand(1,nPoints),...
+        'pixelSize',pixelSize,'beadDiameter',beadDiameter);
 %3D
-[refimg3D, beadcenters3D, ~, Av3D] = simGaussianSpots3D([256 256 15], ...
-        sigma,'npoints', nPoints, 'Border', 'periodic', 'A',0.3+rand(1,nPoints));
+% [refimg3D, beadcenters3D, ~, Av3D] = simGaussianSpots3D([meshPtsFwdSol meshPtsFwdSol thickness], ...
+%         sigma,'npoints', nPoints, 'Border', 'periodic', 'A',0.5+rand(1,nPoints));
     
 %noise addition
-refimg = refimg+0.05*rand(ymax,xmax)*max(refimg(:));
-%add 3D noise addition
+refimg = refimg+0.05*rand(numPix_y,numPix_x)*max(refimg(:));
+%add 3D noise addition in future
 
-%saving and writing reference image
-save(append(chrRef,chr1),'refimg');
-save(append(chrRef,chr13D),'refimg3D')
+%writing reference image
 imwrite(uint16(refimg*2^16/max(max(refimg))),[refPath filesep refstring]);
+%add saving 3D refimg
     
 % //Generate force field
 forceType = 'groupForce';
-[x_mat_u, y_mat_u]=meshgrid(xmin:1:xmax,ymin:1:ymax);
-force_x = assumedForceAniso2D(1,x_mat_u,y_mat_u,(xmax/2),ymax/2,0,fx,d,d,forceType);
-force_y = assumedForceAniso2D(2,x_mat_u,y_mat_u,(xmax/2),ymax/2,0,fy,d,d,forceType);
+[x_mat_u, y_mat_u]=meshgrid(xmin:1:numPix_x,ymin:1:numPix_y);
+force_x = assumedForceAniso2D(1,x_mat_u,y_mat_u,(numPix_x/2),numPix_y/2,0,fx,d,d,forceType);
+force_y = assumedForceAniso2D(2,x_mat_u,y_mat_u,(numPix_x/2),numPix_y/2,0,fy,d,d,forceType);
 force_z = [];
 
 
@@ -72,8 +75,9 @@ force_z = [];
 structModel=createpde('structural','static-solid');
 
 % //Define geometry *******************************************************
-%reduce to micron size geometry, 72 nm/pix
-gm=multicuboid(256,256,15);
+%meshPtsFwdSol is multiplied by pixelSize to ensure FEM operates in 
+%nanometers rather than pixels
+gm=multicuboid(meshPtsFwdSol*pixelSize,meshPtsFwdSol*pixelSize,thickness);
 structModel.Geometry=gm;
 
 % //Specify material properties *******************************************
@@ -84,16 +88,16 @@ structuralBC(structModel,'Face',1,'Constraint','fixed');
 
 % //Apply force distribution on face 2 ************************************
 if isempty(force_z)
-force_z=zeros(256); %can be replaced by real data if a normal force is desired
+force_z=zeros(meshPtsFwdSol); %can be replaced by real data if a normal force is desired
 end
-[xLoc,yLoc]=ndgrid(-128:127,-128:127);
+[xLoc,yLoc]=ndgrid(-meshPtsFwdSol/2:(meshPtsFwdSol/2)-1,-meshPtsFwdSol/2:(meshPtsFwdSol/2)-1);
 forceInterpX=griddedInterpolant(xLoc,yLoc,force_x);
 forceInterpY=griddedInterpolant(xLoc,yLoc,force_y);
 forceInterpZ=griddedInterpolant(xLoc,yLoc,force_z);
 %define function handle
 hydrogelForce = @(location,state)[forceInterpX(location.x,location.y); ...
                                   forceInterpY(location.x,location.y); ...
-                                  forceInterpZ(location.x,location.y)];
+                                  forceInterpZ(location.x,location.y);];
 %pass function handle and define BCs
 structuralBoundaryLoad(structModel,'Face',2,'SurfaceTraction',hydrogelForce,'Vectorize','on');
 
@@ -108,24 +112,26 @@ figure(2)
 pdeplot3D(structModel,'ColorMapData',structModelResults.Displacement.Magnitude, ...
     'Deformation',structModelResults.Displacement)
 figure(4)
-pdeplot3D(structModel,'ColorMapData',structModelResults.Displacement.uz, ...
+pdeplot3D(structModel,'ColorMapData',structModelResults.Displacement.z, ...
     'Deformation',structModelResults.Displacement)
 
 % //Shifting bead locations to apply interpDisp at those locations ********
 %2D
-bead_xshifted=bead_x-128.5;     %bead_x from simGaussianBeads.m
-bead_yshifted=bead_y-128.5;     %bead_y from simGaussianBeads.m
-bead_zshifted=15*ones(nPoints,1); %bead_z can be included in future
+%Shifting bead locations because the outputs from simGaussianBeads are not
+%centered around the origin which is required for our FEM model.
+bead_xshifted=bead_x-(meshPtsFwdSol/2)+0.5;     %bead_x from simGaussianBeads.m
+bead_yshifted=bead_y-(meshPtsFwdSol/2)+0.5;     %bead_y from simGaussianBeads.m
+bead_zshifted=thickness*ones(nPoints,1); %bead_z can be included in future
 %3D
-bead_x3Dshifted=beadcenters3D(:,1)-128.5; %beadcenters3D from simGaussianSpots3D.m
-bead_y3Dshifted=beadcenters3D(:,2)-128.5; %beadcenters3D from simGaussianSpots3D.m
-bead_z3Dshifted=beadcenters3D(:,3)-0.5;   %beadcenters3D from simGaussianSpots3D.m
+% bead_x3Dshifted=beadcenters3D(:,1)-(meshPtsFwdSol/2)+0.5; %beadcenters3D from simGaussianSpots3D.m
+% bead_y3Dshifted=beadcenters3D(:,2)-(meshPtsFwdSol/2)+0.5; %beadcenters3D from simGaussianSpots3D.m
+% bead_z3Dshifted=beadcenters3D(:,3)-0.5;   %beadcenters3D from simGaussianSpots3D.m
 
 % //Interpolating displacements at bead locations using interpDisp ********
 %2D
 interpDisp=interpolateDisplacement(structModelResults,bead_xshifted,bead_yshifted,bead_zshifted);
 %3D
-interpDisp3D=interpolateDisplacement(structModelResults,bead_x3Dshifted,bead_y3Dshifted,bead_z3Dshifted);
+% interpDisp3D=interpolateDisplacement(structModelResults,bead_x3Dshifted,bead_y3Dshifted,bead_z3Dshifted);
 
 % //Reshaping to match bead location vectors ******************************
 %2D
@@ -133,51 +139,58 @@ bead_ux = reshape(interpDisp.ux,size(bead_x));
 bead_uy = reshape(interpDisp.uy,size(bead_y));
 magsFEA = sqrt((bead_ux.^2)+(bead_uy.^2));
 %3D
-bead_ux3D = reshape(interpDisp3D.ux,size(beadcenters3D(:,1)));
-bead_uy3D = reshape(interpDisp3D.uy,size(beadcenters3D(:,2)));
-bead_uz3D = reshape(interpDisp3D.uz,size(beadcenters3D(:,3)));
-beadcenters3Ddisp = [bead_ux3D bead_uy3D bead_uz3D];
-newbeadcenters3D = beadcenters3D + beadcenters3Ddisp;
-xmax3D = ceil(max(newbeadcenters3D(:,1)));
-ymax3D = ceil(max(newbeadcenters3D(:,2)));
-zmax3D = ceil(max(newbeadcenters3D(:,3)));
+% bead_ux3D = reshape(interpDisp3D.ux,size(beadcenters3D(:,1)));
+% bead_uy3D = reshape(interpDisp3D.uy,size(beadcenters3D(:,2)));
+% bead_uz3D = reshape(interpDisp3D.uz,size(beadcenters3D(:,3)));
+% beadcenters3Ddisp = [bead_ux3D bead_uy3D bead_uz3D];
+% newbeadcenters3D = beadcenters3D + beadcenters3Ddisp;
+% xmax3D = ceil(max(newbeadcenters3D(:,1)));
+% ymax3D = ceil(max(newbeadcenters3D(:,2)));
+% zmax3D = ceil(max(newbeadcenters3D(:,3)));
 
 % //Plotting interpolated displacement ************************************
 figure(3)
 q2=quiver(bead_xshifted,bead_yshifted,bead_ux,bead_uy);
-xlim([-128 128]); ylim([-128 128]);
+xlim([-(meshPtsFwdSol/2) (meshPtsFwdSol/2)]); ylim([-(meshPtsFwdSol/2) (meshPtsFwdSol/2)]);
 xlabel('X'); ylabel('Y'); title('Ground Truth Displacement Field - 1 kPa');
 
 %\\ Colormapping **********************************************************
 %Get the current colormap
-currentColormap = colormap(gca);
-%Now determine the color to make each arrow using a colormap
-[~, ~, ind] = histcounts(magsFEA, size(currentColormap, 1));
-
-%Now map this to a colormap to get RGB
-cmap = uint8(ind2rgb(ind(:), currentColormap) * 255);
-cmap(:,:,4) = 255;
-cmap = permute(repmat(cmap, [1 3 1]), [2 1 3]);
-
-%We repeat each color 3 times (using 1:3 below) because each arrow has 3 vertices
-set(q2.Head, ...
-    'ColorBinding', 'interpolated', ...
-    'ColorData', reshape(cmap(1:3,:,:), [], 4).');   %'
-
-%We repeat each color 2 times (using 1:2 below) because each tail has 2 vertices
-set(q2.Tail, ...
-    'ColorBinding', 'interpolated', ...
-    'ColorData', reshape(cmap(1:2,:,:), [], 4).');
-
-set(gca,'Color','k');
+% currentColormap = colormap(gca);
+% %Now determine the color to make each arrow using a colormap
+% [~, ~, ind] = histcounts(magsFEA, size(currentColormap, 1));
+% 
+% %Now map this to a colormap to get RGB
+% cmap = uint8(ind2rgb(ind(:), currentColormap) * 255);
+% cmap(:,:,4) = 255;
+% cmap = permute(repmat(cmap, [1 3 1]), [2 1 3]);
+% 
+% %We repeat each color 3 times (using 1:3 below) because each arrow has 3 vertices
+% set(q2.Head, ...
+%     'ColorBinding', 'interpolated', ...
+%     'ColorData', reshape(cmap(1:3,:,:), [], 4).');   %'
+% 
+% %We repeat each color 2 times (using 1:2 below) because each tail has 2 vertices
+% set(q2.Tail, ...
+%     'ColorBinding', 'interpolated', ...
+%     'ColorData', reshape(cmap(1:2,:,:), [], 4).');
+% 
+% set(gca,'Color','k');
 
 %\\ Generating Deformed Bead Image ****************************************
 %2D
-beadimg = simGaussianBeads(xmax,ymax, sigma, ...
-    'x',bead_x + bead_ux,'y',bead_y + bead_uy,'A',Av,'Border', 'padded');
-save(append(chrBead,chr1),'beadimg');
+nanElements = sum(isnan(bead_ux));
+bead_ux(isnan(bead_ux)) = 0;
+bead_uy(isnan(bead_uy)) = 0;
+beadimg = simGaussianBeads(numPix_x,numPix_y, sigma, ...
+    'x',bead_x + bead_ux,'y',bead_y + bead_uy,'A',Av,...
+    'pixelSize',20,'beadDiameter',20,'Border','periodic');
 imwrite(uint16(beadimg*2^16/max(max(beadimg))),[imgPath filesep imgstring]);
 %3D
-beadimg3D = simGaussianSpots3D([xmax3D ymax3D zmax3D], sigma, ...
-    'X', newbeadcenters3D,'npoints', nPoints, 'A', Av3D, 'Border', 'padded', 'Verbose' ,'on');
-save(append(chrBead,chr13D),'beadimg3D');
+% beadimg3D = simGaussianSpots3D([xmax3D ymax3D zmax3D], sigma, ...
+%     'X', newbeadcenters3D,'npoints', nPoints, 'A', Av3D,'Border', 'periodic');
+%add saving 3D beadimg
+
+%\\ Saving outputs for comparison after TFM processing ********************
+disp(strcat('Removed ',num2str(nanElements),'NaN elements.'))
+save(strcat('outputs_',chr1,'.mat'),'bead_ux','bead_uy','force_x','force_y'); %chr1 defined initially by user, matches image file naming
