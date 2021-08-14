11610fa0e0da4ad7d48cd0a0467670f0292d6601
Sangyoon-adm <sangyoon-adm@Sang-MacBook-Pro.local>
2021-08-15 01:42:14 +0900

    Added to produce 'clean' tracksNA that has NaNs outside tracked range.

---
+++ focaladhesion/removeOutOfRangeAmpFromTrack.m
@@ -0,0 +1,21 @@
+function tracksNA = removeOutOfRangeAmpFromTrack(tracksNA)
+%function tracksNA = removeOutOfRangeAmpFromTrack(tracksNA) removes amp
+%that is outside startingFrameExtraExtra and endingFrameExtraExtra and save
+%to tracksNA again.
+%   Sangyoon Han, August 2021
+nTracks = numel(tracksNA);
+for ii=1:nTracks
+    sF=tracksNA(ii).startingFrameExtraExtra;
+    eF=tracksNA(ii).endingFrameExtraExtra;
+    nFrames = tracksNA(ii).iFrames(end);
+    % Making out of range NaNs
+    if sF>1
+        tracksNA(ii).amp(1:sF-1)=NaN;
+    end
+    if eF<nFrames
+        tracksNA(ii).amp(eF+1:nFrames)=NaN;
+    end
+end
+
+end
+
