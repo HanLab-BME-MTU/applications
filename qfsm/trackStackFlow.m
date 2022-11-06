e4fc2edfebff56fb07e65a720e302372e12ffee7
Sangyoon J Han <sjhan@mtu.edu>
2022-11-06 10:04:40 -0500

    Added narrowFactor

--- qfsm/trackStackFlow.m
+++ qfsm/trackStackFlow.m
@@ -515,10 +515,10 @@ parfor k = 1:nPoints
         % we narrow down the template size, essentially around only one
         % bead in the middle. Will try with narrowingFactor, say 0.3 as
         % default - Sangyoon 
-        narrowFactor = 0.4; % This should be an input from the dialog box.
+        narrowFactor = 0.34; % This should be an input from the dialog box.
         halfCorL= round((corL-1)/2 * narrowFactor);
         refineFactor = 10;%round(10*20/corL); % by this, the pixel value will be magnified.
-        refineRange = 1.0; % in pixel
+        refineRange = 2.0; % in pixel
         incFactor2 = 1;
 %         if pass==1 || pass==2
 %             incFactor=1;
