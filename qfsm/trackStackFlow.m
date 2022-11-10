8e8c581e1fefd9c548caab7113f5853cfe62ec5c
Sangyoon Han <sjhan@mtu.edu>
2022-11-09 19:56:58 -0500

    Setting narrowFactor to 1 again.

--- qfsm/trackStackFlow.m
+++ qfsm/trackStackFlow.m
@@ -515,7 +515,9 @@ parfor k = 1:nPoints
         % we narrow down the template size, essentially around only one
         % bead in the middle. Will try with narrowingFactor, say 0.3 as
         % default - Sangyoon 
-        narrowFactor = 0.34; % This should be an input from the dialog box.
+        narrowFactor = 1; %0.34; % This should be an input from the dialog box.
+        % Turned out that narrowFactor introduces more errors to the
+        % displacement field. So make it to 1 by default. -2022.11.9. SJH
         halfCorL= round((corL-1)/2 * narrowFactor);
         refineFactor = 10;%round(10*20/corL); % by this, the pixel value will be magnified.
         refineRange = 2.0; % in pixel
