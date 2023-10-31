276784575ebb23b5ab8c01bcf4bc3065dbb73e33
Sangyoon Han <sjhan@mtu.edu>
2023-10-31 15:57:40 -0400

    colorcode base change to 180 from 360

--- sheetmig/cellposeDetection/cellposeVid.py
+++ sheetmig/cellposeDetection/cellposeVid.py
@@ -23,7 +23,7 @@ from matplotlib.colors import hsv_to_rgb
 def get_color_from_angle(angle):
     """Get a color corresponding to an angle using HSV colormap"""
     # Convert angle from [-180, 180] to [0, 1]
-    hue = (angle) / 360.0
+    hue = (angle) / 180.0
     # Convert hue to RGB color
     return hsv_to_rgb((hue, 1, 1))
 
